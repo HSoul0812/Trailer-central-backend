55999806d3fe10db9cebfdc415c66ee9ae348f26
Oleh <olehsychupwork@gmail.com>
2019-10-15 23:14:25 +0300

    #DMS-50 Install and Setup Swagger

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    /**
+     * @OA\Info(
+     *   title="Parts API",
+     *   version="1.0",
+     *   @OA\Contact(
+     *     email="olehsychupwork@gmail.com",
+     *     name="Trailercentral"
+     *   )
+     * )
+     */
+}
