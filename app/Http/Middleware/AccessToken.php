5a7f20b127aead949a0926557d538d16c4577aa5
Geovane <geovane.jr.dp6@gmail.com>
2019-10-17 05:49:05 -0400

    #DMS-53 Created a token middleware

---
+++ app/Http/Middleware/AccessToken.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User\User;
+use App\Models\User\AuthToken;
+
+class AccessToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->header('access-token')) {
+            $accessToken = AuthToken::where('access_token', $request->header('access-token'))->first();
+            if ($accessToken && $accessToken->user) {
+                return $next($request);
+            }
+        }
+        return response('Invalid access token.', 403);
+    }
+}
