e177733b248da26839057fbb1d93cdad29cf21a0
David Conway Jr <david.a.conway.jr@gmail.com>
2020-02-12 16:11:17 +0000

    * TCWWW-175 Fixed Matching Optional Regex Headers

--- app/Services/Import/Parts/CsvImportService.php
+++ app/Services/Import/Parts/CsvImportService.php
@@ -41,8 +41,8 @@ class CsvImportService implements CsvImportServiceInterface
     const IMAGE = 'Image';
     const VIDEO_EMBED_CODE = 'Video Embed Code';
 
-    const BIN_ID = '/Bin \d+ ID/';
-    const BIN_QTY = '/Bin \d+ qty/';
+    const BIN_ID = '/Bin\s+\d+\s+ID/';
+    const BIN_QTY = '/Bin\s+\d+\s+qty/';
     
     protected $bulkUploadRepository;
     protected $partsRepository;
@@ -225,7 +225,7 @@ class CsvImportService implements CsvImportServiceInterface
         }
 
         // Loop Optional Headers
-        foreach($this->optionalHeaderValues as $regex) {
+        foreach($this->optionalHeaderValues as $regex => $allowed) {
             // Pattern is Regex?
             if(@preg_match($regex, null) !== false){
                 return preg_match($regex, $val) ? true : false;
