d50b26ae110e177d1c6fba86ef09ff60b36d29fe
acfabro <acfabro@gmail.com>
2020-04-25 00:08:00 +0800

    #DMS-413 min/max changes for bulk uploader

--- app/Services/Import/Parts/CsvImportService.php
+++ app/Services/Import/Parts/CsvImportService.php
@@ -17,13 +17,13 @@ use App\Repositories\Parts\PartRepositoryInterface;
 use Illuminate\Support\Facades\Log;
 
 /**
- * 
+ *
  *
  * @author Eczek
  */
-class CsvImportService implements CsvImportServiceInterface 
+class CsvImportService implements CsvImportServiceInterface
 {
-    
+
     const VENDOR = 'Vendor';
     const BRAND = 'Brand';
     const TYPE = 'Type';
@@ -39,16 +39,18 @@ class CsvImportService implements CsvImportServiceInterface
     const DESCRIPTION = 'Description';
     const SHOW_ON_WEBSITE = 'Show on website';
     const IMAGE = 'Image';
+    const STOCK_MIN = 'Stock Minimum';
+    const STOCK_MAX = 'Stock Maximum';
     const VIDEO_EMBED_CODE = 'Video Embed Code';
 
     const BIN_ID = '/Bin\s+\d+\s+ID/i';
     const BIN_QTY = '/Bin\s+\d+\s+qty/i';
-    
+
     protected $bulkUploadRepository;
     protected $partsRepository;
     protected $binRepository;
     protected $bulkUpload;
-    
+
     protected $allowedHeaderValues = [
         self::VENDOR => true,
         self::BRAND => true,
@@ -65,6 +67,8 @@ class CsvImportService implements CsvImportServiceInterface
         self::DESCRIPTION => true,
         self::SHOW_ON_WEBSITE => true,
         self::IMAGE => true,
+        self::STOCK_MIN => true,
+        self::STOCK_MAX => true,
         self::VIDEO_EMBED_CODE => true
     ];
 
@@ -74,17 +78,17 @@ class CsvImportService implements CsvImportServiceInterface
     ];
 
     private $validationErrors = [];
-        
+
     private $indexToheaderMapping = [];
-    
+
     public function __construct(BulkUploadRepositoryInterface $bulkUploadRepository, PartRepositoryInterface $partRepository, BinRepositoryInterface $binRepository)
     {
         $this->bulkUploadRepository = $bulkUploadRepository;
         $this->partsRepository = $partRepository;
         $this->binRepository = $binRepository;
     }
-    
-    public function run() 
+
+    public function run()
     {
         echo "Running...".PHP_EOL;
         Log::info('Starting import for bulk upload ID: ' . $this->bulkUpload->id);
@@ -94,25 +98,25 @@ class CsvImportService implements CsvImportServiceInterface
                 Log::info('Invalid bulk upload ID: ' . $this->bulkUpload->id . ' setting validation_errors...');
                 $this->bulkUploadRepository->update(['id' => $this->bulkUpload->id, 'status' => BulkUpload::VALIDATION_ERROR, 'validation_errors' => json_encode($this->validationErrors)]);
                 return false;
-            } 
+            }
         } catch (\Exception $ex) {
              Log::info('Invalid bulk upload ID: ' . $this->bulkUpload->id . ' setting validation_errors...');
             $this->bulkUploadRepository->update(['id' => $this->bulkUpload->id, 'status' => BulkUpload::VALIDATION_ERROR, 'validation_errors' => json_encode($this->validationErrors)]);
             return false;
         }
-        
+
         echo "Data Valid... Importing...".PHP_EOL;
         Log::info('Validation passed for bulk upload ID: ' . $this->bulkUpload->id . ' proceeding with import...');
         $this->import();
         return true;
     }
-    
+
     public function setBulkUpload(BulkUpload $bulkUpload)
     {
         $this->bulkUpload = $bulkUpload;
     }
-    
-    protected function import() 
+
+    protected function import()
     {
         echo "Importing.... 123".PHP_EOL;
         $this->streamCsv(function($csvData, $lineNumber) {
@@ -140,15 +144,15 @@ class CsvImportService implements CsvImportServiceInterface
                 Log::info('Error found on part for bulk upload : ' . $this->bulkUpload->id . ' : ' . $ex->getMessage());
 //                throw new \Exception("Image inaccesible");
             }
-                       
-        });            
-        
+
+        });
+
         $this->bulkUploadRepository->update(['id' => $this->bulkUpload->id, 'status' => BulkUpload::COMPLETE]);
     }
-    
-    protected function validate() 
+
+    protected function validate()
     {
-        $this->streamCsv(function($csvData, $lineNumber) {            
+        $this->streamCsv(function($csvData, $lineNumber) {
             foreach($csvData as $index => $value) {
                 if ($lineNumber === 1) {
                     if($this->isOptionalHeader($value)) {
@@ -159,65 +163,65 @@ class CsvImportService implements CsvImportServiceInterface
                     } else {
                         $this->allowedHeaderValues[$value] = 'allowed';
                         $this->indexToheaderMapping[$index] = $value;
-                    }                
+                    }
                 } else {
                     if ($errorMessage = $this->isDataValid($this->indexToheaderMapping[$index], $value)) {
                         $this->validationErrors[] = $this->printError($lineNumber, $index + 1, $errorMessage);
                     }
-                }                    
-            }                
+                }
+            }
         });
-        
+
         foreach($this->allowedHeaderValues as $header => $headerValue) {
             if ($headerValue !== 'allowed') {
                 $this->validationErrors[] = $this->printError(1, $header, "Header ".$header." not present.");
             }
         }
-        
+
         if (count($this->validationErrors) > 0) {
             return false;
         }
-        
+
         return true;
     }
-    
-    private function streamCsv($callback) 
+
+    private function streamCsv($callback)
     {
         $adapter = Storage::disk('s3')->getAdapter();
         $client = $adapter->getClient();
         $client->registerStreamWrapper();
-        
+
         if (!($stream = fopen("s3://{$adapter->getBucket()}/{$this->bulkUpload->import_source}", 'r'))) {
             throw new \Exception('Could not open stream for reading file: ['.$this->bulkUpload->import_source.']');
         }
-        
+
         $lineNumber = 1;
         while (!feof($stream)) {
-             $isEmptyRow = true;            
+             $isEmptyRow = true;
              $csvData = fgetcsv($stream);
-             
+
              foreach($csvData as $value) {
                  if (!empty($value)) {
                      $isEmptyRow = false;
                      break;
                  }
              }
-             
+
              if ($isEmptyRow) {
                  continue;
              }
-             
+
              $callback($csvData, $lineNumber++);
              flush();
         }
     }
-    
-    private function isAllowedHeader($val) 
+
+    private function isAllowedHeader($val)
     {
         return isset($this->allowedHeaderValues[$val]);
     }
-    
-    private function isOptionalHeader($val) 
+
+    private function isOptionalHeader($val)
     {
         // In Optional Headers?
         if(isset($this->optionalHeaderValues[$val])) {
@@ -235,22 +239,22 @@ class CsvImportService implements CsvImportServiceInterface
         }
         return false;
     }
-    
-    private function printError($line, $column, $errorMessage) 
+
+    private function printError($line, $column, $errorMessage)
     {
         return "$errorMessage in line $line at column $column";
     }
-    
-    private function csvToPartData($csvData) 
+
+    private function csvToPartData($csvData)
     {
         $formattedImages = [];
         $keyToIndexMapping = [];
         foreach($this->indexToheaderMapping as $index => $value) {
             $keyToIndexMapping[$value] = $index;
-        }        
-        
+        }
+
         $vendor = Vendor::where('name', $csvData[$keyToIndexMapping[self::VENDOR]])->first();
-        
+
         $part = [];
         $part['dealer_id'] = $this->bulkUpload->dealer_id;
         $part['vendor_id'] = !empty($vendor) ? $vendor->id : null;
@@ -260,18 +264,20 @@ class CsvImportService implements CsvImportServiceInterface
         $part['subcategory'] = $csvData[$keyToIndexMapping[self::CATEGORY]];
         $part['sku'] = $csvData[$keyToIndexMapping[self::SKU]];
         $part['price'] = empty($csvData[$keyToIndexMapping[self::PRICE]]) ? 0 : $csvData[$keyToIndexMapping[self::PRICE]];
-        $part['dealer_cost'] = empty($csvData[$keyToIndexMapping[self::DEALER_COST]]) ? 0 : $csvData[$keyToIndexMapping[self::DEALER_COST]];        
+        $part['dealer_cost'] = empty($csvData[$keyToIndexMapping[self::DEALER_COST]]) ? 0 : $csvData[$keyToIndexMapping[self::DEALER_COST]];
         $part['msrp'] = empty($csvData[$keyToIndexMapping[self::MSRP]]) ? 0 : $csvData[$keyToIndexMapping[self::MSRP]];
         $part['weight'] = $csvData[$keyToIndexMapping[self::WEIGHT]];
         $part['weight_rating'] = $csvData[$keyToIndexMapping[self::WEIGHT_RATING]];
-        $part['description'] = $csvData[$keyToIndexMapping[self::DESCRIPTION]];        
+        $part['description'] = $csvData[$keyToIndexMapping[self::DESCRIPTION]];
         $part['show_on_website'] = strtolower($csvData[$keyToIndexMapping[self::SHOW_ON_WEBSITE]]) === 'yes' ? true : false;
         $part['title'] = $csvData[$keyToIndexMapping[self::TITLE]];
-        
+        $part['stock_min'] = $csvData[$keyToIndexMapping[self::STOCK_MIN]];
+        $part['stock_max'] = $csvData[$keyToIndexMapping[self::STOCK_MAX]];
+
         if (isset($keyToIndexMapping[self::VIDEO_EMBED_CODE]) && isset($csvData[$keyToIndexMapping[self::VIDEO_EMBED_CODE]])) {
             $part['video_embed_code'] = $csvData[$keyToIndexMapping[self::VIDEO_EMBED_CODE]];
-        }        
-        
+        }
+
         if (!empty($csvData[$keyToIndexMapping[self::IMAGE]])) {
             $images = explode(',', $csvData[$keyToIndexMapping[self::IMAGE]]);
             foreach($images as $index => $imageUrl) {
@@ -287,18 +293,18 @@ class CsvImportService implements CsvImportServiceInterface
         $part['bins'] = $this->binRepository->getAllBinsCsv($part['dealer_id'], $csvData, $keyToIndexMapping);
 
         // Return Part Data
-        return $part;          
+        return $part;
     }
-    
+
     /**
      * Returns true if valid or error message if invalid
-     * 
+     *
      * @param string $type
-     * @param string $value 
+     * @param string $value
      * @return bool|string
      */
-    private function isDataValid($type, $value) 
-    {       
+    private function isDataValid($type, $value)
+    {
         switch($type) {
             case self::VENDOR:
                 if (!empty($value)) {
@@ -312,7 +318,7 @@ class CsvImportService implements CsvImportServiceInterface
                 if (empty($value)) {
                     return "Brand cannot be empty.";
                 }
-                
+
                 $brand = Brand::where('name', $value)->first();
                 if (empty($brand)) {
                     return "Brand {$value} does not exist in the system.";
@@ -322,7 +328,7 @@ class CsvImportService implements CsvImportServiceInterface
                 if (empty($value)) {
                     return "Type cannot be empty.";
                 }
-                
+
                 $type = Type::where('name', $value)->first();
                 if (empty($type)) {
                     return "Type {$value} does not exist in the system.";
@@ -337,7 +343,7 @@ class CsvImportService implements CsvImportServiceInterface
                 if (empty($value)) {
                     return "Category cannot be empty.";
                 }
-                
+
                 $category = Category::where('name', $value)->first();
                 if (empty($category)) {
                     return "Category {$value} does not exist in the system.";
@@ -346,14 +352,14 @@ class CsvImportService implements CsvImportServiceInterface
             case self::SKU:
                 if (empty($value)) {
                     return "SKU cannot be empty.";
-                }                
-                
+                }
+
                 break;
             case self::SHOW_ON_WEBSITE:
                 if (!empty($value)) {
                    if (strtolower($value) != 'no' && strtolower($value) != 'yes') {
                         return "Show on website {$value} is not valid. Needs to be yes or no.";
-                   } 
+                   }
                 }
                 break;
             case self::IMAGE:
@@ -363,14 +369,14 @@ class CsvImportService implements CsvImportServiceInterface
                         if (filter_var($imageUrl, FILTER_VALIDATE_URL) === FALSE) {
                             return "Images need to be comma separated and valid URLs";
                         }
-                    }    
+                    }
                 }
                 break;
             case (preg_match(self::BIN_ID, $type) ? true : false) :
                 if (empty($value)) {
                     return "Bin cannot be empty.";
                 }
-                
+
                 $bin = Bin::where('bin_name', $value)->where('dealer_id', $this->bulkUpload->dealer_id)->first();
                 if (empty($bin)) {
                     return "Bin {$value} does not exist in the system.";
@@ -382,6 +388,12 @@ class CsvImportService implements CsvImportServiceInterface
                 }
                 else if(!is_numeric($value)) {
                     return "Bin quantity must be numeric.";
+                }
+                break;
+            case self::STOCK_MIN:
+            case self::STOCK_MAX:
+                if (!is_numeric($value) || $value < 0) {
+                    return "Stock Min/Max should be a positive number";
                 }
                 break;
         }
