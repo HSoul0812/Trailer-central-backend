cb0c1ef6c510fb1567e043f971fc2b567f0dde5c
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-05-26 18:18:30 -0500

    * TCWWW-312 Configured Blog Post API Fillable Fields

--- app/Models/Website/Blog/Post.php
+++ app/Models/Website/Blog/Post.php
@@ -25,30 +25,18 @@ class Post extends Model
      * @var array
      */
     protected $fillable = [
-        'dealer_id',
-        'vendor_id',
-        'vehicle_specific_id',
-        'manufacturer_id',
-        'brand_id',
-        'type_id',
-        'category_id',
-        'qb_id',
-        'subcategory',
+        'website_id',
+        'post_content',
+        'meta_keywords',
+        'meta_description',
         'title',
-        'alternative_part_number',
-        'sku',
-        'price',
-        'dealer_cost',
-        'msrp',
-        'weight',
-        'weight_rating',
-        'description',
-        'qty',
-        'show_on_website',
-        'is_vehicle_specific',
-        'video_embed_code',
-        'stock_min',
-        'stock_max',
+        'url_path',
+        'entity_config',
+        'date_created',
+        'date_updated',
+        'date_published',
+        'status',
+        'deleted',
     ];
 
     /**
@@ -72,14 +60,6 @@ class Post extends Model
     public function toSearchableArray()
     {
         $array = $this->toArray();
-
-        $array['brand'] = (string)$this->brand;
-        $array['manufacturer'] = (string)$this->manufacturer;
-        $array['category'] = (string)$this->category;
-        $array['type'] = (string)$this->type;
-
-        $array['images'] = $this->images->toArray();
-        $array['vehicle_specific'] = $this->vehicleSpecifc;
 
         return $array;
     }
