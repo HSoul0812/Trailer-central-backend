51e04883b84e3d1a9e8a4940ab491c8f10dcce78
pekar <sergeypekar77@gmail.com>
2020-07-02 19:20:28 +0300

    #DS-210 added migration for changing entity_type column in website_entity table

--- database/migrations/2020_07_02_151531_modify_entity_type_column_in_website_entity_table.php
+++ database/migrations/2020_07_02_151531_modify_entity_type_column_in_website_entity_table.php
@@ -16,7 +16,7 @@ class ModifyEntityTypeColumnInWebsiteEntityTable extends Migration
             "ALTER TABLE
                       `website_entity`
                    MODIFY COLUMN
-                      `entity_type` enum('1','2','3','5','6','7','8','9','11','12','15','16','910','25','26','27','28','29','30','31','32','33','34','35','36','37','38') DEFAULT NULL;"
+                      `entity_type` enum('1','2','3','5','6','7','8','9','11','12','15','16','910','25','26','27','28','29','30','31','32','33','34','35','36','37','50') DEFAULT NULL;"
         );
     }
 
