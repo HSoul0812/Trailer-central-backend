bb1301cbe8c7a0cdd6e1e2fbfbd1c02ab246ffda
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-07-02 22:50:35 -0500

    * CRM-204 Fixed Errors With SMS Number Being Empty on Campaign/Blast Create/Update

--- app/Http/Requests/CRM/Text/UpdateBlastRequest.php
+++ app/Http/Requests/CRM/Text/UpdateBlastRequest.php
@@ -16,7 +16,7 @@ class UpdateBlastRequest extends Request {
         'template_id' => 'integer',
         'campaign_name' => 'string',
         'campaign_subject' => 'string',
-        'from_sms_number' => 'string',
+        'from_sms_number' => 'nullable|string',
         'action' => 'in:inquired,purchased',
         'location_id' => 'nullable|integer',
         'send_after_days' => 'nullable|integer',
