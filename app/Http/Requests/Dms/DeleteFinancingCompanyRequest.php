f3260b6818a1c6871a60ff559787e1aef52b4104
acfabro <acfabro@gmail.com>
2020-07-11 01:25:34 +0800

    #DMS-516 revised ownership validation

--- app/Http/Requests/Dms/DeleteFinancingCompanyRequest.php
+++ app/Http/Requests/Dms/DeleteFinancingCompanyRequest.php
@@ -4,12 +4,26 @@
 namespace App\Http\Requests\Dms;
 
 
-use Dingo\Api\Http\FormRequest;
+use App\Http\Requests\Request;
+use App\Models\CRM\Dms\FinancingCompany;
 
-class DeleteFinancingCompanyRequest extends FormRequest
+class DeleteFinancingCompanyRequest extends Request
 {
     protected $rules = [
-        'id' => 'required|integer' // todo add a 'owned' validator
+        'id' => 'required|integer'
     ];
+
+    protected function getObject()
+    {
+        return new FinancingCompany();
+    }
+
+    protected function getObjectIdValue() {
+        return $this->input('id');
+    }
+
+    protected function validateObjectBelongsToUser() {
+        return true;
+    }
 
 }
