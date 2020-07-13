b01d8ede4cd371de4e612fd4bee8db5818d2b31f
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-07-13 14:33:25 -0500

    * CRM-226 Fixed Getting Leads on Blast Repository

--- app/Repositories/CRM/Text/BlastRepository.php
+++ app/Repositories/CRM/Text/BlastRepository.php
@@ -270,10 +270,10 @@ class BlastRepository implements BlastRepositoryInterface {
                      ->where('website_lead.dealer_id', $dealerId);
 
         // Is Archived?!
-        if($campaign->included_archived === -1 || $campaign->include_archived === '-1') {
+        if($blast->included_archived === -1 || $blast->include_archived === '-1') {
             $query = $query->where('website_lead.is_archived', 0);
-        } elseif($campaign->included_archived !== 0 && $campaign->include_archived === '0') {
-            $query = $query->where('website_lead.is_archived', $campaign->include_archived);
+        } elseif($blast->included_archived !== 0 && $blast->include_archived === '0') {
+            $query = $query->where('website_lead.is_archived', $blast->include_archived);
         }
 
         // Get Categories
