5fec227eb16dd0bfc8ab57696925fc6aaab0141d
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-07-15 02:25:44 -0500

    * CRM-234 Fixed InteractionEmail Override By Mistake

--- app/Mail/InteractionEmail.php
+++ app/Mail/InteractionEmail.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class AutoAssignEmail extends Mailable
+class InteractionEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
