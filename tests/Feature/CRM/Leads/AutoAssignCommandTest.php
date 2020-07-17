4eb7fb26b45840bc60dbc4ca5aa7d246b0773cd5
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-07-16 23:36:57 -0500

    * CRM-242 Updated Auto Assign Command Test

--- tests/Feature/CRM/Leads/AutoAssignCommandTest.php
+++ tests/Feature/CRM/Leads/AutoAssignCommandTest.php
@@ -6,12 +6,15 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Mail;
 use App\Models\CRM\Leads\Lead;
+use App\Mail\AutoAssignEmail;
 use Tests\TestCase;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 class AutoAssignCommandTest extends TestCase
 {
+    use RefreshDatabase;
+
     // Initialize Test Constants
     const TEST_DEALER_ID = 1001;
     const TEST_LOCATION_ID = [11998, 12084, 14427];
@@ -30,13 +33,28 @@ class AutoAssignCommandTest extends TestCase
      */
     public function testDealer()
     {
+        // Initialize Time
+        date_default_timezone_set(env('DB_TIMEZONE'));
+        $datetime = new \DateTime();
+        $datetime->setTimezone(new \DateTimeZone(env('DB_TIMEZONE')));
+
+        // Build Random Factory Leads
+        $leads = factory(Lead::class, 10)->create();
 
         // Fake Mail
         Mail::fake();
 
         // Call Leads Assign Command
-        $this->artisan('leads:assign:auto ' . self::TEST_WEBSITE_ID)
+        $this->artisan('leads:assign:auto ' . self::TEST_DEALER_ID)
             ->assertSuccessful()
             ->assertExitCode(0);
+
+        // Loop Leads
+        foreach($leads as $lead) {
+            // Assert a message was sent to the given leads...
+            Mail::assertSent(AutoAssignMail::class, function ($mail) use ($lead) {
+                return $mail->hasTo($lead->email_address);
+            });
+        }
     }
 }
