23e8189ef16beba73deb0a4ec09a6973ff5a70ac
JaidynReiman <david.a.conway.jr@gmail.com>
2020-07-18 15:12:57 -0500

    * CRM-242 Fixed Error on Lead Factory

--- database/factories/LeadFactory.php
+++ database/factories/LeadFactory.php
@@ -8,18 +8,18 @@ use Faker\Generator as Faker;
 
 $factory->define(Lead::class, function (Faker $faker) {
     // Select Random Values
-    $websiteKey = array_rand(AutoAssignCommandTest::TEST_WEBSITE_ID);
-    $locationKey = array_rand(AutoAssignCommandTest::TEST_LOCATION_ID);
-    $inventoryKey = array_rand(AutoAssignCommandTest::TEST_INVENTORY_ID);
-    $titleKey = array_rand(AutoAssignCommandTest::TEST_FORM_TITLE);
+    $websiteKey = array_rand(AutoAssignTest::TEST_WEBSITE_ID);
+    $locationKey = array_rand(AutoAssignTest::TEST_LOCATION_ID);
+    $inventoryKey = array_rand(AutoAssignTest::TEST_INVENTORY_ID);
+    $titleKey = array_rand(AutoAssignTest::TEST_FORM_TITLE);
 
     // Return Overrides
     return [
-        'website_id' => AutoAssignCommandTest::TEST_WEBSITE_ID,
-        'dealer_id' => AutoAssignCommandTest::TEST_DEALER_ID[$websiteKey],
-        'dealer_location_id' => AutoAssignCommandTest::TEST_LOCATION_ID[$locationKey],
-        'inventory_id' => AutoAssignCommandTest::TEST_INVENTORY_ID[$inventoryKey],
-        'title' => AutoAssignCommandTest::TEST_FORM_TITLE[$titleKey],
+        'website_id' => AutoAssignTest::TEST_WEBSITE_ID,
+        'dealer_id' => AutoAssignTest::TEST_DEALER_ID[$websiteKey],
+        'dealer_location_id' => AutoAssignTest::TEST_LOCATION_ID[$locationKey],
+        'inventory_id' => AutoAssignTest::TEST_INVENTORY_ID[$inventoryKey],
+        'title' => AutoAssignTest::TEST_FORM_TITLE[$titleKey],
         'referral' => $faker->url,
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
