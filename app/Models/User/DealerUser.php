cc5efecfc536f0710e635810ea3aa68013642ef1
JaidynReiman <david.a.conway.jr@gmail.com>
2020-07-21 20:51:28 -0500

    * CRM-259 Fixed Making DealerUser Authenticatable

--- app/Models/User/DealerUser.php
+++ app/Models/User/DealerUser.php
@@ -59,6 +59,21 @@ class DealerUser extends Model implements Authenticatable
     public function getRememberToken() {}
 
     /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setRememberToken($value) {}
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName() {}
+
+    /**
      * Get Access Token
      * 
      * @return type
