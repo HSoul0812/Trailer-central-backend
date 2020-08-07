d1b4394dee10d444ce4a4f4a13e408b39bdd7d6d
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-08-06 20:58:16 -0500

    * CRM-295 Moved createsApplication Back to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,14 +3,27 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Contracts\Console\Kernel;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-
     protected function accessToken()
     {
         return env('TESTS_DEFAULT_ACCESS_TOKEN', '123');
+    }
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
     }
 
 }
