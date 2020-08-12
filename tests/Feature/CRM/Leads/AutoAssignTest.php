01918c5e4eb04e338a37111a93f31ff95a031477
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-08-12 11:39:36 -0500

    * CRM-300 Test Finding Exact Match Sales People

--- tests/Feature/CRM/Leads/AutoAssignTest.php
+++ tests/Feature/CRM/Leads/AutoAssignTest.php
@@ -483,9 +483,6 @@ class AutoAssignTest extends TestCase
                 }
 
                 // Set Sales Person ID
-                if(empty($salesPerson->id)) {
-                    var_dump($salesPerson);
-                }
                 $this->roundRobin[$dealer->id][$locationId][$salesType] = $salesPerson->id;
             }
 
