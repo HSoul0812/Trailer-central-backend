7e7821e4a1ed881e751c902a7d5c921ac2a77554
JaidynReiman <david.a.conway.jr@gmail.com>
2020-08-17 18:28:38 -0500

    * CRM-298 Removed Debug on Campaign/Blast Find Leads

--- app/Repositories/CRM/Text/CampaignRepository.php
+++ app/Repositories/CRM/Text/CampaignRepository.php
@@ -336,7 +336,7 @@ class CampaignRepository implements CampaignRepositoryInterface {
         }
 
         // Return Filtered Query
-        $query = $query->where(function (Builder $query) use($campaign) {
+        return $query->where(function (Builder $query) use($campaign) {
             return $query->where('website_lead.dealer_location_id', $campaign->location_id)
                          ->orWhereRaw('(website_lead.dealer_location_id = 0 AND inventory.dealer_location_id = ?)', [$campaign->location_id]);
         })->whereRaw('DATE_ADD(website_lead.date_submitted, INTERVAL +' . $campaign->send_after_days . ' DAY) < NOW()')
