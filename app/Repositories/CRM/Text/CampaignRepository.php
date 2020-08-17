7472a8508c1b5dd54c97b06e54dafa78c3a8228c
JaidynReiman <david.a.conway.jr@gmail.com>
2020-08-17 15:18:27 -0500

    * CRM-298 Debug Campaign/Blast Find Leads Query

--- app/Repositories/CRM/Text/CampaignRepository.php
+++ app/Repositories/CRM/Text/CampaignRepository.php
@@ -336,10 +336,13 @@ class CampaignRepository implements CampaignRepositoryInterface {
         }
 
         // Return Filtered Query
-        return $query->where(function (Builder $query) use($campaign) {
+        $query = $query->where(function (Builder $query) use($campaign) {
             return $query->where('website_lead.dealer_location_id', $campaign->location_id)
                          ->orWhereRaw('(website_lead.dealer_location_id = 0 AND inventory.dealer_location_id = ?)', [$campaign->location_id]);
         })->whereRaw('DATE_ADD(website_lead.date_submitted, INTERVAL +' . $campaign->send_after_days . ' DAY) < NOW()')
           ->whereRaw('(FLOOR(UNIX_TIMESTAMP(NOW() - website_lead.date_submitted) / (60 * 60 * 24)) - ' . $campaign->send_after_days . ') > 10');
+        echo $query->getSql();
+        die;
+        return $query;
     }
 }
