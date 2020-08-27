1e1dbe52cceb5baa8b6d5f8bd0e542051949430d
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-08-26 23:34:45 -0500

    * CRM-299 Fixed Deliver Blast Test Leads

--- tests/Feature/CRM/Text/DeliverBlastTest.php
+++ tests/Feature/CRM/Text/DeliverBlastTest.php
@@ -8,7 +8,7 @@ use App\Models\CRM\Leads\Lead;
 use App\Models\CRM\Text\Blast;
 use App\Models\CRM\Text\Template;
 use App\Models\User\NewDealerUser;
-use Faker\Provider\DateTime as FakerDate;
+use Faker\Factory as Faker;
 use Tests\TestCase;
 
 class DeliverBlastTest extends TestCase
@@ -23,6 +23,11 @@ class DeliverBlastTest extends TestCase
     protected $dealerLocation;
 
     /**
+     * Faker\Generator $faker
+     */
+    protected $faker;
+
+    /**
      * Set Up Test
      */
     public function setUp(): void
@@ -33,6 +38,9 @@ class DeliverBlastTest extends TestCase
         $this->blasts = $this->app->make('App\Repositories\CRM\Text\BlastRepositoryInterface');
         $this->templates = $this->app->make('App\Repositories\CRM\Text\TemplateRepositoryInterface');
         $this->dealerLocation = $this->app->make('App\Repositories\User\DealerLocationRepositoryInterface');
+
+        // Create Faker
+        $this->faker = new Faker();
     }
 
     /**
@@ -192,14 +200,11 @@ class DeliverBlastTest extends TestCase
             }
         }
 
-        // Create Faker
-        $faker = new FakerDate();
-
         // Create 10 Leads in Last 15 Days
         for($n = 0; $n < 10; $n++) {
             // Get Random Date Since "Send After Days"
             factory(Lead::class)->create([
-                'date_submitted' => $faker->dateTimeBetween('-' . $blast->send_after_days . ' days')
+                'date_submitted' => $this->faker->dateTimeBetween('-' . $blast->send_after_days . ' days')
             ]);
         }
 
@@ -208,7 +213,7 @@ class DeliverBlastTest extends TestCase
         for($n = 0; $n < 5; $n++) {
             // Get Random Date Since "Send After Days"
             $leads[] = factory(Lead::class)->create([
-                'date_submitted' => $faker->dateTimeBetween('-' . ($blast->send_after_days + 10) . ' days', '-' . $blast->send_after_days . ' days')
+                'date_submitted' => $this->faker->dateTimeBetween('-' . ($blast->send_after_days + 10) . ' days', '-' . $blast->send_after_days . ' days')
             ]);
         }
         return $leads;
