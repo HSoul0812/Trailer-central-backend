a4b7312edf5364f9f3f52f1cfde63b52bbc6542f
Alberto <vaihtlumin@gmail.com>
2020-09-10 18:53:57 -0400

    #DMS-718

---
+++ database/migrations/inventory/2020_09_10_184106_add_qb_sync_indexes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ADdQbSyncIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inventory', function (Blueprint $table) {            
+            $table->index(['send_to_quickbooks', 'bill_id', 'qb_sync_processed']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inventory', function (Blueprint $table) {
+            $table->dropIndex(['send_to_quickbooks', 'bill_id', 'qb_sync_processed']);
+        });
+    }
+}
