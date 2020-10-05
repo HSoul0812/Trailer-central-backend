677700c31108515fcaf21573dfd3e63cc225f84e
Alberto <vaihtlumin@gmail.com>
2020-10-05 18:01:22 -0400

    #CRM-338

--- app/Http/Requests/CRM/Text/CreateTextRequest.php
+++ app/Http/Requests/CRM/Text/CreateTextRequest.php
@@ -12,7 +12,7 @@ use App\Http\Requests\Request;
 class CreateTextRequest extends Request {
 
     protected $rules = [
-        'log_message' => 'required|string|max:160',
+        'log_message' => 'required|string',
         'from_number' => 'required|regex:/(0-9)?[0-9]{10}/',
         'to_number' => 'required|regex:/(0-9)?[0-9]{10}/',
         'date_sent' => 'nullable|date_format:Y-m-d H:i:s'
