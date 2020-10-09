e93b9b2e5c25464e530a4a52dfbb19d66b445a07
acfabro <acfabro@gmail.com>
2020-10-09 23:44:17 +0800

    #DMS-736

---
+++ app/Models/CRM/Dms/ServiceOrder/LaborCode.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Models\CRM\Dms\ServiceOrder;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class LaborCode extends Model
+{
+    protected $table = 'dms_settings_labor_code';
+}
