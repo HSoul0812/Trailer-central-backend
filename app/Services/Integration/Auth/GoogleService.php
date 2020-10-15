053c174f158ee5adc5675c4e2b6e7e6433883c01
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-10-14 23:07:13 -0500

    * CRM-345 Added Support for Expired Tokens

--- app/Services/Integration/Auth/GoogleService.php
+++ app/Services/Integration/Auth/GoogleService.php
@@ -31,14 +31,17 @@ class GoogleService implements GoogleServiceInterface
         }
 
         // Initialize Client
-        $this->client = new \Google_Client(['client_id' => $_ENV['GOOGLE_OAUTH_CLIENT_ID']]);
+        $this->client = new \Google_Client([
+            'application_name' => $_ENV['GOOGLE_OAUTH_APP_NAME'],
+            'client_id' => $_ENV['GOOGLE_OAUTH_CLIENT_ID']
+        ]);
         if(empty($this->client)) {
             throw new FailedConnectGapiClientException;
         }
     }
 
     /**
-     * Validate Google API Access Token Exists
+     * Validate Google API Access Token Exists and Refresh if Possible
      * 
      * @param AccessToken $accessToken
      * @return array of validation info
@@ -49,13 +52,24 @@ class GoogleService implements GoogleServiceInterface
             throw new MissingGapiIdTokenException;
         }
 
+        // Configure Client
+        $this->client->setAccessToken([
+            'access_token' => $accessToken->access_token,
+            'id_token' => $accessToken->id_token,
+            'created' => $accessToken->issued_at
+        ]);
+        $this->client->setScopes($accessToken->scope);
+
         // Initialize Vars
         $result = [
-            'is_valid' => false
+            'access_token' => $accessToken->access_token,
+            'is_valid' => false,
+            'is_expired' => true
         ];
 
         // Validate ID Token
         try {
+            // Verify ID Token is Valid
             $payload = $this->client->verifyIdToken($accessToken->id_token);
             if ($payload) {
                 $result['is_valid'] = true;
@@ -64,6 +78,27 @@ class GoogleService implements GoogleServiceInterface
         catch (\Exception $e) {
             // We actually just want to verify this is true or false
             // If it throws an exception, that means its false, the token isn't valid
+            // This exception can be used for other processes but isn't needed in this method
+            //throw new InvalidGapiIdTokenException;
+        }
+
+        // Validate If Expired
+        try {
+            // If there is no previous token or it's expired.
+            if ($client->isAccessTokenExpired()) {
+                // Refresh the token if possible, else fetch a new one.
+                if ($client->getRefreshToken()) {
+                    $result['access_token'] = $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+                    $result['is_expired'] = false;
+                }
+            }
+            // Its Not Expired!
+            else {
+                $result['is_expired'] = false;
+            }
+        } catch (\Exception $e) {
+            // We actually just want to verify this is true or false
+            // If it throws an exception, that means its false, the token is expired
             // This exception can be used for other processes but isn't needed in this method
             //throw new InvalidGapiIdTokenException;
         }
