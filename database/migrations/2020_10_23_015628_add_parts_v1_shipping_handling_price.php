167e13f27e5f018189d520268c9d19f07cfec9d4
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-10-23 02:29:10 -0500

    * DS-319 Fixed Migration for Missing Shipping Fields to Parts

--- database/migrations/2020_10_23_015628_add_parts_v1_shipping_handling_price.php
+++ database/migrations/2020_10_23_015628_add_parts_v1_shipping_handling_price.php
@@ -16,9 +16,9 @@ class AddPartsV1ShippingHandlingPrice extends Migration
         // Add Columns to Parts
         Schema::table('parts_v1', function (Blueprint $table) {
             $table->decimal('shipping_fee', 9, 2)->after('msrp')->nullable();
-            $table->tinyInteger('use_handling_fee', 1)->after('shipping_fee')->nullable();
+            $table->tinyInteger('use_handling_fee')->after('shipping_fee')->nullable();
             $table->decimal('handling_fee', 9, 2)->after('use_handling_fee')->nullable();
-            $table->tinyInteger('fulfillment_type', 1)->after('handling_fee')->nullable();
+            $table->tinyInteger('fulfillment_type')->after('handling_fee')->nullable();
         });
 
         // Add Columns to Part Manufacturer
