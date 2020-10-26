8865c0a7d3d6b64e1534e79eab348b7f0e890c42
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-10-26 15:43:45 -0500

    * DS-354 Made Some Fields Nullable in Part Orders Migration

--- database/migrations/2020_10_26_125052_create_part_orders_table.php
+++ database/migrations/2020_10_26_125052_create_part_orders_table.php
@@ -18,17 +18,17 @@ class CreatePartOrdersTable extends Migration
             $table->bigIncrements('id');
             $table->integer('dealer_id');
             $table->integer('website_id');
-            $table->string('shipto_name');
-            $table->text('shipto_address');
             $table->enum('status', PartOrder::STATUS_FIELDS);
             $table->enum('fulfillment_type', PartOrder::FULFILLMENT_TYPES);
-            $table->string('email_address');
-            $table->string('phone_number', 20);
+            $table->string('email_address')->nullable();
+            $table->string('phone_number', 20)->nullable();
+            $table->string('shipto_name')->nullable();
+            $table->text('shipto_address')->nullable();
             $table->text('cart_items');
             $table->decimal('subtotal', 9, 2);
             $table->decimal('tax', 9, 2);
             $table->decimal('shipping', 9, 2);
-            $table->string('order_key');
+            $table->string('order_key')->nullable();
             $table->timestamps();
         });
     }
