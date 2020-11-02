0dabe9dfc70279a6197447319f090ced73369b23
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-11-02 16:14:58 -0600

    * IN-403 Fixed Catalog Service Returning Result

--- app/Services/Integration/Facebook/CatalogService.php
+++ app/Services/Integration/Facebook/CatalogService.php
@@ -6,6 +6,7 @@ use App\Repositories\Integration\Auth\TokenRepositoryInterface;
 use App\Repositories\Integration\Facebook\CatalogRepositoryInterface;
 use App\Services\Integration\AuthServiceInterface;
 use App\Utilities\Fractal\NoDataArraySerializer;
+use App\Transformers\Integration\Facebook\CatalogTransformer;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
 
