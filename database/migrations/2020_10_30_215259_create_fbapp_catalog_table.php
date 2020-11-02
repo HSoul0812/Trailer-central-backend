80a3f6cae96380832e38191f13fe4465dd789b14
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-11-02 12:32:50 -0600

    * IN-398 Finished Catalog Service and Transformer

--- database/migrations/2020_10_30_215259_create_fbapp_catalog_table.php
+++ database/migrations/2020_10_30_215259_create_fbapp_catalog_table.php
@@ -21,7 +21,7 @@ class CreateFbappCatalogTable extends Migration
             $table->string('account_name');
             $table->integer('user_id')->index();
             $table->text('filters');
-            $table->boolean('is_active')->default(false);
+            $table->boolean('is_active')->default(false)->index();
             $table->timestamps();
 
             $table->index(['dealer_id', 'dealer_location_id']);
