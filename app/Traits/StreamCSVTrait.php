fdd4fe68824debe7396376892aa78874c055c6f3
Alberto <vaihtlumin@gmail.com>
2020-11-03 17:49:44 -0500

    #HOTFIX Added NTPStagCommand

--- app/Traits/StreamCSVTrait.php
+++ app/Traits/StreamCSVTrait.php
@@ -3,19 +3,12 @@
 namespace App\Traits;
 
 use Illuminate\Support\Facades\Storage;
+use App\Traits\S3\StreamTrait;
 
 trait StreamCSVTrait {
     
-    /**     
-     * @var string
-     */
-    protected $s3Bucket;
-    
-    /**     
-     * @var string
-     */
-    protected $s3Key;
-        
+    use StreamTrait;
+            
     /**
      * Applies `$callback($row, $lineNumber)` to each line of the csv
      *
@@ -24,17 +17,10 @@ trait StreamCSVTrait {
      */
     protected function streamCsv($callback)
     {        
-        $adapter = Storage::disk('s3')->getAdapter();
-        $client = $adapter->getClient();
-        $client->registerStreamWrapper();
-        $stream = $this->getS3Stream();         
-
-        // iterate through all lines
-        $lineNumber = 1;
-        while (!feof($stream)) {
-             $isEmptyRow = true;
-             $csvData = fgetcsv($stream);
-
+        $this->stream(function($stream, $lineNumber) use ($callback) {
+            $isEmptyRow = true;
+            $csvData = fgetcsv($stream);
+            
              // see if the row is empty,
              // one value is enough to indicate non empty row
              foreach($csvData as $value) {
@@ -46,31 +32,12 @@ trait StreamCSVTrait {
 
              // skip empty rows
              if ($isEmptyRow) {
-                 continue;
+                 return;
              }
 
              // apply $callback to the row
              $callback($csvData, $lineNumber++);
-             flush();
-        }
-    }
-    
-    private function getS3Stream()
-    {
-        if (empty($this->s3Bucket)) {
-            throw new \Exception("s3 Bucket cannot be empty");
-        }
-        
-        if (empty($this->s3Key)) {
-            throw new \Exception("s3 Key cannot be empty");
-        }
-        
-        // open the file from s3
-        if (!($stream = fopen("s3://{$this->s3Bucket}/{$this->s3Key}", 'r'))) {
-            throw new \Exception('Could not open stream for reading file: ['.$this->s3Key.']');
-        }
-        
-        return $stream;
+        });
     }
     
 }
