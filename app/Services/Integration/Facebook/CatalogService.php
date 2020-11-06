a7912d1e38b302b4c504e6bb89b49fa9f2c4a278
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-11-05 21:43:35 -0600

    * IN-406 Debug Getting Catalog by Page ID

--- app/Services/Integration/Facebook/CatalogService.php
+++ app/Services/Integration/Facebook/CatalogService.php
@@ -148,6 +148,7 @@ class CatalogService implements CatalogServiceInterface
 
             // Get Catalog
             $catalog = $this->catalogs->getByPageId(['page_id' => $integration->page_id]);
+            var_dump($catalog);
 
             // Feed ID Exists?
             $feed = null;
