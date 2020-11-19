15efc6c79ec7a892c2cc282e060afd3255d6e95c
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-11-19 16:33:41 -0600

    * IN-408 Fixed Delete Catalog Service Mock

--- tests/Unit/Services/Integration/Facebook/CatalogServiceTest.php
+++ tests/Unit/Services/Integration/Facebook/CatalogServiceTest.php
@@ -8,7 +8,7 @@ use App\Repositories\Integration\Auth\TokenRepositoryInterface;
 use App\Repositories\Integration\Facebook\PageRepositoryInterface;
 use App\Repositories\Integration\Facebook\CatalogRepositoryInterface;
 use App\Repositories\Repository;
-use App\Services\Integration\Facebook\AuthServiceInterface;
+use App\Services\Integration\AuthServiceInterface;
 use App\Services\Integration\Facebook\CatalogService;
 use App\Services\Integration\Facebook\BusinessServiceInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
@@ -61,8 +61,8 @@ class CatalogServiceTest extends TestCase
         $this->businessServiceMock = Mockery::mock(BusinessServiceInterface::class);
         $this->app->instance(BusinessServiceInterface::class, $this->businessServiceMock);
 
-        $this->authServiceMock = Mockery::mock(AuthServiceInterface::class);
-        $this->app->instance(AuthServiceInterface::class, $this->authServiceMock);
+        //$this->authServiceMock = Mockery::mock(AuthServiceInterface::class);
+        //$this->app->instance(AuthServiceInterface::class, $this->authServiceMock);
 
         $this->tokenRepositoryMock = Mockery::mock(TokenRepositoryInterface::class);
         $this->app->instance(TokenRepositoryInterface::class, $this->tokenRepositoryMock);
@@ -204,6 +204,146 @@ class CatalogServiceTest extends TestCase
 
         // Assert Match
         $this->assertSame($result['validate'], $validate);
+    }
+
+    /**
+     * @covers ::update
+     *
+     * @throws BindingResolutionException
+     */
+    public function testUpdate()
+    {
+        // Get Test Catalog
+        $catalogId = (int) $_ENV['TEST_FB_CATALOG_ID'];
+        $catalog = Catalog::find($catalogId);
+        $validate = ['is_valid' => true, 'is_expired' => false];
+
+        // Update Request Params
+        $updateRequestParams = [
+            'dealer_id' => $catalog->dealer_id,
+            'dealer_location_id' => $catalog->dealer_location_id,
+            'access_token' => $catalog->accessToken->access_token,
+            'id_token' => $catalog->accessToken->id_token,
+            'expires_in' => $catalog->accessToken->expires_in,
+            'expires_at' => $catalog->accessToken->expires_at,
+            'issued_at' => $catalog->accessToken->issued_at,
+            'business_id' => $catalog->business_id,
+            'catalog_id' => $catalog->catalog_id,
+            'account_name' => $catalog->account_name,
+            'account_id' => $catalog->account_id,
+            'page_title' => $catalog->page->title,
+            'page_id' => $catalog->page->page_id,
+            'feed_id' => $catalog->feed_id,
+            'filters' => '',
+            'is_active' => 1
+        ];
+
+        // Update Catalog Params
+        $updateCatalogParams = $updateRequestParams;
+        $updateCatalogParams['fbapp_page_id'] = $catalog->page->id;
+
+        // Relation Auth Params
+        $relationAuthParams = $updateCatalogParams;
+        $relationAuthParams['token_type'] = 'facebook';
+        $relationAuthParams['relation_type'] = 'fbapp_catalog';
+        $relationAuthParams['relation_id'] = $catalog->id;
+
+        // Update Auth Params
+        $updateAuthParams = $relationAuthParams;
+        $updateAuthParams['refresh_token'] = $catalog->accessToken->refresh_token;
+
+        /** @var CatalogService $service */
+        $service = $this->app->make(CatalogService::class);
+
+        // Mock Update Page
+        $this->pageRepositoryMock
+            ->shouldReceive('create')
+            ->once()
+            ->with($updateRequestParams)
+            ->andReturn($catalog->page);
+
+        // Mock Update Catalog
+        $this->catalogRepositoryMock
+            ->shouldReceive('update')
+            ->once()
+            ->with($updateCatalogParams)
+            ->andReturn($catalog);
+
+        // Mock Get Relation Token
+        $this->tokenRepositoryMock
+            ->shouldReceive('getRelation')
+            ->once()
+            ->with($relationAuthParams)
+            ->andReturn([
+                'access_token' => $catalog->accessToken->refresh_token
+            ]);
+
+        // Mock Validate Access Token
+        $this->businessServiceMock
+            ->shouldReceive('validate')
+            ->once()
+            ->with($catalog->accessToken)
+            ->andReturn($validate);
+
+        // Validate Update Catalog Result
+        $result = $service->update($updateRequestParams);
+
+        // Assert Match
+        $this->assertSame($result['data']['id'], $catalogId);
+
+        // Assert Match
+        $this->assertSame($result['validate'], $validate);
+    }
+
+    /**
+     * @covers ::delete
+     *
+     * @throws BindingResolutionException
+     */
+    public function testDelete()
+    {
+        // Get Test Catalog
+        $catalogId = (int) $_ENV['TEST_FB_CATALOG_ID'];
+        $catalog = Catalog::find($catalogId);
+
+        /** @var CatalogService $service */
+        $service = $this->app->make(CatalogService::class);
+
+        // Mock Get Catalog
+        $this->catalogRepositoryMock
+            ->shouldReceive('get')
+            ->once()
+            ->with(['id' => $catalogId])
+            ->andReturn($catalog);
+
+        // Mock Delete Feed
+        $this->businessServiceMock
+            ->shouldReceive('deleteFeed')
+            ->once()
+            ->with($catalog->accessToken, $catalog->catalog_id, $catalog->feed_id);
+
+        // Mock Delete Access Token
+        $this->authServiceMock
+            ->shouldReceive('delete')
+            ->once()
+            ->with([
+                'token_type' => 'facebook',
+                'relation_type' => 'fbapp_catalog',
+                'relation_id' => $catalogId
+            ]);
+
+        // Mock Delete Catalog
+        $this->catalogServiceMock
+            ->shouldReceive('delete')
+            ->once()
+            ->with($catalogId)
+            ->andReturn(true);
+
+        // Validate Delete Catalog Result
+        $result = $service->delete($catalogId);
+
+        // Assert Match
+        $this->assertTrue($result);
     }
 
     /**
