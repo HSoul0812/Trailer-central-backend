c500debb65cc9bd0e9391dbf8bd7afeabfc60fed
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-11-19 14:18:05 -0600

    * IN-408 Created Two Initial Catalog Service Tests

--- tests/Unit/Services/Integration/Facebook/CatalogServiceTest.php
+++ tests/Unit/Services/Integration/Facebook/CatalogServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Services\Integration\Facebook;
 
+use App\Models\Integration\Facebook\Catalog;
 use App\Jobs\Integration\Facebook\CatalogJob;
 use App\Repositories\Integration\Auth\TokenRepositoryInterface;
 use App\Repositories\Integration\Facebook\PageRepositoryInterface;
@@ -9,7 +10,7 @@ use App\Repositories\Integration\Facebook\CatalogRepositoryInterface;
 use App\Repositories\Repository;
 use App\Services\Integration\Facebook\AuthServiceInterface;
 use App\Services\Integration\Facebook\CatalogService;
-use App\Services\Integration\Facebook\BusinessServiceInterface;
+use App\Services\Integration\Facebook\CatalogServiceInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
@@ -19,12 +20,12 @@ use PHPUnit\Framework\MockObject\MockObject;
 use Tests\TestCase;
 
 /**
- * Test for App\Services\Integration\Facebook\BusinessService
+ * Test for App\Services\Integration\Facebook\CatalogService
  *
- * Class BusinessServiceTest
+ * Class CatalogServiceTest
  * @package Tests\Unit\Services\Integration\Facebook
  *
- * @coversDefaultClass \App\Services\Inventory\BusinessService
+ * @coversDefaultClass \App\Services\Inventory\CatalogService
  */
 class CatalogServiceTest extends TestCase
 {
@@ -49,7 +50,7 @@ class CatalogServiceTest extends TestCase
     private $authServiceMock;
 
     /**
-     * @var LegacyMockInterface|BusinessServiceInterface
+     * @var LegacyMockInterface|CatalogServiceInterface
      */
     private $businessServiceMock;
 
@@ -69,8 +70,8 @@ class CatalogServiceTest extends TestCase
         $this->authServiceMock = Mockery::mock(AuthServiceInterface::class);
         $this->app->instance(AuthServiceInterface::class, $this->authServiceMock);
 
-        $this->businessServiceMock = Mockery::mock(BusinessServiceInterface::class);
-        $this->app->instance(BusinessServiceInterface::class, $this->businessServiceMock);        
+        $this->businessServiceMock = Mockery::mock(CatalogServiceInterface::class);
+        $this->app->instance(CatalogServiceInterface::class, $this->businessServiceMock);        
     }
 
     /**
@@ -80,20 +81,136 @@ class CatalogServiceTest extends TestCase
      */
     public function testShow()
     {
-        // Get Test Catalog ID
+        // Get Test Catalog
         $catalogId = $_ENV['TEST_FB_CATALOG_ID'];
+        $catalog = Catalog::find($catalogId);
+        $validate = ['is_valid' => true, 'is_expired' => true];
 
-        /** @var BusinessService $service */
+        /** @var CatalogService $service */
         $service = $this->app->make(CatalogService::class);
+
+        // Mock Get Catalog
+        $this->catalogRepositoryMock
+            ->shouldReceive('get')
+            ->once()
+            ->with(['id' => $catalogId])
+            ->andReturn($catalog);
+
+        // Mock Validate Access Token
+        $this->businessRepositoryMock
+            ->shouldReceive('validate')
+            ->once()
+            ->with($catalog->accessToken)
+            ->andReturn($validate);
 
         // Validate Show Catalog Result
         $result = $service->show($catalogId);
 
-        // Assert is Valid
-        $this->assertTrue($result['validate']['is_valid']);
+        // Assert Match
+        $this->assertSame($result, [
+            'data' => $catalog,
+            'validate' => $validate
+        ]);
+    }
 
-        // Assert Is Not Expired
-        $this->assertFalse($result['validate']['is_expired']);
+    /**
+     * @covers ::create
+     *
+     * @throws BindingResolutionException
+     */
+    public function testCreate()
+    {
+        // Get Test Catalog
+        $catalogId = $_ENV['TEST_FB_CATALOG_ID'];
+        $catalog = Catalog::find($catalogId);
+        $validate = ['is_valid' => true, 'is_expired' => true];
+
+        // Create Page Params
+        $createPageParams = [
+            'page_title' => $catalog->page->title,
+            'page_id' => $catalog->page->page_id,
+            'is_active' => 1
+        ];
+
+        // Create Catalog Params
+        $createCatalogParams = [
+            'dealer_id' => $catalog->dealer_id,
+            'dealer_location_id' => $catalog->dealer_location_id,
+            'fbapp_page_id' => $catalog->page->id,
+            'business_id' => $catalog->business_id,
+            'catalog_id' => $catalog->catalog_id,
+            'account_name' => $catalog->account_name,
+            'account_id' => $catalog->account_id,
+            'feed_id' => $catalog->feed_id,
+            'filters' => '',
+            'is_active' => 1
+        ];
+
+        // Create Auth Params
+        $refreshAuthParams = [
+            'token_type' => 'facebook',
+            'relation_type' => 'fbapp_catalog',
+            'relation_id' => $catalog->id,
+            'access_token' => $catalog->accessToken->access_token
+        ];
+
+        // Create Auth Params
+        $createAuthParams = $refreshAuthParams;
+        $createAuthParams['refresh_token'] = $catalog->accessToken->refresh_token;
+        $createAuthParams['expires_in'] = $catalog->accessToken->expires_in;
+        $createAuthParams['expires_at'] = $catalog->accessToken->expires_at;
+        $createAuthParams['issued_at'] = $catalog->accessToken->issued_at;
+
+        /** @var CatalogService $service */
+        $service = $this->app->make(CatalogService::class);
+
+        // Mock Create Page
+        $this->pageRepositoryMock
+            ->shouldReceive('create')
+            ->once()
+            ->with($createPageParams)
+            ->andReturn($catalog->page);
+
+        // Mock Create Catalog
+        $this->catalogRepositoryMock
+            ->shouldReceive('create')
+            ->once()
+            ->with($createCatalogParams)
+            ->andReturn($catalog);
+
+        // Mock Create Catalog Access Token
+        $this->authRepositoryMock
+            ->shouldReceive('refresh')
+            ->once()
+            ->with($refreshAuthParams)
+            ->andReturn([
+                'refresh_token' => $catalog->accessToken->refresh_token,
+                'expires_in' => $catalog->accessToken->expires_in,
+                'expires_at' => $catalog->accessToken->expires_at
+            ]);
+
+        // Mock Create Catalog Access Token
+        $this->authRepositoryMock
+            ->shouldReceive('create')
+            ->once()
+            ->with($createAuthParams)
+            ->andReturn($catalog->accessToken);
+
+        // Mock Validate Access Token
+        $this->businessRepositoryMock
+            ->shouldReceive('validate')
+            ->once()
+            ->with($catalog->accessToken)
+            ->andReturn($validate);
+
+        // Validate Show Catalog Result
+        $result = $service->show($catalogId);
+
+        // Assert Match
+        $this->assertSame($result, [
+            'data' => $catalog,
+            'validate' => $validate
+        ]);
     }
 
     /**
@@ -141,8 +258,8 @@ class CatalogServiceTest extends TestCase
         Log::shouldReceive('info')
             ->with('Item has been successfully deleted', ['inventoryId' => $inventoryId]);
 
-        // @var BusinessService $service
-        $service = $this->app->make(BusinessService::class);
+        // @var CatalogService $service
+        $service = $this->app->make(CatalogService::class);
 
         $result = $service->delete($inventoryId);
 
