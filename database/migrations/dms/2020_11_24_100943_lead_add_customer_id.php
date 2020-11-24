331a6c6cb3a3c9267447638a1f976c2fd36bc035
acfabro <acfabro@gmail.com>
2020-11-25 00:42:46 +0800

    #DMS-829/815 revised customer creator * added customer_id field to website_lead

---
+++ database/migrations/dms/2020_11_24_100943_lead_add_customer_id.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LeadAddCustomerId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('website_lead', function (Blueprint $table) {
+            $table->integer('customer_id')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('website_lead', function (Blueprint $table) {
+            $table->dropColumn('customer_id');
+        });
+    }
+}
