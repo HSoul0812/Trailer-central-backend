3b82a89dd2c519d547c326204d9f40a54250efce
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-11-30 15:46:18 -0600

    * IN-408 Fixed Business Service Access Token Test

--- tests/Unit/Services/Integration/Facebook/BusinessServiceTest.php
+++ tests/Unit/Services/Integration/Facebook/BusinessServiceTest.php
@@ -30,7 +30,18 @@ class BusinessServiceTest extends TestCase
     public function testValidate()
     {
         // Get Access Token
-        $accessToken = AccessToken::find($_ENV['TEST_FB_TOKEN_ID']);
+        $time = array();
+        $accessToken = factory(AccessToken::class)->make([
+            'token_type' => 'facebook',
+            'relation_type' => 'fbapp_catalog',
+            'relation_id' => $_ENV['TEST_FB_RELATION_ID'],
+            'access_token' => $_ENV['TEST_FB_ACCESS_TOKEN'],
+            'refresh_token' => $_ENV['TEST_FB_REFRESH_TOKEN'],
+            'id_token' => $_ENV['TEST_FB_ID_TOKEN'],
+            'expires_in' => $_ENV['TEST_FB_EXPIRES_IN'],
+            'expires_at' => date("Y-m-d H:i:s", $time + $_ENV['TEST_GOOGLE_EXPIRES_IN']),
+            'issued_at' => date("Y-m-d H:i:s", $time)
+        ]);
 
         /** @var BusinessService $service */
         $service = $this->app->make(BusinessService::class);
