05cc1149109cf0d830d0ec52bb9f5180e9379ae0
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-04 00:57:51 -0600

    * CRM-358 Fixed Gmail Service Getting Messages/Labels

--- app/Services/Integration/Google/GmailServiceInterface.php
+++ app/Services/Integration/Google/GmailServiceInterface.php
@@ -13,12 +13,21 @@ interface GmailServiceInterface {
     public function send($accessToken, $params);
 
     /**
-     * Get All Messages in Specific Folder
+     * Get All Messages With Label
      * 
      * @param AccessToken $accessToken
-     * @param array $params
      * @param string $folder folder name to get messages from; defaults to inbox
+     * @param array $params
      * @return whether the email was sent successfully or not
      */
-    public function getFolder($accessToken, $params, $folder = 'INBOX');
+    public function messages($accessToken, $folder = 'INBOX', $params = []);
+
+    /**
+     * Get All Labels for User
+     * 
+     * @param AccessToken $accessToken
+     * @param string || null $search
+     * @return array of labels || single label
+     */
+    public function labels($accessToken, $search = null);
 }
