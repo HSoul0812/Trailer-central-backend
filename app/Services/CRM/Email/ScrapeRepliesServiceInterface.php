3deb1616d49d7b35d90b915a905831a25d167beb
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-15 17:50:06 -0600

    * CRM-358 Fixed Refreshing Token If Token Isn't Up-To-Date During Scrape Replies

--- app/Services/CRM/Email/ScrapeRepliesServiceInterface.php
+++ app/Services/CRM/Email/ScrapeRepliesServiceInterface.php
@@ -6,9 +6,8 @@ interface ScrapeRepliesServiceInterface {
     /**
      * Import Email Replies
      * 
-     * @param NewDealerUser $dealer
      * @param SalesPerson $salesperson
      * @return false || array of EmailHistory
      */
-    public function import($dealer, $salesperson);
+    public function import($salesperson);
 }
