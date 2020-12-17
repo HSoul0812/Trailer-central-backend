66ef6d9c48ea1ab3f4f629df15a09506087d8a76
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-17 01:26:02 -0600

    * CRM-389 Added Support for Sales Person Details on Create/Update Requests

--- app/Http/Requests/CRM/User/CreateSalesAuthRequest.php
+++ app/Http/Requests/CRM/User/CreateSalesAuthRequest.php
@@ -7,7 +7,6 @@ use App\Http\Requests\Request;
 class CreateSalesAuthRequest extends Request {
 
     protected $rules = [
-        'id' => 'required|integer',
         'token_type' => 'required|valid_token_type',
         'access_token' => 'required|string',
         'refresh_token' => 'string',
@@ -16,7 +15,34 @@ class CreateSalesAuthRequest extends Request {
         'expires_at' => 'date_format:Y-m-d H:i:s',
         'expires_in' => 'integer',
         'scopes' => 'array',
-        'scopes.*' => 'string|max:80'
+        'scopes.*' => 'string|max:80',
+        'first_name' => 'string',
+        'last_name' => 'string',
+        'email' => 'email',
+        'perms' => 'in:admin,user',
+        'is_default' => 'boolean',
+        'is_inventory' => 'boolean',
+        'is_financing' => 'boolean',
+        'is_trade' => 'boolean',
+        'signature' => 'nullable|string',
+        'dealer_location_id' => 'nullable|dealer_location_valid',
+        'smtp' => 'array',
+        'smtp.email' => 'nullable|email',
+        'smtp.password' => 'nullable|string',
+        'smtp.server' => 'nullable|string',
+        'smtp.port' => 'nullable|integer',
+        'smtp.security' => 'nullable|sales_security_type',
+        'smtp.auth' => 'nullable|sales_auth_type',
+        'smtp.failed' => 'nullable|boolean',
+        'imap' => 'array',
+        'imap.email' => 'nullable|email',
+        'imap.password' => 'nullable|string',
+        'imap.server' => 'nullable|string',
+        'imap.port' => 'nullable|integer',
+        'imap.security' => 'nullable|sales_security_type',
+        'imap.failed' => 'nullable|boolean',
+        'folders' => 'array',
+        'folders.*' => 'string'
     ];
 
 }
