bb7b3dca8accda897be68cfa601ad643184c6877
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-23 10:21:04 -0600

    * CRM-393 Removed Restriction From Processing Google Import

--- app/Console/Commands/CRM/Email/ScrapeReplies.php
+++ app/Console/Commands/CRM/Email/ScrapeReplies.php
@@ -166,8 +166,7 @@ class ScrapeReplies extends Command
         $this->info("{$this->command} dealer #{$dealer->id} found " . count($salespeople) . " active salespeople with imap credentials to process");
         foreach($salespeople as $salesperson) {
             // Not Correct Sales Person?!
-            if(!empty($salesperson->googleToken) ||
-              (!empty($this->salesPersonId) && $salesperson->id !== $this->salesPersonId)) {
+            if(!empty($this->salesPersonId) && $salesperson->id !== $this->salesPersonId) {
                 // Clear Memory
                 unset($salesperson);
                 continue;
