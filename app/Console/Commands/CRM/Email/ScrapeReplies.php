d9466e15a79ea2431b87cf57672c3089c591d925
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-22 18:40:42 -0600

    * CRM-393 Added Debug Code for Memory Tracking

--- app/Console/Commands/CRM/Email/ScrapeReplies.php
+++ app/Console/Commands/CRM/Email/ScrapeReplies.php
@@ -220,7 +220,7 @@ class ScrapeReplies extends Command
                 $imports = $this->service->import($dealer, $salesperson, $folder);
                 $this->info('Imported ' . $imports . ' Email Replies for Sales Person #' .
                             $salesperson->id . ' Folder ' . $folder->name .
-                            ', Memory Usage: ' . round(memory_get_usage() / 1048576, 2) . ' MB'););
+                            ', Memory Usage: ' . round(memory_get_usage() / 1048576, 2) . ' MB'));
                 $imported += $imports;
             } catch(\Exception $e) {
                 $this->error('Error Importing Sales Person #' .
