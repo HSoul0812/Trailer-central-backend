a081890ea64db85c41d99879d43ddbb87bd87ff0
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-23 13:03:09 -0600

    * CRM-394 Debug Issues With Restricting Dealers to Boundaries on Scrape Replies

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -82,7 +82,7 @@ class UserRepository implements UserRepositoryInterface {
         else if(!empty($params['bound_lower'])) {
             $dealers = $dealers->where('id', '>=', $params['bound_lower']);
         }
-        echo $dealers->getQuery();
+        echo $dealers->getQuery()->toString();
 
         // Return Results
         return $dealers->get();
