fa895ef8ae3a800cd2cac25993344850cd7bfabe
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-29 15:16:34 -0600

    * CRM-387 Fixed Hitting Messages in Gmail Test

--- tests/Feature/CRM/Email/ScrapeRepliesTest.php
+++ tests/Feature/CRM/Email/ScrapeRepliesTest.php
@@ -107,10 +107,10 @@ class ScrapeRepliesTest extends TestCase
             // Mock Messages
             foreach($replies as $message) {
                 // Should Receive Full Message Details Once Per Folder Per Message!
-                $item = new \stdclass;
-                $item->id = $message->messageId;
                 $mock->shouldReceive('message')
-                     ->withArgs([$item])
+                     ->with(Mockery::on(function($obj) use($message) {
+                        return ($obj->id === $message->message_id);
+                     }))
                      ->times(count($folders))
                      ->andReturn([
                         'message_id' => $message->message_id,
@@ -129,10 +129,11 @@ class ScrapeRepliesTest extends TestCase
             // Mock Unused Messages
             foreach($unused as $message) {
                 // Should Receive Full Message Details Once Per Folder Per Message!
-                $item = new \stdclass;
-                $item->id = $message->messageId;
                 $mock->shouldReceive('message')
-                     ->withArgs([$item])
+                     ->with(Mockery::on(function($obj) use($message) {
+                        return ($obj->id === $message->message_id);
+                     }))
+                     ->withArgs([$message->message_id])
                      ->times(count($folders))
                      ->andReturn([
                         'message_id' => $message->message_id,
