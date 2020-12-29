6cf31fc87447fa1b1bdb0579fbaabf30bf053d70
David A Conway Jr <david.a.conway.jr@gmail.com>
2020-12-29 15:35:56 -0600

    * CRM-387 Debug Scrape Replies IMAP Overview Method Match

--- tests/Feature/CRM/Email/ScrapeRepliesTest.php
+++ tests/Feature/CRM/Email/ScrapeRepliesTest.php
@@ -203,6 +203,7 @@ class ScrapeRepliesTest extends TestCase
 
 
         // Mock Imap Service
+        var_dump($messages);
         $this->mock(ImapServiceInterface::class, function ($mock) use($salesPerson, $folders, $messages, $replies, $unused) {            
             // Should Receive Messages With Args Once Per Folder!
             $mock->shouldReceive('messages')
