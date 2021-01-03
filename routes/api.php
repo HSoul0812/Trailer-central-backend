20d3d5178fc2da93d705cb7ca5bc6374d28647d6
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-01-03 16:48:05 -0600

    * IN-494 Fixed Field Map Middleware

--- routes/api.php
+++ routes/api.php
@@ -240,8 +240,8 @@ $api->version('v1', function ($route) {
     $route->group(['middleware' => 'forms.field-map.validate'], function ($route) {
         $route->get('website/forms/field-map', 'App\Http\Controllers\v1\Website\Forms\FieldMapController@index');
         $route->put('website/forms/field-map', 'App\Http\Controllers\v1\Website\Forms\FieldMapController@create');
-        $route->get('website/forms/field-map/{type}', 'App\Http\Controllers\v1\Website\Forms\FieldMapController@get');
         $route->get('website/forms/field-map/types', 'App\Http\Controllers\v1\Website\Forms\FieldMapController@types');
+        $route->get('website/forms/field-map/{type}', 'App\Http\Controllers\v1\Website\Forms\FieldMapController@get');
     });
 
 
