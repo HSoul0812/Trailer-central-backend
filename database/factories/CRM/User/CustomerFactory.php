8a4b59884b844066fd5b32ef38b797d12262485d
pekar <sergeypekar77@gmail.com>
2021-01-12 17:22:34 +0200

    #DMS-861 fixed customer factory

--- database/factories/CRM/User/CustomerFactory.php
+++ database/factories/CRM/User/CustomerFactory.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 /** @var Factory $factory */
 
 use App\Models\CRM\User\Customer;
-use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factory;
 use Faker\Generator as Faker;
+use Tests\TestCase;
 
 $factory->define(Customer::class, static function (Faker $faker, array $attributes): array {
 
