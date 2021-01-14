d0e97adee8947a9b07ece4cd8995d49b2ecff06e
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-01-13 23:47:07 -0600

    * CRM-399 Created Lead Import Table

---
+++ database/migrations/2021_01_14_004233_create_lead_import_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeadImportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lead_import', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('dealer_id');
+            $table->integer('dealer_id_location')->index();
+            $table->text('email');
+            $table->timestamps();
+
+            $table->unique(['dealer_id', 'dealer_location_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lead_import');
+    }
+}
