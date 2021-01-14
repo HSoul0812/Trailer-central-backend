8fa4eb497734a8b7564634f318ec0907fa34ab62
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-01-13 21:26:48 -0600

    * CRM-399 Wrote Migration for Import ADF

---
+++ database/migrations/2021_01_13_221029_support_lead_email_import_format.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SupportLeadEmailImportFormat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Add Import Format
+        Schema::table('lead_email', function (Blueprint $table) {
+            $table->int('import_format')->nullable()->after('export_format');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop Import Format
+        Schema::table('lead_email', function (Blueprint $table) {
+            $table->dropColumn('import_format');
+        });
+    }
+}
