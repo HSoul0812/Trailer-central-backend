07a2388cb82c96c4bac64d8a10ef5f1f971acc76
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-01-18 21:10:52 -0600

    * IN-519 Fixed Skipping Invalid Listings From Catalog Job Instead of From Worker

--- app/Jobs/Integration/Facebook/CatalogJob.php
+++ app/Jobs/Integration/Facebook/CatalogJob.php
@@ -344,6 +344,11 @@ class CatalogJob extends Job
         }
         ksort($row);
 
+        // Skipp if Fields Missing
+        if(empty($row['title']) || empty($row['description']) || empty($row['model'])) {
+            return false;
+        }
+
         // Add Cleaned Results to CSV
         fputcsv($file, $row);
 
@@ -416,7 +421,7 @@ class CatalogJob extends Job
 
         // Append Description
         $listing->description = isset($listing->description) ? trim($listing->description) : '';
-        if($listing->dealer_id == 8757) {
+        if($listing->dealer_id == 8757 && !empty($listing->description)) {
             $listing->description .= 'In some cases, pricing may not include freight, prep, doc/title fees, additional equipment, or sales tax';
         }
 
