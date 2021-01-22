698ad23a863d585a11df74d440f661e643ee73eb
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-01-22 02:02:55 -0600

    * PRTBND-160 Fixed Testing for Correct Number of Moves

--- tests/Feature/CRM/Leads/Import/ADFTest.php
+++ tests/Feature/CRM/Leads/Import/ADFTest.php
@@ -219,37 +219,37 @@ class ADFTest extends TestCase
         // Assert Leads Exist
         foreach($leads as $lead) {
             // Assert a lead was saved...
-            /*$this->assertDatabaseHas('website_lead', [
+            $this->assertDatabaseHas('website_lead', [
                 'website_id' => $websiteId,
                 'dealer_id' => $lead->dealer_id,
                 'dealer_location_id' => $lead->dealer_location_id,
-                'inventory_id' => $lead->inventory_id,
+                //'inventory_id' => $lead->inventory_id,
                 'first_name' => $lead->first_name,
                 'last_name' => $lead->last_name,
                 'email_address' => $lead->email_address,
                 'phone_number' => $lead->phone_number
-            ]);*/
+            ]);
         }
 
         // Assert Leads With No Location Exist
         foreach($noloc as $lead) {
             // Assert a lead was saved...
-            /*$this->assertDatabaseHas('website_lead', [
+            $this->assertDatabaseHas('website_lead', [
                 'website_id' => $websiteId,
                 'dealer_id' => $lead->dealer_id,
                 'dealer_location_id' => 0,
-                'inventory_id' => $lead->inventory_id,
+                //'inventory_id' => $lead->inventory_id,
                 'first_name' => $lead->first_name,
                 'last_name' => $lead->last_name,
                 'email_address' => $lead->email_address,
                 'phone_number' => $lead->phone_number
-            ]);*/
+            ]);
         }
 
         // Assert Leads With No Inventory Exist
         foreach($noinv as $lead) {
             // Assert a lead was saved...
-            /*$this->assertDatabaseHas('website_lead', [
+            $this->assertDatabaseHas('website_lead', [
                 'website_id' => $websiteId,
                 'dealer_id' => $lead->dealer_id,
                 'dealer_location_id' => 0,
@@ -258,7 +258,7 @@ class ADFTest extends TestCase
                 'last_name' => $lead->last_name,
                 'email_address' => $lead->email_address,
                 'phone_number' => $lead->phone_number
-            ]);*/
+            ]);
         }
 
         // Assert Leads Don't Exist
