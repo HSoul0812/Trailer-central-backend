999b0130baffccef3d2730cd7f2c7cdc98b8918f
pekar <sergeypekar77@gmail.com>
2021-01-25 16:03:16 +0200

    #PRTBND-283 optimized sql query

--- app/Repositories/Dms/ServiceOrder/ServiceItemTechnicianRepository.php
+++ app/Repositories/Dms/ServiceOrder/ServiceItemTechnicianRepository.php
@@ -52,7 +52,12 @@ class ServiceItemTechnicianRepository extends RepositoryAbstract implements Serv
      */
     public function serviceReport($params) :array
     {
-        $dbParams = ['dealerId' => $params['dealer_id']];
+        $dbParams = [
+            'dealerId' => $params['dealer_id'],
+            'dealerId2' => $params['dealer_id'],
+            'dealerId3' => $params['dealer_id'],
+            'dealerId4' => $params['dealer_id'],
+        ];
         $where = 'WHERE technician.dealer_id = :dealerId ';
 
         if (!empty($params['from_date']) && !empty($params['to_date'])) {
@@ -123,7 +128,8 @@ class ServiceItemTechnicianRepository extends RepositoryAbstract implements Serv
                     INNER JOIN
                         qb_invoices
                         ON qb_invoices.id = ii.invoice_id
-                    WHERE qi.type = 'trailer' OR qi.type = 'deposit_down_payment'
+                    WHERE qi.dealer_id = :dealerId2
+                        AND (qi.type = 'trailer' OR qi.type = 'deposit_down_payment')
                     GROUP BY ii.invoice_id
                 ) sales_units ON i.id = sales_units.invoice_id
 
@@ -135,7 +141,8 @@ class ServiceItemTechnicianRepository extends RepositoryAbstract implements Serv
                     FROM qb_invoice_items ii
                     LEFT JOIN qb_items qi ON qi.id=ii.item_id
                     LEFT JOIN parts_v1 pa ON qi.item_primary_id=pa.id
-                    WHERE qi.type = 'part'
+                    WHERE qi.dealer_id = :dealerId3
+                        AND qi.type = 'part'
                     GROUP BY ii.invoice_id
                 ) sales_parts ON i.id = sales_parts.invoice_id
 
@@ -146,7 +153,8 @@ class ServiceItemTechnicianRepository extends RepositoryAbstract implements Serv
                        SUM(COALESCE(qi.cost, 0)) cost_amount
                     FROM qb_invoice_items ii
                     LEFT JOIN qb_items qi ON qi.id=ii.item_id
-                    WHERE qi.type = 'labor'
+                    WHERE qi.dealer_id = :dealerId4
+                        AND qi.type = 'labor'
                     GROUP BY ii.invoice_id
                 ) sales_labor ON i.id = sales_labor.invoice_id
                 GROUP BY us.id) sales ON r_order.unit_sale_id=sales.sale_id
