06125b49f00918154a00c619d220d1607cfba428
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-01-24 19:40:25 -0600

    * PRTBND-285 Fixed Lead Import Endpoint

--- app/Http/Requests/CRM/Leads/Import/UpdateImportRequest.php
+++ app/Http/Requests/CRM/Leads/Import/UpdateImportRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\CRM\Leads\Import;
 
 use App\Http\Requests\Request;
-use App\Models\CRM\Leads\LeadImport;
 
 class UpdateImportRequest extends Request {
 
@@ -12,17 +11,4 @@ class UpdateImportRequest extends Request {
         'emails' => 'required|array',
         'emails.*' => 'required|email'
     ];
-
-    protected function getObject() {
-        return new LeadImport();
-    }
-
-    protected function getObjectIdValue() {
-        return $this->id;
-    }
-
-    protected function validateObjectBelongsToUser() {
-        return true;
-    }
-
 }
