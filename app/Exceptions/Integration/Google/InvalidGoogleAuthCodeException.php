f2e66232123c4885607d8c0ef9e8efa59f3c8b96
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-02-01 22:55:20 -0600

    * PRTBND-290 Added Exception if Fetching Access Token With Invalid Auth Code

---
+++ app/Exceptions/Integration/Google/InvalidGoogleAuthCodeException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions\Integration\Google;
+
+/**
+ * Class InvalidGmailAuthMessageException
+ *
+ * Use this instead of \Exception to throw any kind of invalid authentication code for google
+ *
+ * @package App\Exceptions\Integration\Auth
+ */
+class InvalidGoogleAuthCodeException extends \Exception
+{
+    
+    protected $message = 'Invalid auth code while trying to get google access token!';
+
+}
