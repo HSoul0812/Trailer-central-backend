39791e6ee2c9c43096e89d58f40ef8e41d8601a2
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-02-02 16:14:38 -0600

    * PRTBND-290 Split Auto Assign/Scrape Replies Logs Into Separate Log Files

--- config/logging.php
+++ config/logging.php
@@ -54,6 +54,20 @@ return [
             'days' => 14,
         ],
 
+        'autoassign' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/commands/auto-assign.log'),
+            'level' => 'debug',
+            'days' => 3,
+        ],
+
+        'scrapereplies' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/commands/scrape-replies.log'),
+            'level' => 'debug',
+            'days' => 3,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
