93fc962b609f95f1ce50c259f374bf0880ff3b25
Rober Díaz <rad8329@gmail.com>
2021-02-02 08:44:26 -0500

    #PRTBND-173 Test-class per class pattern

--- tests/Integration/Http/Contollers/Jobs/MonitoredJobsControllerTest.php
+++ tests/Integration/Http/Contollers/Jobs/MonitoredJobsControllerTest.php
@@ -2,26 +2,34 @@
 
 declare(strict_types=1);
 
-namespace Tests\Integration\Http\Contollers\Jobs\MonitoredJobsController;
+namespace Tests\Integration\Http\Contollers\Jobs;
 
 use App\Http\Controllers\v1\Jobs\MonitoredJobsController;
 use App\Http\Requests\Jobs\GetMonitoredJobsRequest;
+use App\Models\Common\MonitoredJob;
 use Dingo\Api\Exception\ResourceException;
+use Exception;
+use Faker\Factory as Faker;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Tests\database\seeds\Common\MonitoredJobSeeder;
 use Tests\TestCase;
 
 /**
- * @covers \App\Http\Controllers\v1\Jobs\MonitoredJobsController::index
+ * @covers \App\Http\Controllers\v1\Jobs\MonitoredJobsController
  */
-class IndexTest extends TestCase
+class MonitoredJobsControllerTest extends TestCase
 {
     /**
      * @var MonitoredJobSeeder
      */
     private $seeder;
 
-    public function testInvalidParameter(): void
+    /**
+     * @covers ::index
+     */
+    public function testIndexWithInvalidParameter(): void
     {
         // Given I'm using the controller "MonitoredJobsController"
         $controller = app(MonitoredJobsController::class);
@@ -51,8 +59,10 @@ class IndexTest extends TestCase
      * @param callable $expectedTotal callable(): int
      * @param int $expectedLastPage
      * @param callable $expectedJobs callable(): \Illuminate\Support\Collection<MonitoredJob>
+     *
+     * @covers ::index
      */
-    public function testListJobsPerDealer(array $params,
+    public function testIndexListJobsPerDealer(array $params,
                                           callable $expectedTotal,
                                           int $expectedLastPage,
                                           callable $expectedJobs): void
@@ -74,6 +84,60 @@ class IndexTest extends TestCase
         self::assertSame(200, $response->status());
         // And I should see that the expected total of monitored jobs is the same as monitored jobs retrieved
         self::assertSame($expectedTotal($this->seeder), $paginator->total());
+    }
+
+    /**
+     * @throws Exception when Uuid::uuid4 cannot generate a uuid
+     *
+     * @covers ::status
+     */
+    public function testMonitoredJobDoesNotExists(): void
+    {
+        // Given I have a token of non-existent monitored job
+        $token = Uuid::uuid4()->toString();
+        // And I'm using the a "MonitoredJobsController" controller
+        $controller = app(MonitoredJobsController::class);
+
+        // Then I expect to see an "HttpException" to be thrown
+        $this->expectException(HttpException::class);
+        // And I also expect to see an specific exception message
+        $this->expectExceptionMessage('The job was not found');
+
+        // When I call the status action using the token of non-existent monitored job
+        $controller->status($token);
+    }
+
+    /**
+     * @throws Exception when Uuid::uuid4 cannot generate a uuid
+     *
+     * @covers ::status
+     */
+    public function testListJobsPerDealer(): void
+    {
+        /** @var MonitoredJob  $job */
+        /** @var array<string, float> $response */
+
+        $faker = Faker::create();
+
+        // Given I have few monitored jobs
+        $this->seeder->seed();
+
+        // And I'm a dealer with a specific id
+        $dealerId = $this->seeder->dealers[0]->getKey();
+
+        // And I've randomly picked one of my monitored jobs
+        $job = $this->seeder->jobs[$dealerId]->get($faker->unique()->numberBetween(0, 7));
+
+        // And I'm using the a "MonitoredJobsController" controller
+        $controller = app(MonitoredJobsController::class);
+
+        // When I call the status action using the token of my picked monitored job
+        $response = $controller->status($job->token);
+
+        // Then I should see that response status is 200
+        self::assertSame(200, $response->status());
+        // And I should see that the expected response has a specific structure
+        self::assertSame(['message' => 'Still processing', 'progress' => $job->progress], $response->original);
     }
 
     /**
