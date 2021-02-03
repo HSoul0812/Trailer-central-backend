30557b81e0ceb984652527cb92071651aae4f400
Marcel <marcel@trailercentral.com>
2021-02-03 10:00:07 -0500

    #PRTBND-353 fixed get vendors api issue

--- app/Repositories/Parts/VendorRepository.php
+++ app/Repositories/Parts/VendorRepository.php
@@ -5,7 +5,6 @@ namespace App\Repositories\Parts;
 use App\Repositories\Parts\VendorRepositoryInterface;
 use App\Exceptions\NotImplementedException;
 use App\Models\Parts\Vendor;
-use App\Models\Parts\Part;
 
 /**
  *  
@@ -28,10 +27,7 @@ class VendorRepository implements VendorRepositoryInterface {
 
     public function getAll($params) {
         if (isset($params['dealer_id'])) {
-            $query = Part::with('vendor');
-            $query = $query->whereIn('dealer_id', $params['dealer_id'])
-                    ->whereNotNull('vendor_id')
-                    ->groupBy('parts_v1.vendor_id');
+            $query = Vendor::where('dealer_id', $params['dealer_id']);
         } else {
             $query = Vendor::where('id', '>', 0);
         }        
@@ -41,29 +37,19 @@ class VendorRepository implements VendorRepositoryInterface {
         }
         
         if (isset($params['show_on_part'])) {
-            $query = $query->whereHas('vendor', function($q) use ($params) {
-                $q->where('show_on_part', $params['show_on_part']);
-            });
+            $query = $query->where('show_on_part', $params['show_on_part']);
         }
         
         if (isset($params['show_on_inventory'])) {
-            $query = $query->whereHas('vendor', function($q) use ($params) {
-                $q->where('show_on_inventory', $params['show_on_inventory']);
-            });
+            $query = $query->where('show_on_inventory', $params['show_on_inventory']);
         }
         
         if (isset($params['show_on_floorplan'])) {
-            $query = $query->whereHas('vendor', function($q) use ($params) {
-                $q->where('show_on_floorplan', $params['show_on_floorplan']);
-            });
+            $query = $query->where('show_on_floorplan', $params['show_on_floorplan']);
         }
                 
-        if (isset($params['name']) && !isset($params['dealer_id'])) {
+        if (isset($params['name'])) {
             $query = $query->where('name', 'like', '%'.$params['name'].'%');
-        } else if (isset($params['name'])) {
-            $query = $query->whereHas('category', function($q) use ($params) {
-                $q->where('name', 'like', '%'.$params['name'].'%');
-            });
         }
         
         return $query->paginate($params['per_page'])->appends($params);
