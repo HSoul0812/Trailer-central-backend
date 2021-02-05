16ad2cc99f08a75576c3cade34e842d03664b31d
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-02-04 21:51:52 -0600

    * PRTBND-361 Updated Returning Status Repository getAll

--- app/Http/Controllers/v1/CRM/Leads/LeadStatusController.php
+++ app/Http/Controllers/v1/CRM/Leads/LeadStatusController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\v1\CRM\Leads;
 
+use App\Http\Requests\CRM\Leads\GetLeadsStatusRequest;
 use App\Http\Controllers\RestfulController;
 use App\Repositories\CRM\Leads\StatusRepositoryInterface;
+use App\Transformers\SimpleTransformer;
 use Dingo\Api\Http\Request;
-use App\Http\Requests\CRM\Leads\GetLeadsStatusRequest;
 
 class LeadStatusController extends RestfulController
 {
@@ -27,6 +28,7 @@ class LeadStatusController extends RestfulController
     public function __construct(StatusRepositoryInterface $status)
     {
         $this->status = $status;
+        $this->transformer = new SimpleTransformer;
     }
 
     public function index(Request $request) {
@@ -34,7 +36,7 @@ class LeadStatusController extends RestfulController
         $requestData = $request->all();
 
         if ($request->validate()) {             
-            return $this->response->collection($this->status->getAll($request->all()));
+            return $this->response->collection($this->status->getAll($request->all()), $this->transformer);
         }
         
         return $this->response->errorBadRequest();
