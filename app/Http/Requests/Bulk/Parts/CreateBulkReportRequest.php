5656f9627cb20c06364480cfa40dddd034cc3f1d
Rober Díaz <rad8329@gmail.com>
2021-02-08 22:29:33 -0500

    #PRTBND-173 Create and get bulk report request were added

---
+++ app/Http/Requests/Bulk/Parts/CreateBulkReportRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Bulk\Parts;
+
+use App\Http\Requests\Request;
+
+class CreateBulkReportRequest extends Request
+{
+    protected $rules = [
+        'dealer_id' => 'required|integer',
+        'token' => 'uuid'
+    ];
+}
