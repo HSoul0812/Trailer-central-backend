d6ff29a090cd6d1de0780e6a2c49609344601bd5
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-02-24 17:33:33 -0600

    * PRTBND-389 Fixed User Settings Service Provider

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         App\Providers\IntegrationServiceProvider::class,
         App\Providers\InteractionServiceProvider::class,
         App\Providers\CatchAllOptionsRequestsProvider::class,
+        App\Providers\UserServiceProvider::class,
 
         /*
          * 3rd party stuff
