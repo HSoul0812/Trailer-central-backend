addfb80a8c58bef4304fffa68e30fe4e0cfc9e9c
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-02-24 23:46:44 -0600

    * PRTBND-389 Created Tests for Settings Endpoint

--- app/Models/User/Settings.php
+++ app/Models/User/Settings.php
@@ -5,7 +5,33 @@ namespace App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Settings extends Model
-{ 
+{
+    /**
+     * @var array of setting fields
+     */
+    const SETTING_FIELDS = [
+        'lock_state',
+        'inventory_table_color_mode',
+        'website_leads_cdk_source_id',
+        'website_leads_cdk_error',
+        'credit_card_processor',
+        'invoice_template',
+        'qb_data_retrieve',
+        'quote_print_template',
+        'quote_print_header_content',
+        'quote_mv_no_visible',
+        'quote_plate_no_visible',
+        'label_printer',
+        'receipt_printer',
+        'only_send_inventory_with_bill',
+        'salesperson_visible',
+        'service_price_editable',
+        'quote_inspection_info',
+        'quote_unit_description_visible',
+        'decimals'
+    ];
+
+
     const TABLE_NAME = 'dealer_admin_settings';
 
     /**
