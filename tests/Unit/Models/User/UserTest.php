a9067314720889739e4cb3d75f7a3dbb590b392b
pekar <sergeypekar77@gmail.com>
2021-03-02 17:31:39 +0200

    #TCAP-10 added permissions tests

--- tests/Unit/Models/User/UserTest.php
+++ tests/Unit/Models/User/UserTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Models\User;
 use App\Models\User\Interfaces\PermissionsInterface;
 use App\Models\User\User;
 use Illuminate\Support\Collection;
+use Mockery\LegacyMockInterface;
 use Tests\TestCase;
 
 /**
@@ -22,7 +23,12 @@ class UserTest extends TestCase
      */
     public function testGetPermissions()
     {
+        /** @var LegacyMockInterface|User $user */
         $user = $this->getEloquentMock(User::class);
+
+        $user
+            ->shouldReceive('getPermissions')
+            ->passthru();;
 
         $result = $user->getPermissions();
 
@@ -35,7 +41,16 @@ class UserTest extends TestCase
      */
     public function testHasPermission()
     {
+        /** @var LegacyMockInterface|User $user */
         $user = $this->getEloquentMock(User::class);
+
+        $user
+            ->shouldReceive('getPermissions')
+            ->passthru();
+
+        $user
+            ->shouldReceive('hasPermission')
+            ->passthru();;
 
         foreach (PermissionsInterface::FEATURES as $feature) {
             foreach (PermissionsInterface::PERMISSION_LEVELS as $permissionLevel) {
