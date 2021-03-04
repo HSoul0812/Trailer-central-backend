40f808f97e26e496c5b005753923f04d08e1f14d
Alberto <vaihtlumin@gmail.com>
2021-03-04 15:15:18 -0500

    #PRTBND-200

--- app/Transformers/Integration/CollectorTransformer.php
+++ app/Transformers/Integration/CollectorTransformer.php
@@ -52,7 +52,8 @@ class CollectorTransformer extends TransformerAbstract
             'cdk_password' => $collector->cdk_password,
             'cdk_username' => $collector->cdk_username,
             'ids_token' => $collector->ids_token,
-            'ids_default_location' => $collector->ids_default_location
+            'ids_default_location' => $collector->ids_default_location,
+            'use_factory_mapping' => $collector->use_factory_mapping
         ];
     }
 }
