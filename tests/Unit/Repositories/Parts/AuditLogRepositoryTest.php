cf68e1dbbe28bd02d2c336c5b176fd6e5e7e9d42
Alberto <vaihtlumin@gmail.com>
2021-03-05 14:47:34 -0500

    #PRTBND-473

--- tests/Unit/Repositories/Parts/AuditLogRepositoryTest.php
+++ tests/Unit/Repositories/Parts/AuditLogRepositoryTest.php
@@ -9,7 +9,7 @@ use Mockery;
 use Tests\TestCase;
 
 /**
- * @coversDefaultClass App\Repositories\Dms\ServiceOrderRepository
+ * @coversDefaultClass App\Repositories\Parts\AuditLogRepository
  */
 class AuditLogRepositoryTest extends TestCase
 {
@@ -40,57 +40,8 @@ class AuditLogRepositoryTest extends TestCase
      */
     public function testGetByYear(): void 
     {
-        $serviceRepoParams = [
-            'id' =>  $this->serviceOrderMock->id,
-            'status' => ServiceOrder::SERVICE_ORDER_STATUS['picked_up']
-        ];
-                
-        $this->serviceOrderMock
-                ->shouldReceive('save')
-                ->once()
-                ->andReturn(new ServiceOrder([
-                    'id' => $this->serviceOrderMock->id,
-                    'status' => ServiceOrder::SERVICE_ORDER_STATUS['ready_for_pickup']
-                ]));
         
-        $this->serviceOrderMock
-                ->shouldReceive('fill')
-                ->once()
-                ->with($serviceRepoParams);
-        
-        $this->serviceOrderMock
-                ->shouldReceive('findOrFail')
-                ->once()
-                ->with($this->serviceOrderMock->id)
-                ->andReturn($this->serviceOrderMock);        
-        
-
-        $serviceRepo = $this->app->make(ServiceOrderRepository::class);
-
-        $result = $serviceRepo->update($serviceRepoParams);
-        $this->assertEquals($result->id, $this->serviceOrderMock->id);
     }
     
-    /**
-     * @covers ::get
-     */
-    public function testGetServiceOrder(): void 
-    {
-        $serviceRepoParams = [
-            'id' =>  $this->serviceOrderMock->id,
-            'status' => ServiceOrder::SERVICE_ORDER_STATUS['picked_up']
-        ];                
-        
-        $this->serviceOrderMock
-                ->shouldReceive('findOrFail')
-                ->once()
-                ->with($this->serviceOrderMock->id)
-                ->andReturn($this->serviceOrderMock);        
-
-        $serviceRepo = $this->app->make(ServiceOrderRepository::class);
-
-        $result = $serviceRepo->get($serviceRepoParams);
-        $this->assertEquals($result->id, $this->serviceOrderMock->id);
-    }
 
 }
