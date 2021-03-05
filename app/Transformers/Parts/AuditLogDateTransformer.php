42c58ae2d8d0147e0054d2e3298f04ba2114ff02
Alberto <vaihtlumin@gmail.com>
2021-03-05 15:36:25 -0500

    #PRTBND-473

--- app/Transformers/Parts/AuditLogDateTransformer.php
+++ app/Transformers/Parts/AuditLogDateTransformer.php
@@ -15,8 +15,8 @@ class AuditLogDateTransformer extends TransformerAbstract
             'qty' => $auditLog->balance,
             'sku' => $auditLog->part ? $auditLog->part->sku : 'part-deleted',
             'created_at' => $auditLog->created_at->format('Y-m-d'),
-            'total_cost' => $auditLog->part->dealer_cost * $auditLog->balance,
-            'total_retail' => $auditLog->part->price * $auditLog->balance,
+            'total_cost' => round($auditLog->part->dealer_cost * $auditLog->balance, 2),
+            'total_retail' => round($auditLog->part->price * $auditLog->balance, 2),
             'vendor_name' => $auditLog->part->vendor ? $auditLog->part->vendor->name : null,
             'part_title' => $auditLog->part->title
         ];
