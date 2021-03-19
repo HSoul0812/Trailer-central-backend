3d9f3d64290180d00eac71d01715cd0081d4f3f4
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-03-19 18:05:55 -0500

    * PRTBND-545 Fixed Getting Inventory MFG By ID or By Name

--- app/Http/Requests/Inventory/GetManufacturersRequest.php
+++ app/Http/Requests/Inventory/GetManufacturersRequest.php
@@ -9,8 +9,8 @@ class GetManufacturersRequest extends Request {
     protected $rules = [
         'per_page' => 'integer',
         'search_term' => 'string|nullable',
-        'id' => 'array',
-        'id.*' => 'inventory_mfg_exists',
+        'ids' => 'array',
+        'ids.*' => 'inventory_mfg_exists',
         'name' => 'array',
         'name.*' => 'inventory_mfg_valid',
     ];
