549b7084cf4da7ad23dc7796069efdad8f800b22
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-02 01:27:15 -0500

    * PRTBND-492 Fixed Inquiry Email Interface Errors

--- app/Services/CRM/Leads/InquiryEmailServiceInterface.php
+++ app/Services/CRM/Leads/InquiryEmailServiceInterface.php
@@ -4,11 +4,19 @@ namespace App\Services\CRM\Leads;
 
 interface InquiryEmailServiceInterface {
     /**
-     * Send Inquiry Email for Lead
+     * Send Email for Lead
      * 
-     * @param int $leadId
-     * @param array $params
+     * @param LeadInquiry $inquiry
      * @throws SendInquiryFailedException
+     * @return bool
      */
-    public function send(int $leadId, array $params);
+    public function send(InquiryLead $inquiry): bool;
+
+    /**
+     * Fill Inquiry Lead Details From Request Params
+     * 
+     * @param array $params
+     * @return InquiryLead
+     */
+    public function fill(array $params): InquiryLead;
 }
