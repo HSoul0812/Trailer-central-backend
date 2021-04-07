5d1469f7c49d3a4cd06ce2b346c3dcaedf8a9df8
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-07 14:28:57 -0500

    * PRTBND-665 Debug Website Config Settings

--- app/Services/CRM/Leads/InquiryEmailService.php
+++ app/Services/CRM/Leads/InquiryEmailService.php
@@ -91,6 +91,7 @@ class InquiryEmailService implements InquiryEmailServiceInterface
 
         // Get Inquiry From Details For Website
         $config = $this->websiteConfig->getValueOrDefault($params['website_id'], 'general/item_email_from');
+        var_dump($config);
         $params['logo'] = $config['logo'];
         $params['logo_url'] = $config['logoUrl'];
         $params['from_name'] = $config['fromName'];
