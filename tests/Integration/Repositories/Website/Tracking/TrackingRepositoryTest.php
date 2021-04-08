352f90f6c75d86a9d64ae5f741a88fe1e39802dc
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-08 14:09:25 -0500

    * PRTBND-680 Fixed Tracking Seeder

--- tests/Integration/Repositories/Website/Tracking/TrackingRepositoryTest.php
+++ tests/Integration/Repositories/Website/Tracking/TrackingRepositoryTest.php
@@ -8,7 +8,7 @@ use App\Models\Website\Tracking\Tracking;
 use App\Repositories\Website\Tracking\TrackingRepository;
 use App\Repositories\Website\Tracking\TrackingRepositoryInterface;
 use Illuminate\Contracts\Container\BindingResolutionException;
-use Tests\database\seeds\Website\Tracking\WebsiteTrackingSeeder;
+use Tests\database\seeds\Website\Tracking\WebsiteTrackingSeeder as TrackingSeeder;
 use Tests\TestCase;
 use Tests\Unit\WithMySqlConstraintViolationsParser;
 
