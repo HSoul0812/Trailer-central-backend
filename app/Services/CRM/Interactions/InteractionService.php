2b2597f632e10ddc038ab2e647a1e1e4961e39cd
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-11 17:07:44 -0500

    * PRTBND-651 Fixed Getting Sales Person/User on Send Email

--- app/Services/CRM/Interactions/InteractionService.php
+++ app/Services/CRM/Interactions/InteractionService.php
@@ -4,6 +4,8 @@ namespace App\Services\CRM\Interactions;
 
 use App\Models\CRM\Leads\Lead;
 use App\Models\CRM\Interactions\Interaction;
+use App\Models\CRM\User\SalesPerson;
+use App\Models\User\User;
 use App\Repositories\CRM\Interactions\InteractionsRepositoryInterface;
 use App\Repositories\CRM\Interactions\EmailHistoryRepositoryInterface;
 use App\Repositories\Integration\Auth\TokenRepositoryInterface;
