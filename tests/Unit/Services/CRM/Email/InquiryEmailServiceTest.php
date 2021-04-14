50dd3bf8bb558d8889ab7da0bc165579d1613d60
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-13 22:17:20 -0500

    * PRTBND-676 Debug InquiryEmailServiceTest Assert Sent

--- tests/Unit/Services/CRM/Email/InquiryEmailServiceTest.php
+++ tests/Unit/Services/CRM/Email/InquiryEmailServiceTest.php
@@ -125,7 +125,7 @@ class InquiryEmailServiceTest extends TestCase
 
         // Assert a message was sent to the dealer...
         Mail::assertSent(InquiryEmail::class, function ($mail) use ($inquiry) {
-            var_dump($inquiry);
+            var_dump($inquiry->inquiryEmail);
             if(empty($inquiry->inquiryEmail)) {
                 return false;
             }                
