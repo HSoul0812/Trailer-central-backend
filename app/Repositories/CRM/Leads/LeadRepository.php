3bbefa4148d564907237f0f37998405ca684c4c8
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-14 18:15:29 -0500

    * PRTBND-662 Fixed Find All Matches on Lead Merge

--- app/Repositories/CRM/Leads/LeadRepository.php
+++ app/Repositories/CRM/Leads/LeadRepository.php
@@ -14,6 +14,7 @@ use App\Models\CRM\Leads\LeadStatus;
 use App\Models\CRM\Leads\LeadType;
 use App\Models\Inventory\Inventory;
 use App\Repositories\Traits\SortTrait;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
 
