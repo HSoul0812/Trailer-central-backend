69a1574465ce9f9532bbb3272bd44a7f838e414f
Rober Díaz <rad8329@gmail.com>
2021-04-15 16:38:33 -0500

    #PRTBND-708 Models and DTOs for the CVR file abstraction were added

---
+++ app/Models/Integration/CVR/CvrFilePayload.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Integration\CVR;
+
+use App\Models\Common\MonitoredJobPayload;
+use File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @property-read string $document filepath
+ */
+class CvrFilePayload extends MonitoredJobPayload
+{
+    /**
+     * @var string
+     */
+    protected $document;
+
+    public function asArray(): array
+    {
+        return ['document' => $this->document];
+    }
+
+    /**
+     * @param UploadedFile|string $file
+     */
+    public function setDocument($file): void
+    {
+        $this->document = $file;
+
+        if ($file instanceof UploadedFile) {
+
+            $this->document = $this->generateFileNameFromUploadFile($file);
+
+            Storage::disk('tmp')->put($this->document, $file);
+        }
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @return string a unique filename
+     */
+    private function generateFileNameFromUploadFile(UploadedFile $file): string
+    {
+        $uniqueFileName = uniqid('cvr-' . date('Ymd'), true);
+        $extension = File::extension($file->getClientOriginalName());
+
+        return Storage::disk('tmp')->path(
+            sprintf('%s.%s', str_replace('.', '-', $uniqueFileName), $extension)
+        );
+    }
+}
