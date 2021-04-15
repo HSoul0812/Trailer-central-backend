254e1baa49e08f416c0c8ced0d4438c23ef27749
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-15 14:27:12 -0500

    * PRTBND-676 Debug Spam Email Test

--- tests/Unit/Services/CRM/Email/InquiryEmailServiceTest.php
+++ tests/Unit/Services/CRM/Email/InquiryEmailServiceTest.php
@@ -242,12 +242,12 @@ class InquiryEmailServiceTest extends TestCase
             if($mail->hasTo(InquiryLead::INQUIRY_DEV_TO[0]['email'])) {
                 $successes++;
             }
+            var_dump(InquiryLead::INQUIRY_DEV_TO[0]['email']);
 
             // BCC Does NOT Exist?
             if($this->assertTrue(empty($mail->bcc[0]['address']))) {
                 $successes++;
             }
-            var_dump($successes);
 
             // Must Be 2!
             return ($successes === 2);
