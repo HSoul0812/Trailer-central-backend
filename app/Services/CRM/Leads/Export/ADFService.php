5434dd5ca3159c6330194a86c83f0bc1d0bbe3a2
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-14 21:20:04 -0500

    * PRTBND-662 Fixed Exporting ADF Email Correctly

--- app/Services/CRM/Leads/Export/ADFService.php
+++ app/Services/CRM/Leads/Export/ADFService.php
@@ -30,7 +30,7 @@ class ADFService implements ADFServiceInterface {
      * @return bool
      */
     public function export(InquiryLead $inquiry, Lead $lead) : bool {
-        $leadEmail = $this->leadEmailRepository->find($inquiry->dealerId, $inquiry->locationId);
+        $leadEmail = $this->leadEmailRepository->find($inquiry->dealerId, $inquiry->dealerLocationId);
         if ($leadEmail->export_format !== LeadEmail::EXPORT_FORMAT_ADF) {
             return false;
         }
