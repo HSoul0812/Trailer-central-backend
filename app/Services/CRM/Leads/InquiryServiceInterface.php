5e67ebbca9a09e2c37bde4677efa0cfce2b6932f
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-17 01:13:43 -0500

    * PRTBND-692 Fixed Returning Both Merge and Lead on Inquiry Endpoints

--- app/Services/CRM/Leads/InquiryServiceInterface.php
+++ app/Services/CRM/Leads/InquiryServiceInterface.php
@@ -2,14 +2,34 @@
 
 namespace App\Services\CRM\Leads;
 
-use App\Models\CRM\Leads\Lead;
+use App\Services\CRM\Leads\DTOs\InquiryLead;
 
 interface InquiryServiceInterface {
+    /**
+     * Create Inquiry
+     * 
+     * @param array $params
+     * @return array{data: Lead,
+     *               merge: null|Interaction}
+     */
+    public function create(array $params): array;
+
     /**
      * Send Inquiry
      * 
      * @param array $params
-     * @return Lead
+     * @return array{data: Lead,
+     *               merge: null|Interaction}
      */
-    public function send(array $params): Lead;
+    public function send(array $params): array;
+
+    /**
+     * Merge or Create Lead
+     * 
+     * @param InquiryLead $inquiry
+     * @param array $params
+     * @return array{data: Lead,
+     *               merge: null|Interaction}
+     */
+    public function mergeOrCreate(InquiryLead $inquiry, array $params): array;
 }
