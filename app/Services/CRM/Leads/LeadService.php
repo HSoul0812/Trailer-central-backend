9d9c2ec00649c8b0402e3144e83137a4ea8df0fb
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-17 21:03:16 -0500

    * PRTBND-692 Debug LeadServiceTest CreateMultiTypes

--- app/Services/CRM/Leads/LeadService.php
+++ app/Services/CRM/Leads/LeadService.php
@@ -87,7 +87,9 @@ class LeadService implements LeadServiceInterface
      */
     public function create(array $rawParams): Lead {
         // Fix Params
+        var_dump($rawParams);
         $params = $this->fixCleanParams($rawParams);
+        var_dump($params);
 
         // Create Lead
         $lead = $this->leads->create($params);
