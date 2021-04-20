c4150dbf2dcc949cad8cd90479223c4934fe1764
Rober Díaz <rad8329@gmail.com>
2021-04-20 15:39:06 -0500

    #PRTBND-729 Fee charged type was added to `qb_items`

--- database/migrations/dealer/2021_04_17_110929_prepare_db_schema_for_liability_related_to_fees.php
+++ database/migrations/dealer/2021_04_17_110929_prepare_db_schema_for_liability_related_to_fees.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\CRM\Dms\Quickbooks\Item;
 use App\Models\User\DealerLocationQuoteFee;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,17 +17,18 @@ class PrepareDbSchemaForLiabilityRelatedToFees extends Migration
      */
     public function up(): void
     {
-        Schema::table('dealer_location_quote_fee', static function (Blueprint $table) {
-            $charged_types = [
-                DealerLocationQuoteFee::CHARGED_TYPE_INCOME,
-                DealerLocationQuoteFee::CHARGED_TYPE_LIABILITY,
-                DealerLocationQuoteFee::CHARGED_TYPE_COMBINED
-            ];
+        $charged_types = [
+            DealerLocationQuoteFee::CHARGED_TYPE_INCOME,
+            DealerLocationQuoteFee::CHARGED_TYPE_LIABILITY,
+            DealerLocationQuoteFee::CHARGED_TYPE_COMBINED
+        ];
 
+        Schema::table('dealer_location_quote_fee', static function (Blueprint $table) use ($charged_types) {
             $table->decimal('cost_amount', 10, 2)
                 ->nullable()
                 ->default(0)
                 ->after('amount');
+
             $table->enum('fee_charged_type', $charged_types)
                 ->default(DealerLocationQuoteFee::CHARGED_TYPE_DEFAULT)
                 ->after('accounting_class');
@@ -38,6 +40,17 @@ class PrepareDbSchemaForLiabilityRelatedToFees extends Migration
                 ->after('income_acc_id')
                 ->comment('Liability account where it needs to be sent');
         });
+
+        Schema::table('qb_items', static function (Blueprint $table) use ($charged_types) {
+            $table->enum('fee_charged_type', $charged_types)
+                ->nullable()
+                ->after('vendor_id')
+                ->comment('Specific column for fees, used to determine which QBO account the amount need to be sent ');
+        });
+
+        DB::table('qb_items')
+            ->where('type', Item::ITEM_TYPES['ADD_ON'])
+            ->update(['fee_charged_type' => DealerLocationQuoteFee::CHARGED_TYPE_DEFAULT]);
     }
 
     /**
@@ -54,6 +67,10 @@ class PrepareDbSchemaForLiabilityRelatedToFees extends Migration
 
         Schema::table('qb_item_category', static function (Blueprint $table) {
             $table->dropColumn('liability_acc_id');
+        });
+
+        Schema::table('qb_items', static function (Blueprint $table) {
+            $table->dropColumn('fee_charged_type');
         });
     }
 }
