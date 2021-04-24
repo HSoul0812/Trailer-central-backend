ee9393584e644446eb72945d69e579b9877e1279
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-04-23 19:15:51 -0500

    * PRTBND-488 Fixed Lead Source Integration Test Collection Match Error

--- tests/Integration/Repositories/CRM/Leads/SourceRepositoryTest.php
+++ tests/Integration/Repositories/CRM/Leads/SourceRepositoryTest.php
@@ -456,12 +456,8 @@ class SourceRepositoryTest extends TestCase
             return $seeder->dealer->getKey();
         };
 
-        $defaultSourcesLambda = static function (SourceSeeder $seeder) {
-            return $seeder->defaultSources->count();
-        };
-
         return [                 // array $parameters, int $expectedTotal
-            'By dummy dealer' => [['user_id' => $dealerIdLambda], $defaultSourcesLambda + 3],
+            'By dummy dealer' => [['user_id' => $dealerIdLambda], $seeder->defaultSources->count() + 3],
         ];
     }
 
