a1daf35f4dd980fd78913b15064baa511cfe8b00
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-05 15:44:28 -0500

    * PRTBND-789 Fixed Handling Send Blast Email Params Differently

--- app/Services/CRM/Email/EmailBuilderServiceInterface.php
+++ app/Services/CRM/Email/EmailBuilderServiceInterface.php
@@ -6,9 +6,30 @@ interface EmailBuilderServiceInterface {
     /**
      * Send Lead Emails for Blast
      * 
-     * @param array $params
+     * @param int $id ID of Blast to Send Emails For
+     * @param array<int> ID's of Leads to Send Emails For Blast
      * @throws SendBlastEmailsFailedException
      * @return bool
      */
-    public function sendBlast(array $params): bool;
+    public function sendBlast(int $id, array $leads): bool;
+
+    /**
+     * Send Lead Emails for Campaign
+     * 
+     * @param int $id ID of Campaign to Send Emails For
+     * @param array<int> ID's of Leads to Send Emails For Blast
+     * @throws SendCampaignEmailsFailedException
+     * @return bool
+     */
+    //public function sendCampaign(int $id, array $leads): bool;
+
+    /**
+     * Send Lead Emails for Template
+     *
+     * @param int $id ID of Template to Send Emails For
+     * @param string $email single email address to send template to
+     * @throws SendTemplateEmailFailedException
+     * @return bool
+     */
+    //public function sendTemplate(int $id, string $email): bool;
 }
