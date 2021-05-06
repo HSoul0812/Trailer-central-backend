ca4fa4a4c393e819eb6d71d1b1024ad01fba27e6
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-06 17:27:05 -0500

    * PRTBND-792 Fixed wasSent on Campaign/Blast Repositories

--- app/Repositories/CRM/Email/BlastRepositoryInterface.php
+++ app/Repositories/CRM/Email/BlastRepositoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\CRM\Email;
 
+use App\Models\CRM\Email\BlastSent;
 use App\Repositories\Repository;
 
 interface BlastRepositoryInterface extends Repository {
@@ -9,7 +10,16 @@ interface BlastRepositoryInterface extends Repository {
      * Mark Blast as Sent
      * 
      * @param array $params
-     * return BlastSent
+     * @return BlastSent
      */
-    public function sent($params);
+    public function sent(array $params): BlastSent;
+
+    /**
+     * Was Blast Already Sent?
+     * 
+     * @param int $blastId
+     * @param int $leadId
+     * @return bool
+     */
+    public function wasSent(int $blastId, int $leadId): bool;
 }
--- app/Repositories/CRM/Email/CampaignRepositoryInterface.php
+++ app/Repositories/CRM/Email/CampaignRepositoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\CRM\Email;
 
+use App\Models\CRM\Text\CampaignSent;
 use App\Repositories\Repository;
 
 interface CampaignRepositoryInterface extends Repository {
@@ -9,7 +10,16 @@ interface CampaignRepositoryInterface extends Repository {
      * Mark Campaign as Sent
      * 
      * @param array $params
-     * return CampaignSent
+     * @return CampaignSent
      */
-    public function sent($params);
+    public function sent(array $params): CampaignSent;
+
+    /**
+     * Was Campaign Already Sent?
+     * 
+     * @param int $campaignId
+     * @param int $leadId
+     * @return bool
+     */
+    public function wasSent(int $campaignId, int $leadId): bool;
 }
