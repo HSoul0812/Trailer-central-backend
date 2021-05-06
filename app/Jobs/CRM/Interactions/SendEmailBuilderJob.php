017d22dfe9b209cebbddde1a60bd01a053ca60ba
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-06 16:03:45 -0500

    * PRTBND-792 Fixed Setting SMTP Info on SendEmailBuilderJob

--- app/Jobs/CRM/Interactions/SendEmailBuilderJob.php
+++ app/Jobs/CRM/Interactions/SendEmailBuilderJob.php
@@ -141,7 +141,7 @@ class SendEmailBuilderJob extends Job
         }
         // Get SMTP Config
         else {
-            $this->setSmtp($this->config->smtpConfig);
+            $this->setSmtpConfig($this->config->smtpConfig);
 
             // Send Email
             Mail::to($this->config->getToEmail())->send(new EmailBuilderEmail($parsedEmail));
