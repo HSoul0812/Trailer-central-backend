230be0333649856c5eeedc9c50a45d037dcdb702
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-06 20:05:11 -0500

    * PRTBND-792 Fixed Refreshing Gmail Access Token in EmailBuilderJob

--- app/Jobs/CRM/Interactions/SendEmailBuilderJob.php
+++ app/Jobs/CRM/Interactions/SendEmailBuilderJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs\CRM\Interactions;
 use App\Jobs\Job;
 use App\Mail\CRM\Interactions\EmailBuilderEmail;
 use App\Models\CRM\Interactions\EmailHistory;
+use App\Models\Integration\Auth\AccessToken;
 use App\Repositories\CRM\Email\CampaignRepositoryInterface;
 use App\Repositories\CRM\Email\BlastRepositoryInterface;
 use App\Repositories\CRM\Email\TemplateRepositoryInterface;
