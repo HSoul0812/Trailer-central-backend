643db957f9fe3ef3a0266fbc5c14d645e0d2c4ba
Rober Díaz <rad8329@gmail.com>
2021-05-10 21:40:39 -0500

    #PRTBND-178 `entity_type_id` was exclude of the fillables fields of `dealer_location_sales_tax_item` table

--- app/Models/User/DealerLocationSalesTaxItemV1.php
+++ app/Models/User/DealerLocationSalesTaxItemV1.php
@@ -12,4 +12,21 @@ use Illuminate\Database\Query\Builder;
 class DealerLocationSalesTaxItemV1 extends DealerLocationSalesTaxItem
 {
     protected $table = 'dealer_location_sales_tax_item';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "dealer_location_id",
+        "item_type",
+        "tax_pct",
+        "tax_cap",
+        "standard",
+        "tax_exempt",
+        "out_of_state_reciprocal",
+        "out_of_state_non_reciprocal",
+        "settngs"
+    ];
 }
