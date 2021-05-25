d67f9153209f75f557404259bf522ccd3e45ad02
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-25 15:43:09 -0500

    * PRTBND-826 Fixed InstructionFormRequest Error on FormController

--- app/Http/Controllers/v1/Dms/Printer/FormController.php
+++ app/Http/Controllers/v1/Dms/Printer/FormController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\RestfulController;
 use App\Repositories\Dms\Printer\FormRepositoryInterface;
 use App\Http\Requests\Dms\Printer\GetFormRequest;
 use App\Http\Requests\Dms\Printer\ShowFormRequest;
+use App\Http\Requests\Dms\Printer\InstructionFormRequest;
 use App\Services\Dms\Printer\FormServiceInterface;
 use App\Transformers\Dms\Printer\FormTransformer;
 use Dingo\Api\Http\Request;
