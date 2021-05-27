826bb571d170dec39155a6c0091969526ad11736
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-27 17:07:51 -0500

    * PRTBND-826 Updated Response AJAX From Get Form Instruction Endpoint

--- app/Services/Dms/Printer/ESCP/FormService.php
+++ app/Services/Dms/Printer/ESCP/FormService.php
@@ -27,11 +27,71 @@ class FormService implements FormServiceInterface
     public function getFormInstruction(int $dealerId, int $formId, array $params): array
     {
         return [
-            '\x1B' . '\x31',
-            '\x1B' . '\x30',
-            '\x1B' . '\x6C' . ' 40',
-            '\x1B' . '\x51' . ' 90',
-            'This is a Test Print Wow!' . '\x0A',
+            "\x1B" . "\x69" . "\x61" . "\x00", // set printer to ESC/P mode
+            "\x1B" . "\x40", // clear memory buffer
+            "\x1B" . "\x4F", "\x1B" . "\x69" . "\x00" . "\x00", // clear margins
+            "\x1B" . "\x30", // set line feed smallest point
+            "\x1B" . "\x6B" . "\x0B", "\x1B" . "\x58" . "\x00" . "\x15", // set font and font size
+
+            "\x1B" . "\x61" . "\x30",
+            "\x1B" . "\x24" . "\x00",
+            "TEST LEFT ALIGN",
+            "\x1B" . "\x61" . "\x32",
+            "\x1B" . "\x24" . "\x00",
+            "TEST RIGHT ALIGN" . "\x0A",
+            /*getLineBreaks(4), "\x1B" . "\x24" . "\x00",
+            getWhitespace(53) . "DR2407" . getLineBreaks(9),
+
+            "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "John Doe", "\x1B" . "\x24" . "\x02",
+            getWhitespace(53) . "DL - 237298" . getLineBreaks(5),
+
+            "\x1B" . "\x24" . "\x00",                                                                                                                                                                            
+            getWhitespace(4) . "100 Main Street", "\x1B" . "\x24" . "\x00",
+            getWhitespace(29) . "Springfield", "\x1B" . "\x24" . "\x00",
+            getWhitespace(44) . "TN", "\x1B" . "\x24" . "\x02",
+            getWhitespace(53) . "39021" . getLineBreaks(5),
+
+            "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "1HD1FMW166Y641723", "\x1B" . "\x24" . "\x00",
+            getWhitespace(29) . "2020", "\x1B" . "\x24" . "\x00",
+            getWhitespace(37) . "AMER", "\x1B" . "\x24" . "\x00",
+            getWhitespace(44) . "Alum", "\x1B" . "\x24" . "\x02",
+            getWhitespace(53) . "Rebel/Matador/Marlin" . getLineBreaks(5),
+
+            "\x1B" . "\x45", "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "X", "\x1B" . "\x24" . "\x00",
+            getWhitespace(9) . "X", "\x1B" . "\x24" . "\x00",
+            getWhitespace(15) . "X", "\x1B" . "\x24" . "\x00",
+            getWhitespace(23) . "X", "\x1B" . "\x24" . "\x00",
+            getWhitespace(29) . "X", "\x1B" . "\x24" . "\x02",
+            getWhitespace(36) . "X", "\x1B" . "\x46", "\x1B" . "\x24" . "\x02",
+            getWhitespace(55) . "19999.99" . getLineBreaks(5),
+
+            "\x1B" . "\x24" . "\x00",
+            getWhitespace(13) . "Jane Doe", "\x1B" . "\x24" . "\x00",
+            getWhitespace(53) . "05/26/2021" . getLineBreaks(14),
+
+            "\x1B" . "\x24" . "\x00",
+            getWhitespace(50) . "90,000" . getLineBreaks(4),
+
+            "\x1B" . "\x45", "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "X" . getLineBreaks(5), "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "X" . getLineBreaks(5), "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "X" . getLineBreaks(5), "\x1B" . "\x46", "\x1B" . "\x24" . "\x02",
+            getWhitespace(53) . "05/26/2021" . getLineBreaks(20),
+
+            "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "100 Main Street", "\x1B" . "\x24" . "\x00",
+            getWhitespace(29) . "Springfield", "\x1B" . "\x24" . "\x00",
+            getWhitespace(44) . "TN", "\x1B" . "\x24" . "\x02",
+            getWhitespace(53) . "39021" . getLineBreaks(5),
+
+            "\x1B" . "\x24" . "\x00",
+            getWhitespace(4) . "n/a", "\x1B" . "\x24" . "\x00",
+            getWhitespace(44) . "05/23/2021", "\x1B" . "\x24" . "\x02",
+            getWhitespace(53) . "DL - 237298",*/
+            "\x0C" // <--- Tells the printer to print 
         ];
     }
 }
