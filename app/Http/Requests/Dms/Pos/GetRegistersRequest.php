9635794ffec65012e4462a85943dea2eaa408b59
Hafiz Abdul Rehman <hafiz@oceansapart.com>
2021-05-28 18:33:19 +0200

    #PRTBND-754: stricter request validation rules

--- app/Http/Requests/Dms/Pos/GetRegistersRequest.php
+++ app/Http/Requests/Dms/Pos/GetRegistersRequest.php
@@ -7,7 +7,7 @@ use App\Http\Requests\Request;
 class GetRegistersRequest extends Request {
 
     protected $rules = [
-        'dealer_id' => 'integer',
+        'dealer_id' => 'integer|min:1|required|exists:dealer,dealer_id',
     ];
 
 }
