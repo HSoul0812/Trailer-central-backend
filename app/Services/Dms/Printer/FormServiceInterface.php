3b2f79a6fbfc822151a5a1c53b6f912f1d6f93c0
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-05-27 23:50:18 -0500

    * PRTBND-826 Fixed Printer Instructions Service Interface

--- app/Services/Dms/Printer/FormServiceInterface.php
+++ app/Services/Dms/Printer/FormServiceInterface.php
@@ -3,16 +3,14 @@
 namespace App\Services\Dms\Printer;
 
 interface FormServiceInterface {
-    
+
     /**
-     * Returns a string array containing code with print instructions for the specific
-     * dealer, form and printer
+     * Get Instructions for Form
      * 
-     * @param int $dealerId
      * @param int $formId
-     * @param array $params
-     * @return array<string>
+     * @param int $unitSaleId
+     * @return array
      */
-    public function getFormInstruction(int $dealerId, int $formId, array $params): array;
+    public function getFormInstruction(int $formId, int $unitSaleId): array;
     
 }
