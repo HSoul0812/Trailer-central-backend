a519f327d76970fbcf50658eec86b996c3876db0
Hafiz Abdul Rehman <hafiz@oceansapart.com>
2021-05-28 18:29:33 +0200

    #PRTBND-754: small refactor and unit test for repository

--- app/Repositories/Dms/Pos/RegisterRepository.php
+++ app/Repositories/Dms/Pos/RegisterRepository.php
@@ -7,6 +7,14 @@ use App\Repositories\RepositoryAbstract;
 
 class RegisterRepository extends RepositoryAbstract implements RegisterRepositoryInterface
 {
+    /* @var Outlet */
+    private $outlet;
+
+    public function __construct(Outlet $model)
+    {
+        $this->outlet = $model;
+    }
+
     /**
      * Searches all the outlets with open registers by dealer_id
      *
@@ -15,7 +23,7 @@ class RegisterRepository extends RepositoryAbstract implements RegisterRepositor
      */
     public function getAllByDealerId(int $dealerId)
     {
-        return Outlet::select('crm_pos_register.id', 'crm_pos_outlet.config', 'crm_pos_outlet.register_name')
+        return $this->outlet->select('crm_pos_register.id', 'crm_pos_outlet.config', 'crm_pos_outlet.register_name')
             ->join('crm_pos_register', 'crm_pos_register.outlet_id', '=', 'crm_pos_outlet.id')
             ->whereNull('crm_pos_register.close_date')
             ->where('crm_pos_outlet.dealer_id', $dealerId)
