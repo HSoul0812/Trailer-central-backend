7f9ec4a8ba63d58f1c33c699e764f0e5feff59e0
Mert Oksuz <mert.o@onthegosystems.com>
2021-05-29 16:46:44 +0300

    trait removed, functions moved to related request class

--- app/Http/Requests/Dms/Quickbooks/DeleteQuickbookApprovalRequest.php
+++ app/Http/Requests/Dms/Quickbooks/DeleteQuickbookApprovalRequest.php
@@ -10,14 +10,22 @@ use App\Models\CRM\Dms\Quickbooks\QuickbookApproval;
  */
 class DeleteQuickbookApprovalRequest extends Request {
 
-    use DeleteQuickbookRequestTrait;
-
     protected function getRules(): array
     {
         return [
             'dealer_id' => 'integer|min:1|required|exists:dealer,dealer_id',
             'id' => 'integer|required|exists:quickbook_approval,id'
         ];
+    }
+
+    public function getId(): int
+    {
+        return (int)$this->input('id');
+    }
+
+    public function getDealerId(): int
+    {
+        return (int)$this->input('dealer_id');
     }
 
     protected function getObject(): QuickbookApproval
