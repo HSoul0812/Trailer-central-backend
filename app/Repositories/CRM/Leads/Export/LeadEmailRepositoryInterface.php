4b5053e9cbdd405eb2c6c4b0e8b4aeaa82c249a0
Alberto <vaihtlumin@gmail.com>
2021-06-09 18:04:04 -0400

    Added adf settings

--- app/Repositories/CRM/Leads/Export/LeadEmailRepositoryInterface.php
+++ app/Repositories/CRM/Leads/Export/LeadEmailRepositoryInterface.php
@@ -5,6 +5,8 @@ namespace App\Repositories\CRM\Leads\Export;
 use App\Repositories\Repository;
 use App\Models\CRM\Leads\Lead;
 use App\Models\CRM\Leads\Export\LeadEmail;
+use Illuminate\Database\Eloquent\Collection;
+use App\DTO\CRM\Leads\Export\LeadEmail as LeadEmailDTO;
 
 interface LeadEmailRepositoryInterface extends Repository {
     
@@ -16,4 +18,18 @@ interface LeadEmailRepositoryInterface extends Repository {
      */
     public function getLeadEmailByLead(Lead $lead) : LeadEmail;
     
+    /**
+     * 
+     * @param Collection leads<LeadEmailDTO>
+     * @return bool
+     */
+    public function updateBulk(Collection $leads) : Collection;
+    
+    /**
+     * 
+     * @param int $dealerId
+     * @param int $locationId
+     * @return LeadEmail|null
+     */
+    public function getByDealerIdAndLocation(int $dealerId, int $locationId) : ?LeadEmail;
 }
