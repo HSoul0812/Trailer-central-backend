266550a6b06534771702b5404b7ea7ce93afefb2
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-06-17 21:05:26 -0500

    * PRTBND-975 Fixed Validating SMTP on SalesPerson Controller

--- app/Http/Requests/CRM/User/ValidateSalesPeopleRequest.php
+++ app/Http/Requests/CRM/User/ValidateSalesPeopleRequest.php
@@ -8,7 +8,7 @@ class ValidateSalesPeopleRequest extends Request {
     protected $rules = [
         'type' => 'required|in:smtp,imap',
         'username' => 'required|email',
-        'password' => 'required|confirmed',
+        'password' => 'required',
         'security' => 'required|in:tls,ssl',
         'host' => 'required',
         'port' => 'required|integer',
