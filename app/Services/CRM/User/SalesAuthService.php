7ab276a24823b2584b37d96a7fa1b75327a9b005
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-06-18 20:01:16 -0500

    * PRTBND-975 Fixed Implementing IMAP Validation on Endpoint

--- app/Services/CRM/User/SalesAuthService.php
+++ app/Services/CRM/User/SalesAuthService.php
@@ -6,6 +6,7 @@ use App\Models\CRM\User\SalesPerson;
 use App\Repositories\CRM\User\SalesPersonRepositoryInterface;
 use App\Repositories\Integration\Auth\TokenRepositoryInterface;
 use App\Services\CRM\Email\DTOs\SmtpConfig;
+use App\Services\CRM\Email\DTOs\ImapConfig;
 use App\Services\CRM\Email\ImapServiceInterface;
 use App\Services\Integration\AuthServiceInterface;
 use App\Traits\SmtpHelper;
