39360149d0d1641a9019a6f2406582674085d2bc
HuascarOna <huascar@codigojade.com>
2021-06-22 10:18:34 -0500

    #PRTBND-931 added migration for xlm export for marine world

---
+++ database/migrations/integrations/2021_06_21_131949_add_marineworld_integration.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddMarineworldIntegration extends Migration
+{
+
+    private const MARINEWORLD_PARAMS = [
+        'integration_id' => 75,
+        'code' => 'marineworld',
+        'module_name' => 'marineworld',
+        'module_status' => 'idle',
+        'name' => 'MarineWorld',
+        'description' => null,
+        'domain' => '',
+        'create_account_url' => '',
+        'active' => 1,
+        'filters' => 'a:0:{}',
+        'frequency' => 21600,
+        'settings' => 'a:0:{}',
+        'include_sold' => 0,
+        'uses_staging' => 1,
+        'show_for_integrated' => 0
+    ]; 
+
+    private const MARINEWORLD_DEALER = [
+        'integration_id' => 75,
+        'dealer_id' => 9554,
+        'active' => 1,
+        'settings' => [],
+        'location_ids' => '',
+        'msg_title' => '',
+        'msg_body' => '',
+        'msg_date' => '0000-00-00 00:00:00',
+        'include_pending_sale' => 0
+    ]; 
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      DB::table('integration')->insert(self::MARINEWORLD_PARAMS);
+
+      $marineWorldDealer = self::MARINEWORLD_DEALER;
+      $marineWorldDealer['created_at'] = (new \DateTime())->format('Y:m:d H:i:s');
+      $marineWorldDealer['settings'] = serialize($marineWorldDealer['settings']);
+
+      DB::table('integration_dealer')->insert($marineWorldDealer);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      DB::table('integration_dealer')->delete(self::MARINEWORLD_PARAMS['integration_id']);
+      DB::table('integration')->delete(self::MARINEWORLD_PARAMS['integration_id']);
+    }
+}
