1e74897403fda788a18c4c2187d146cc9c8df57b
Rober Díaz <rad8329@gmail.com>
2021-06-25 18:07:25 -0500

    #PRTBND-1030 Labors and parts discounts are being considered on the RO list payload
    FOR #PRTBND-342

--- app/Transformers/Dms/ServiceOrderTransformer.php
+++ app/Transformers/Dms/ServiceOrderTransformer.php
@@ -16,7 +16,7 @@ class ServiceOrderTransformer extends TransformerAbstract
         'serviceItems', 'partItems', 'miscPartItems', 'otherItems', 'invoice', 'inventory'
     ];
 
-    public function transform($serviceOrder)
+    public function transform(ServiceOrder $serviceOrder): array
     {
         return [
             'id' => $serviceOrder->id,
@@ -30,6 +30,8 @@ class ServiceOrderTransformer extends TransformerAbstract
             'closed_at' => $serviceOrder->closed_at,
             'type'      => $serviceOrder->type,
             'total_price' => $serviceOrder->total_price,
+            'labor_discount' => $serviceOrder->labor_discount,
+            'part_discount' => $serviceOrder->part_discount,
             'invoice' => $serviceOrder->invoice,
             'receipts' => $this->getReceipts($serviceOrder),
             'location' => $serviceOrder->dealerLocation ? $serviceOrder->dealerLocation->name : null,
@@ -63,17 +65,17 @@ class ServiceOrderTransformer extends TransformerAbstract
     {
         return $this->item($serviceOrder->invoice, new InvoiceTransformer());
     }
-    
+
     private function getReceipts(ServiceOrder $serviceOrder)
     {
         $receipts = [];
-        
+
         if ($serviceOrder->invoice && $serviceOrder->invoice->payments) {
             foreach($serviceOrder->invoice->payments as $payment) {
                 $receipts[] = $payment->receipts;
             }
         }
-        
+
         return $receipts;
     }
 
