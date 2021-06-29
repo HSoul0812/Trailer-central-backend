fde4ea27dec8dc4c685e93d9642bce40f89f68b5
Zeshan Khattak <zeshan77@gmail.com>
2021-06-29 23:54:43 +0500

    #PRTBND-1031 Add shipping field to payload

--- app/Transformers/Dms/ServiceOrderTransformer.php
+++ app/Transformers/Dms/ServiceOrderTransformer.php
@@ -37,6 +37,7 @@ class ServiceOrderTransformer extends TransformerAbstract
             'paid_amount' => (float) $serviceOrder->paid_amount,
             'status' => $serviceOrder->status,
             'status_name' => ServiceOrder::SERVICE_ORDER_STATUS[$serviceOrder->status],
+            'shipping' => $serviceOrder->shipping > 0 ? $serviceOrder->shipping : '0.0',
         ];
     }
 
