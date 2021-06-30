8492ba68aa9fd8c781c14aefa3ab478530ac88d3
Rober Díaz <rad8329@gmail.com>
2021-06-30 09:57:07 -0500

    #PRTBND-964 Migration script to add dealer location to invoices

---
+++ database/migrations/dms/2021_06_28_145125_add_location_to_invoices.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLocationToInvoices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('qb_invoices', function (Blueprint $table) {
+            $table->integer('dealer_location_id')
+                ->unsigned()
+                ->after('dealer_id')
+                ->nullable()
+                ->comment('being used after #PRTBND-964');
+
+            $table->foreign('dealer_location_id','qb_invoices_dealer_location_id_foreign')
+                ->references('dealer_location_id')
+                ->on('dealer_location')
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('qb_invoices', function (Blueprint $table) {
+            $table->dropForeign('qb_invoices_dealer_location_id_foreign');
+            $table->dropColumn('dealer_location_id');
+        });
+    }
+}
