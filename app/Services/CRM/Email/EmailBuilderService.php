22be2c532b4db2f07397b53215889e704da2d3d5
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-06-30 14:21:28 -0500

    * PRTBND-663 Fixed Sending Default Lead Emails on Campaign/Blast

--- app/Services/CRM/Email/EmailBuilderService.php
+++ app/Services/CRM/Email/EmailBuilderService.php
@@ -337,7 +337,7 @@ class EmailBuilderService implements EmailBuilderServiceInterface
         $smtpConfig = !empty($salesPerson->id) ? SmtpConfig::fillFromSalesPerson($salesPerson) : null;
 
         // Get SMTP Config
-        if(!empty($smtpConfig->isAuthTypeGmail())) {
+        if(!empty($smtpConfig) && $smtpConfig->isAuthTypeGmail()) {
             // Get Access Token
             $accessToken = $this->refreshAccessToken($smtpConfig->accessToken);
             $smtpConfig->setAccessToken($accessToken);
@@ -346,7 +346,7 @@ class EmailBuilderService implements EmailBuilderServiceInterface
             $finalEmail = $this->gmail->send($smtpConfig, $parsedEmail);
         }
         // Get NTLM Config
-        elseif(!empty($smtpConfig->isAuthTypeNtlm())) {
+        elseif(!empty($smtpConfig) && $smtpConfig->isAuthTypeNtlm()) {
             // Send NTLM Email
             $finalEmail = $this->ntlm->send($config->dealerId, $smtpConfig, $parsedEmail);
         }
