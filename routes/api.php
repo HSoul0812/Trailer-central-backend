e7d8379a2e808095085d2122b8bf96688dfbdbed
Rober Díaz <rad8329@gmail.com>
2021-07-06 15:50:14 -0500

    #PRTBND-964 `user/dealer-location/check/{name}` endpoint was registered

--- routes/api.php
+++ routes/api.php
@@ -370,7 +370,7 @@ $api->version('v1', function ($route) {
     $route->group(['middleware' => 'accesstoken.validate'], function ($route) {
         $route->get('user', 'App\Http\Controllers\v1\User\SignInController@details');
         $route->post('user/check-admin-password', 'App\Http\Controllers\v1\User\SignInController@checkAdminPassword');
-        
+
         $route->get('user/secondary-users', 'App\Http\Controllers\v1\User\SecondaryUsersController@index');
         $route->post('user/secondary-users', 'App\Http\Controllers\v1\User\SecondaryUsersController@create');
         $route->put('user/secondary-users', 'App\Http\Controllers\v1\User\SecondaryUsersController@updateBulk');
@@ -454,6 +454,7 @@ $api->version('v1', function ($route) {
         $route->get('user/dealer-location', 'App\Http\Controllers\v1\User\DealerLocationController@index');
         $route->delete('user/dealer-location/{id}', 'App\Http\Controllers\v1\User\DealerLocationController@destroy')->where('id', '[0-9]+');
         $route->get('user/dealer-location/{id}', 'App\Http\Controllers\v1\User\DealerLocationController@show')->where('id', '[0-9]+');
+        $route->get('user/dealer-location/check/{name}', 'App\Http\Controllers\v1\User\DealerLocationController@check');
         $route->post('user/dealer-location/{id}', 'App\Http\Controllers\v1\User\DealerLocationController@update')->where('id', '[0-9]+');
         $route->put('user/dealer-location', 'App\Http\Controllers\v1\User\DealerLocationController@create');
         $route->get('user/dealer-location-quote-fees', 'App\Http\Controllers\v1\User\DealerLocationController@quoteFees');
