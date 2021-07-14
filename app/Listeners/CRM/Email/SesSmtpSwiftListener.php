261ef0be0dcac2e9591366f00f770b63ce055110
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-07-14 02:05:34 -0500

    * PRTBND-1066 Fixed Getting Headers on Swift Event Listener

--- app/Listeners/CRM/Email/SesSmtpSwiftListener.php
+++ app/Listeners/CRM/Email/SesSmtpSwiftListener.php
@@ -2,41 +2,23 @@
 
 namespace App\Listeners\CRM\Email;
 
-use Swift_Events_SendEvent;
-use Swift_Events_SendListener;
+use Swift_Events_ResponseEvent;
+use Swift_Events_ResponseListener;
 
 /**
  * Class SesSmtpSwiftListener
  *
  * @package App\Listeners\CRM\Email
  */
-class SesSmtpSwiftListener implements Swift_Events_SendListener
+class SesSmtpSwiftListener implements Swift_Events_ResponseListener
 {
-    /**
-     * Invoked immediately before the Message is sent.
-     */
-    public function beforeSendPerformed(Swift_Events_SendEvent $evt)
-    {
-        // not used
-    }
-
     /**
      * Invoked immediately after the Message is sent.
      */
-    public function sendPerformed(Swift_Events_SendEvent $evt)
+    public function responseReceived(Swift_Events_ResponseEvent $evt)
     {
         // Check Event Results
-        $message = $evt->getMessage();
-
-        // SES Message Tags
-        $messageTags = $message->getHeaders()->get('X-SES-MESSAGE-TAGS');
-        if(!empty($messageTags)) {
-            $email = $messageTags->getValue();
-            print_r($email);
-
-            // Get Result
-            $result = $evt->getResult();
-            print_r($result);
-        }
+        $response = $evt->getResponse();
+        print_r($response);
     }
 }
