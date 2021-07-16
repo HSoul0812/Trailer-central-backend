69fe9ad921adb16573fe390bb2832d1d69995ec5
pekar <sergeypekar77@gmail.com>
2021-07-16 18:12:31 +0300

    #PRTBND-935 added tests

--- tests/Unit/Services/Website/WebsiteDealerUrlServiceTest.php
+++ tests/Unit/Services/Website/WebsiteDealerUrlServiceTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit\Services\Website;
 
 use App\Models\Region;
 use App\Models\User\DealerLocation;
+use App\Models\User\User;
+use App\Models\Website\WebsiteDealerUrl;
 use App\Repositories\User\DealerLocationRepositoryInterface;
 use App\Repositories\Website\WebsiteDealerUrlRepositoryInterface;
 use App\Services\Website\WebsiteDealerUrlService;
@@ -44,15 +46,17 @@ class WebsiteDealerUrlServiceTest extends TestCase
      * @covers ::generateByLocationId
      * @dataProvider locationParamsProvider
      */
-    public function testGenerateByLocationIdWithWebsiteDealerUrl(int $locationId, MockInterface $location)
+    public function testGenerateByLocationIdWithoutWebsiteDealerUrl(int $dealerId, int $locationId, MockInterface $location)
     {
-        $regionName = 'test_region_name';
-
-
         $region = $this->getEloquentMock(Region::class);
-        $region->region_name = $regionName;
+        $region->region_name = 'test_region_name';
 
+        $dealer = $this->getEloquentMock(User::class);
+        $dealer->name = 'test_name';
+
+        $location->dealer_id = $dealerId;
         $location->locationRegion = $region;
+        $location->user = $dealer;
 
         $this->dealerLocationRepository
             ->shouldReceive('get')
@@ -61,8 +65,34 @@ class WebsiteDealerUrlServiceTest extends TestCase
             ->andReturn($location);
 
         $this->websiteDealerUrlRepository
+            ->shouldReceive('exists')
+            ->once()
+            ->with(\Mockery::on(function ($arg) use ($locationId) {
+                return isset($arg['not_location_id'])
+                    && $arg['not_location_id'] === $locationId
+                    && isset($arg['url'])
+                    && stripos($arg['url'], '/trailer-dealer-in-') === 0;
+            }))
+            ->andReturn(false);
+
+        $this->websiteDealerUrlRepository
+            ->shouldReceive('exists')
+            ->once()
+            ->with(['location_id' => $locationId])
+            ->andReturn(false);
+
+        $this->websiteDealerUrlRepository
             ->shouldReceive('create')
-            ->never();
+            ->with(\Mockery::on(function ($arg) use ($locationId, $dealerId) {
+                return isset($arg['location_id'])
+                    && $arg['location_id'] === $locationId
+                    && isset($arg['dealer_id'])
+                    && $arg['dealer_id'] === $dealerId
+                    && isset($arg['url'])
+                    && stripos($arg['url'], '/trailer-dealer-in-') === 0;
+            }))
+            ->once()
+            ->andReturn($this->getEloquentMock(WebsiteDealerUrl::class));
 
         $this->websiteDealerUrlRepository
             ->shouldReceive('update')
@@ -72,14 +102,77 @@ class WebsiteDealerUrlServiceTest extends TestCase
         $service = $this->app->make(WebsiteDealerUrlService::class);
         $result = $service->generateByLocationId($locationId);
 
-        $this->assertFalse($result);
+        $this->assertTrue($result);
     }
 
     /**
      * @covers ::generateByLocationId
      * @dataProvider locationParamsProvider
      */
-    public function testGenerateByLocationIdWithoutRegion(int $locationId, MockInterface $location)
+    public function testGenerateByLocationIdWithWebsiteDealerUrl(int $dealerId, int $locationId, MockInterface $location)
+    {
+        $region = $this->getEloquentMock(Region::class);
+        $region->region_name = 'test_region_name';
+
+        $dealer = $this->getEloquentMock(User::class);
+        $dealer->name = 'test_name';
+
+        $location->dealer_id = $dealerId;
+        $location->locationRegion = $region;
+        $location->user = $dealer;
+
+        $this->dealerLocationRepository
+            ->shouldReceive('get')
+            ->once()
+            ->with(['dealer_location_id' => $locationId])
+            ->andReturn($location);
+
+        $this->websiteDealerUrlRepository
+            ->shouldReceive('exists')
+            ->once()
+            ->with(\Mockery::on(function ($arg) use ($locationId) {
+                return isset($arg['not_location_id'])
+                    && $arg['not_location_id'] === $locationId
+                    && isset($arg['url'])
+                    && stripos($arg['url'], '/trailer-dealer-in-') === 0;
+            }))
+            ->andReturn(false);
+
+        $this->websiteDealerUrlRepository
+            ->shouldReceive('exists')
+            ->once()
+            ->with(['location_id' => $locationId])
+            ->andReturn(true);
+
+        $this->websiteDealerUrlRepository
+            ->shouldReceive('update')
+            ->with(\Mockery::on(function ($arg) use ($locationId, $dealerId) {
+                return isset($arg['location_id'])
+                    && $arg['location_id'] === $locationId
+                    && isset($arg['dealer_id'])
+                    && $arg['dealer_id'] === $dealerId
+                    && isset($arg['url'])
+                    && stripos($arg['url'], '/trailer-dealer-in-') === 0;
+            }))
+            ->once()
+            ->andReturn($this->getEloquentMock(WebsiteDealerUrl::class));
+
+        $this->websiteDealerUrlRepository
+            ->shouldReceive('create')
+            ->never();
+
+        /** @var WebsiteDealerUrlService $service */
+        $service = $this->app->make(WebsiteDealerUrlService::class);
+        $result = $service->generateByLocationId($locationId);
+
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @covers ::generateByLocationId
+     * @dataProvider locationParamsProvider
+     */
+    public function testGenerateByLocationIdWithoutRegion(int $dealerId, int $locationId, MockInterface $location)
     {
         $location->locationRegion = null;
 
@@ -110,6 +203,7 @@ class WebsiteDealerUrlServiceTest extends TestCase
     public function locationParamsProvider(): array
     {
         return [[
+            99999,
             1111,
             $this->getEloquentMock(DealerLocation::class)
         ]];
