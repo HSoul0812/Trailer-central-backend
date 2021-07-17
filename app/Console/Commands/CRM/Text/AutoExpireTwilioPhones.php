3e0400464edb9a030665cf14655b0f169390fcb1
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-07-16 22:47:22 -0500

    * PRTBND-1088 Fixed Namespace on AutoExpireTwilioPhones

--- app/Console/Commands/CRM/Text/AutoExpireTwilioPhones.php
+++ app/Console/Commands/CRM/Text/AutoExpireTwilioPhones.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\CRM\Text;
 
 use App\Repositories\CRM\Text\NumberServiceInterface;
 use App\Services\CRM\Text\TextServiceInterface;
