6c5c89fb055b829e2855f58f3b6ca28cf7a06eec
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-07-16 23:02:54 -0500

    * PRTBND-1088 Enabled Processing Missing Numbers From Twilio

--- app/Console/Commands/CRM/Text/AutoExpireTwilioPhones.php
+++ app/Console/Commands/CRM/Text/AutoExpireTwilioPhones.php
@@ -74,7 +74,7 @@ class AutoExpireTwilioPhones extends Command
         // Get All Numbers From Twilio Missing in Our DB
         $numbers = $this->service->missing();
         foreach($numbers as $number) {
-            $this->info("Processing number {$number->phone_number} missing from DB");
+            $this->info("Processing number {$number} missing from DB");
             //$this->service->delete($number);
         }
     }
