c2b3d567387348ca1586b3de08662c398b072d0a
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-07-19 13:45:13 -0500

    * PRTBND-1088 Debug Deleting 20 Twilio Numbers

--- app/Services/CRM/Text/TwilioService.php
+++ app/Services/CRM/Text/TwilioService.php
@@ -139,10 +139,16 @@ class TwilioService implements TextServiceInterface
      */
     public function delete(string $number): bool {
         try {
+            // Prepend + to Phone
+            if(strpos($number, '+') === false) {
+                $number = '+' . $number;
+            }
+
             // Get All Incoming Phone Numbers Matching Provided Number
             $success = true;
             $numbers = $this->twilio->incomingPhoneNumbers->read(["phoneNumber" => $number], 20);
             foreach ($numbers as $record) {
+                $this->log->info('Found Twilio Phone Number ' . $record->phoneNumber . ' to Delete');
                 $this->twilio->incomingPhoneNumbers($record->sid)->delete();
             }
 
