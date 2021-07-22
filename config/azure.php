059611a100236f8ac3099b4550ad3996eb8519e3
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-07-22 15:26:58 -0500

    * PRTBND-1052 Fixed Azure Config Environment Variables

--- config/azure.php
+++ config/azure.php
@@ -12,20 +12,20 @@ return [
 
     // App Key Settings
     'app'             => [
-        'id'     => env('OAUTH_APP_ID', ''),
-        'secret' => env('OAUTH_APP_SECRET', ''),
+        'id'     => env('MS_AZURE_CLIENT_ID', ''),
+        'secret' => env('MS_AZURE_CLIENT_SECRET', ''),
     ],
 
     // Redirect URI
-    'redirectUri'     => env('OAUTH_REDIRECT_URI', 'https://crm.trailercentral.com/oauth-result/office365'),
+    'redirectUri'     => env('MS_AZURE_REDIRECT_URI', 'https://crm.trailercentral.com/oauth-result/office365'),
 
     // Required Scopes
-    'scopes'          => env('OAUTH_SCOPES', 'openid profile offline_access IMAP.AccessAsUser.All SMTP.Send offline_access email'),
+    'scopes'          => env('MS_AZURE_SCOPES', 'openid profile offline_access IMAP.AccessAsUser.All SMTP.Send offline_access email'),
 
     // Authority Settings
     'authority'       => [
-        'root'      => env('OAUTH_AUTHORITY', 'https://login.microsoftonline.com/common'),
-        'authorize' => env('OAUTH_AUTHORIZE_ENDPOINT', '/oauth2/v2.0/authorize'),
-        'token'     => env('OAUTH_TOKEN_ENDPOINT', '/oauth2/v2.0/token'),
+        'root'      => env('MS_AZURE_AUTHORITY', 'https://login.microsoftonline.com/common'),
+        'authorize' => env('MS_AZURE_AUTHORIZE', '/oauth2/v2.0/authorize'),
+        'token'     => env('MS_AZURE_TOKEN', '/oauth2/v2.0/token'),
     ]
 ];
