e169b8fba10eb1e9db978e1db4c2e06819837e13
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-07-23 16:06:03 -0500

    * PRTBND-1052 Fixed Migration for Office 365

--- database/migrations/2021_07_05_212406_add_office365_oauth_support.php
+++ database/migrations/2021_07_05_212406_add_office365_oauth_support.php
@@ -14,15 +14,15 @@ class AddOffice365OauthSupport extends Migration
     public function up()
     {
         Schema::table('integration_token', function (Blueprint $table) {
-            // Set Nullable on Additional Fields
-            $table->integer('expires_in')->nullable()->change();
-            $table->timestamp('expires_at')->nullable()->change();
-            $table->timestamp('issued_at')->nullable()->change();
-
             // Add New Indexes
             $table->index(['relation_type', 'relation_id']);
             $table->index(['access_token']);
         });
+
+        // Update Integration Token Expires In/Expires At/Issued At
+        DB::statement("ALTER TABLE integration_token MODIFY COLUMN expires_in INT(11) NULL");
+        DB::statement("ALTER TABLE integration_token MODIFY COLUMN expires_at TIMESTAMP NULL");
+        DB::statement("ALTER TABLE integration_token MODIFY COLUMN expires_at TIMESTAMP NULL");
 
         // Update Integration Token Relation Type
         DB::statement("ALTER TABLE integration_token MODIFY COLUMN relation_type ENUM('" . implode("', '", array_keys(AccessToken::RELATION_TYPES)) . "')");
