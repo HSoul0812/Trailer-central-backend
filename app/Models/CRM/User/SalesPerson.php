ace95c5a137e6fac2822f35768fa38af8146cd44
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-08-11 22:37:37 -0500

    * PRTBND-1099 Fixed Returning Default Folders on Sales Person Endpoint

--- app/Models/CRM/User/SalesPerson.php
+++ app/Models/CRM/User/SalesPerson.php
@@ -13,6 +13,7 @@ use App\Utilities\JsonApi\Filterable;
 use App\Services\CRM\Email\DTOs\ConfigValidate;
 use App\Services\CRM\Email\DTOs\ImapConfig;
 use App\Traits\SmtpHelper;
+use League\Fractal\Resource\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
