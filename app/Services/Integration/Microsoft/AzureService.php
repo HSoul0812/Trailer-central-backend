88e6575eef938015f5c969cb45e7cad1dfce52bc
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-08-16 22:12:38 -0500

    * PRTBND-1099 Fixed Using Default Scopes on Login Endpoint if Missing From Endpoint

--- app/Services/Integration/Microsoft/AzureService.php
+++ app/Services/Integration/Microsoft/AzureService.php
@@ -271,7 +271,7 @@ class AzureService implements AzureServiceInterface
         if(!empty($scopes)) {
             $scopes = array_merge(self::DEFAULT_SCOPES, $scopes);
         } else {
-            $scopes = explode(" ", config('azure.app.scopes'));
+            $scopes = explode(" ", config('azure.scopes'));
         }
 
         // Loop Defaults and Prepend Outlook
