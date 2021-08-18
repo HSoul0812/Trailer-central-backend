720deaf933bf004aaf81b5c2195e74aaae09a3af
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-08-18 16:12:56 -0500

    * PRTBND-1099 Fixed Error Refreshing Access Token on InteractionService

--- app/Services/CRM/Interactions/InteractionService.php
+++ app/Services/CRM/Interactions/InteractionService.php
@@ -5,6 +5,7 @@ namespace App\Services\CRM\Interactions;
 use App\Models\CRM\Leads\Lead;
 use App\Models\CRM\Interactions\Interaction;
 use App\Models\CRM\User\SalesPerson;
+use App\Models\Integration\Auth\AccessToken;
 use App\Models\User\User;
 use App\Repositories\CRM\Leads\StatusRepositoryInterface;
 use App\Repositories\CRM\Interactions\InteractionsRepositoryInterface;
