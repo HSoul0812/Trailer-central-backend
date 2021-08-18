e4714f1ba31b583db9d5fac65dc58dc30a6f638e
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-08-18 12:54:10 -0500

    * PRTBND-1099 Debug Getting Message Details on Office 365

--- app/Services/Integration/Microsoft/OfficeService.php
+++ app/Services/Integration/Microsoft/OfficeService.php
@@ -185,6 +185,7 @@ class OfficeService extends AzureService implements OfficeServiceInterface
     public function message(Message $message): ParsedEmail {
         // Get Headers/Body/Attachments
         $body = $message->getBody();
+        $this->log->info('Got message details: ' . print_r($message, true));
 
         // Get From/To
         $from = $message->getFrom();
