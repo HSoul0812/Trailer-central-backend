af217cb627302909435eb8e4ea8c1abafdc4cf5f
Rober Díaz <rad8329@gmail.com>
2021-09-13 15:52:32 -0500

    #DLFTT-22 Was written a integration test case for `TableAware` trait Also, this works for #DLFTT-22

---
+++ tests/Integration/App/Support/Traits/TableAwareTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Integration\App\Support\Traits;
+
+use App\Models\Inventory\InventoryLog;
+use App\Models\Leads\LeadLog;
+use Tests\Common\IntegrationTestCase;
+
+class TableAwareTest extends IntegrationTestCase
+{
+    /**
+     * @covers \App\Models\Inventory\InventoryLog::getTableName
+     * @covers \App\Models\Leads\LeadLog::getTableName
+     */
+    public function testHasAnStaticInstancePerModel(): void
+    {
+        // it's just necessary to test two different models
+        $expectedInventoryTableName = 'inventory_logs';
+        $expectedLeadsTableName = 'lead_logs';
+
+        $inventoryTableName = InventoryLog::getTableName();
+        $leadsTableName = LeadLog::getTableName();
+
+        $this->assertSame($expectedInventoryTableName, $inventoryTableName);
+        $this->assertSame($expectedLeadsTableName, $leadsTableName);
+    }
+}
