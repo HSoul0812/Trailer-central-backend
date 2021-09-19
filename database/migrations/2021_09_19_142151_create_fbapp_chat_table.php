adbb4cc3fa1a65fbd7e51c121cf0b00435384171
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-09-19 15:51:37 -0500

    * FCLNP-33 Created Migration for FB Chat

---
+++ database/migrations/2021_09_19_142151_create_fbapp_chat_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFbappChatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fbapp_chat', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('sales_person_id');
+            $table->bigInteger('page_id')->index();
+            $table->timestamps();
+
+            $table->index(['user_id', 'sales_person_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fbapp_chat');
+    }
+}
