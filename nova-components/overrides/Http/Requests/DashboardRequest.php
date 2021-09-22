b9cbd693dfcda071d227aca955b55ac53f13915f
Rober Díaz <rad8329@gmail.com>
2021-09-21 21:17:51 -0500

    #DLFTT-25 Was defined the Nova overrides path to be able binding request to dashboard cards -

---
+++ nova-components/overrides/Http/Requests/DashboardRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laravel\Nova\Http\Requests;
+
+use App\Nova\Nova;
+use Illuminate\Support\Collection;
+
+class DashboardRequest extends NovaRequest
+{
+    /**
+     * Get all the possible cards for the request.
+     *
+     * @param string $dashboard
+     * @noinspection PhpMissingParamTypeInspection
+     */
+    public function availableCards($dashboard): Collection
+    {
+        if ($dashboard === 'main') {
+            return collect(Nova::$defaultDashboardCards)
+                ->unique()
+                ->filter
+                ->authorize($this)
+                ->values();
+        }
+
+        return Nova::availableDashboardCardsForDashboard($dashboard, $this);
+    }
+}
