296a37cda9ee7f8ae87e86dfd3a07fd3ee69be90
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-09-23 14:16:32 -0500

    * FCLNP-33 Fixed Chat Transformer Endless Loop

--- app/Transformers/Integration/Facebook/ChatTransformer.php
+++ app/Transformers/Integration/Facebook/ChatTransformer.php
@@ -4,7 +4,6 @@ namespace App\Transformers\Integration\Facebook;
 
 use League\Fractal\TransformerAbstract;
 use App\Models\Integration\Facebook\Chat;
-use App\Transformers\CRM\User\SalesPersonTransformer;
 use App\Transformers\Integration\Facebook\PageTransformer;
 use App\Transformers\Integration\Auth\TokenTransformer;
 use App\Transformers\User\UserTransformer;
@@ -13,20 +12,13 @@ class ChatTransformer extends TransformerAbstract
 {
     protected $defaultIncludes = [];
 
-    protected $availableIncludes = [
-        'salesPerson'
-    ];
+    protected $availableIncludes = [];
 
 
     /**
      * @var UserTransformer
      */
     protected $userTransformer;
-
-    /**
-     * @var SalesPersonTransformer
-     */
-    protected $salesPersonTransformer;
 
     /**
      * @var PageTransformer
@@ -39,11 +31,9 @@ class ChatTransformer extends TransformerAbstract
     protected $tokenTransformer;
 
     public function __construct(
-        SalesPersonTransformer $salesPerson,
         PageTransformer $page,
         TokenTransformer $token
     ) {
-        $this->salesPersonTransformer = $salesPerson;
         $this->pageTransformer = $page;
         $this->tokenTransformer = $token;
     }
@@ -59,13 +49,5 @@ class ChatTransformer extends TransformerAbstract
             'created_at' => $chat->created_at,
             'updated_at' => $chat->updated_at
         ];
-    }
-
-    public function includeSalesPerson(Chat $chat)
-    {
-        if(!empty($chat->salesPerson)) {
-            return $this->item($chat->salesPerson, $this->salesPersonTransformer);
-        }
-        return $this->null();
     }
 }
