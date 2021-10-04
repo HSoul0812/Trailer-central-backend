85ec452632c1e766c73891d38032d6ffcd1a1bc5
Alberto <vaihtlumin@gmail.com>
2021-10-04 12:27:39 -0400

    #NR-208

--- app/Repositories/Parts/PartRepository.php
+++ app/Repositories/Parts/PartRepository.php
@@ -465,7 +465,9 @@ class PartRepository implements PartRepositoryInterface {
 
                         }
                     }
-                }
+                } else {
+                    $part->images()->delete();
+                } 
 
                 if (isset($params['bins'])) {
                     $part->bins()->delete();
