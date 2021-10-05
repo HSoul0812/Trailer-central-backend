d4ea729c6a30e7c453dc94746e977af9117ea70a
Rober Díaz <rad8329@gmail.com>
2021-10-05 16:23:20 -0500

    #DLFTT-24 Was implemented lead factory

--- app/Models/Leads/LeadLog.php
+++ app/Models/Leads/LeadLog.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models\Leads;
 
 use App\Support\Traits\TableAware;
+use Database\Factories\Leads\LeadLogFactory;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,13 +13,14 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property int                      $id
  * @property int                      $trailercentral_id
- * @property int                      $inventory_id
  * @property string                   $first_name
  * @property string                   $last_name
  * @property string                   $email_address
  * @property array                    $meta              json data
  * @property DateTimeInterface|string $submitted_at
  * @property DateTimeInterface|string $created_at
+ *
+ * @method static LeadLogFactory factory(...$parameters)
  */
 class LeadLog extends Model
 {
@@ -34,7 +36,6 @@ class LeadLog extends Model
      */
     protected $fillable = [
         'trailercentral_id',
-        'inventory_id',
         'first_name',
         'last_name',
         'email_address',
---
+++ database/factories/Leads/LeadLogFactory.php
@@ -0,0 +1,67 @@
+<?php
+
+/** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
+
+declare(strict_types=1);
+
+namespace Database\Factories\Leads;
+
+use App\Models\Leads\LeadLog;
+use App\Support\Traits\WithGetter;
+use Database\Seeders\WithArtifacts;
+use Faker\Generator;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+/**
+ * @property Generator $faker
+ */
+class LeadLogFactory extends Factory
+{
+    use WithArtifacts;
+    use WithGetter;
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = LeadLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @throws \JsonException when cannot load the json file
+     */
+    public function definition(): array
+    {
+        $manufacturer = $this->getManufacturers()->random();
+
+        return [
+            'trailercentral_id' => $this->faker->randomDigitNotZero(),
+            'vin'               => Str::upper(Str::random('21')),
+            'first_name'        => $this->faker->firstName(),
+            'last_name'         => $this->faker->lastName(),
+            'email_address'     => $this->faker->email(),
+            'manufacturer'      => $manufacturer['name'],
+            'brand'             => $this->faker->randomElement($manufacturer['brands']),
+            'meta'              => [],
+            'submitted_at'      => $this->faker->dateTimeBetween('-8 months'),
+            'created_at'        => $this->faker->dateTimeBetween('-8 months'),
+        ];
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function getManufacturers(): Collection
+    {
+        return $this->fromJson('leads/manufactures-brands.json');
+    }
+
+    public function getFaker(): Generator
+    {
+        return $this->faker;
+    }
+}
