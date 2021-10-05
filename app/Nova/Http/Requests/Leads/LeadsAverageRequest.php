3641c44563c7b928c3681cd0cc6fdc17427710dd
Rober Díaz <rad8329@gmail.com>
2021-10-05 16:29:33 -0500

    #DLFTT-24 Was implemented leads average request

---
+++ app/Nova/Http/Requests/Leads/LeadsAverageRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nova\Http\Requests\Leads;
+
+use App\Nova\Http\Requests\AbstractAverageRequest;
+
+class LeadsAverageRequest extends AbstractAverageRequest implements LeadsAverageRequestInterface
+{
+}
