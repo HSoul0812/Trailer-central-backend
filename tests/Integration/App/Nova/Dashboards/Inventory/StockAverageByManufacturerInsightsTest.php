c441e96121860020df43bec33df2455fbf954be9
Rober Díaz <rad8329@gmail.com>
2021-10-06 22:59:52 -0500

    Integrations test cases for `StockAverageByManufacturerInsightsTest`, ensuring the concrete insight report is working as expected

---
+++ tests/Integration/App/Nova/Dashboards/Inventory/StockAverageByManufacturerInsightsTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\App\Nova\Dashboards\Inventory;
+
+use App\Nova\Dashboards\Inventory\StockAverageByManufacturerInsights;
+use App\Nova\Http\Requests\InsightRequestInterface;
+use App\Nova\Http\Requests\Inventory\StockAverageRequest;
+use Database\Seeders\Inventory\AverageStockSeeder;
+use Tests\Common\IntegrationTestCase;
+use TrailerTrader\Insights\AreaChart;
+
+class StockAverageByManufacturerInsightsTest extends IntegrationTestCase
+{
+    /**
+     * Test that SUT is returning a payload with a well known insights by week and by day.
+     */
+    public function testHasAnExpectedResponse(): void
+    {
+        $this->seed(AverageStockSeeder::class);
+
+        $dashboard = app(StockAverageByManufacturerInsights::class);
+
+        $this->testByWeeks($dashboard);
+        $this->testByDays($dashboard);
+    }
+
+    private function testByWeeks(StockAverageByManufacturerInsights $dashboard): void
+    {
+        $request = new StockAverageRequest();
+
+        $response = $dashboard->cards($request);
+
+        /** @var AreaChart $chart */
+        $chart = $response[0];
+        $meta = $chart->meta();
+
+        self::assertIsArray($response);
+        self::assertInstanceOf(AreaChart::class, $chart);
+
+        // series assertions
+        self::assertArrayHasKey('series', $meta);
+        self::assertArrayHasKey('data', $meta['series'][0]);
+
+        $series = collect($meta['series'][0]['data']);
+
+        self::assertGreaterThanOrEqual(39, $series->count());
+        self::assertEquals(6, $series->first());
+        self::assertEquals(3, $series->last());
+
+        // option categories assertions
+        self::assertArrayHasKey('options', $meta);
+        self::assertObjectHasAttribute('xAxis', $meta['options']);
+
+        $categories = collect($meta['options']->xAxis['categories']);
+
+        self::assertArrayHasKey('categories', $meta['options']->xAxis);
+        self::assertGreaterThanOrEqual(39, $categories->count());
+        self::assertSame('2020-53', $categories->first());
+    }
+
+    private function testByDays(StockAverageByManufacturerInsights $dashboard): void
+    {
+        $request = new StockAverageRequest(['period' => InsightRequestInterface::PERIOD_PER_DAY]);
+        $response = $dashboard->cards($request);
+
+        /** @var AreaChart $chart */
+        $chart = $response[0];
+        $meta = $chart->meta();
+
+        self::assertIsArray($response);
+        self::assertInstanceOf(AreaChart::class, $chart);
+
+        // series assertions
+        self::assertArrayHasKey('series', $meta);
+        self::assertArrayHasKey('data', $meta['series'][0]);
+
+        $series = collect($meta['series'][0]['data']);
+
+        self::assertGreaterThanOrEqual(278, $series->count());
+        self::assertEquals(3, $series->first());
+        self::assertEquals(0, $series->last());
+
+        // option categories assertions
+        self::assertArrayHasKey('options', $meta);
+        self::assertObjectHasAttribute('xAxis', $meta['options']);
+
+        $categories = collect($meta['options']->xAxis['categories']);
+
+        self::assertArrayHasKey('categories', $meta['options']->xAxis);
+        self::assertGreaterThanOrEqual(278, $categories->count());
+        self::assertSame('2021-01-01', $categories->first());
+    }
+}
