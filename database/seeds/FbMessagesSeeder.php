e0af6f7aa8cfc4c134380424d7cd3488fd5f912e
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-10-12 12:27:04 -0500

    * FCLNP-11 Implement Method for Hidden Facebook Leads

--- database/seeds/FbMessagesSeeder.php
+++ database/seeds/FbMessagesSeeder.php
@@ -23,19 +23,43 @@ class FbMessagesSeeder extends Seeder
         // Create Page
         $page = factory(Page::class)->create(['dealer_id' => self::DEALER_ID]);
 
+
         // Create FB User
-        $fbUser = factory(FbUser::class)->create();
+        $nonLead = factory(Lead::class)->create([
+            'dealer_id' => $page->dealer_id,
+            'lead_type' => LeadType::TYPE_NONLEAD
+        ]);
+        $fbUser1 = factory(FbUser::class)->create([
+            'name' => $nonLead->full_name,
+            'email' => $nonLead->email_address
+        ]);
+        $fbLead1 = factory(FbLead::class)->create([
+            'page_id' => $page->page_id,
+            'user_id' => $fbUser1->user_id,
+            'lead_id' => $nonLead->identifier
+        ]);
+
 
         // Create FB Lead
-        $fbLead = factory(FbLead::class)->create([
-            'page_id' => $page->page_id
+        $realLead = factory(Lead::class)->create([
+            'dealer_id' => $page->dealer_id,
+            'lead_type' => LeadType::TYPE_FB
+        ]);
+        $fbUser2 = factory(FbUser::class)->create([
+            'name' => $realLead->full_name,
+            'email' => $realLead->email_address
+        ]);
+        $fbLead2 = factory(FbLead::class)->create([
+            'page_id' => $page->page_id,
+            'user_id' => $fbUser2->user_id,
+            'lead_id' => $realLead->identifier
         ]);
 
 
         // Create Conversation With FB User
         $conversation1 = factory(Conversation::class)->create([
             'page_id' => $page->page_id,
-            'user_id' => $fbUser->user_id
+            'user_id' => $fbLead1->user_id
         ]);
         $messagesSeed1 = [
             ['conversation_id' => $conversation1->conversation_id, 'from_id' => $conversation1->user_id, 'to_id' => $conversation1->page_id],
@@ -56,7 +80,7 @@ class FbMessagesSeeder extends Seeder
         // Create Conversation With FB Lead
         $conversation2 = factory(Conversation::class)->create([
             'page_id' => $page->page_id,
-            'user_id' => $fbLead->user_id
+            'user_id' => $fbLead2->user_id
         ]);
         $messagesSeed2 = [
             ['conversation_id' => $conversation2->conversation_id, 'from_id' => $conversation2->user_id, 'to_id' => $conversation2->page_id],
