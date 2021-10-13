b4bc622e819528a1a5b4c2afd32529103d0ea75c
Mert Oksuz <mert.o@onthegosystems.com>
2021-10-13 13:12:05 +0300

    #EC-62 integration test added

---
+++ tests/Integration/Http/Controllers/Ecommerce/ShippingControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Tests\Integration\Http\Controllers\Ecommerce;
+
+
+use Tests\database\seeds\Ecommerce\ShippingSeeder;
+use Tests\TestCase;
+
+class ShippingControllerTest extends TestCase
+{
+    public function testCalculateCosts()
+    {
+        $shippingSeeder = new ShippingSeeder();
+        $shippingSeeder->seed();
+
+        $response = $this->json("POST", '/api/ecommerce/shipping-costs',
+            [
+                'customer_details' => $shippingSeeder->customerDetails,
+                'shipping_details' => $shippingSeeder->shippingDetails,
+                'items' => $shippingSeeder->products
+            ],
+            [
+                'access-token' => $shippingSeeder->authToken->access_token
+            ]
+        );
+
+        $response->assertStatus(200);
+
+        $responseJson = json_decode($response->getContent(), true);
+
+        $this->assertArrayHasKey('cost', $responseJson);
+        $this->assertEquals(54, $responseJson['cost']);
+    }
+
+    public function testFailAuthKey()
+    {
+        $shippingSeeder = new ShippingSeeder();
+        $shippingSeeder->seed();
+
+        $response = $this->json("POST", '/api/ecommerce/shipping-costs',
+            [
+                'customer_details' => $shippingSeeder->customerDetails,
+                'shipping_details' => $shippingSeeder->shippingDetails,
+                'items' => $shippingSeeder->products
+            ]
+        );
+
+        $response->assertStatus(403);
+    }
+}
