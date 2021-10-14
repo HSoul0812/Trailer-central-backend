40e1981995ad890658f39ea02161d7fe5e39004e
Mert Oksuz <mert.o@onthegosystems.com>
2021-10-14 18:01:56 +0300

    #EC-64 add show endpoint for completed order

--- app/Services/Ecommerce/CompletedOrder/CompletedOrderService.php
+++ app/Services/Ecommerce/CompletedOrder/CompletedOrderService.php
@@ -4,6 +4,9 @@ namespace App\Services\Ecommerce\CompletedOrder;
 
 use App\Models\Ecommerce\CompletedOrder\CompletedOrder;
 use App\Repositories\Ecommerce\CompletedOrderRepositoryInterface;
+use App\Repositories\Parts\Textrail\PartRepository;
+use App\Transformers\Ecommerce\CompletedOrderTransformer;
+use League\Fractal\Resource\Collection;
 
 class CompletedOrderService implements CompletedOrderServiceInterface
 {
@@ -11,13 +14,18 @@ class CompletedOrderService implements CompletedOrderServiceInterface
     /** @var CompletedOrderRepositoryInterface */
     private $completedOrderRepository;
 
+    /** @var PartRepository */
+    private $textRailPartRepoitory;
+
     /**
      * CompletedOrderService constructor.
      * @param CompletedOrderRepositoryInterface $completedOrderRepository
+     * @param PartRepository $textRailPartRepoitory
      */
-    public function __construct(CompletedOrderRepositoryInterface $completedOrderRepository)
+    public function __construct(CompletedOrderRepositoryInterface $completedOrderRepository, PartRepository $textRailPartRepoitory)
     {
         $this->completedOrderRepository = $completedOrderRepository;
+        $this->textRailPartRepoitory = $textRailPartRepoitory;
     }
 
     public function create(array $params): CompletedOrder
--- app/Transformers/Ecommerce/CompletedOrderTransformer.php
+++ app/Transformers/Ecommerce/CompletedOrderTransformer.php
@@ -2,12 +2,34 @@
 namespace App\Transformers\Ecommerce;
 
 use App\Models\Ecommerce\CompletedOrder\CompletedOrder;
+use App\Repositories\Ecommerce\CompletedOrderRepositoryInterface;
+use App\Repositories\Parts\Textrail\PartRepository;
+use App\Transformers\Parts\Textrail\PartsTransformer;
 use League\Fractal\TransformerAbstract;
 
 class CompletedOrderTransformer extends TransformerAbstract
 {
+    /** @var PartRepository */
+    private $textRailPartRepository;
+
+    /**
+     * CompletedOrderTransformer constructor.
+     * @param PartRepository $textRailPartRepository
+     */
+    public function __construct(PartRepository $textRailPartRepository)
+    {
+        $this->textRailPartRepository = $textRailPartRepository;
+    }
+
     public function transform(CompletedOrder $completedOrder)
     {
+        $parts = json_decode($completedOrder->parts, true);
+
+        $partCollection = [];
+        foreach ($parts as $part) {
+            $partCollection[] = $this->textRailPartRepository->getById($part['id']);
+        }
+
         return [
             'id' => $completedOrder->id,
             'hook_event_id' => $completedOrder->event_id,
@@ -20,7 +42,9 @@ class CompletedOrderTransformer extends TransformerAbstract
             'shipping_address' => $completedOrder->shipping_address,
             'billing_address' => $completedOrder->billing_address,
             'postal_code' => $completedOrder->postal_code,
-            'created_at' => $completedOrder->created_at
+            'created_at' => $completedOrder->created_at,
+            'status' => $completedOrder->status,
+            'parts' => $partCollection,
         ];
     }
 }
