ac2720b0a6ee52636ced7402abd4e9de0ad49692
songxunzhao <zhao@operatebeyond.com>
2021-10-15 06:28:53 -0500

    #ECB-42: Change create request form

--- app/Http/Requests/Website/User/CreateRequest.php
+++ app/Http/Requests/Website/User/CreateRequest.php
@@ -5,14 +5,22 @@ namespace App\Http\Requests\Website\User;
 
 
 use App\Http\Requests\Request;
+use Illuminate\Validation\Rule;
 
 class CreateRequest extends Request
 {
-    protected $rules = [
-        'first_name' => 'required',
-        'middle_name' => 'nullable',
-        'last_name' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|min:8'
-    ];
+    protected $rules = [];
+    protected function getRules(): array
+    {
+        return [
+            'first_name' => 'required',
+            'middle_name' => 'nullable',
+            'last_name' => 'required',
+            'email' => ['required', 'email',
+                Rule::unique('website_user')->where('website_id', $this->website_id)
+            ],
+            'password' => 'required|min:8',
+            'website_id' => 'required',
+        ];
+    }
 }
