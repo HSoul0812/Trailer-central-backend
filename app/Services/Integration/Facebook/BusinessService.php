7e03f9566cb78f273751ee89bcf562c07212a9eb
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-10-17 23:02:13 -0500

    * FCLNP-11 Fixed Getting Page Token From Business SDK

--- app/Services/Integration/Facebook/BusinessService.php
+++ app/Services/Integration/Facebook/BusinessService.php
@@ -8,6 +8,7 @@ use App\Exceptions\Integration\Facebook\FailedCreateProductFeedException;
 use App\Exceptions\Integration\Facebook\MissingFacebookAccessTokenException;
 use App\Exceptions\Integration\Facebook\ExpiredFacebookAccessTokenException;
 use App\Exceptions\Integration\Facebook\FailedReceivingLongLivedTokenException;
+use App\Models\Integration\Auth\AccessToken;
 use App\Repositories\Integration\Facebook\PageRepositoryInterface;
 use App\Services\CRM\Interactions\Facebook\DTOs\ChatConversation;
 use App\Services\CRM\Interactions\Facebook\DTOs\ChatMessage;
