4b9a7c69954caa972a28199fb8be515b12d14bbe
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-10-19 23:29:36 -0500

    * FCLNP-11 Finished Webhook Configuration

--- app/Services/CRM/Interactions/Facebook/WebhookServiceInterface.php
+++ app/Services/CRM/Interactions/Facebook/WebhookServiceInterface.php
@@ -3,14 +3,14 @@
 namespace App\Services\CRM\Interactions\Facebook;
 
 use App\Http\Requests\CRM\Interactions\Facebook\MessageWebhookRequest;
-use App\Models\CRM\Interactions\Facebook\Message;
+use Illuminate\Support\Collection;
 
 interface WebhookServiceInterface {
     /**
      * Handle Message From Webhook
      * 
      * @param MessageWebhookRequest $request
-     * @return Message
+     * @return Collection<Message>
      */
-    public function message(MessageWebhookRequest $request): Message;
+    public function message(MessageWebhookRequest $request): Collection;
 }
