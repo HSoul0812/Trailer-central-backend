b8c66f112db3b3c6977f912d45af080c9e4d2ae8
Vishnu D <vishnu@trailercentral.com>
2021-10-23 00:23:40 +0530

    DMS-234: Quote Feature Test Case

---
+++ tests/Feature/Dms/QuoteFeatureTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Feature\Dms;
+
+use App\Models\CRM\Dms\UnitSale;
+use Tests\TestCase;
+
+/**
+ * Class QuoteFeatureTest
+ *
+ * @package Tests\Feature\Dms
+ */
+class QuoteFeatureTest extends TestCase
+{
+    private const API_QUOTES_ARCHIVE = '/api/user/quotes';
+
+    private const API_QUOTES_BULK_ARCHIVE = '/api/user/quotes/bulk-archive';
+
+    private const INVALID_ENTITY_MESSAGE = [
+        'message' => 'The given data was invalid.',
+    ];
+
+    // use a known sample ID from the database with valid values
+    protected $dealerId = 1001;
+
+    public function testListHttpOk()
+    {
+        $response = $this
+            ->withHeaders(['access-token' => $this->accessToken()])
+            ->get(self::API_QUOTES_ARCHIVE);
+
+        $response->assertStatus(200);
+    }
+
+    public function testBulkUpdateMissingAccessToken()
+    {
+        $response = $this
+            ->put(self::API_QUOTES_BULK_ARCHIVE);
+
+        $response->assertStatus(403);
+    }
+
+    public function testBulkUpdateMissingRequiredData()
+    {
+        $this
+            ->withHeaders(['access-token' => $this->accessToken()])
+            ->put(self::API_QUOTES_BULK_ARCHIVE)
+            ->assertStatus(422)
+            ->assertJsonFragment(
+                $this->generateQuoteIdsValidationMessage('The quote ids field is required.')
+            );
+    }
+
+    public function testBulkUpdateInvalidDataType()
+    {
+        $this
+            ->withHeaders(['access-token' => $this->accessToken()])
+            ->put(self::API_QUOTES_BULK_ARCHIVE, [
+                'quote_ids' => 12345,
+            ])
+            ->assertStatus(422)
+            ->assertJsonFragment(
+                $this->generateQuoteIdsValidationMessage('The quote ids needs to be an array.')
+            );
+    }
+
+    public function generateQuoteIdsValidationMessage(string $message)
+    {
+        return $this->generateValidationErrors([
+            'quote_ids' => [
+                $message,
+            ],
+        ]);
+    }
+
+    public function testBulkUpdateValidData()
+    {
+        $quote = $this->makeQuotes();
+        $this
+            ->withHeaders(['access-token' => $this->accessToken()])
+            ->put(self::API_QUOTES_BULK_ARCHIVE, [
+                'quote_ids' => [
+                    $quote->getKey(),
+                ],
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'message' => 'success',
+            ]);
+
+        $this->assertDatabaseHas('dms_unit_sale', [
+            'id' => $quote->getKey(),
+            'is_archived' => true,
+        ]);
+
+        $quote->forceDelete();
+    }
+
+    public function makeQuotes()
+    {
+        $model = new UnitSale();
+        $model->dealer_id = $this->dealerId;
+        $model->is_archived = false;
+
+        $model->save();
+
+        return $model;
+    }
+
+    /**
+     * @param mixed ...$errors
+     *
+     * @return array
+     */
+    protected function generateValidationErrors(...$errors): array
+    {
+        return [
+            'errors' => array_merge_recursive(...$errors),
+        ] + self::INVALID_ENTITY_MESSAGE;
+    }
+}
