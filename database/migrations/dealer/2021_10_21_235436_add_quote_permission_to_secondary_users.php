bbd7bb535319bcfbb0c83225046abe910ece4bc6
HuascarOna <huascar@codigojade.com>
2021-10-25 09:46:21 -0500

    #EC-74 updated migration with constants small fix

--- database/migrations/dealer/2021_10_21_235436_add_quote_permission_to_secondary_users.php
+++ database/migrations/dealer/2021_10_21_235436_add_quote_permission_to_secondary_users.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use App\Models\User\Interfaces\PermissionsInterface;
 
 class AddQuotePermissionToSecondaryUsers extends Migration
 {
@@ -16,14 +17,14 @@ class AddQuotePermissionToSecondaryUsers extends Migration
     {
       $dealerUsers = DB::table('dealer_user_permissions')
           ->select('dealer_user_id', 'permission_level')
-          ->where('feature', 'crm')->get();
+          ->where('feature', PermissionsInterface::CRM)->get();
       
       foreach ($dealerUsers as $dealer_user) {
-        if ($dealer_user->permission_level != 'cannot_see') {
+        if ($dealer_user->permission_level != PermissionsInterface::CANNOT_SEE_PERMISSION) {
           DB::table('dealer_user_permissions')->insert([
               'dealer_user_id' => $dealer_user->dealer_user_id,
-              'feature' => 'crm',
-              'permission_level' => 'can_see_and_change'
+              'feature' => PermissionsInterface::QUOTES,
+              'permission_level' => PermissionsInterface::CAN_SEE_AND_CHANGE_PERMISSION
           ]);
         }
       }
