e0a1a2db7b6b53ec5355b34bed78cee2fbae8d38
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-10-29 13:49:01 -0500

    * FCLNP-53 Fixed Facebook Messaging Type on Endpoint

--- app/Http/Requests/CRM/Interactions/Facebook/SendMessageRequest.php
+++ app/Http/Requests/CRM/Interactions/Facebook/SendMessageRequest.php
@@ -14,6 +14,6 @@ class SendMessageRequest extends Request
     protected $rules = [
         'lead_id' => 'required|integer',
         'message' => 'required|string',
-        'type' => 'optional|messaging_type_valid'
+        'type' => 'nullable|messaging_type_valid'
     ];
 }
