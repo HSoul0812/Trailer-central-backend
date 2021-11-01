3295d095737fe72e640b75fc3c4cc859905faf26
Rober Díaz <rad8329@gmail.com>
2021-11-01 08:37:05 -0500

    #EC-73 [Mert CR] the hardcode profile image was moved to a config file

--- app/Transformers/User/UserTransformer.php
+++ app/Transformers/User/UserTransformer.php
@@ -7,8 +7,6 @@ use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
-    private const PROFILE_IMAGE = 'https://static-trailercentral.s3.amazonaws.com/files/download+(1).png';
-
     protected $defaultIncludes = [
         'permissions'
     ];
@@ -21,7 +19,7 @@ class UserTransformer extends TransformerAbstract
              'created_at' => $user->created_at ?? $user->user->created_at,
              'name' => $user->name ?? $user->user->name,
              'email' => $user->email ?? $user->user->email,
-             'profile_image' => self::PROFILE_IMAGE,
+             'profile_image' => config('user.profile.image'),
              'website' => $user->website
         ];
     }
