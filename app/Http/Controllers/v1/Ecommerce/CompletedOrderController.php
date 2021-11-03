d71282e7e1c611d1cef9a3af1c7dbc32a8ae425a
Rober Díaz <rad8329@gmail.com>
2021-11-03 15:51:14 -0500

    #EC-88 Order controller is using middlewares to intercept auth token
    - `CompletedOrderController::create` is handling API request coming from Stripe/Dealer website according to the needs:
      - When it is a request from Stripe, the dealer_id is not required
      - When it is a request from Dealer website, the dealer_id is required

--- app/Http/Controllers/v1/Ecommerce/CompletedOrderController.php
+++ app/Http/Controllers/v1/Ecommerce/CompletedOrderController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\v1\Ecommerce;
 
-use App\Http\Controllers\RestfulController;
+use App\Http\Controllers\RestfulControllerV2;
 use App\Http\Requests\Ecommerce\CreateCompletedOrderRequest;
-use App\Http\Requests\Ecommerce\GetCompletedOrderRequest;
+use App\Http\Requests\Ecommerce\GetAllCompletedOrderRequest;
+use App\Http\Requests\Ecommerce\GetSingleCompletedOrderRequest;
 use App\Repositories\Ecommerce\CompletedOrderRepositoryInterface;
 use App\Repositories\Parts\PartRepositoryInterface;
 use App\Repositories\Parts\Textrail\PartRepository;
@@ -13,7 +14,7 @@ use App\Transformers\Ecommerce\CompletedOrderTransformer;
 use Dingo\Api\Http\Request;
 use Dingo\Api\Http\Response;
 
-class CompletedOrderController extends RestfulController
+class CompletedOrderController extends RestfulControllerV2
 {
     /** @var CompletedOrderServiceInterface */
     private $completedOrderService;
@@ -39,16 +40,39 @@ class CompletedOrderController extends RestfulController
         $this->completedOrderService = $completedOrderService;
         $this->completedOrderRepo = $completedOrderRepo;
         $this->textRailPartRepo = $textRailPartRepo;
+
+        $this->middleware('setDealerIdOnRequest')->only(['index', 'show']);
+
+        /**
+         * Since create action will be called from a payment gateway (Stripe), and the dealer website,
+         * we need to make sure that the dealer is set on the request only when the request is coming from the dealer website.
+         * Then, it will be handle by the repository ensuring that when the order is a new one and the dealer is not set,
+         * it will throw an exception.
+         */
+        $this->middleware('setDealerIdWhenAuthenticatedOnRequest')->only(['create']);
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * @throws \Dingo\Api\Exception\ResourceException when there were some validation error
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException when there was a bad request
+     *
+     * @noinspection PhpDocMissingThrowsInspection
+     * @noinspection PhpUnhandledExceptionInspection
+     */
     public function index(Request $request): Response
     {
-        $listRequest = new GetCompletedOrderRequest($request->all());
+        $listRequest = new GetAllCompletedOrderRequest($request->all());
 
         if ($listRequest->validate()) {
             return $this->response
-                ->paginator($this->completedOrderRepo->getAll($request->all()), new CompletedOrderTransformer($this->textRailPartRepo))
-                ->addMeta('totals', $this->completedOrderRepo->getGrandTotals());
+                ->paginator(
+                    $this->completedOrderRepo->getAll($listRequest->all()),
+                    new CompletedOrderTransformer($this->textRailPartRepo)
+                )
+                ->addMeta('totals', $this->completedOrderRepo->getGrandTotals($listRequest->dealer_id));
         }
 
         return $this->response->errorBadRequest();
@@ -57,8 +81,12 @@ class CompletedOrderController extends RestfulController
     /**
      * @param Request $request
      * @return Response
-     * @throws \App\Exceptions\Requests\Validation\NoObjectIdValueSetException
-     * @throws \App\Exceptions\Requests\Validation\NoObjectTypeSetException
+     *
+     * @throws \Dingo\Api\Exception\ResourceException when there were some validation error
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException when there was a bad request
+     *
+     * @noinspection PhpDocMissingThrowsInspection
+     * @noinspection PhpUnhandledExceptionInspection
      */
     public function create(Request $request): Response
     {
@@ -73,8 +101,25 @@ class CompletedOrderController extends RestfulController
         return $this->createdResponse($order->id);
     }
 
-    public function show(int $id)
+    /**
+     * @param int $id
+     * @param Request $request
+     * @return Response
+     *
+     * @throws \Dingo\Api\Exception\ResourceException when there were some validation error
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException when there was a bad request
+     *
+     * @noinspection PhpDocMissingThrowsInspection
+     * @noinspection PhpUnhandledExceptionInspection
+     */
+    public function show(int $id, Request $request): Response
     {
-        return $this->response->item($this->completedOrderRepo->get(['id' => $id]), new CompletedOrderTransformer($this->textRailPartRepo));
+        $orderRequest = new GetSingleCompletedOrderRequest($request->all() + ['order_id' => $id]);
+
+        if ($orderRequest->validate()) {
+            return $this->response->item($this->completedOrderRepo->get(['id' => $id]), new CompletedOrderTransformer($this->textRailPartRepo));
+        }
+
+        $this->response->errorBadRequest();
     }
 }
