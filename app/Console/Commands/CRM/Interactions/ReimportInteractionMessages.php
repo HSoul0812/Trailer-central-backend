eb711051240e6a1f6ee2310be5cb474a66efaa23
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-03 22:50:46 -0500

    * FCLNP-51 Implemented Reimport as Command

---
+++ app/Console/Commands/CRM/Interactions/ReimportInteractionMessages.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands\CRM\Interactions;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ReimportInteractionMessages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'interaction:messages:reimport {reimport?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Re-import interaction messages into elastic search.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Try..Catch
+        try {
+            // Get ES Interaction Message
+            $uri = env('ELASTICSEARCH_HOST') . '/interaction_message' . '?pretty';
+
+            // Create Client
+            $client = new Client();
+
+            // Delete Request From Client
+            $client->request('DELETE', $uri);
+        } catch(\Exception $e) {
+            $this->error('Error occurred clearing interaction_message index on ES: ', $e->getMessage());
+        }
+
+
+        // Reimport?!
+        $reimport = $this->argument('boundLower');
+        if(!empty($reimport)) {
+            // Try..Catch
+            try {
+                // Re-Import ES Messages
+                Artisan::call('scout:import "App\\\\Models\\\\CRM\\\\Interactions\\\\InteractionMessage" -v');
+            } catch(\Exception $e) {
+                $this->error('Error occurred re-imported index on ES: ', $e->getMessage());
+            }
+        }
+    }
+}
