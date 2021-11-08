780ae1684a87ae2003d2f9fb4d3060aa05e7ceb9
Rober Díaz <rad8329@gmail.com>
2021-11-08 15:32:28 -0500

    #EC-93 `PartsServiceProvider` was refactored to remove responsibilities which belongs to `EcommerceServiceProvider`

--- app/Services/Ecommerce/DataProvider/DataProviderInterface.php
+++ app/Services/Ecommerce/DataProvider/DataProviderInterface.php
@@ -1,8 +1,12 @@
 <?php
-namespace App\Services\Ecommerce\DataProvider;
 
+namespace App\Services\Ecommerce\DataProvider;
 
 interface DataProviderInterface
 {
+    /**
+     * @param array $params
+     * @return array{cost: float, tax: float, cart_id: string, customer_id: ?int, carrier_code: string, method_code: string}
+     */
     public function estimateShippingCost(array $params);
 }
