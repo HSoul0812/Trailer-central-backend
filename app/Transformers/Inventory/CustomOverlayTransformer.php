bbb28e573e40a1ef53564afca7b948f1fd7d1a84
Rober Díaz <rad8329@gmail.com>
2021-11-09 15:06:13 -0500

    #EC-79 `POST /api/inventory/overlay` and `POST /api/inventory/bulk-overlay` were added

--- app/Transformers/Inventory/CustomOverlayTransformer.php
+++ app/Transformers/Inventory/CustomOverlayTransformer.php
@@ -6,13 +6,13 @@ use League\Fractal\TransformerAbstract;
 use App\Models\Inventory\CustomOverlay;
 
 class CustomOverlayTransformer extends TransformerAbstract {
-    
-    public function transform(CustomOverlay $customOverlay) {
+
+    public function transform(CustomOverlay $customOverlay): array
+    {
         return [
             'id' => $customOverlay->id,
             'name' => $customOverlay->name,
             'value' => $customOverlay->value
         ];
     }
-    
 }
