642f087d5317606d96027d589c4e11fa095a4d62
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-11 00:30:39 -0600

    * FCLNP-9 Fixed Error Getting Conversations/Messages

--- app/Http/Controllers/v1/CRM/Interactions/Facebook/MessageController.php
+++ app/Http/Controllers/v1/CRM/Interactions/Facebook/MessageController.php
@@ -65,7 +65,7 @@ class MessageController extends RestfulController
         $request = new GetMessagesRequest($requestData);
 
         if ($request->validate()) {
-            return $this->response->collection($this->repository->getAll($request->all()), $this->transformer);
+            return $this->response->paginate($this->repository->getAll($request->all()), $this->transformer);
         }
 
         return $this->response->errorBadRequest();
