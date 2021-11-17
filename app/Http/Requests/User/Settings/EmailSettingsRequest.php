d1c00bf90f7cf5b4e73d63fef4e29d2e27a6b16a
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-17 16:35:48 -0600

    * FCLNP-26 Fixed Configuring Email Settings Transformer

---
+++ app/Http/Requests/User/Settings/EmailSettingsRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\User\Settings;
+
+use App\Http\Requests\Request;
+
+/**
+ * Email Settings Request
+ * 
+ * @author David A Conway Jr.
+ */
+class EmailSettingsRequest extends Request {
+    
+    protected $rules = [
+        'dealer_id' => 'required|integer|exists:dealer,dealer_id',
+        'sales_person_id' => 'integer|exists:sales_person,id'
+    ];
+    
+}
