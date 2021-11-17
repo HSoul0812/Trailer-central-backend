5e6f870261c0590245e4066cc21b0ec67933229c
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-17 16:32:46 -0600

    * FCLNP-26 Fixed Configuring Send Email Default

--- app/Services/CRM/Interactions/InteractionEmailServiceInterface.php
+++ app/Services/CRM/Interactions/InteractionEmailServiceInterface.php
@@ -3,20 +3,30 @@
 namespace App\Services\CRM\Interactions;
 
 use App\Services\CRM\Email\DTOs\SmtpConfig;
+use App\Services\CRM\User\DTOs\EmailSettings;
 use App\Services\Integration\Common\DTOs\ParsedEmail;
 use Illuminate\Support\Collection;
 
 interface InteractionEmailServiceInterface {
     /**
-     * Send Email With Params
+     * Get Email Config Settings
      * 
      * @param int $dealerId
-     * @param SmtpConfig $smtpConfig
+     * @param null|int $salesPersonId
+     * @return EmailSettings
+     */
+    public function config(int $dealerId, ?int $salesPersonId = null): EmailSettings;
+
+    /**
+     * Send Email With Params
+     * 
+     * @param EmailSettings $emailConfig
+     * @param null|SmtpConfig $smtpConfig
      * @param ParsedEmail $parsedEmail
      * @throws SendEmailFailedException
      * @return ParsedEmail
      */
-    public function send(int $dealerId, SmtpConfig $smtpConfig, ParsedEmail $parsedEmail): ParsedEmail;
+    public function send(EmailSettings $emailConfig, ?SmtpConfig $smtpConfig, ParsedEmail $parsedEmail): ParsedEmail;
 
     /**
      * Store Uploaded Attachments
