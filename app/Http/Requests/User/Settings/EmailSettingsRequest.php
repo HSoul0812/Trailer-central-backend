7b8760cda1272ee5834b62bfd7b962d7b0fd3859
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-17 23:21:52 -0600

    * FCLNP-26 Fixed Getting Email Settings From Config

--- app/Http/Requests/User/Settings/EmailSettingsRequest.php
+++ app/Http/Requests/User/Settings/EmailSettingsRequest.php
@@ -13,7 +13,7 @@ class EmailSettingsRequest extends Request {
     
     protected $rules = [
         'dealer_id' => 'required|integer|exists:dealer,dealer_id',
-        'sales_person_id' => 'integer|exists:sales_person,id'
+        'sales_person_id' => 'integer|sales_person_valid'
     ];
     
 }
