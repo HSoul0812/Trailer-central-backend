55fa005741e002ac646eda0a2690af4dbfba2fe8
Rober Díaz <rad8329@gmail.com>
2021-11-19 16:04:16 -0500

    #EC-93 Add migration script to create Magento refunds fields on `ecommerce_order_refunds`

---
+++ database/migrations/ecommerce/2021_11_17_145604_add_other_fees_to_ecommerce_order_refunds.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOtherFeesToEcommerceOrderRefunds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('ecommerce_order_refunds', function (Blueprint $table) {
+            $table->integer('textrail_id')
+                ->after('reason')
+                ->unsigned()
+                ->nullable()
+                ->unique()
+                ->comment('TexTrail memo/refund id');
+
+            $table->integer('textrail_rma')
+                ->after('textrail_id')
+                ->unsigned()
+                ->nullable()
+                ->unique()
+                ->comment('TexTrail used RMA for returns');
+
+            $table->decimal('parts_amount', 10)
+                ->unsigned()
+                ->after('total_amount')
+                ->default(0)
+                ->comment('total refunded amount for parts');
+
+            $table->decimal('shipping_amount', 10)
+                ->unsigned()
+                ->after('parts_amount')
+                ->default(0);
+
+            $table->decimal('handling_amount', 10)
+                ->unsigned()
+                ->after('shipping_amount')
+                ->default(0);
+
+            $table->decimal('adjustment_amount', 10)
+                ->unsigned()
+                ->after('handling_amount')
+                ->default(0)
+                ->comment('custom refund amount');
+
+            $table->decimal('tax_amount', 10)
+                ->unsigned()
+                ->after('adjustment_amount')
+                ->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('ecommerce_order_refunds', function (Blueprint $table) {
+            $table->dropColumn([
+                'textrail_id',
+                'textrail_rma',
+                'parts_amount',
+                'shipping_amount',
+                'handling_amount',
+                'adjustment_amount',
+                'tax_amount'
+            ]);
+        });
+    }
+}
