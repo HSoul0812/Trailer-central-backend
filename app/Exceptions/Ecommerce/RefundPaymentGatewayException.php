9480a29e354c0834c5e198b203a5882a492814cd
Rober Díaz <rad8329@gmail.com>
2021-11-19 17:43:46 -0500

    #EC-93 `RefundHttpClientException` was implemented to have a better exception handling of those kind errors

--- app/Exceptions/Ecommerce/RefundPaymentGatewayException.php
+++ app/Exceptions/Ecommerce/RefundPaymentGatewayException.php
@@ -4,7 +4,38 @@ declare(strict_types=1);
 
 namespace App\Exceptions\Ecommerce;
 
-class RefundPaymentGatewayException extends \Exception
+/**
+ * @property null|string $gatewayErrorCode the JSON deserialized body
+ */
+class RefundPaymentGatewayException extends RefundHttpClientException
 {
+    /** @var null|string */
+    protected $gatewayErrorCode;
 
+    /**
+     * Creates a new API error exception.
+     *
+     * @param string $message the exception message
+     * @param null|int $httpStatus the HTTP status code
+     * @param null|string $httpBody the HTTP body as a string
+     * @param null|array $jsonBody the JSON deserialized body
+     * @param null|array $httpHeaders the HTTP headers array
+     * @param null|string $gatewayErrorCode the Stripe error code
+     *
+     * @return static
+     */
+    public static function factory(
+        string  $message,
+        ?int    $httpStatus = null,
+        ?string $httpBody = null,
+        ?array  $jsonBody = null,
+        ?array  $httpHeaders = null,
+        ?string $gatewayErrorCode = null
+    ): RefundPaymentGatewayException
+    {
+        $instance = parent::factory($message, $httpBody, $httpStatus, $jsonBody, $httpHeaders);
+        $instance->gatewayErrorCode = $gatewayErrorCode;
+
+        return $instance;
+    }
 }
