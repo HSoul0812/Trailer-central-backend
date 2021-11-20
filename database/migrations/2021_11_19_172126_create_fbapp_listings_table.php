a93f6eebfef3230ffcbfa64ee65e03cb2fc27928
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-19 19:22:23 -0600

    * FME-27 Fixed Marketplace Listings Migration

--- database/migrations/2021_11_19_172126_create_fbapp_listings_table.php
+++ database/migrations/2021_11_19_172126_create_fbapp_listings_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Marketing\Facebook\Marketplace;
+use App\Models\Marketing\Facebook\Listings;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
