08351b7dc75498bdcd6afcef7f3c0e2c117607f1
Rober Díaz <rad8329@gmail.com>
2021-11-25 08:42:34 -0500

    #EC-93 Was removed an exception catch unused in `RefundController::cancelOrder`

--- app/Http/Controllers/v1/Ecommerce/RefundController.php
+++ app/Http/Controllers/v1/Ecommerce/RefundController.php
@@ -98,8 +98,6 @@ class RefundController extends RestfulControllerV2
                 $refund = $this->service->cancelOrder(RefundBag::fromTextrailOrderId($textrailOrderId));
 
                 return $this->createdResponse($refund->id);
-            } catch (RefundException $exception) {
-                throw new ResourceException('Validation Failed', $exception->getErrors(), $exception);
             } catch (\Throwable $exception) {
                 throw new HttpException($exception->getCode() > 0 ? $exception->getCode() : 500, $exception->getMessage(), $exception);
             }
