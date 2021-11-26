f8ac68e2c5a35b26de0ce208b2ba17358dc93e5a
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-26 15:50:49 -0600

    * FME-20 Fixed Marketplace Dispatch Service Instantiable

--- app/Providers/MarketingServiceProvider.php
+++ app/Providers/MarketingServiceProvider.php
@@ -8,24 +8,12 @@ use App\Repositories\Marketing\Facebook\FilterRepository;
 use App\Repositories\Marketing\Facebook\FilterRepositoryInterface;
 use App\Services\Marketing\Facebook\MarketplaceService;
 use App\Services\Marketing\Facebook\MarketplaceServiceInterface;
+use App\Services\Dispatch\Facebook\MarketplaceService as MarketplaceDispatchService;
+use App\Services\Dispatch\Facebook\MarketplaceServiceInterface as MarketplaceDispatchServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class MarketingServiceProvider extends ServiceProvider
 {
-    // Register any Validation Rules
-    public function boot()
-    {
-        // validation rules
-        \Validator::extend('bin_exists', 'App\Rules\Parts\BinExists@passes');
-        \Validator::extend('type_exists', 'App\Rules\Parts\TypeExists@passes');
-        \Validator::extend('category_exists', 'App\Rules\Parts\CategoryExists@passes');
-        \Validator::extend('brand_exists', 'App\Rules\Parts\BrandExists@passes');
-        \Validator::extend('part_exists', 'App\Rules\Parts\PartExists@passes');
-        \Validator::extend('cycle_count_exists', 'App\Rules\Parts\CycleCountExists@passes');
-        \Validator::extend('manufacturer_exists', 'App\Rules\Parts\ManufacturerExists@passes');
-        \Validator::extend('sku_type', 'App\Rules\Parts\SkuType@passes');
-    }
-
     /**
      * Register any application services.
      *
@@ -35,10 +23,14 @@ class MarketingServiceProvider extends ServiceProvider
     {
         // Integration Services
         $this->app->bind(MarketplaceServiceInterface::class, MarketplaceService::class);
-        
+
         // Integration Repositories
         $this->app->bind(MarketplaceRepositoryInterface::class, MarketplaceRepository::class);
         $this->app->bind(FilterRepositoryInterface::class, FilterRepository::class);
+
+        
+        // Dispatch Services
+        $this->app->bind(MarketplaceDispatchServiceInterface::class, MarketplaceDispatchService::class);
     }
 
 }
