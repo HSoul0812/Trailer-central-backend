bae6fade5a55e33e9bb036849d3e3eea4a37d30a
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-26 15:59:16 -0600

    * FME-20 Fixed Marketplace Login Request

--- app/Services/Dispatch/Facebook/MarketplaceServiceInterface.php
+++ app/Services/Dispatch/Facebook/MarketplaceServiceInterface.php
@@ -6,7 +6,10 @@ interface MarketplaceServiceInterface {
     /**
      * Login to Marketplace
      * 
+     * @param string $uuid
+     * @param string $ip
+     * @param string $version
      * @return string
      */
-    public function login(): string;
+    public function login(string $uuid, string $ip, string $version): string;
 }
