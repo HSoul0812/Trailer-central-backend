d6854a931827637a78d071a5aeb648dbc67a2035
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-11-29 20:09:33 -0600

    * FME-20 Fixed Validating Dispatch FB Access Token

--- app/Http/Middleware/Dispatch/FacebookValidate.php
+++ app/Http/Middleware/Dispatch/FacebookValidate.php
@@ -45,13 +45,8 @@ class FacebookValidate extends ValidRoute {
     public function handle($request, Closure $next)
     {
         $auth = Auth::user();
-        var_dump($auth);
-        die;
-        if ($request->header('access-token')) {
-            $accessToken = AuthToken::where('access_token', $request->header('access-token'))->first();
-            if ($accessToken && $accessToken->user->name === 'dispatch-facebook') {
-                return parent::handle($request, $next);
-            }
+        if (!empty($auth) && $auth->name === 'dispatch_facebook') {
+            return parent::handle($request, $next);
         }
 
         return response('Valid facebook dispatch token is required.', 403);
