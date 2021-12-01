dea05424ba581778dec3f48f98cd176b1695d131
Robert Diaz <rad8329@gmail.com>
2021-12-01 13:00:28 -0500

    #EC-120 `AfterRemoteRefundException` was renamed to `RefundFailureException` to be able using it in multiples places

--- app/Exceptions/Ecommerce/RefundFailureException.php
+++ app/Exceptions/Ecommerce/RefundFailureException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Exceptions\Ecommerce;
 
-class AfterRemoteRefundException extends \RuntimeException
+class RefundFailureException extends \RuntimeException
 {
     /** @var array */
     private $context = [];
