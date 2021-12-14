96733ee904b45037f303c665fd8046c32b08ea95
Robert Diaz <rad8329@gmail.com>
2021-12-14 12:47:20 -0500

    #DLFTT-29 `AbstractAverageRequest` allow to provide more periods

--- app/Nova/Http/Requests/InsightRequestInterface.php
+++ app/Nova/Http/Requests/InsightRequestInterface.php
@@ -9,6 +9,7 @@ interface InsightRequestInterface extends CardRequestInterface
     public const PERIOD_PER_DAY = 'per_day';
     public const PERIOD_PER_WEEK = 'per_week';
     public const PERIOD_PER_MONTH = 'per_month';
+    public const PERIOD_PER_QUARTER = 'per_quarter';
     public const PERIOD_PER_YEAR = 'per_year';
 
     public function getPeriod(): string;
