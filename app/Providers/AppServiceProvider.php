8e6ee7c2ef3b2b9f351946189daa47e2c33554ee
David A Conway Jr <david.a.conway.jr@gmail.com>
2021-12-22 19:22:07 -0600

    * DMM-190 Debug APP_LOG_QUERIES

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -253,6 +253,7 @@ class AppServiceProvider extends ServiceProvider
         ]);
 
         // log all queries
+        var_dump(env('APP_LOG_QUERIES'));
         if (env('APP_LOG_QUERIES')) {
             DB::listen(function($query) {
                 Log::info(
