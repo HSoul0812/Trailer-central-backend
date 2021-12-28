0990449e2eea69125d16be16e434075a68373e45
Mel <melvin@operatebeyond.com>
2021-12-28 07:01:54 +0000

    feat: added down method to migration

--- database/migrations/2021_12_05_184511_add_closed_quote_to_status_on_service_order_table.php
+++ database/migrations/2021_12_05_184511_add_closed_quote_to_status_on_service_order_table.php
@@ -26,7 +26,7 @@ class AddClosedQuoteToStatusOnServiceOrderTable extends Migration
     public function down()
     {
         Schema::table('dms_repair_order', function (Blueprint $table) {
-            //
+            DB::statement("ALTER TABLE dms_repair_order MODIFY COLUMN status ENUM('picked_up','ready_for_pickup','on_tech_clipboard','waiting_custom','waiting_parts','warranty_processing','quote','work_available')");     
         });
     }
 }
