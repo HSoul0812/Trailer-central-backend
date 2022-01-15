1f6dda77a5ee63a0c4f81ae6a30b01bd8385726f
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-01-14 20:39:32 -0600

    * FME-57 Debug KEYS With Tunnels Prefix

--- config/database.php
+++ config/database.php
@@ -123,7 +123,6 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
@@ -132,6 +131,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'cache' => [
