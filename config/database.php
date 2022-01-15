fbda7c9a9c8609e25ff3b94d24a0a9003c02a03d
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-01-14 20:34:53 -0600

    * FME-57 Fixed Getting Dealer Tunnels Correctly With Prefix

--- config/database.php
+++ config/database.php
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            //'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
@@ -154,7 +154,8 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1')
+            'database' => env('REDIS_CACHE_DB', '0'),,
+            'prefix' => env('REDIS_PREFIX_TUNNELS', 'tunnels:'),
         ],
     ],
 ];
