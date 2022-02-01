0beb2799ef076d5f2c1e63090e5e7eecc2d28208
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-01-31 23:58:07 -0600

    * MARK-16 Fixed Primary Image

--- app/Models/Marketing/Craigslist/ActivePost.php
+++ app/Models/Marketing/Craigslist/ActivePost.php
@@ -165,8 +165,7 @@ class ActivePost extends Model
     public function getCurrentImageAttribute(): string {
         // Return Primary Image Inventory
         if(!empty($this->inventory) && !empty($this->inventory->primary_image)) {
-            var_dump($this->inventory->primary_image);
-            return $this->inventory->primary_image->filename;
+            return $this->inventory->primary_image->image->filename;
         }
         return '';
     }
