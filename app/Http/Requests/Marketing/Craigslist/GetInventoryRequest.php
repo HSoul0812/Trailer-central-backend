11daccbe05484aaef741477ebe6b6bf66fe0538e
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-02-15 17:23:22 -0600

    * MARK-23 Updated Query to Handle All Needed Functionality

--- app/Http/Requests/Marketing/Craigslist/GetInventoryRequest.php
+++ app/Http/Requests/Marketing/Craigslist/GetInventoryRequest.php
@@ -9,16 +9,16 @@ class GetInventoryRequest extends Request {
 
     protected $rules = [
         'dealer_id' => 'required|integer',
-        'profile_id' => 'integer|valid_clapp_profile',
+        'profile_id' => 'required|integer|valid_clapp_profile',
         'slot_id' => 'integer',
         'per_page' => 'integer',
-        'sort' => 'in:title,-title,vin,-vin,manufacturer,-manufacturer,fp_balance,-fp_balance,fp_interest_paid,-fp_interest_paid,true_cost,-true_cost,fp_committed,-fp_committed,fp_vendor,-fp_vendor,status,-status,created_at,-created_at,updated_at,-updated_at,stock,-stock,category,-category,price,-price,sales_price,-sales_price',
+        'sort' => 'in:title,-title,stock,-stock,manufacturer,-manufacturer,category,-category,price,-price,status,-status,posted_at,-posted_at,scheduled_at,-scheduled_at,created_at,-created_at,updated_at,-updated_at',
         'search_term' => 'string',
+        'images_greater_than' => 'integer',
+        'images_less_than' => 'integer',
+        'units_with_true_Cost' => 'boolean',
         'dealer_location_id' => 'dealer_location_valid',
-        'status' => 'integer',
-        'inventory_ids' => 'array',
-        'inventory_ids.*' => 'integer',
-        'attribute_names' => 'array',
+        'status' => 'integer'
     ];
 
     public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null) {
--- app/Repositories/Marketing/Craigslist/InventoryRepository.php
+++ app/Repositories/Marketing/Craigslist/InventoryRepository.php
@@ -21,116 +21,92 @@ class InventoryRepository implements InventoryRepositoryInterface
 
 
     private $sortOrders = [
+        'inventory_id' => [
+            'field' => 'inventory.inventory_id',
+            'direction' => 'DESC'
+        ],
+        '-inventory_id' => [
+            'field' => 'inventory.inventory_id',
+            'direction' => 'ASC'
+        ],
         'title' => [
-            'field' => 'title',
+            'field' => 'inventory.title',
             'direction' => 'DESC'
         ],
         '-title' => [
-            'field' => 'title',
-            'direction' => 'ASC'
-        ],
-        'manufacturer' => [
-            'field' => 'manufacturer',
-            'direction' => 'DESC'
-        ],
-        '-manufacturer' => [
-            'field' => 'manufacturer',
-            'direction' => 'ASC'
-        ],
-        'vin' => [
-            'field' => 'vin',
-            'direction' => 'DESC'
-        ],
-        '-vin' => [
-            'field' => 'vin',
-            'direction' => 'ASC'
-        ],
-        'fp_balance' => [
-            'field' => 'fp_balance',
-            'direction' => 'DESC'
-        ],
-        '-fp_balance' => [
-            'field' => 'fp_balance',
-            'direction' => 'ASC'
-        ],
-        'fp_interest_paid' => [
-            'field' => 'fp_interest_paid',
-            'direction' => 'DESC'
-        ],
-        '-fp_interest_paid' => [
-            'field' => 'fp_interest_paid',
-            'direction' => 'ASC'
-        ],
-        'fp_committed' => [
-            'field' => 'fp_committed',
-            'direction' => 'DESC'
-        ],
-        '-fp_committed' => [
-            'field' => 'fp_committed',
-            'direction' => 'ASC'
-        ],
-        'fp_vendor' => [
-            'field' => 'fp_vendor',
-            'direction' => 'DESC'
-        ],
-        '-fp_vendor' => [
-            'field' => 'fp_vendor',
-            'direction' => 'ASC'
-        ],
-        'created_at' => [
-            'field' => 'created_at',
-            'direction' => 'DESC'
-        ],
-        '-created_at' => [
-            'field' => 'created_at',
-            'direction' => 'ASC'
-        ],
-        'updated_at' => [
-            'field' => 'updated_at',
-            'direction' => 'DESC'
-        ],
-        '-updated_at' => [
-            'field' => 'updated_at',
+            'field' => 'inventory.title',
             'direction' => 'ASC'
         ],
         'stock' => [
-            'field' => 'stock',
+            'field' => 'inventory.stock',
             'direction' => 'DESC'
         ],
         '-stock' => [
-            'field' => 'stock',
+            'field' => 'inventory.stock',
             'direction' => 'ASC'
         ],
         'category' => [
-            'field' => 'category',
+            'field' => 'inventory.category',
             'direction' => 'DESC'
         ],
         '-category' => [
-            'field' => 'category',
+            'field' => 'inventory.category',
+            'direction' => 'ASC'
+        ],
+        'manufacturer' => [
+            'field' => 'inventory.manufacturer',
+            'direction' => 'DESC'
+        ],
+        '-manufacturer' => [
+            'field' => 'inventory.manufacturer',
             'direction' => 'ASC'
         ],
         'price' => [
-            'field' => 'price',
+            'field' => 'inventory.price',
             'direction' => 'DESC'
         ],
         '-price' => [
-            'field' => 'price',
-            'direction' => 'ASC'
-        ],
-        'sales_price' => [
-            'field' => 'sales_price',
-            'direction' => 'DESC'
-        ],
-        '-sales_price' => [
-            'field' => 'sales_price',
+            'field' => 'inventory.price',
             'direction' => 'ASC'
         ],
         'status' => [
-            'field' => 'status',
+            'field' => 'clapp_posts.status',
             'direction' => 'DESC'
         ],
         '-status' => [
-            'field' => 'status',
+            'field' => 'clapp_posts.status',
+            'direction' => 'ASC'
+        ],
+        'posted_at' => [
+            'field' => 'clapp_posts.added',
+            'direction' => 'DESC'
+        ],
+        '-posted_at' => [
+            'field' => 'clapp_posts.added',
+            'direction' => 'ASC'
+        ],
+        'scheduled_at' => [
+            'field' => 'clapp_session.session_scheduled',
+            'direction' => 'DESC'
+        ],
+        '-scheduled_at' => [
+            'field' => 'clapp_session.session_scheduled',
+            'direction' => 'ASC'
+        ],
+        'created_at' => [
+            'field' => 'inventory.created_at',
+            'direction' => 'DESC'
+        ],
+        '-created_at' => [
+            'field' => 'inventory.created_at',
+            'direction' => 'ASC'
+        ],
+        'updated_at' => [
+            'field' => 'inventory.updated_at',
+            'direction' => 'DESC'
+        ],
+        '-updated_at' => [
+            'field' => 'inventory.updated_at',
             'direction' => 'ASC'
         ]
     ];
@@ -208,54 +184,40 @@ class InventoryRepository implements InventoryRepositoryInterface
         array $select = ['inventory.*']
     ) : GrimzyBuilder {
         /** @var Builder $query */
-        $query = Inventory::query()
-            ->select($select)
-            ->crossJoin(ActivePost::getTableName(), ActivePost::getTableName().'.inventory_id',
-                    '=', Inventory::getTableName().'.inventory_id')
-            ->crossJoin(Profile::getTableName(), Profile::getTableName().'.profile_id',
-                    '=', ActivePost::getTableName().'.profile_id')
-            ->where(function($query) use($params) {
-                $query->where(Inventory::getTableName().'.dealer_id', '=', $params['dealer_id'])
-                      ->orWhere(Profile::getTableName().'.dealer_id', '=', $params['dealer_id']);
-            });
+        $query = Inventory::query()->select($select)
+            ->crossJoin(Profile::getTableName())
+            ->leftJoin(Post::getTableName(), function($query) {
+                $query->where(Inventory::getTableName().'.inventory_id', '=', Post::getTableName().'.inventory_id')
+                      ->where(Profile::getTableName().'.profile_id', '=', Post::getTableName().'.profile_id');
+            })
+            ->leftJoin(ActivePost::getTableName(), function($query) {
+                $query->where(Inventory::getTableName().'.inventory_id', '=', ActivePost::getTableName().'.inventory_id')
+                      ->where(Profile::getTableName().'.profile_id', '=', ActivePost::getTableName().'.profile_id');
+            })
+            ->where(Inventory::getTableName().'.dealer_id', '=', $params['dealer_id'])
+            ->where(Profile::getTableName().'.profile_id', '=', $params['profile_id'])
+            /*->where(function ($query) {
+                $query->where(function ($query) {
+                    $query->where(Inventory::getTableName().'.is_archived', '=', 1)
+                          ->orWhere(Inventory::getTableName().'.show_on_website', '=', 0);
+                })->where();
+            })*/
+            ->with('orderedImages');
 
         if (isset($params['include']) && is_string($params['include'])) {
             $query = $query->with(explode(',', $params['include']));
-        }
-
-        $attributesEmpty = true;
-
-        if (isset($params['attribute_names'])) {
-            foreach ($params['attribute_names'] as $value) {
-                if (!empty($value)) {
-                    $attributesEmpty = false;
-                    break;
-                }
-            }
         }
 
         if ($withDefault) {
             $query->where('status', '<>', Inventory::STATUS_QUOTE);
         }
 
-        if (isset($params['status'])) {
-            $query = $query->where('status', $params['status']);
-        }
-
         if (isset($params['condition'])) {
             $query = $query->where('condition', $params['condition']);
         }
 
-        if (isset($params['dealer_id'])) {
-            $query = $query->where('inventory.dealer_id', $params['dealer_id']);
-        }
-
         if (isset($params['dealer_location_id'])) {
             $query = $query->where('inventory.dealer_location_id', $params['dealer_location_id']);
-        }
-
-        if (isset($params['inventory_ids']) && is_array($params['inventory_ids'])) {
-            $query = $query->whereIn('inventory.inventory_id', $params['inventory_ids']);
         }
 
         if (isset($params['is_archived'])) {
@@ -263,26 +225,12 @@ class InventoryRepository implements InventoryRepositoryInterface
             $query = $query->where('inventory.is_archived', $params['is_archived']);
         }
 
-        if ($withDefault) {
-            $query = $query->where(self::DEFAULT_GET_PARAMS[self::CONDITION_AND_WHERE]);
-        }
-
-        if (isset($params[self::CONDITION_AND_WHERE]) && is_array($params[self::CONDITION_AND_WHERE])) {
-            $query = $query->where($params[self::CONDITION_AND_WHERE]);
-        }
-
-        if (isset($params[self::CONDITION_AND_WHERE_IN]) && is_array($params[self::CONDITION_AND_WHERE_IN])) {
-            foreach ($params[self::CONDITION_AND_WHERE_IN] as $field => $values) {
-                $query = $query->whereIn($field, $values);
-            }
-        }
-
         if (isset($params['search_term'])) {
             $query = $query->where(function ($q) use ($params) {
-                $q->where('stock', 'LIKE', '%' . $params['search_term'] . '%')
-                  ->orWhere('title', 'LIKE', '%' . $params['search_term'] . '%')
-                  ->orWhere('inventory.description', 'LIKE', '%' . $params['search_term'] . '%')
-                  ->orWhere('vin', 'LIKE', '%' . $params['search_term'] . '%')
+                $q->where(Inventory::getTableName().'.stock', 'LIKE', '%' . $params['search_term'] . '%')
+                  ->orWhere(Inventory::getTableName().'.title', 'LIKE', '%' . $params['search_term'] . '%')
+                  ->orWhere(Inventory::getTableName().'.description', 'LIKE', '%' . $params['search_term'] . '%')
+                  ->orWhere(Inventory::getTableName().'.vin', 'LIKE', '%' . $params['search_term'] . '%')
                   ->orWhereHas('floorplanVendor', function ($query) use ($params) {
                     $query->where('name', 'LIKE', '%' . $params['search_term'] . '%');
                   });
@@ -290,12 +238,7 @@ class InventoryRepository implements InventoryRepositoryInterface
         }
 
         if (isset($params['sort'])) {
-            if ($params['sort'] === 'fp_vendor' || $params['sort'] === '-fp_vendor') {
-                $direction = $params['sort'] === 'fp_vendor' ? 'DESC' : 'ASC';
-                $query = $query->leftJoin('qb_vendors', 'qb_vendors.id', '=', 'inventory.fp_vendor')->orderBy('qb_vendors.name', $direction);
-            } else {
-                $query = $this->addSortQuery($query, $params['sort']);
-            }
+            $query = $this->addSortQuery($query, $params['sort']);
         }
 
         if (isset($params['images_greater_than']) || isset($params['images_less_than'])) {
@@ -332,162 +275,5 @@ class InventoryRepository implements InventoryRepositoryInterface
             $currentPage,
             ["path" => URL::to('/')."/api/inventory"]
         ))->appends($params);
-    }
-
-    /**
-     * @param array $newImages
-     * @return InventoryImage[]
-     */
-    private function createImages(array $newImages): array
-    {
-        $inventoryImageObjs = [];
-
-        foreach ($newImages as $newImage) {
-            $imageObj = new Image($newImage);
-            $imageObj->save();
-
-            $inventoryImageObj = new InventoryImage($newImage);
-            $inventoryImageObj->image_id = $imageObj->image_id;
-
-            $inventoryImageObjs[] = $inventoryImageObj;
-        }
-
-        return $inventoryImageObjs;
-    }
-
-    /**
-     * @param array $newFiles
-     * @return InventoryFile[]
-     */
-    private function createFiles(array $newFiles): array
-    {
-        $inventoryFilesObjs = [];
-
-        foreach ($newFiles as $newFile) {
-            $fileObj = new File($newFile);
-            $fileObj->save();
-
-            $inventoryFileObj = new InventoryFile($newFile);
-            $inventoryFileObj->file_id = $fileObj->id;
-
-            $inventoryFilesObjs[] = $inventoryFileObj;
-        }
-
-        return $inventoryFilesObjs;
-    }
-
-    /**
-     * @param array $attributes
-     * @return AttributeValue[]
-     */
-    private function createAttributes(array $attributes): array
-    {
-        $attributeObjs = [];
-
-        foreach ($attributes as $attribute) {
-            if (!is_array($attribute)) {
-                continue;
-            }
-            $attributeObjs[] = new AttributeValue($attribute);
-        }
-
-        return $attributeObjs;
-    }
-
-    /**
-     * @param array $features
-     * @return InventoryFeature[]
-     */
-    private function createFeatures(array $features): array
-    {
-        $featureObjs = [];
-
-        foreach ($features as $feature) {
-            $featureObjs[] = new InventoryFeature($feature);
-        }
-
-        return $featureObjs;
-    }
-
-    /**
-     * @param array $clapps
-     * @return InventoryClapp[]
-     */
-    private function createClapps(array $clapps): array
-    {
-        $clappObjs = [];
-
-        foreach (array_filter($clapps) as $field => $value) {
-            $clappObjs[] = new InventoryClapp(['field' => $field, 'value' => $value]);
-        }
-
-        return $clappObjs;
-    }
-
-    /**
-     * @param Inventory $item
-     * @param array $images
-     */
-    private function updateImages(Inventory $item, array $images)
-    {
-        foreach ($images as $existingImage) {
-            if (!isset($existingImage['image_id'])) {
-                continue;
-            }
-
-            $inventoryImageFields = with(new InventoryImage())->getFillable();
-            $inventoryImageParams = array_intersect_key($existingImage, array_combine($inventoryImageFields, array_fill(0, count($inventoryImageFields), 0)));
-
-            $item->inventoryImages()->where('image_id', '=', $existingImage['image_id'])->update($inventoryImageParams);
-        }
-    }
-
-    /**
-     * @param Inventory $item
-     * @param array $existingFiles
-     */
-    private function updateFiles(Inventory $item, array $existingFiles)
-    {
-        foreach ($existingFiles ?? [] as $existingFile) {
-            if (!isset($existingFile['file_id'])) {
-                continue;
-            }
-
-            $fileFields = with(new File())->getFillable();
-            $fileParams = array_intersect_key($existingFile, array_combine($fileFields, array_fill(0, count($fileFields), 0)));
-
-            $inventoryFileFields = with(new InventoryFile())->getFillable();
-            $inventoryFileParams = array_intersect_key($existingFile, array_combine($inventoryFileFields, array_fill(0, count($inventoryFileFields), 0)));
-
-            $item->files()->where('file.id', '=', $existingFile['file_id'])->update($fileParams);
-            $item->inventoryFiles()->where('file_id', '=', $existingFile['file_id'])->update($inventoryFileParams);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getAndIncrementTimesViewed(array $params): Inventory
-    {
-        $inventory = $this->get($params);
-        $inventory->times_viewed += 1;
-        $inventory->save();
-        return $inventory;
-    }
-
-    /**
-     * @param int $dealerId
-     *
-     * @return Collection
-     */
-    public function getTitles(int $dealerId): Collection
-    {
-        $params = [
-            'dealer_id' => $dealerId,
-        ];
-
-        $query = $this->buildInventoryQuery($params, false, ['inventory_id', 'title', 'vin']);
-
-        return $query->get();
     }
 }
