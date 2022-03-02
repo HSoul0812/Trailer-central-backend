dd421d0aff40e55f4d8a88931ba2fc9084c666c9
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-03-01 19:25:12 -0600

    * FME-70 Fixed Creating Collection of TfaType

--- app/Services/Marketing/Facebook/MarketplaceService.php
+++ app/Services/Marketing/Facebook/MarketplaceService.php
@@ -8,6 +8,7 @@ use App\Repositories\Marketing\Facebook\FilterRepositoryInterface;
 use App\Repositories\CRM\Text\NumberRepositoryInterface;
 use App\Repositories\User\DealerLocationRepositoryInterface;
 use App\Services\CRM\Text\TextServiceInterface;
+use App\Services\Marketing\Facebook\DTOs\TfaType;
 use Illuminate\Support\Collection;
 
 /**
