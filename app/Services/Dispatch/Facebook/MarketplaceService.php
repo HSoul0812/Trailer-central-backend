c6c4190c09e2f0988372b91765b75cec9aa9a363
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-03-17 18:12:49 -0500

    * MARK-39 Fixed Error on MarketplaceService

--- app/Services/Dispatch/Facebook/MarketplaceService.php
+++ app/Services/Dispatch/Facebook/MarketplaceService.php
@@ -6,6 +6,7 @@ use App\Models\User\AuthToken;
 use App\Models\User\Integration\Integration;
 use App\Models\Marketing\Facebook\Marketplace;
 use App\Models\Marketing\Facebook\Listings;
+use App\Models\Marketing\Facebook\Error;
 use App\Repositories\Marketing\TunnelRepositoryInterface;
 use App\Repositories\Marketing\Facebook\MarketplaceRepositoryInterface;
 use App\Repositories\Marketing\Facebook\ListingRepositoryInterface;
