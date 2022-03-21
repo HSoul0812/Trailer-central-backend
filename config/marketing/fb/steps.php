18b81fe8b3bd384109151dd6ae6a71ed3134632f
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-03-21 18:32:25 -0500

    * MARK-52 Updated Steps to Latest Extension Updates

--- config/marketing/fb/steps.php
+++ config/marketing/fb/steps.php
@@ -14,9 +14,14 @@ return [
     'list' => env('FB_MARKETING_STEPS', implode(",", [
         'start-script',
         'stop-script',
+        'verify-inventory',
         'login-fb',
+        'login-validate',
+        'goto-marketing',
         'goto-new-listing',
-        'start-posting'
+        'start-posting',
+        'get-listing-id',
+        'close-and-clear-cookies'
     ])),
 
 
@@ -24,9 +29,14 @@ return [
     'selectors' => [
         'start-script' => env('FB_MARKETING_STEP_SELECTORS_START', 'common'),
         'stop-script' => env('FB_MARKETING_STEP_SELECTORS_STOP', 'common'),
+        'verify-inventory' => env('FB_MARKETING_STEP_SELECTORS_INVENTORY', 'common'),
         'login-fb' => env('FB_MARKETING_STEP_SELECTORS_LOGIN', 'common,login'),
-        'goto-new-listing' => env('FB_MARKETING_STEP_SELECTORS_GOTO', 'common,listings'),
-        'start-posting' => env('FB_MARKETING_STEP_SELECTORS_POSTING', 'common,posting.vehicle')
+        'login-validate' => env('FB_MARKETING_STEP_SELECTORS_LOGIN_VALIDATE', 'common,login'),
+        'goto-marketing' => env('FB_MARKETING_STEP_SELECTORS_MARKETPLACE', 'common,listings'),
+        'goto-new-listing' => env('FB_MARKETING_STEP_SELECTORS_NEW_LISTING', 'common,listings'),
+        'start-posting' => env('FB_MARKETING_STEP_SELECTORS_POSTING', 'common,posting.vehicle'),
+        'get-listing-id' => env('FB_MARKETING_STEP_SELECTORS_LISTING_ID', 'common,listings'),
+        'close-and-clear-cookies' => env('FB_MARKETING_STEP_SELECTORS_CLOSE', 'common')
     ],
 
 
@@ -34,8 +44,13 @@ return [
     'logs' => [
         'start-script' => env('FB_MARKETING_STEP_LOG_START', 'Started Facebook Marketplace Extension script.'),
         'stop-script' => env('FB_MARKETING_STEP_LOG_STOP', 'Stopped Facebook Marketplace Extension script.'),
+        'verify-inventory' => env('FB_MARKETING_STEP_LOG_INVENTORY', 'Verify current dealer integration has inventory to post.'),
         'login-fb' => env('FB_MARKETING_STEP_LOG_LOGIN', 'Logging in to Facebook.'),
-        'goto-new-listing' => env('FB_MARKETING_STEP_LOG_GOTO', 'Preparing to create a new listing with the ID #:inventoryId.'),
-        'start-posting' => env('FB_MARKETING_STEP_LOG_POSTING', 'Starting posting to :action: a listing with the ID #:inventoryId.')
+        'login-validate' => env('FB_MARKETING_STEP_LOG_LOGIN_VALIDATE', 'Validating dealer account successfully logged in.'),
+        'goto-marketing' => env('FB_MARKETING_STEP_LOG_MARKETING', 'Validating we can posting to Facebook Marketplace.'),
+        'goto-new-listing' => env('FB_MARKETING_STEP_LOG_NEW_LISTING', 'Preparing to create a new listing with the ID #:inventoryId.'),
+        'start-posting' => env('FB_MARKETING_STEP_LOG_POSTING', 'Starting posting to :action a listing with the ID #:inventoryId.'),
+        'get-listing-id' => env('FB_MARKETING_STEP_LOG_LISTING_ID', 'Successfully :actiond a listing with the ID #:inventoryId, getting facebook listing id.'),
+        'close-and-clear-cookies' => env('FB_MARKETING_STEP_LOG_CLOSE', 'Finished posting all inventory for the current dealer integration, moving to next one.')
     ]
 ];
