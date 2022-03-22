0198b97b0a3df11ab27724be3b77d55945cfdd03
Carlos Loaiza <carlos@operatebeyond.com>
2022-03-22 14:51:24 -0500

    #NTGRTNS_104 fix query for update.

--- database/migrations/integrations/2022_03_14_123239_update_trailerworld_gooseneck_pull_type.php
+++ database/migrations/integrations/2022_03_14_123239_update_trailerworld_gooseneck_pull_type.php
@@ -42,19 +42,18 @@ SQL;
 
     private const QueryUpdateInv = <<<'SQL'
 UPDATE
-	inventory
-SET
-	category = 'gooseneck_bodies'
-WHERE
-	inventory_id in(
-		SELECT
-			i.inventory_id FROM inventory AS i
-			JOIN ( SELECT DISTINCT
-					TRIM(substring_index(substring_index(`data`, '"vin":"', -1), '","description"', 1)) AS `vin` FROM transaction_execute_queue
-				WHERE
-					`api` = 'trailerworld'
-					AND(`data` LIKE '%"hitch_type":"Gooseneck"%' AND `data` LIKE '%"category":"Gooseneck%')
-					AND queued_at > '2021-12-31') AS teq ON teq.vin = i.vin AND i.dealer_id = 11320);
+	inventory AS ii
+	JOIN ( SELECT DISTINCT
+			TRIM(substring_index(substring_index(`data`, '"vin":"', - 1), '","description"', 1)) AS `vin`
+		FROM
+			transaction_execute_queue
+		WHERE
+			`api` = 'trailerworld'
+			AND(`data` LIKE '%"hitch_type":"Gooseneck"%'
+				AND `data` LIKE '%"category":"Gooseneck%')
+			AND queued_at > '2021-12-31') AS teq ON ii.vin = teq.vin
+		AND ii.dealer_id = 11320
+SET ii.category = ii.category = 'gooseneck_bodies';
 SQL;
 
 
