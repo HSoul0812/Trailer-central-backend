92a6fd52c2341c89c41785a086f7bbffdf83c102
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-03-22 01:53:01 -0500

    * MARK-39 Fixed Handling Errors After Expiration

--- app/Repositories/Marketing/Facebook/ErrorRepository.php
+++ app/Repositories/Marketing/Facebook/ErrorRepository.php
@@ -7,6 +7,7 @@ use App\Exceptions\Marketing\Facebook\NoMarketplaceErrorToDismissException;
 use App\Models\Marketing\Facebook\Error;
 use App\Repositories\Traits\SortTrait;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
 
 class ErrorRepository implements ErrorRepositoryInterface {
     use SortTrait;
