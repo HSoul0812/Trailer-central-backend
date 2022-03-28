04e08ad58f3e911f88effdcc6839d832991c314f
Ethan Rivas <ethan@operatebeyond.com>
2022-03-28 16:04:42 -0600

    #NTGRTNS-78/79 Showroom transformer created

---
+++ app/Services/Showroom/ShowroomBulkUpdateVisibilityServiceInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Services\Showroom;
+
+interface ShowroomBulkUpdateVisibilityServiceInterface {
+    //put your code here
+}
---
+++ app/Services/Showroom/ShowroomBulkUpdateYearService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services\Showroom;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+use App\Repositories\Showroom\ShowroomBulkUpdateRepository;
+
+class ShowroomBulkUpdateYearService implements ShowroomBulkUpdateYearServiceInterface
+{
+
+    /**
+     * @var array
+     */
+    private $params;
+
+    /**
+     * @var ShowroomBulkUpdateRepository
+     */
+    private $manufacturerRepository;
+
+    /**
+     * @param array $params
+     */
+    public function __construct(array $params) {
+        $this->params = $params;
+        $this->manufacturerRepository = new ShowroomBulkUpdateRepository();
+    }
+
+    /**
+     * Updates Showrooms year
+     *
+     * @throws Exception
+     */
+    public function update()
+    {
+        try {
+            $manufacturers = $this->manufacturerRepository->get([
+                'manufacturer' => $this->params['manufacturer'],
+                'year' => $this->params['from']
+            ]);
+
+            foreach ($manufacturers as $key => $manufacturer) {
+                $this->manufacturerRepository->bulkUpdate(
+                    $this->params['manufacturer'],
+                    [
+                        'year' => $this->params['to']
+                    ]
+                );
+            }
+
+            Log::info('Manufacturer years updated successfully', $this->params);
+        } catch (Exception $e) {
+            Log::error('Manufacturer year update error. Message - ' . $e->getMessage(), $e->getTrace());
+
+            throw $e;
+        }
+    }
+
+}
