0ac0fe711c340e4e3e13ea000988345f784ba37b
HuascarOna <huascar@operatebeyond.com>
2022-03-28 18:14:08 -0500

    #sentry removed testing route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     redirect('/api');
 });
-
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
