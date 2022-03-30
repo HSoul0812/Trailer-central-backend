c3a24e19d839fd1cf3c7b4fc00f4ab6a53412bd6
Carlos Loaiza <carlos@operatebeyond.com>
2022-03-30 10:01:47 -0500

    #NTGRTNS-201 Update Status for Double J Trailers older units

---
+++ database/migrations/integrations/2022_03_30_103329_update_double_j_trailers_archived_units.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Inventory\Inventory;
+use Illuminate\Support\Facades\DB;
+
+class UpdateDoubleJTrailersArchivedUnits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        DB::transaction(function () {
+            Inventory::where('dealer_id', 1099)
+                ->where('archived_at', '<', '2021')
+                ->update(['status' => inventory::STATUS_SOLD]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+
+    }
+}
