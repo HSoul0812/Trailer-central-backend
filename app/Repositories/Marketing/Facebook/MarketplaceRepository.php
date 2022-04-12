c1c941ddba22e12115dcf09b4e0b890fbfdccbc9
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-04-11 20:20:57 -0500

    * MARK-71 Fixed Limiting Marketplace Entries By Imported At Range

--- app/Repositories/Marketing/Facebook/MarketplaceRepository.php
+++ app/Repositories/Marketing/Facebook/MarketplaceRepository.php
@@ -144,7 +144,7 @@ class MarketplaceRepository implements MarketplaceRepositoryInterface {
         // Import Range Provided
         if (!empty($params['import_range'])) {
             $query = $query->where(Marketplace::getTableName() . '.imported_at', '>',
-                                    DB::raw('DATE_SUB(NOW(), INTERVAL ' . $params['import_range'] . ' HOURS)'));
+                                    DB::raw('DATE_SUB(NOW(), INTERVAL ' . $params['import_range'] . ' HOUR)'));
         }
 
         // Skip Integrations With Non-Expired Errors
