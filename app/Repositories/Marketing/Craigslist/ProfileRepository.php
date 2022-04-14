db28513374c72671c7dfb004c634848da04c5bdb
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-04-14 12:04:06 -0500

    * MARK-76 Fixed Returning Only By Dealer Profiles on Scheduler

--- app/Repositories/Marketing/Craigslist/ProfileRepository.php
+++ app/Repositories/Marketing/Craigslist/ProfileRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Marketing\Craigslist;
 
 use App\Exceptions\NotImplementedException;
 use App\Models\Marketing\Craigslist\Session;
+use App\Models\Marketing\Craigslist\Category;
 use App\Repositories\Traits\SortTrait;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
