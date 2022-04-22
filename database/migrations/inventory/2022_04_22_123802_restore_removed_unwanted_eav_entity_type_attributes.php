a2c026e494028195b153bac129cebaf10431950a
Norris Oduro <norris@operatebeyond.com>
2022-04-22 16:47:54 +0000

    Merged in hotfix/CDW-468 (pull request #148)
    CDW-468: Extra Attributes: Within the Attributes section for certain Inventory Types there are additional fields
    
    * Restore deleted eav_attributes
    
    
    Approved-by: Mert Oksuz

---
+++ database/migrations/inventory/2022_04_22_123802_restore_removed_unwanted_eav_entity_type_attributes.php
@@ -0,0 +1,78 @@
+<?php
+
+use App\Models\Inventory\Attribute;
+
+use App\Models\Inventory\EntityType;
+
+use App\Models\Inventory\EntityTypeAttribute;
+
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Support\Facades\DB;
+
+class RestoreRemovedUnwantedEavEntityTypeAttributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('eav_entity_type_attribute')->insert([
+            [
+                'entity_type_id' => EntityType::where('name', 'rv')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'livingquarters')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'shortwall_length')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'sleeping_capacity')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        EntityTypeAttribute::where([
+            'entity_type_id' => EntityType::where('name', 'rv')->value('entity_type_id'),
+            'attribute_id' => Attribute::where('code', 'livingquarters')->value('attribute_id')
+        ])->delete();
+
+        EntityTypeAttribute::where([
+            'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
+            'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+        ])->delete();
+
+        EntityTypeAttribute::where([
+            'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
+            'attribute_id' => Attribute::where('code', 'shortwall_length')->value('attribute_id')
+        ])->delete();
+
+        EntityTypeAttribute::where([
+            'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
+            'attribute_id' => Attribute::where('code', 'sleeping_capacity')->value('attribute_id')
+        ])->delete();
+
+        EntityTypeAttribute::where([
+            'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
+            'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+        ])->delete();
+    }
+}
