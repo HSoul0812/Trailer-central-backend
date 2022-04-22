42fa094c575f20831925e53876c396e3a1403ca8
Norris Oduro <norrisjibril@gmail.com>
2022-04-22 13:13:56 +0000

    Simplify query

--- database/migrations/inventory/2022_04_22_085359_restore_removed_unwanted_eav_entity_type_attributes.php
+++ database/migrations/inventory/2022_04_22_085359_restore_removed_unwanted_eav_entity_type_attributes.php
@@ -16,28 +16,26 @@ class RestoreRemovedUnwantedEavEntityTypeAttributes extends Migration
     public function up()
     {
         DB::table('eav_entity_type_attribute')->insert([
-            'entity_type_id' => EntityType::where('name', 'rv')->value('entity_type_id'),
-            'attribute_id' => Attribute::where('code', 'livingquarters')->value('attribute_id')
-        ]);
-
-        DB::table('eav_entity_type_attribute')->insert([
-            'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
-            'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
-        ]);
-
-        DB::table('eav_entity_type_attribute')->insert([
-            'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
-            'attribute_id' => Attribute::where('code', 'shortwall_length')->value('attribute_id')
-        ]);
-
-        DB::table('eav_entity_type_attribute')->insert([
-            'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
-            'attribute_id' => Attribute::where('code', 'sleeping_capacity')->value('attribute_id')
-        ]);
-
-        DB::table('eav_entity_type_attribute')->insert([
-            'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
-            'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+            [
+                'entity_type_id' => EntityType::where('name', 'rv')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'livingquarters')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'horsetrailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'shortwall_length')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'sleeping_capacity')->value('attribute_id')
+            ],
+            [
+                'entity_type_id' => EntityType::where('name', 'trailer')->value('entity_type_id'),
+                'attribute_id' => Attribute::where('code', 'slideouts')->value('attribute_id')
+            ]
         ]);
     }
 
