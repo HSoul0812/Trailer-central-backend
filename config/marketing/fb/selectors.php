27b248dfbd820de9cec92b2429ef34f780ffd71f
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-04-23 01:23:47 -0500

    * MARK-84 Added Two Factor Auth Detection

--- config/marketing/fb/selectors.php
+++ config/marketing/fb/selectors.php
@@ -15,7 +15,8 @@ return [
         'accountMenu' => env('FB_MARKETING_SELECTORS_COMMON_ACCOUNT', '*[role=navigation] *[aria-label="Account"]'),
         'logout' => env('FB_MARKETING_SELECTORS_COMMON_LOGOUT', '*[aria-label="Account"][role=dialog] > div > div > div > div > div > div > div > :nth-child(5) div[data-visualcompletion] > *[role=button]'),
         'detectAccountDisabledText' => env('FB_MARKETING_SELECTORS_DETECT_ACCOUNT_DISABLED', 'div > span:contains(\'Your account has been disabled\')'),
-        'detectRequestAReviewButton' => env('FB_MARKETING_SELECTORS_DETECT_REQUEST_A_REVIEW_BUTTON', 'div[aria-label="Request a Review"]')
+        'detectRequestAReviewButton' => env('FB_MARKETING_SELECTORS_DETECT_REQUEST_A_REVIEW_BUTTON', 'div[aria-label="Request a Review"]'),
+        'detectTempBlock' => env('FB_MARKETING_SELECTORS_DETECT_BLOCK', 'div > h3:contains("You\'re Temporarily Blocked")')
     ],
 
     // Login Selectors
@@ -29,7 +30,6 @@ return [
 
     // Login Validator
     'loginValidate' => [
-        'detectTempBlock' => env('FB_MARKETING_SELECTORS_VALIDATE_DETECT_BLOCK', 'div > h3:contains("You\'re Temporarily Blocked")'),
         'detectTwoFactor' => env('FB_MARKETING_SELECTORS_VALIDATE_DETECT_TWO_FACTOR', 'div > strong:contains(\'Two-Factor Authentication Required\')'),
         'detectMissingMobileNumber' => env('FB_MARKETING_SELECTORS_VALIDATE_DETECT_MOBILE', '.mobileMirrorHeading:contains("Add a Mobile Number")'),
         'detectLoginValidate' => env('FB_MARKETING_SELECTORS_VALIDATE_DETECT_APPROVAL', 'div > strong:contains(\'Login approval needed\')'),
