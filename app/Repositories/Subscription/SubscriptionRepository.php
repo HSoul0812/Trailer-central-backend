6b37b59d40407c3b42cfbd777df8cd67427dae00
Ethan Rivas <ethan@operatebeyond.com>
2022-05-02 16:25:40 -0500

    #NTGRTNS-225 Subscriptions Repository and Interface

---
+++ app/Repositories/Subscription/SubscriptionRepository.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Repositories\Subscription;
+
+use App\Services\Subscription\StripeService;
+use Dingo\Api\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+class SubscriptionRepository implements SubscriptionRepositoryInterface {
+
+    /**
+     * @var Auth $user
+     */
+    private $user;
+
+    /**
+     * @var StripeService $service
+     */
+    private $service;
+
+    /**
+     * Create a new SubscriptionRepository instance.
+     *
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+        $this->service = new StripeService($this->user);
+    }
+
+    /**
+     * Retrieves all subscriptions from a given user
+     *
+     * @param array $params
+     * @return mixed
+     */
+    public function getAll($params) {
+        return $this->service->getSubscriptions()->data;
+    }
+
+    /**
+     * Retrieves a customer with subscriptions and card information
+     *
+     * @return mixed
+     */
+    public function getCustomer() {
+        return $this->service->getCustomer();
+    }
+
+    /**
+     * @param $params
+     * @return void
+     */
+    public function create($params)
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * @param $params
+     * @return void
+     */
+    public function update($params)
+    {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * @param $params
+     * @return void
+     */
+    public function get($params)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @param $params
+     * @return void
+     */
+    public function delete($params)
+    {
+        // TODO: Implement delete() method.
+    }
+}
