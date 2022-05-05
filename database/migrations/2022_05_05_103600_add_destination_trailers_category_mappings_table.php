9da74d369614223a1d426fbccf03354d326eb837
zhao songxun <songxunzhao1991@gmail.com>
2022-05-05 10:56:38 -0500

    #TR-405: import types

--- database/migrations/2022_05_05_103600_add_destination_trailers_category_mappings_table.php
+++ database/migrations/2022_05_05_103600_add_destination_trailers_category_mappings_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Parts\CategoryMappings;
+use App\Models\Parts\Type;
 
 class AddDestinationTrailersCategoryMappingsTable extends Migration
 {
