848bf6820696f088561208898221023a8c75c8ac
zhao songxun <songxunzhao1991@gmail.com>
2022-05-06 06:28:52 -0500

    #TR-390: fix sysconfig unit tests

--- tests/Unit/App/Repositories/SysConfig/SysConfigRepositoryTest.php
+++ tests/Unit/App/Repositories/SysConfig/SysConfigRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\App\Repositories\SysConfig;
 
 use App\Repositories\SysConfig\SysConfigRepository;
 use App\Repositories\SysConfig\SysConfigRepositoryInterface;
+use Database\Seeders\SysConfig\BannerSeeder;
 use Database\Seeders\SysConfig\FilterSeeder;
 use Tests\Common\TestCase;
 
@@ -13,17 +14,21 @@ class SysConfigRepositoryTest extends TestCase
         $repository = $this->getConcreteRepository();
         $filters = $repository->getAll(['key' => 'filter/size/']);
         self::assertEquals($filters->count(), 6);
+
+        $banners = $repository->getAll(['key' => 'banner/']);
+        self::assertEquals($banners->count(), 14);
     }
 
     public function testGetAllWithoutParams() {
         $repository = $this->getConcreteRepository();
-        $filters = $repository->getAll([]);
-        self::assertEquals($filters->count(),34);
+        $configs = $repository->getAll([]);
+        self::assertEquals($configs->count(),48);
     }
     public function setUp(): void
     {
         parent::setUp();
         $this->seed(FilterSeeder::class);
+        $this->seed(BannerSeeder::class);
     }
 
     private function getConcreteRepository(): SysConfigRepository {
