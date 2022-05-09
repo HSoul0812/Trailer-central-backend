d2b3f391aa171fb1afd77457d2104bf4a6839d68
Ethan Rivas <ethan@operatebeyond.com>
2022-05-09 08:50:47 -0500

    #NTGRTNS-230 Update customer transformer to include more data

--- app/Transformers/Subscription/CustomerTransformer.php
+++ app/Transformers/Subscription/CustomerTransformer.php
@@ -10,8 +10,12 @@ class CustomerTransformer extends TransformerAbstract
     {
         return [
             'id' => $params->id,
+            'name' => $params->name,
+            'email' => $params->email,
+            'phone' => $params->phone,
             'subscriptions' => $params->subscriptions->data ?? [],
-            'card' => $params->sources->data ?? []
+            'card' => $params->sources->data ?? [],
+            'transactions' => $params->transactions ?? []
         ];
     }
 }
