d3e1bd837f530de9b5529fc57ac7a05ca3cf5b15
zhao songxun <songxunzhao1991@gmail.com>
2022-05-09 21:21:32 -0500

    #TR-390: replace images

--- database/seeders/SysConfig/BannerSeeder.php
+++ database/seeders/SysConfig/BannerSeeder.php
@@ -8,20 +8,22 @@ use Illuminate\Support\Facades\DB;
 class BannerSeeder extends Seeder
 {
     const BANNER_CONFIGS = [
-        ['banner/lamar/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/lamar/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/carry-on/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/carry-on/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/haulmark/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/haulmark/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/wells cargo/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/wells cargo/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/american hauler/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/american hauler/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/big tex trailers/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/big tex trailers/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/pj trailers/desktop', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
-        ['banner/pj trailers/mobile', 'https://s3.amazonaws.com/crm-trailercentral-dev/new-cargo-enclosed'],
+        ['banner/lamar/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-LamarBanners-v1_Desktop-20220505-113050.png'],
+        ['banner/lamar/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-LamarBanners-v1_Mobile-20220505-113050.png'],
+        ['banner/carry-on/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-CarryOnBanners-v1_Desktop-20220505-113414.png'],
+        ['banner/carry-on/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-CarryOnBanners-v1_Mobile-20220505-113414.png'],
+        ['banner/haulmark/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-HaulmarkBanners-v1_Desktop-20220505-115252.png'],
+        ['banner/haulmark/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-HaulmarkBanners-v1_Mobile-20220505-115252.png'],
+        ['banner/wells cargo/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-WellsCargoBanners-v1_Desktop-20220505-133430.png'],
+        ['banner/wells cargo/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-WellsCargoBanners-v1_Mobile-20220505-133430.png'],
+        ['banner/american hauler/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-AmericanHaulerBanners-v1_Desktop-20220505-123526.png'],
+        ['banner/american hauler/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-AmericanHaulerBanners-v1_Mobile-20220505-123526.png'],
+        ['banner/big tex trailers/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-BigTexBanners-v1_Desktop-20220505-124600.png'],
+        ['banner/big tex trailers/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-BigTexBanners-v1_Mobile-20220505-124600.png'],
+        ['banner/pj trailers/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-PJBanners-v1_Desktop-20220505-125942.png'],
+        ['banner/pj trailers/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-PJBanners-v1_Mobile-20220505-125942.png'],
+        ['banner/norstar/desktop', 'https://trailertrader.s3.amazonaws.com/banners/TC-NorstarBanners-v1_Desktop-20220505-132316.png'],
+        ['banner/norstar/mobile', 'https://trailertrader.s3.amazonaws.com/banners/TC-NorstarBanners-v1_Mobile-20220505-132316.png'],
     ];
 
     /**
