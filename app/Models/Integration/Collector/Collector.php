25503e0b87bfcf8091639660928d60a4844d06a4
Ethan Rivas <ethan@operatebeyond.com>
2022-05-13 14:05:25 -0500

    #NTGRTNS-247 Add last_run column to collector table

--- app/Models/Integration/Collector/Collector.php
+++ app/Models/Integration/Collector/Collector.php
@@ -180,7 +180,13 @@ class Collector extends Model implements Filterable
         'api_max_records',
         'api_pagination',
         'ignore_manually_added_units',
-        'is_bdv_enabled'
+        'is_bdv_enabled',
+        'last_run',
+        'run_errors'
+    ];
+
+    protected $casts = [
+        'last_run' => 'datetime',
     ];
 
     public function dealers(): BelongsTo
