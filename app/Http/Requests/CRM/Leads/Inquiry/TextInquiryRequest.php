5a9ff3d603b667d63bc4b5f612f48ed4dd0b6c29
Norris Oduro <norrisjibril@gmail.com>
2022-05-20 17:23:34 +0000

    CDW-593: Add banned lead texts

--- app/Http/Requests/CRM/Leads/Inquiry/TextInquiryRequest.php
+++ app/Http/Requests/CRM/Leads/Inquiry/TextInquiryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\CRM\Leads\Inquiry;
 
 use App\Http\Requests\Request;
+use App\Rules\CRM\Leads\BannedLeadTextsRule;
 
 class TextInquiryRequest extends Request
 {
@@ -14,7 +15,7 @@ class TextInquiryRequest extends Request
             'dealer_location_id' => 'nullable|dealer_location_valid',
             'inventory_id' => 'nullable|exists:inventory,inventory_id,dealer_id,' . $this->dealer_id,
             'phone_number' => 'required|min:10',
-            'sms_message' => 'required',
+            'sms_message' => ['required', new BannedLeadTextsRule()],
             'customer_name' => 'required',
             'inventory_name' => 'nullable|string',
             'cookie_session_id' => 'nullable|string',
