d3903ef05c5556d83cc5a1370842d223f4583d0d
Shailesh B <shailesh@operatebeyond.com>
2022-05-20 13:42:37 +0530

    Make Registration pcnt tax box editable

---
+++ database/migrations/2022_05_20_040438_add_new_column_to_dealer_location_sales_tax_item_v2.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnToDealerLocationSalesTaxItemV2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement(<<<SQL
+            ALTER TABLE `dealer_location_sales_tax_item_v2` ADD `registration_title`
+                text
+                NULL
+                AFTER `settngs`;
+            SQL
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dealer_location_sales_tax_item_v2', function (Blueprint $table) {
+            $table->dropColumn('registration_title');
+        });
+    }
+}
