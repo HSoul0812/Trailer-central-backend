991691d0f85f4a34321ee02565ea9c33137e5786
Shailesh B <shailesh@operatebeyond.com>
2022-05-24 20:13:24 +0530

    DMSS-615 Create New Column on dealer_location_sales_tax_item table

--- database/migrations/2022_05_20_040438_add_new_column_to_dealer_location_sales_tax_item.php
+++ database/migrations/2022_05_20_040438_add_new_column_to_dealer_location_sales_tax_item.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNewColumnToDealerLocationSalesTaxItemV2 extends Migration
+class AddNewColumnToDealerLocationSalesTaxItem extends Migration
 {
     /**
      * Run the migrations.
