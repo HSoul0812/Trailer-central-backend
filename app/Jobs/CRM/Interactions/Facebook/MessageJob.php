537a9984084ff4efec556a385fe8b5cdaafa9df4
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-05-31 17:57:21 -0500

    * DMSS-638 Fixed MessageJob Logs

--- app/Jobs/CRM/Interactions/Facebook/MessageJob.php
+++ app/Jobs/CRM/Interactions/Facebook/MessageJob.php
@@ -6,6 +6,7 @@ use App\Jobs\Job;
 use App\Models\Integration\Auth\AccessToken;
 use App\Services\CRM\Interactions\Facebook\MessageServiceInterface;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Log;
 
 class MessageJob extends Job
 {
