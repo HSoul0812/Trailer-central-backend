55757e49cb96aca1a83e651accb79a210849a7be
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-05-31 19:47:23 -0500

    * MARJ-108 Ran Tests on Inventory ID

--- app/Repositories/Marketing/Facebook/ListingRepository.php
+++ app/Repositories/Marketing/Facebook/ListingRepository.php
@@ -52,9 +52,9 @@ class ListingRepository implements ListingRepositoryInterface {
         if(!empty($params['facebook_id'])) {
             $listing = Listings::where('facebook_id', $params['facebook_id'])->first();
             if(!empty($listing->id)) {
-                if((int) $listing->inventory_id !== (int) $params['inventory_id']) {
+                if($listing->inventory_id === (int) $params['inventory_id']) {
                     return $this->update($params);
-                } elseif((int) $listing->inventory_id === (int) $params['inventory_id']) {
+                } elseif($listing->inventory_id !== (int) $params['inventory_id']) {
                     $params['facebook_id'] = 0;
                 }
             }
