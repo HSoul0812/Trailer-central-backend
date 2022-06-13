b3ca61aee640ddc7250995497fb4dcf6be5c4410
Ethan Rivas <ethan@operatebeyond.com>
2022-06-13 11:30:51 -0500

    #NTGRTNS-281 Inventory bulk upload model and payload results

---
+++ app/Models/Bulk/Inventory/BulkUpload.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\Bulk\Inventory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BulkUpload extends Model {
+
+    const VALIDATION_ERROR = 'validation_error';
+    const PROCESSING = 'processing';
+    const COMPLETE = 'complete';
+    const STATUS_FAILED = 'failed';
+    const EXCEPTION_ERROR = 'exception_error';
+
+    protected $table = 'inventory_bulk_upload';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'dealer_id',
+        'status',
+        'import_source',
+        'validation_errors',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    public function getValidationErrors()
+    {
+        if (empty($this->validation_errors)) {
+            return null;
+        }
+
+        return json_decode($this->validation_errors);
+    }
+}
