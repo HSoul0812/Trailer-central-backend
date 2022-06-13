b8800465fcaac118727d85b4d5b881a5c040de0f
Ethan Rivas <ethan@operatebeyond.com>
2022-06-13 11:25:53 -0500

    #NTGRTNS-281 Inventory bulk upload repository and interface

---
+++ app/Repositories/Bulk/Inventory/BulkUploadRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repositories\Bulk\Inventory;
+
+use App\Repositories\Bulk\Inventory\BulkUploadRepositoryInterface;
+use App\Exceptions\NotImplementedException;
+use App\Models\Bulk\Inventory\BulkUpload;
+use App\Jobs\Bulk\Inventory\ProcessBulkUpload;
+use Illuminate\Support\Facades\Storage;
+
+class BulkUploadRepository implements BulkUploadRepositoryInterface {
+
+    public function create($params) {
+        $csvKey = $this->storeCsv($params['csv_file']);
+
+        $params['status'] = BulkUpload::PROCESSING;
+        $params['import_source'] = $csvKey;
+
+        $bulkUpload = BulkUpload::create($params);
+        dispatch((new ProcessBulkUpload($bulkUpload))->onQueue('inventory'));
+
+        return $bulkUpload;
+    }
+
+    public function delete($params) {
+        throw new NotImplementedException;
+    }
+
+    public function get($params) {
+        return BulkUpload::where(array_key_first($params), current($params))->first();
+    }
+
+    public function getAll($params) {
+
+        if (!isset($params['per_page'])) {
+            $params['per_page'] = 100;
+        }
+
+        return BulkUpload::where('dealer_id', $params['dealer_id'])->paginate($params['per_page'])->appends($params);
+    }
+
+    public function update($params) {
+        $bulkUpload = BulkUpload::findOrFail($params['id']);
+        $bulkUpload->fill($params);
+        return $bulkUpload->save();
+    }
+
+    /**
+     * Stores CSV on S3 and returns its URL
+     *
+     * @param InputFile $file
+     * @return string
+     */
+    private function storeCsv($file) {
+        $fileKey = Storage::disk('s3')->putFile(uniqid().'/'.$file->getClientOriginalName(), $file, 'public');
+        return $fileKey;
+    }
+}
