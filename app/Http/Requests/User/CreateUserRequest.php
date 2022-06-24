9d13fc8cc84466b58a0e2021261ef6a45c8bcdc3
songxunzhao <zhao@operatebeyond.com>
2022-06-24 10:33:02 -0500

    #TR-227: add clsf_active validation rule

--- app/Http/Requests/User/CreateUserRequest.php
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -2,13 +2,18 @@
 namespace App\Http\Requests\User;
 
 use App\Http\Requests\Request;
+use Illuminate\Validation\Rule;
 
 class CreateUserRequest extends Request
 {
-    protected $rules = [
-        'name' => 'required',
-        'email' => 'required|email|unique:App\Models\User\User,email',
-        'password' => 'required|alpha_num|min:12',
-        'from' => 'in:trailercentral,trailertrader'
-    ];
+    protected function getRules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:App\Models\User\User,email',
+            'password' => 'required|alpha_num|min:12',
+            'from' => 'in:trailercentral,trailertrader',
+            'clsf_active' => ['required', Rule::in([0, 1])],
+        ];
+    }
 }
