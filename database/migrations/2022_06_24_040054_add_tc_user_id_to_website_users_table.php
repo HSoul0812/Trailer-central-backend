679bf28acce3460ac605fe58e42f40400d19c4ad
zhao songxun <songxunzhao1991@gmail.com>
2022-06-29 10:33:53 -0500

    #TR-474: Fix migration

---
+++ database/migrations/2022_06_24_040054_add_tc_user_id_to_website_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTcUserIdToWebsiteUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasColumn('website_users', 'tc_user_id')) {
+            Schema::table('website_users', function (Blueprint $table) {
+                $table->unsignedInteger('tc_user_id')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('website_users', function (Blueprint $table) {
+            $table->dropColumn('tc_user_id');
+        });
+    }
+}
