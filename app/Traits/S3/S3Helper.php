61ebcb2483b3587921189da900d2bdb9bcbfd86f
Mert Öksüz <xrefresh@gmail.com>
2022-07-08 18:40:59 +0300

    #CDW-681 use new env for cdn url

--- app/Traits/S3/S3Helper.php
+++ app/Traits/S3/S3Helper.php
@@ -13,6 +13,10 @@ trait S3Helper
      */
     protected function getS3BaseUrl(): string
     {
+        if (!empty(env('CDN_URL_STORAGE'))) {
+            return env('CDN_URL_STORAGE');
+        }
+
         $urlMetadata = parse_url(env('AWS_URL'));
 
         return $urlMetadata['scheme'] . '://' . $urlMetadata['host'] . '/'.env('AWS_BUCKET');
