f27f8b61348c372bf24e490773d77bd64324bc07
zhao songxun <songxunzhao1991@gmail.com>
2022-07-08 09:07:14 -0500

    #TR-227: remove rules

--- app/Http/Requests/WebsiteUser/RegisterUserRequest.php
+++ app/Http/Requests/WebsiteUser/RegisterUserRequest.php
@@ -8,19 +8,6 @@ use Illuminate\Validation\Rules\Password;
 
 class RegisterUserRequest extends Request implements CreateRequestInterface
 {
-    protected array $rules = [
-        'first_name' => 'required|string',
-        'last_name' => 'required|string',
-        'address' => 'nullable|string',
-        'zipcode' => 'nullable|string',
-        'city' => 'nullable|string',
-        'state' => 'nullable|string',
-        'email' => 'email|required|unique:App\Models\WebsiteUser\WebsiteUser,email',
-        'phone_number' => 'nullable|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-        'mobile_number' => 'nullable|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-        'password' => 'required|alpha_num|min:8',
-    ];
-
     protected function getRules(): array
     {
         return [
