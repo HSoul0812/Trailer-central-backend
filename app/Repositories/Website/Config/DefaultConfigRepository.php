778ee5b83798c644e5ab9440f01ea83b5296f819
Rober Diaz <rad8329@gmail.com>
2022-07-11 17:34:23 -0500

    #CDW-656 Default website variables are being sorted by `sor_order`

--- app/Repositories/Website/Config/DefaultConfigRepository.php
+++ app/Repositories/Website/Config/DefaultConfigRepository.php
@@ -2,12 +2,22 @@
 
 namespace App\Repositories\Website\Config;
 
-use App\Repositories\Website\Config\DefaultConfigRepositoryInterface;
 use App\Exceptions\NotImplementedException;
 use App\Models\Website\Config\WebsiteConfigDefault;
 
 class DefaultConfigRepository implements DefaultConfigRepositoryInterface {
-    
+
+    private $sortOrders = [
+        'sort_order' => [
+            'field' => 'sort_order',
+            'direction' => 'DESC'
+        ],
+        '-sort_order' => [
+            'field' => 'sort_order',
+            'direction' => 'ASC'
+        ]
+    ];
+
     public function create($params) {
         throw new NotImplementedException;
     }
@@ -22,17 +32,23 @@ class DefaultConfigRepository implements DefaultConfigRepositoryInterface {
 
     /**
      * Get All Default Website Config
-     * 
+     *
      * @param array $params
      * @return Collection<WebsiteConfigDefault>
      */
     public function getAll($params) {
         $query = WebsiteConfigDefault::select('*');
-                
+
+        if (!isset($params['sort'])) {
+            $params['sort'] = '-sort_order';
+        }
+
         if (isset($params['key'])) {
             $query = $query->where('key', $params['key']);
         }
-        
+
+        $query->orderBy($this->sortOrders[$params['sort']]['field'], $this->sortOrders[$params['sort']]['direction']);
+
         return $query->get();
     }
 
