5ddd35a2e5fb85b70e2d1e20412139a6df9bc0cf
Rober Diaz <rad8329@gmail.com>
2022-07-12 10:54:40 -0500

    #CDW-656 `inventory/filters/enable_filters` custom available values source was implemented

--- app/Repositories/Website/Config/AvailableValues/EnabledFiltersRepository.php
+++ app/Repositories/Website/Config/AvailableValues/EnabledFiltersRepository.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace App\Repositories\Website\Config\AvailableValues;
 
+use Illuminate\Database\Connection;
+use Illuminate\Support\Collection;
+
 class EnabledFiltersRepository implements AvailableValuesRepositoryInterface
 {
-    public function pull(int $websiteId): string
+    /** @var Connection */
+    private $db;
+
+    public function __construct(Connection $db)
     {
-        return 'hello world '.$websiteId;
+        $this->db = $db;
+    }
+
+    public function pull(int $websiteId): Collection
+    {
+        return $this->db->table('inventory_filter')
+            ->select('attribute')
+            ->where('attribute', '!=', 'dealer')
+            ->get()
+            ->pluck('attribute');
     }
 }
