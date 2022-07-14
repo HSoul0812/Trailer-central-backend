389cc5b80c57ca9d7210b383854b6b96996febdd
zhao songxun <songxunzhao1991@gmail.com>
2022-07-13 20:34:04 -0500

    #TR-385: change website user password.

--- app/Notifications/WebsiteUserPasswordReset.php
+++ app/Notifications/WebsiteUserPasswordReset.php
@@ -38,8 +38,12 @@ class WebsiteUserPasswordReset extends Notification
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+            ->subject(Lang::get("TrailerTrader | Password Recovery"))
+            ->line(Lang::get('We have received a request to reset your password on TrailerTrader.com.'))
+            ->line(Lang::get('If you have not initiated this process, please erase this email.
+            You can continue to access TrailerTrader.com with the email and password
+            that you have initially set.'))
+            ->line(Lang::get('If you have forgotten your password,click on the following link to create a new password: '))
             ->action(Lang::get('Reset Password'), $url)
             ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
             ->line(Lang::get('If you did not request a password reset, no further action is required.'));
