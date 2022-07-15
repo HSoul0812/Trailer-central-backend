d4fb0e6ec67fa6e02f6b09e81b74c109cbb84adb
Mert Öksüz <xrefresh@gmail.com>
2022-07-15 22:51:37 +0300

    #CDW-734 hide type for ecommerce filters

--- app/Repositories/Website/Parts/FilterRepository.php
+++ app/Repositories/Website/Parts/FilterRepository.php
@@ -7,11 +7,14 @@ use App\Exceptions\NotImplementedException;
 use App\Models\Parts\Filter;
 
 /**
- *  
+ *
  * @author Eczek
  */
 class FilterRepository implements FilterRepositoryInterface {
 
+    const EXCLUDED_ATTRIBUTES = [
+        'type',
+    ];
 
     public function create($params) {
         throw new NotImplementedException;
@@ -27,6 +30,10 @@ class FilterRepository implements FilterRepositoryInterface {
 
     public function getAll($params) {
         return Filter::where('is_visible', 1)->orderBy('position', 'asc')->get();
+    }
+
+    public function getAllEcomm() {
+        return Filter::where('is_visible', 1)->whereNotIn('attribute', self::EXCLUDED_ATTRIBUTES)->orderBy('position')->get();
     }
 
     public function update($params) {
