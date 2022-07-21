964030af1701ad04ceb042a7f58734909b932bb6
Mert Öksüz <xrefresh@gmail.com>
2022-07-21 15:59:16 +0300

    #CDW-689 missing put route

--- routes/api.php
+++ routes/api.php
@@ -336,6 +336,7 @@ $api->version('v1', function ($route) {
     $route->put('website/{websiteId}/website-config', 'App\Http\Controllers\v1\Website\Config\WebsiteConfigController@createOrUpdate')->where('websiteId', '[0-9]+');
 
     $route->get('website/{websiteId}/extra-website-config', 'App\Http\Controllers\v1\Website\Config\ExtraWebsiteConfigController@index');
+    $route->put('website/{websiteId}/extra-website-config', 'App\Http\Controllers\v1\Website\Config\ExtraWebsiteConfigController@createOrUpdate');
 
     $route->get('website/{websiteId}/call-to-action', 'App\Http\Controllers\v1\Website\Config\CallToActionController@index');
     $route->put('website/{websiteId}/call-to-action', 'App\Http\Controllers\v1\Website\Config\CallToActionController@createOrUpdate')->where('websiteId', '[0-9]+');
