3e778859e5796009470f375cf511ea620939a91c
Ioan Bercea <ioan@trailercentral.com>
2022-07-29 10:58:46 +0000

    refined FB Marketplace Accounts index and detail

--- database/migrations/dealer/2022_07_21_050000_update_dealer_fbm_overview_fix.php
+++ database/migrations/dealer/2022_07_21_050000_update_dealer_fbm_overview_fix.php
@@ -46,17 +46,19 @@ class UpdateDealerFbmOverviewFix extends Migration
         return "
         CREATE VIEW `trailercentral`.`fbme_listings` AS 
 
-        SELECT i.stock, fbl.marketplace_id, fbl.facebook_id,
-        (
-                SELECT COUNT(1)+1
-                FROM fbapp_listings AS x
-                WHERE 
-                    x.marketplace_id = fbl.marketplace_id 
-                    and x.id > fbl.id
-        ) AS rank
+        SELECT 
+            inv.stock AS SKU, 
+            fbl.marketplace_id AS integration_id, 
+            fbl.created_at AS listed_at, 
+            fbl.facebook_id AS facebook_listing_id,
+            (
+                    SELECT COUNT(1)+1
+                    FROM fbapp_listings AS x
+                    WHERE x.marketplace_id = fbl.marketplace_id AND fbl.id > x.id
+            ) AS rank
         FROM fbapp_listings AS fbl 
-        LEFT JOIN inventory AS i ON fbl.inventory_id=i.inventory_id
-        ORDER BY fbl.id DESC;";
+        LEFT JOIN inventory AS inv ON fbl.inventory_id=inv.inventory_id
+        WHERE fbl.created_at > DATE_SUB(now(), INTERVAL 1 WEEK)";
     }
 
     private function createView2(): string
@@ -78,15 +80,30 @@ class UpdateDealerFbmOverviewFix extends Migration
                 (IFNULL((SELECT created_at FROM fbapp_listings WHERE marketplace_id = fbm.id ORDER BY id DESC LIMIT 1),'1000-01-01 00:00:00')) >
                 (IFNULL((SELECT created_at FROM fbapp_errors WHERE marketplace_id = fbm.id ORDER BY id DESC LIMIT 1),'1000-01-01 00:00:00'))
             ) AS last_run_status,
-            GROUP_CONCAT(stock SEPARATOR ' | ') AS units_posted,
-            IFNULL((SELECT CONCAT(step, ' - ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id ORDER BY id DESC LIMIT 1), 'no error') AS last_error
+            GROUP_CONCAT(fbme_l1.SKU SEPARATOR ' | ') AS units_posted_today,
+            GROUP_CONCAT(fbme_l2.SKU SEPARATOR ' | ') AS units_posted_1dayago,
+            GROUP_CONCAT(fbme_l3.SKU SEPARATOR ' | ') AS units_posted_2dayago,
+            GROUP_CONCAT(fbme_l4.SKU SEPARATOR ' | ') AS units_posted_3dayago,
+            GROUP_CONCAT(fbme_l5.SKU SEPARATOR ' | ') AS units_posted_4dayago,
+            GROUP_CONCAT(fbme_l6.SKU SEPARATOR ' | ') AS units_posted_5dayago,
+            IFNULL((SELECT CONCAT(step, ': ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id AND created_at=CURDATE() ORDER BY id DESC LIMIT 1), 'no error') AS error_today,
+            IFNULL((SELECT CONCAT(step, ': ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id AND created_at=DATE_SUB(curdate(), INTERVAL 1 DAY) ORDER BY id DESC LIMIT 1), 'no error') AS error_1dayago,
+            IFNULL((SELECT CONCAT(step, ': ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id AND created_at=DATE_SUB(curdate(), INTERVAL 2 DAY) ORDER BY id DESC LIMIT 1), 'no error') AS error_2dayago,
+            IFNULL((SELECT CONCAT(step, ': ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id AND created_at=DATE_SUB(curdate(), INTERVAL 3 DAY) ORDER BY id DESC LIMIT 1), 'no error') AS error_3dayago,
+            IFNULL((SELECT CONCAT(step, ': ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id AND created_at=DATE_SUB(curdate(), INTERVAL 4 DAY) ORDER BY id DESC LIMIT 1), 'no error') AS error_4dayago,
+            IFNULL((SELECT CONCAT(step, ': ', error_message) FROM fbapp_errors WHERE marketplace_id=fbm.id AND created_at=DATE_SUB(curdate(), INTERVAL 5 DAY) ORDER BY id DESC LIMIT 1), 'no error') AS error_5dayago
 
         FROM dealer AS d
         INNER JOIN fbapp_marketplace AS fbm ON fbm.dealer_id=d.dealer_id
-        LEFT JOIN fbme_listings ON fbme_listings.marketplace_id=fbm.id AND fbme_listings.rank<7
+        LEFT JOIN fbme_listings AS fbme_l1 ON fbme_l1.integration_id=fbm.id AND DATE(fbme_l1.listed_at) = CURDATE()
+        LEFT JOIN fbme_listings AS fbme_l2 ON fbme_l2.integration_id=fbm.id AND DATE(fbme_l2.listed_at) = DATE_SUB(curdate(), INTERVAL 1 DAY)
+        LEFT JOIN fbme_listings AS fbme_l3 ON fbme_l3.integration_id=fbm.id AND DATE(fbme_l3.listed_at) = DATE_SUB(curdate(), INTERVAL 2 DAY)
+        LEFT JOIN fbme_listings AS fbme_l4 ON fbme_l4.integration_id=fbm.id AND DATE(fbme_l4.listed_at) = DATE_SUB(curdate(), INTERVAL 3 DAY)
+        LEFT JOIN fbme_listings AS fbme_l5 ON fbme_l5.integration_id=fbm.id AND DATE(fbme_l5.listed_at) = DATE_SUB(curdate(), INTERVAL 4 DAY)
+        LEFT JOIN fbme_listings AS fbme_l6 ON fbme_l6.integration_id=fbm.id AND DATE(fbme_l6.listed_at) = DATE_SUB(curdate(), INTERVAL 5 DAY)
 
-        GROUP BY d.dealer_id, d.name, fbm.fb_username
+        GROUP BY d.dealer_id, fbm.id
 
-        ORDER BY d.dealer_id DESC";
+        ORDER BY d.dealer_id, fbm.id DESC";
     }
 }
