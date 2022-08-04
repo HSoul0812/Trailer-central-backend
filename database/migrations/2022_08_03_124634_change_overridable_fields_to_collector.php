317203f14a63f3996f7a7210ca8e698befb1fdf0
Ethan Rivas <ethan@operatebeyond.com>
2022-08-03 22:05:25 -0500

    #NTGRTNS-295 Overridable fields change data type

---
+++ database/migrations/2022_08_03_124634_change_overridable_fields_to_collector.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeOverridableFieldsToCollector extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('collector', function (Blueprint $table) {
+            $table->text('overridable_fields')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('collector', function (Blueprint $table) {
+            $table->string('overridable_fields', 254)->change();
+        });
+    }
+}
