9a9399831c18b998761f80010d23e49aa555331d
dayalae <diego@trailercentral.com>
2022-08-09 11:32:59 -0500

    Email Blast tests and repository updates.

--- app/Repositories/CRM/Email/BlastRepository.php
+++ app/Repositories/CRM/Email/BlastRepository.php
@@ -2,11 +2,10 @@
 
 namespace App\Repositories\CRM\Email;
 
-use App\Exceptions\NotImplementedException;
 use App\Models\CRM\Leads\Lead;
 use App\Models\CRM\Email\Blast;
 use App\Models\CRM\Email\BlastSent;
-use App\Repositories\CRM\Email\BlastRepositoryInterface;
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
@@ -14,22 +13,35 @@ use Carbon\Carbon;
 
 class BlastRepository implements BlastRepositoryInterface
 {
+    private $blastModel;
+    private $blastSentModel;
+    private $leadModel;
+
+    public function __construct(Blast $blast, BlastSent $blastSent, Lead $lead)
+    {
+        $this->blastModel = $blast;
+        $this->blastSentModel = $blastSent;
+        $this->leadModel = $lead;
+    }
+
     public function create($params)
     {
-        return Blast::create($params);
+        return $this->blastModel::create($params);
     }
 
     public function delete($params)
     {
-        return Blast::destroy($params['id']);
+        return $this->blastModel::destroy($params['id']);
     }
 
-    public function get($params) {
-        return Blast::findOrFail($params['id']);
+    public function get($params)
+    {
+        return $this->blastModel::findOrFail($params['id']);
     }
 
-    public function getAll($params) {
-        $query = Blast::with('template')->with('brands')->with('categories');
+    public function getAll($params)
+    {
+        $query = $this->blastModel::with('template')->with('brands')->with('categories');
 
         if (!isset($params['per_page'])) {
             $params['per_page'] = 20;
@@ -48,7 +60,7 @@ class BlastRepository implements BlastRepositoryInterface
         }
 
         if (isset($params['send_date'])) {
-            if($params['send_date'] === 'due_now') {
+            if ($params['send_date'] === 'due_now') {
                 $query = $query->where('send_date', '<', Carbon::now()->toDateTimeString());
             } else {
                 $query = $query->where('send_date', '<', $params['send_date']);
@@ -77,10 +89,10 @@ class BlastRepository implements BlastRepositoryInterface
             $blast->fill($params)->save();
 
             DB::commit();
-        } catch (\Exception $ex) {
+        } catch (Exception $ex) {
             DB::rollBack();
-            Log::error('Text blast update error. Message - ' . $ex->getMessage() , $ex->getTrace());
-            throw new \Exception('Text blast update error');
+            Log::error('Text blast update error. Message - ' . $ex->getMessage(), $ex->getTrace());
+            throw new Exception('Text blast update error');
         }
 
         return $blast;
@@ -92,9 +104,10 @@ class BlastRepository implements BlastRepositoryInterface
      * @param int $userId
      * @return Collection of Blast
      */
-    public function getAllActive(int $userId): Collection {
-        return Blast::where('user_id', $userId)->where('delivered', 0)
-                    ->where(function(Builder $query) {
+    public function getAllActive(int $userId): Collection
+    {
+        return $this->blastModel::where('user_id', $userId)->where('delivered', 0)
+                    ->where(function (Builder $query) {
                         $query->where('cancelled', 0)
                               ->orWhereNull('cancelled');
                     })->where('email_template_id', '<>', 0)
@@ -108,13 +121,14 @@ class BlastRepository implements BlastRepositoryInterface
      * @param int $blastId
      * @param int $leadId
      * @param null|string $messageId = null
-     * @throws \Exception
+     * @throws Exception
      * @return BlastSent
      */
-    public function sent(int $blastId, int $leadId, ?string $messageId = null): BlastSent {
+    public function sent(int $blastId, int $leadId, ?string $messageId = null): BlastSent
+    {
         // Get Sent?
         $sent = $this->getSent($blastId, $leadId);
-        if(!empty($sent->email_blasts_id)) {
+        if (!empty($sent->email_blasts_id)) {
             return $sent;
         }
 
@@ -122,16 +136,16 @@ class BlastRepository implements BlastRepositoryInterface
 
         try {
             // Create Blast Sent
-            $sent = BlastSent::create([
+            $sent = $this->blastSentModel::create([
                 'email_blasts_id' => $blastId,
                 'lead_id' => $leadId,
                 'message_id' => $messageId ?? ''
             ]);
 
             DB::commit();
-        } catch (\Exception $ex) {
+        } catch (Exception $ex) {
             DB::rollBack();
-            throw new \Exception($ex->getMessage());
+            throw new Exception($ex->getMessage());
         }
 
         return $sent;
@@ -143,14 +157,16 @@ class BlastRepository implements BlastRepositoryInterface
      * @param int $blastId
      * @param int $leadId
      * @param string|null $messageId
-     * @throws \Exception
+     * @param int $emailHistoryId
      * @return BlastSent
+     * @throws Exception
      */
-    public function updateSent(int $blastId, int $leadId, ?string $messageId, int $emailHistoryId): BlastSent {
+    public function updateSent(int $blastId, int $leadId, ?string $messageId, int $emailHistoryId): BlastSent
+    {
         // Get Blast Sent Entry
-        $sent = BlastSent::where('email_blasts_id', $blastId)->where('lead_id', $leadId)->first();
+        $sent = $this->blastSentModel::where('email_blasts_id', $blastId)->where('lead_id', $leadId)->first();
 
-        if(empty($sent->email_blasts_id)) {
+        if (empty($sent->email_blasts_id)) {
             return $this->sent($blastId, $leadId, $messageId);
         }
 
@@ -176,12 +192,13 @@ class BlastRepository implements BlastRepositoryInterface
      * @param string $email
      * @return bool
      */
-    public function wasSent(int $blastId, string $email): bool {
+    public function wasSent(int $blastId, string $email): bool
+    {
         // Get Blast Sent Entry
-        $sent = BlastSent::select(BlastSent::getTableName().'.*')
-                         ->join(Lead::getTableName(), Lead::getTableName().'.identifier', '=', BlastSent::getTableName().'.lead_id')
-                         ->where(BlastSent::getTableName() . '.email_blasts_id', $blastId)
-                         ->where(Lead::getTableName() . '.email_address', $email)->first();
+        $sent = $this->blastSentModel::select($this->blastSentModel::getTableName().'.*')
+                         ->join($this->leadModel::getTableName(), $this->leadModel::getTableName().'.identifier', '=', $this->blastSentModel::getTableName().'.lead_id')
+                         ->where($this->blastSentModel::getTableName() . '.email_blasts_id', $blastId)
+                         ->where($this->leadModel::getTableName() . '.email_address', $email)->first();
 
         // Was Blast Sent?
         return !empty($sent->email_blasts_id);
@@ -194,9 +211,10 @@ class BlastRepository implements BlastRepositoryInterface
      * @param int $leadId
      * @return null|BlastSent
      */
-    public function getSent(int $blastId, int $leadId): ?BlastSent {
+    public function getSent(int $blastId, int $leadId): ?BlastSent
+    {
         // Get Blast Sent Entry
-        return BlastSent::where('email_blasts_id', $blastId)->where('lead_id', $leadId)->first();
+        return $this->blastSentModel::where('email_blasts_id', $blastId)->where('lead_id', $leadId)->first();
     }
 
     /**
@@ -206,7 +224,8 @@ class BlastRepository implements BlastRepositoryInterface
      * @param int $leadId
      * @return bool
      */
-    public function wasLeadSent(int $blastId, int $leadId): bool {
+    public function wasLeadSent(int $blastId, int $leadId): bool
+    {
         // Get Blast Sent Entry
         $sent = $this->getSent($blastId, $leadId);
 
--- tests/Integration/Http/Controllers/CRM/Email/BlastControllerTest.php
+++ tests/Integration/Http/Controllers/CRM/Email/BlastControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Integration\Http\Controllers\CRM\Email;
 
 use App\Models\CRM\Email\Blast;
+use App\Transformers\CRM\Email\BlastTransformer;
 use Illuminate\Support\Carbon;
 use Tests\Integration\IntegrationTestCase;
 use Laravel\Lumen\Testing\DatabaseTransactions;
@@ -16,11 +17,10 @@ use App\Models\User\NewDealerUser;
  *
  * @coversDefaultClass \App\Http\Controllers\v1\CRM\Email\BlastController
  */
-
 class BlastControllerTest extends IntegrationTestCase
 {
     use DatabaseTransactions;
-    
+
     /**
      * @var BlastSeeder
      */
@@ -30,6 +30,78 @@ class BlastControllerTest extends IntegrationTestCase
      * @var string
      */
     private $accessToken;
+
+    /**
+     * @var string
+     */
+    private const API_ENDPOINT = '/api/user/emailbuilder/blast';
+
+    /**
+     * @var array
+     */
+    private const SINGLE_JSON_STRUCTURE = [
+        'data' => [
+            'id',
+            'template_id',
+            'location_id',
+            'location',
+            'send_after_days',
+            'action',
+            'unit_category',
+            'campaign_name',
+            'user_id',
+            'from_email_address',
+            'campaign_subject',
+            'include_archived',
+            'send_date',
+            'delivered',
+            'cancelled',
+            'categories',
+            'brands',
+            'total_sent',
+            'factory_campaign_id',
+            'approved'
+        ]
+    ];
+
+    /**
+     * @var array
+     */
+    private const PAGINATED_JSON_STRUCTURE = [
+        'data' => [
+            '*' => [
+                'id',
+                'template_id',
+                'location_id',
+                'location',
+                'send_after_days',
+                'action',
+                'unit_category',
+                'campaign_name',
+                'user_id',
+                'from_email_address',
+                'campaign_subject',
+                'include_archived',
+                'send_date',
+                'delivered',
+                'cancelled',
+                'categories',
+                'brands',
+                'total_sent',
+                'factory_campaign_id',
+                'approved'
+            ]
+        ],
+        'meta' => [
+            'pagination' => [
+                'total',
+                'count',
+                'per_page',
+                'current_page',
+                'total_pages'
+            ]
+        ]
+    ];
 
     public function setUp(): void
     {
@@ -46,7 +118,6 @@ class BlastControllerTest extends IntegrationTestCase
             'salt' => md5((string)$this->seeder->user->user_id),
             'auto_import_hide' => 0,
             'auto_msrp' => 0
-
         ]);
         $this->seeder->dealer->newDealerUser()->save($newDealerUser);
     }
@@ -60,118 +131,181 @@ class BlastControllerTest extends IntegrationTestCase
         parent::tearDown();
     }
 
+    /**
+     * @group CRM
+     * @covers ::index
+     */
     public function testIndex()
     {
+        // Get the data from the API
         $response = $this->json(
             'GET',
-            '/api/user/emailbuilder/blast',
+            self::API_ENDPOINT,
             [],
             ['access-token' => $this->accessToken]
         );
 
+        // Check that we have a valid response from the API
         $response->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    '*' => [
-                        'id',
-                        'template_id',
-                        'location_id',
-                        'location',
-                        'send_after_days',
-                        'action',
-                        'unit_category',
-                        'campaign_name',
-                        'user_id',
-                        'from_email_address',
-                        'campaign_subject',
-                        'include_archived',
-                        'send_date',
-                        'delivered',
-                        'cancelled',
-                        'categories',
-                        'brands',
-                        'total_sent',
-                        'factory_campaign_id',
-                        'approved'
-                    ]
-                ],
-                'meta' => [
-                    'pagination' => [
-                        'total',
-                        'count',
-                        'per_page',
-                        'current_page',
-                        'total_pages'
-                    ]
-                ]
-            ]);
+            ->assertJsonStructure(self::PAGINATED_JSON_STRUCTURE);
 
+        // Is the data in the expected format?
         $expectedData = [];
         foreach ($this->seeder->createdBlasts as $blast) {
-            $expectedData[] = [
-                'id' => (int)$blast->email_blasts_id,
-                'template_id' => (int)$blast->email_template_id,
-                'location_id' => $blast->location_id,
-                'location' => $blast->location,
-                'send_after_days' => (int)$blast->send_after_days,
-                'action' => strtoupper($blast->action),
-                'unit_category' => $blast->unit_category,
-                'categories' => $blast->categories->toArray(),
-                'brands' => $blast->brands->toArray(),
-                'campaign_name' => $blast->campaign_name,
-                'user_id' => (int)$blast->user_id,
-                'from_email_address' => $blast->from_email_address,
-                'campaign_subject' => $blast->campaign_subject,
-                'include_archived' => $blast->include_archived,
-                'send_date' => $blast->send_date,
-                'delivered' => $blast->delivered,
-                'cancelled' => $blast->cancelled,
-                'total_sent' => $blast->sents()->count(),
-                'factory_campaign_id' => $blast->factory ? $blast->factory->id : null,
-                'approved' => $blast->factory ? $blast->factory->is_approved : true,
-            ];
+            $expectedData[] = $this->expectedDataFormat($blast);
         }
         $this->assertResponseDataEquals($response, $expectedData, false);
     }
 
+    /**
+     * @group CRM
+     * @covers ::show
+     */
     public function testShow()
     {
+        // Get a blast from the seeder
         $blast = $this->seeder->createdBlasts[0];
 
+        // Get the data from the API endpoint
         $response = $this->json(
             'GET',
-            '/api/user/emailbuilder/blast/' . $blast->email_blasts_id,
+            self::API_ENDPOINT . '/' . $blast->email_blasts_id,
             [],
             ['access-token' => $this->accessToken]
         );
 
+        // Check that we have a valid response from the API
         $response->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    'id',
-                    'template_id',
-                    'location_id',
-                    'location',
-                    'send_after_days',
-                    'action',
-                    'unit_category',
-                    'campaign_name',
-                    'user_id',
-                    'from_email_address',
-                    'campaign_subject',
-                    'include_archived',
-                    'send_date',
-                    'delivered',
-                    'cancelled',
-                    'categories',
-                    'brands',
-                    'total_sent',
-                    'factory_campaign_id',
-                    'approved'
-                ]
-            ]);
+            ->assertJsonStructure(self::SINGLE_JSON_STRUCTURE);
 
-        $expectedData = [
+        // Is the data in the expected format?
+        $expectedData = $this->expectedDataFormat($blast);
+        $this->assertResponseDataEquals($response, $expectedData, false);
+    }
+
+    /**
+     * @group CRM
+     * @covers ::create
+     */
+    public function testCreate()
+    {
+        // Grab a template from the seeder
+        $template = $this->seeder->template;
+
+        // Initialize the required data for the blast
+        $rawBlast = [
+            'email_template_id' => $template->id,
+            'campaign_name' => 'Test Campaign',
+            'send_date' => Carbon::tomorrow()->format('Y-m-d H:i:s'),
+            'action' => Blast::ACTION_UNCONTACTED,
+            'send_after_days' => '3',
+        ];
+
+        // PUT the data into the API
+        $response = $this->json(
+            'PUT',
+            self::API_ENDPOINT,
+            $rawBlast,
+            ['access-token' => $this->accessToken]
+        );
+
+        // Check if we have a valid response from the API
+        $response->assertStatus(200)
+            ->assertJsonStructure(self::SINGLE_JSON_STRUCTURE);
+
+        // Check that the data was assigned correctly
+        $this->assertSame(
+            $this->seeder->dealer->newDealerUser->user_id,
+            $response['data']['user_id'],
+            "The user doesn't match"
+        );
+        $this->assertSame($template->id, $response['data']['template_id'], "The template id doesn't match");
+
+        // Corroborate the record was inserted in the database
+        $this->assertDatabaseHas(Blast::getTableName(), [
+            'email_blasts_id' => $response['data']['id']
+        ]);
+    }
+
+    /**
+     * @group CRM
+     * @covers ::update
+     */
+    public function testUpdate()
+    {
+        // Grab a template and a blast from the seeder
+        $template = $this->seeder->template;
+        $blast = $this->seeder->createdBlasts[0];
+
+        // Inivialize some data for the updated blast
+        $updatedInfo = [
+            'email_template_id' => $template->id,
+            'campaign_name' => 'Updated Campaign',
+            'send_date' => Carbon::tomorrow()->format('Y-m-d H:i:s'),
+            'action' => Blast::ACTION_CONTACTED,
+            'send_after_days' => '2',
+        ];
+
+        // POST the data into de API
+        $response = $this->json(
+            'POST',
+            self::API_ENDPOINT . '/' . $blast->email_blasts_id,
+            $updatedInfo,
+            ['access-token' => $this->accessToken]
+        );
+
+        // Check if we have a valid response from the API
+        $response->assertStatus(200)
+            ->assertJsonStructure(self::SINGLE_JSON_STRUCTURE);
+
+        // Check the data was assigned correctly
+        $this->assertSame(
+            $this->seeder->dealer->newDealerUser->user_id,
+            $response['data']['user_id'],
+            "The user doesn't match"
+        );
+        $this->assertSame(
+            $updatedInfo['campaign_name'],
+            $response['data']['campaign_name'],
+            "The blast campaign's name doesn't match"
+        );
+
+        // Corroborate the record was updated in the database
+        $this->assertDatabaseHas(Blast::getTableName(), [
+            'email_blasts_id' => $response['data']['id'],
+            'campaign_name' => $updatedInfo['campaign_name']
+        ]);
+    }
+
+    /**
+     * @group CRM
+     * @covers ::destroy
+     */
+    public function testDestroy()
+    {
+        // Grab a blast from the seeder
+        $blast = $this->seeder->createdBlasts[0];
+
+        // Send a DELETE request to the API
+        $response = $this->json(
+            'DELETE',
+            self::API_ENDPOINT . '/' . $blast->email_blasts_id,
+            [],
+            ['access-token' => $this->accessToken]
+        );
+
+        // Check that the response is a no content response
+        $response->assertStatus(204);
+
+        // Corroborate the record was deleted in the database
+        $this->assertDatabaseMissing(Blast::getTableName(), [
+           'email_blasts_id' => $blast->id
+        ]);
+    }
+
+    private function expectedDataFormat($blast): array
+    {
+        return [
             'id' => (int)$blast->email_blasts_id,
             'template_id' => (int)$blast->email_template_id,
             'location_id' => $blast->location_id,
@@ -193,120 +327,5 @@ class BlastControllerTest extends IntegrationTestCase
             'factory_campaign_id' => $blast->factory ? $blast->factory->id : null,
             'approved' => $blast->factory ? $blast->factory->is_approved : true,
         ];
-
-        $this->assertResponseDataEquals($response, $expectedData, false);
-    }
-
-    public function testCreate()
-    {
-        $template = $this->seeder->template;
-
-        $rawBlast = [
-            'email_template_id' => $template->id,
-            'campaign_name' => 'Test Campaign',
-            'send_date' => Carbon::tomorrow()->format('Y-m-d H:i:s'),
-            'action' => Blast::ACTION_UNCONTACTED,
-            'send_after_days' => '3',
-        ];
-
-        $response = $this->json(
-            'PUT',
-            '/api/user/emailbuilder/blast',
-            $rawBlast,
-            ['access-token' => $this->accessToken]
-        );
-
-        $response->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    'id',
-                    'template_id',
-                    'location_id',
-                    'location',
-                    'send_after_days',
-                    'action',
-                    'unit_category',
-                    'campaign_name',
-                    'user_id',
-                    'from_email_address',
-                    'campaign_subject',
-                    'include_archived',
-                    'send_date',
-                    'delivered',
-                    'cancelled',
-                    'categories',
-                    'brands',
-                    'total_sent',
-                    'factory_campaign_id',
-                    'approved'
-                ],
-            ]);
-
-        $this->assertSame($this->seeder->dealer->newDealerUser->user_id, $response['data']['user_id'], "The user doesn't match");
-        $this->assertSame($template->id, $response['data']['template_id'], "The template id doesn't match");
-    }
-
-    public function testUpdate()
-    {
-        $template = $this->seeder->template;
-        $blast = $this->seeder->createdBlasts[0];
-
-        $updatedInfo = [
-            'email_template_id' => $template->id,
-            'campaign_name' => 'Updated Campaign',
-            'send_date' => Carbon::tomorrow()->format('Y-m-d H:i:s'),
-            'action' => Blast::ACTION_CONTACTED,
-            'send_after_days' => '2',
-        ];
-
-        $response = $this->json(
-            'POST',
-            '/api/user/emailbuilder/blast/' . $blast->email_blasts_id,
-            $updatedInfo,
-            ['access-token' => $this->accessToken]
-        );
-
-        $response->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    'id',
-                    'template_id',
-                    'location_id',
-                    'location',
-                    'send_after_days',
-                    'action',
-                    'unit_category',
-                    'campaign_name',
-                    'user_id',
-                    'from_email_address',
-                    'campaign_subject',
-                    'include_archived',
-                    'send_date',
-                    'delivered',
-                    'cancelled',
-                    'categories',
-                    'brands',
-                    'total_sent',
-                    'factory_campaign_id',
-                    'approved'
-                ],
-            ]);
-
-        $this->assertSame($this->seeder->dealer->newDealerUser->user_id, $response['data']['user_id'], "The user doesn't match");
-        $this->assertSame($updatedInfo['campaign_name'], $response['data']['campaign_name'], "The blast campaign's name doesn't match");
-    }
-
-    public function testDestroy()
-    {
-        $blast = $this->seeder->createdBlasts[0];
-
-        $response = $this->json(
-            'DELETE',
-            '/api/user/emailbuilder/blast/' . $blast->email_blasts_id,
-            [],
-            ['access-token' => $this->accessToken]
-        );
-
-        $response->assertStatus(204);
     }
 }
