f97fd23056b2346517c2e3015676ce15f371a95c
Vishnu D <vishnu@trailercentral.com>
2022-08-18 22:47:50 +0530

    DMSS-839: Get Part will give 404 if part not found
    https://operatebeyond.atlassian.net/browse/DMSS-839
    
    # Conflicts:
    #	routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -207,7 +207,7 @@ $api->version('v1', function ($route) {
     $route->get('parts', 'App\Http\Controllers\v1\Parts\PartsController@index');
     $route->put('parts', 'App\Http\Controllers\v1\Parts\PartsController@create');
     $route->get('parts/search', 'App\Http\Controllers\v1\Parts\PartsController@search');
-    $route->get('parts/{id}', 'App\Http\Controllers\v1\Parts\PartsController@show')->where('id', '[0-9]+');
+    $route->get('parts/{part}', 'App\Http\Controllers\v1\Parts\PartsController@display')->where('id', '[0-9]+');
     $route->post('parts/{id}', 'App\Http\Controllers\v1\Parts\PartsController@update')->where('id', '[0-9]+');
     $route->delete('parts/{id}', 'App\Http\Controllers\v1\Parts\PartsController@destroy')->where('id', '[0-9]+');
 
