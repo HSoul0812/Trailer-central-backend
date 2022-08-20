fe1acb9fd18e35e68dd577e19d4ca9ecbeb5eaeb
Rober Diaz <rad8329@gmail.com>
2022-08-20 13:08:03 -0500

    #CDW-848 Too long password are being preventing via Request rules

--- app/Http/Requests/User/FinishPasswordResetRequest.php
+++ app/Http/Requests/User/FinishPasswordResetRequest.php
@@ -4,10 +4,10 @@ namespace App\Http\Requests\User;
 
 use App\Http\Requests\Request;
 
-class FinishPasswordResetRequest extends Request 
+class FinishPasswordResetRequest extends Request
 {
     protected $rules = [
         'code' => 'required',
-        'password' => 'required'
+        'password' => ['required', 'min:6', 'max:8']
     ];
 }
