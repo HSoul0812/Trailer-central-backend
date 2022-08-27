bd9df2b79cc7448b26f078632db59e7eced488aa
Rober Diaz <rad8329@gmail.com>
2022-08-27 16:11:55 -0500

    #CDW-812 `EntityType` constants were added to avoid magic numbers

--- app/Models/Inventory/EntityType.php
+++ app/Models/Inventory/EntityType.php
@@ -11,6 +11,26 @@ class EntityType extends Model {
 
     public const TABLE_NAME = 'eav_entity_type';
 
+    const TYPE_TRAILER = 1;
+    const TYPE_HOSE_TRAILER = 2;
+    const TYPE_RV = 3;
+    const TYPE_VEHICLE = 4;
+    const TYPE_WATERCRAFT = 5;
+    const TYPE_EQUIPMENT = 6;
+    const TYPE_SEMI_TRAILER = 7;
+    const TYPE_SPORTS_VEHICLE = 8;
+
+    const ENTITY_TYPE_LABELS = [
+        self::TYPE_TRAILER => 'Trailer',
+        self::TYPE_HOSE_TRAILER => 'Horse Trailer',
+        self::TYPE_RV => 'RV',
+        self::TYPE_VEHICLE => 'Vehicle',
+        self::TYPE_WATERCRAFT => 'Watercraft',
+        self::TYPE_EQUIPMENT => 'Equipment',
+        self::TYPE_SEMI_TRAILER => 'Semi-Trailer',
+        self::TYPE_SPORTS_VEHICLE => 'Sports Vehicle',
+    ];
+
     /**
      * The table associated with the model.
      *
