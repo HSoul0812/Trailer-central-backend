02235fc061e484b0749794df0bfca8992aa1b616
Rober Diaz <rad8329@gmail.com>
2022-08-31 22:25:24 -0500

    #CDW-812 `ElasticSearchEngine::ensureIndexIsCreated` was implemented

--- app/Indexers/ElasticSearchEngine.php
+++ app/Indexers/ElasticSearchEngine.php
@@ -4,6 +4,10 @@ namespace App\Indexers;
 
 use ElasticAdapter\Exceptions\BulkRequestException;
 use ElasticAdapter\Indices\Index;
+use ElasticAdapter\Indices\Mapping;
+use ElasticAdapter\Indices\Settings;
+use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
 
 class ElasticSearchEngine extends \ElasticScoutDriver\Engine
 {
@@ -12,37 +16,61 @@ class ElasticSearchEngine extends \ElasticScoutDriver\Engine
      *
      * @param \Illuminate\Database\Eloquent\Collection $models
      * @return void
-     * @throws BulkRequestException when some item was not able to be sent
+     * @throws BulkRequestException when some item was not able to be sent/updated
      */
     public function update($models): void
     {
-        if ($models->isEmpty()) {
-            return;
-        }
-
-        $first = $models->first();
-
-        if ($first &&
-            method_exists($first, 'indexConfigurator') &&
-            $first->indexConfigurator() &&
-            !$this->indexManager->exists($first->indexConfigurator()->name()
-            )
-        ) {
-            // when the index is not create, it will avoid the automatic index creation and
-            // will create it with the proper mappings and settings
-            $this->indexManager->create(new Index(
-                    $first->indexConfigurator()->name(),
-                    $first->indexConfigurator()->mapping(),
-                    $first->indexConfigurator()->settings()
-                )
-            );
-        }
+        $this->ensureIndexIsCreated($models);
 
         try {
             parent::update($models);
         } catch (BulkRequestException $exception) {
             // we will need to handle in a better way for each item which was not able to be sent
             throw $exception;
+        }
+    }
+
+    /**
+     * @param string $name
+     * @param array{mapping:Mapping|null, settings: Settings|null} $options
+     * @return void
+     * @throws InvalidArgumentException when the primary key options was provided (it is not possible to change it)
+     */
+    public function createIndex($name, array $options = []): void
+    {
+        if (isset($options['primaryKey'])) {
+            throw new InvalidArgumentException('It is not possible to change the primary key name');
+        }
+
+        $mapping = $options['mapping'] ?? null;
+        $settings = $options['settings'] ?? null;
+
+        $this->indexManager->create(new Index($name, $mapping, $settings));
+    }
+
+    /**
+     * When the model has a index configurator it will ensure the index mapping and settings are properly defined
+     */
+    private function ensureIndexIsCreated(\Illuminate\Database\Eloquent\Collection $models): void
+    {
+        if ($models->isEmpty()) {
+            return;
+        }
+
+        /** @var IndexConfigurator $configurator */
+        /** @var Model|WithIndexConfigurator $first */
+
+        $first = $models->first();
+
+        if ($first &&
+            method_exists($first, 'indexConfigurator') &&
+            ($configurator = $first->indexConfigurator()) &&
+            !$this->indexManager->exists($configurator->name())
+        ) {
+            $this->createIndex(
+                $configurator->name(),
+                ['mapping' => $configurator->mapping(), 'settings' => $configurator->settings()]
+            );
         }
     }
 }
