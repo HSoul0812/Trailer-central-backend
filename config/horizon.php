532c21627b276d67413ab3c1d3f0c7e48cac52c9
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-09-01 01:14:18 -0500

    * Reduced Processes on Release

--- config/horizon.php
+++ config/horizon.php
@@ -222,7 +222,7 @@ return [
                 'queue' => ['scrapereplies'],
                 'balance' => 'auto',
                 'minProcesses' => 1,
-                'maxProcesses' => 10,
+                'maxProcesses' => 5,
                 'tries' => 1,
                 'timeout' => 600,
             ],
