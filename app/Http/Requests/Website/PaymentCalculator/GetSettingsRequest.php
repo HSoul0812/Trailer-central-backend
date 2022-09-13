88f07e883d7b0223510cf8b8344ce2217096864c
Rober Diaz <rad8329@gmail.com>
2022-09-13 18:15:04 -0500

    #CDW-899 All payment calculator request consider the dealer ownership

--- app/Http/Requests/Website/PaymentCalculator/GetSettingsRequest.php
+++ app/Http/Requests/Website/PaymentCalculator/GetSettingsRequest.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Requests\Website\PaymentCalculator;
 
-use App\Http\Requests\Request;
+use App\Http\Requests\WithDealerRequest;
 
-class GetSettingsRequest extends Request {
-    
-    protected $rules = [
-        'inventory_condition' => 'in:used,new',
-        'inventory_price' => 'numeric',
-        'entity_type_id' => 'integer',
-        'financing' => 'in:financing,no_financing'
-    ];
-    
+/**
+ * @property boolean $grouped
+ */
+class GetSettingsRequest extends WithDealerRequest {
+
+    protected function getRules(): array
+    {
+        return array_merge(parent::getRules(), [
+            'website_id' => 'integer|min:1|required|exists:website,id,dealer_id,' . $this->dealer_id,
+            'inventory_condition' => 'in:used,new',
+            'inventory_price' => 'numeric',
+            'entity_type_id' => 'integer',
+            'financing' => 'in:financing,no_financing',
+            'grouped' => 'boolean'
+        ]);
+    }
 }
