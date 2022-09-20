8ba0cecc85003db3e5d71ed4de304e6ee73e6e9c
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-09-20 17:41:22 -0500

    * Reduced Max Processes on Scrape Replies

--- config/horizon.php
+++ config/horizon.php
@@ -286,7 +286,7 @@ return [
                 'queue' => ['scrapereplies'],
                 'balance' => 'auto',
                 'minProcesses' => 5,
-                'maxProcesses' => 300,
+                'maxProcesses' => 100,
                 'tries' => 1,
                 'timeout' => 600,
             ],
