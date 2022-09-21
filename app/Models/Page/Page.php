8503c9154ad451f000ba6a914ef296ea656b345a
Ethan Rivas <ethan@operatebeyond.com>
2022-09-21 14:04:12 -0500

    #NTGRTNS-476 Page model

---
+++ app/Models/Page/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Page;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    protected $table = 'pages';
+    public $timestamps = false;
+
+    protected $fillable = [
+        "name",
+        "url",
+        "description"
+    ];
+}
