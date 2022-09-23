9cc1e28efe9bb2fc0b9977c09a354d6c67949869
Pond <pond@trailercentral.com>
2022-09-23 16:38:08 +0700

    DMSS-971: Replace switch with dictionary lookup of closures

--- app/Services/Import/Parts/NewCsvImportService.php
+++ app/Services/Import/Parts/NewCsvImportService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Import\Parts;
 
 use App\Events\Parts\PartQtyUpdated;
+use App\Exceptions\Services\Import\Parts\EmptySKUException;
 use App\Models\Bulk\Parts\BulkUpload;
 use App\Models\Parts\Brand;
 use App\Models\Parts\Category;
@@ -11,6 +12,7 @@ use App\Models\Parts\Vendor;
 use App\Repositories\Bulk\Parts\BulkUploadRepositoryInterface;
 use App\Repositories\Parts\BinRepositoryInterface;
 use App\Repositories\Parts\PartRepositoryInterface;
+use Closure;
 use DB;
 use Exception;
 use League\Flysystem\FilesystemInterface;
@@ -322,100 +324,23 @@ class NewCsvImportService implements CsvImportServiceInterface
     {
         $part = [];
 
+        $headerHandlers = $this->getHeaderHandlers();
+
         // We only loop through the indexes that we have in the file
         foreach ($this->headerIndexes as $header => $index) {
-            // For each index, we process them differently
-            $value = $data[$index];
+            // Don't need to do anything if we don't have a handler for this header
+            if (!array_key_exists($header, $headerHandlers)) {
+                continue;
+            }
 
-            switch ($header) {
-                case self::HEADER_VENDOR:
-                    $part['vendor_id'] = $this->getVendorIdByName($value);
-                    if (empty($part['vendor_id'])) {
-                        $this->errors[] = "Issue with line #$line: cannot find vendor $value in the system.";
-                    }
-                    break;
+            $value = $data[$index] ?? null;
 
-                case self::HEADER_BRAND:
-                    $part['brand_id'] = $this->getBrandIdByName($value);
-                    if (empty($part['brand_id'])) {
-                        $this->errors[] = "Issue with line #$line: cannot find brand $value in the system.";
-                    }
-                    break;
-
-                case self::HEADER_TYPE:
-                    $part['type_id'] = $this->getTypeIdByName($value);
-                    if (empty($part['type_id'])) {
-                        $this->errors[] = "Issue with line #$line: cannot find part type $value in the system.";
-                    }
-                    break;
-
-                case self::HEADER_CATEGORY:
-                    $part['category_id'] = $this->getCategoryIdByName($value);
-                    if (empty($part['category_id'])) {
-                        $this->errors[] = "Issue with line #$line: cannot find part category $value in the system.";
-                    }
-                    break;
-
-                case self::HEADER_SUBCATEGORY:
-                    $part['subcategory'] = $this->sanitizeValueToString($value);
-                    break;
-
-                case self::HEADER_TITLE:
-                    $part['title'] = $this->sanitizeValueToString($value);
-                    break;
-
-                case self::HEADER_SKU:
-                    $part['sku'] = $this->sanitizeValueToString($value);
-                    break;
-
-                case self::HEADER_PRICE:
-                    $part['price'] = $this->sanitizeValueToNumber($value);
-                    break;
-
-                case self::HEADER_DEALER_COST:
-                    $part['dealer_cost'] = $this->sanitizeValueToNumber($value, 0);
-                    break;
-
-                case self::HEADER_MSRP:
-                    $part['msrp'] = $this->sanitizeValueToNumber($value);
-                    break;
-
-                case self::HEADER_WEIGHT:
-                    $part['weight'] = $this->sanitizeValueToNumber($value);
-                    break;
-
-                case self::HEADER_WEIGHT_RATING:
-                    $part['weight_rating'] = $this->sanitizeValueToNumber($value);
-                    break;
-
-                case self::HEADER_DESCRIPTION:
-                    $part['description'] = $this->sanitizeValueToString($value);
-                    break;
-
-                case self::HEADER_SHOW_ON_WEBSITE:
-                    $part['show_on_website'] = $this->sanitizeValueToNumber($value, 0);
-                    break;
-
-                case self::HEADER_IMAGE:
-                    // TODO: Implement the real logic here
-                    $part['images'] = null;
-                    break;
-
-                case self::HEADER_STOCK_MIN:
-                    $part['stock_min'] = $this->sanitizeValueToNumber($value);
-                    break;
-
-                case self::HEADER_STOCK_MAX:
-                    $part['stock_max'] = $this->sanitizeValueToNumber($value);
-                    break;
-
-                case self::HEADER_VIDEO_EMBED_CODE:
-                    $part['video_embed_code'] = $this->sanitizeValueToString($value);
-                    break;
-
-                case self::HEADER_ALTERNATE_PART_NUMBER:
-                    $part['alternative_part_number'] = $this->sanitizeValueToString($value);
-                    break;
+            try {
+                call_user_func($headerHandlers[$header], $part, $value, $line);
+            } catch (EmptySKUException $exception) {
+                // In the case of the SKU doesn't exist
+                // we will just skip this row
+                continue;
             }
         }
 
@@ -548,5 +473,191 @@ class NewCsvImportService implements CsvImportServiceInterface
         }
 
         return $filteredValue;
+    }
+
+    /**
+     * Get the handlers for all the headers that we accept
+     *
+     * @return Closure[]
+     */
+    private function getHeaderHandlers(): array
+    {
+        return [
+            self::HEADER_VENDOR => function (array &$part, ?string $value, int $line) {
+                if ($this->storeErrorIfValueIsEmpty(self::HEADER_VENDOR, $line, $value)) {
+                    return;
+                }
+
+                $part['vendor_id'] = $this->getVendorIdByName($value);
+                $this->storeErrorIfValueDoesNotExistInDB(self::HEADER_VENDOR, $line, $part['vendor_id'], $value);
+            },
+            self::HEADER_BRAND => function (array &$part, ?string $value, int $line) {
+                if ($this->storeErrorIfValueIsEmpty(self::HEADER_BRAND, $line, $value)) {
+                    return;
+                }
+
+                $part['brand_id'] = $this->getBrandIdByName($value);
+                $this->storeErrorIfValueDoesNotExistInDB(self::HEADER_BRAND, $line, $part['brand_id'], $value);
+            },
+            self::HEADER_TYPE => function (array &$part, ?string $value, int $line) {
+                if ($this->storeErrorIfValueIsEmpty(self::HEADER_TYPE, $line, $value)) {
+                    return;
+                }
+
+                $part['type_id'] = $this->getTypeIdByName($value);
+                $this->storeErrorIfValueDoesNotExistInDB(self::HEADER_TYPE, $line, $part['type_id'], $value);
+            },
+            self::HEADER_CATEGORY => function (array &$part, ?string $value, int $line) {
+                if ($this->storeErrorIfValueIsEmpty(self::HEADER_CATEGORY, $line, $value)) {
+                    return;
+                }
+
+                $part['category_id'] = $this->getCategoryIdByName($value);
+                $this->storeErrorIfValueDoesNotExistInDB(self::HEADER_CATEGORY, $line, $part['category_id'], $value);
+            },
+            self::HEADER_SUBCATEGORY => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_SUBCATEGORY, $line, $value);
+                $part['subcategory'] = $this->sanitizeValueToString($value, '');
+            },
+            self::HEADER_TITLE => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_TITLE, $line, $value);
+                $part['title'] = $this->sanitizeValueToString($value, '');
+            },
+            /**
+             * @throws EmptySKUException
+             */
+            self::HEADER_SKU => function (array &$part, ?string $value, int $line) {
+                if (empty($value)) {
+                    throw new EmptySKUException("Issue with line #$line: SKU is empty, skipping to skip this line.");
+                }
+
+                $this->storeErrorIfValueIsEmpty(self::HEADER_SKU, $line, $value);
+                $part['sku'] = $this->sanitizeValueToString($value);
+            },
+            self::HEADER_PRICE => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_PRICE, $line, $value);
+                $part['price'] = $this->sanitizeValueToNumber($value);
+            },
+            self::HEADER_DEALER_COST => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_DEALER_COST, $line, $value);
+                $part['dealer_cost'] = $this->sanitizeValueToNumber($value, 0);
+            },
+            self::HEADER_MSRP => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_MSRP, $line, $value);
+                $part['msrp'] = $this->sanitizeValueToNumber($value);
+            },
+            self::HEADER_WEIGHT => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_WEIGHT, $line, $value);
+                $part['weight'] = $this->sanitizeValueToNumber($value);
+            },
+            self::HEADER_WEIGHT_RATING => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_WEIGHT_RATING, $line, $value);
+                $part['weight_rating'] = $this->sanitizeValueToNumber($value);
+            },
+            self::HEADER_DESCRIPTION => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_DESCRIPTION, $line, $value);
+                $part['description'] = $this->sanitizeValueToString($value);
+            },
+            self::HEADER_SHOW_ON_WEBSITE => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_SHOW_ON_WEBSITE, $line, $value);
+                $part['show_on_website'] = $this->sanitizeValueToNumber($value, 0);
+            },
+            self::HEADER_IMAGE => function (array &$part, ?string $value, int $line) {
+                // TODO: Implement the real logic here
+                $part['images'] = null;
+            },
+            self::HEADER_STOCK_MIN => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_STOCK_MIN, $line, $value);
+                $part['stock_min'] = $this->sanitizeValueToNumber($value);
+            },
+            self::HEADER_STOCK_MAX => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_STOCK_MAX, $line, $value);
+                $part['stock_max'] = $this->sanitizeValueToNumber($value);
+            },
+            self::HEADER_VIDEO_EMBED_CODE => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_VIDEO_EMBED_CODE, $line, $value);
+                $part['video_embed_code'] = $this->sanitizeValueToString($value);
+            },
+            self::HEADER_ALTERNATE_PART_NUMBER => function (array &$part, ?string $value, int $line) {
+                $this->storeErrorIfValueIsEmpty(self::HEADER_ALTERNATE_PART_NUMBER, $line, $value);
+                $part['alternative_part_number'] = $this->sanitizeValueToString($value);
+            },
+        ];
+    }
+
+    /**
+     * Get the line issue message string
+     *
+     * @param int $line
+     * @param string $message
+     * @return string
+     */
+    private function getLineIssueMessage(int $line, string $message): string
+    {
+        return "Issue with line #$line: $message.";
+    }
+
+    /**
+     * Get the value is empty message
+     *
+     * @param string $header
+     * @return string
+     */
+    private function getValueIsEmptyMessage(string $header): string
+    {
+        return sprintf("the %s is empty", $header);
+    }
+
+    /**
+     * Get the message to show when the value doesn't exist in DB
+     *
+     * @param string $header
+     * @param string $value
+     * @return string
+     */
+    private function getValueDoesNotExistInTheSystemMessage(string $header, string $value): string
+    {
+        return sprintf("cannot find %s %s in the system", $header, $value);
+    }
+
+    /**
+     * @param string $header
+     * @param int $line
+     * @param string|null $value
+     * @return bool Return true if the value is empty
+     */
+    private function storeErrorIfValueIsEmpty(string $header, int $line, ?string $value): bool
+    {
+        if (!empty($value)) {
+            return false;
+        }
+
+        $this->errors[] = $this->getLineIssueMessage(
+            $line,
+            $this->getValueIsEmptyMessage($header)
+        );
+
+        return true;
+    }
+
+    /**
+     * Store the error if value does not exist in DB
+     *
+     * @param string $header
+     * @param int $line
+     * @param $dataFromDB
+     * @param string|null $value
+     * @return void
+     */
+    private function storeErrorIfValueDoesNotExistInDB(string $header, int $line, $dataFromDB, ?string $value): void
+    {
+        if (!empty($dataFromDB)) {
+            return;
+        }
+
+        $this->errors[] = $this->getLineIssueMessage(
+            $line,
+            $this->getValueDoesNotExistInTheSystemMessage($header, $value)
+        );
     }
 }
