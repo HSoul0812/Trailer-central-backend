9e6989c436d49525bc34559a580b6114311b9cb8
David A Conway Jr <david.a.conway.jr@gmail.com>
2022-09-27 16:02:21 -0500

    * CRM-428 Added CatalogService Model Mocks

--- tests/Unit/Services/Integration/Facebook/BusinessServiceTest.php
+++ tests/Unit/Services/Integration/Facebook/BusinessServiceTest.php
@@ -19,9 +19,24 @@ use Tests\TestCase;
  */
 class BusinessServiceTest extends TestCase
 {
+    /**
+     * @const Catalog Scopes
+     */
+    const TEST_EXPIRES_IN = 60 * 60 * 24 * 60;
+
+
+    /**
+     * @var LegacyMockInterface|BusinessServiceInterface
+     */
+    private $businessServiceMock;
+
+
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->businessServiceMock = Mockery::mock(BusinessServiceInterface::class);
+        $this->app->instance(BusinessServiceInterface::class, $this->businessServiceMock);
     }
 
     /**
@@ -32,14 +47,32 @@ class BusinessServiceTest extends TestCase
      */
     public function testValidate()
     {
-        // Get Access Token
-        $accessToken = $this->createAccessToken();
+        // Mock AccessToken
+        $time = now();
+        $accessToken = $this->getEloquentMock(AccessToken::class);
+        $accessToken->id = 1;
+        $accessToken->dealer_id = 1;
+        $accessToken->token_type = 'facebook';
+        $accessToken->relation_type = 'fbapp_catalog';
+        $accessToken->relation_id = 1;
+        $accessToken->access_token = 1;
+        $accessToken->refresh_token = 1;
+        $accessToken->id_token = 1;
+        $accessToken->expires_in = self::DEFAULT_EXPIRES_IN;
+        $accessToken->expires_at = date("Y-m-d H:i:s", $time + self::DEFAULT_EXPIRES_IN);
+        $accessToken->issued_at = date("Y-m-d H:i:s", $time);
+
+        // Pass Through Business Service Mock
+        $this->businessServiceMock
+             ->shouldReceive('validate')
+             ->passthru();
+
 
         /** @var BusinessService $service */
         $service = $this->app->make(BusinessService::class);
 
         // Validate Test Service
-        $scopes = explode(" ", $_ENV['TEST_FB_SCOPES']);
+        $scopes = explode(' ', config('oauth.fb.' . $this->type . '.scopes'));
         $result = $service->validate($accessToken, $scopes);
 
         // Assert is Valid
@@ -47,29 +80,5 @@ class BusinessServiceTest extends TestCase
 
         // Assert Is Not Expired
         $this->assertFalse($result['is_expired']);
-    }
-
-
-    /**
-     * Create Access Token With Factory
-     */
-    private function createAccessToken()
-    {
-        // Get Access Token
-        $time = strtotime($_ENV['TEST_FB_ISSUED_AT']);
-        $accessToken = factory(AccessToken::class)->make([
-            'token_type' => 'facebook',
-            'relation_type' => 'fbapp_catalog',
-            'relation_id' => $_ENV['TEST_FB_RELATION_ID'],
-            'access_token' => $_ENV['TEST_FB_ACCESS_TOKEN'],
-            'refresh_token' => $_ENV['TEST_FB_REFRESH_TOKEN'],
-            'id_token' => $_ENV['TEST_FB_ID_TOKEN'],
-            'expires_in' => $_ENV['TEST_FB_EXPIRES_IN'],
-            'expires_at' => date("Y-m-d H:i:s", $time + $_ENV['TEST_FB_EXPIRES_IN']),
-            'issued_at' => date("Y-m-d H:i:s", $time)
-        ]);
-
-        // Return Access Token
-        return $accessToken;
     }
 }
