39bd2425074e801ce65efec70fdaac4c26d86e93
calo <carlos@operatebeyond.com>
2022-09-27 10:35:27 -0500

    NTGRTNS-494 Rename Names

--- app/Models/User/LightDealer.php
+++ app/Models/User/LightDealer.php
@@ -2,17 +2,15 @@
 
 namespace App\Models\User;
 
-use App\Models\User\DealerLocation;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder;
 
 /**
- * Class ExtUser
+ * Class LightDealer
  *
- * This ExtUser class is for Dealers on Nova
+ * This LightDealer class is for Dealers on Nova
  *
- * @package App\Models\ExtUser
+ * @package App\Models\LightDealer
  *
  * @property int $dealer_id
  * @property string $name
@@ -20,7 +18,7 @@ use Illuminate\Database\Query\Builder;
  *
  * @method static Builder whereIn($column, $values, $boolean = 'and', $not = false)
  */
-class ExtUser extends Model
+class LightDealer extends Model
 {
 
     public const TABLE_NAME = 'dealer';
