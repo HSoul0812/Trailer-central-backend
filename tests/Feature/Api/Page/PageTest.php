cd1f91595d17369f5d2a61a927159b4ebcb453d7
Ethan Rivas <ethan@operatebeyond.com>
2022-09-28 10:27:51 -0500

    #NTGRTNS-476 Page test case updated with encoding test

--- tests/Feature/Api/Page/PageTest.php
+++ tests/Feature/Api/Page/PageTest.php
@@ -63,4 +63,35 @@ class PageTest extends FeatureTestCase
             $this->assertDatabaseHas('pages', $page);
         }
     }
+
+    /**
+     * Tests if the data returned is with an expected encoding
+     *
+     * @return void
+     */
+    public function testIndexPagesEnconding(): void
+    {
+        $response = $this->get($this->endpoint);
+        $json = json_decode($response->getContent(), true);
+
+        foreach ($json["data"] as $page) {
+            foreach ($page as $key => $value) {
+                $this->assertEquals(
+                    'UTF-8',
+                    mb_detect_encoding(
+                        $key,
+                        'UTF-8'
+                    )
+                );
+
+                $this->assertEquals(
+                    'UTF-8',
+                    mb_detect_encoding(
+                        $value,
+                        'UTF-8'
+                    )
+                );
+            }
+        }
+    }
 }
