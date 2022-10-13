310f8010f73f647e3223b73432622e94aa447b56
Rober <rad8329@gmail.com>
2022-10-13 16:24:44 -0500

    #CDW-859 selected locations aggregation filter is properly handled

--- app/Services/ElasticSearch/Inventory/Builders/SelectQueryBuilder.php
+++ app/Services/ElasticSearch/Inventory/Builders/SelectQueryBuilder.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\ElasticSearch\Inventory\Builders;
 
+use App\Services\ElasticSearch\Inventory\Parameters\DealerLocationIds;
+
 /**
  * Builds a proper ES query for a select, they should be provided as follows:
  *   - category=camping_rv;fifth_wheel_campers: which is a category with options camping_rv & fifth_wheel_campers
@@ -13,35 +15,66 @@ class SelectQueryBuilder implements FieldQueryBuilderInterface
     /** @var string */
     private $field;
 
-    /** @var array */
+    /** @var array|DealerLocationIds */
     private $options;
 
     public function __construct(string $field, string $data)
     {
         $this->field = $field;
+
         $this->options = explode(self::DELIMITER, $data);
+
+        if ($this->field === 'dealerLocationId') {
+            $this->options = DealerLocationIds::fromString($data);
+        }
     }
 
     public function query(): array
     {
-        $optionsQuery = [
-            'bool' => [
-                'filter' => [
-                    [
-                        'terms' => [
-                            $this->field => $this->options
+        switch ($this->field) {
+            case 'dealerLocationId':
+                $optionsQuery = [
+                    'bool' => [
+                        'filter' => [
+                            [
+                                'terms' => [
+                                    $this->field => $this->options->locations()
+                                ]
+                            ]
                         ]
                     ]
-                ]
-            ]
-        ];
+                ];
 
-        switch ($this->field) {
-            // if we would need to handle edges cases, then we need to handle here
+                return [
+                    'post_filter' => $optionsQuery,
+                    'aggregations' => [
+                        'filter_aggregations' => ['filter' => $optionsQuery],
+                        'selected_location_aggregations' => ['filter' => [
+                            'bool' => [
+                                'filter' => [
+                                    [
+                                        'terms' => [
+                                            $this->field => $this->options->locationsForSubAggregatorsFiltering()
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]]
+                    ]
+                ];
             default:
-                // almost any `post_filter` should also append to `filter_aggregations` and `selected_location_aggregations`
-                // but there and edge case when the selected locations (post_filter) could be different for `selected_location_aggregations`
-                // that's the nationwide case for Bishs
+                $optionsQuery = [
+                    'bool' => [
+                        'filter' => [
+                            [
+                                'terms' => [
+                                    $this->field => $this->options
+                                ]
+                            ]
+                        ]
+                    ]
+                ];
+
                 return [
                     'post_filter' => $optionsQuery,
                     'aggregations' => [
