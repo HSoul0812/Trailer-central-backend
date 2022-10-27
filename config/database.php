65cb694bde62acee350f387179832763f4530f8a
Pond <pond@trailercentral.com>
2022-10-27 23:39:42 +0700

    DMSS-954: Hotfix for backup_mysql config

--- config/database.php
+++ config/database.php
@@ -78,7 +78,18 @@ return [
         'backup_mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'read' => [
+                'host' => [
+                    // if no separate read write hosts are configured, then
+                    // the same host will be used for both connectsion
+                    env('DB_READ_HOST', env('DB_HOST', '127.0.0.1')),
+                ],
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_HOST', '127.0.0.1'),
+                ],
+            ],
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
