6de94e1beba07745a4a5aeb7e193b8db471b3577
pekar <sergeypekar77@gmail.com>
2022-11-07 17:01:29 +0200

    #CRM-657 disabled hot potato in horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -163,7 +163,7 @@ return [
                     'ecommerce',
                     'crm-users',
                     'manufacturers',
-                    'hot-potato'
+                    //'hot-potato'
                 ],
                 'balance' => 'simple',
                 'processes' => 3,
@@ -193,7 +193,7 @@ return [
                     'ecommerce',
                     'crm-users',
                     'manufacturers',
-                    'hot-potato'
+                    //'hot-potato'
                 ],
                 'balance' => 'simple',
                 'processes' => 10,
@@ -215,7 +215,7 @@ return [
                     'ecommerce',
                     'crm-users',
                     'manufacturers',
-                    'hot-potato'
+                    //'hot-potato'
                 ],
                 'balance' => 'auto',
                 'minProcesses' => 1,
@@ -289,7 +289,7 @@ return [
                     'ecommerce',
                     'crm-users',
                     'manufacturers',
-                    'hot-potato'
+                    //'hot-potato'
                 ],
                 'balance' => 'auto',
                 'minProcesses' => 1,
