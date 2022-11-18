b5f560122a1ac53c017144658684e79114fbc012
Rober <rad8329@gmail.com>
2022-11-18 10:39:48 -0500

    #CDW-1104 Tuan feedback

--- app/Transformers/Inventory/InventoryImageTransformer.php
+++ app/Transformers/Inventory/InventoryImageTransformer.php
@@ -8,11 +8,13 @@ class InventoryImageTransformer extends MediaFileTransformer
 {
     public function transform(InventoryImage $inventoryImage): array
     {
+        $position = $inventoryImage->position ?: InventoryImage::LAST_IMAGE_POSITION;
+
         return [
             'image_id' => $inventoryImage->image_id,
             'is_default' => $inventoryImage->is_default,
             'is_secondary' => $inventoryImage->is_secondary,
-            'position' => $inventoryImage->position ?: InventoryImage::LAST_IMAGE_POSITION,
+            'position' => $inventoryImage->isDefault() ? InventoryImage::FIRST_IMAGE_POSITION : $position,
             'url' => $this->getBaseUrl() . $inventoryImage->image->filename,
         ];
     }
