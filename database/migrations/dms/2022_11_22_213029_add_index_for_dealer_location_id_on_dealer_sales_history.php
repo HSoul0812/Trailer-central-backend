c1be457bd230765094b3642d07b04f5ecbaf4bec
Li Jiang <master.dev0511@gmail.com>
2022-11-22 21:54:41 +0800

    DMSS-750 Add the new migration to index dealer_location_id column on dealer_sales_history

---
+++ database/migrations/dms/2022_11_22_213029_add_index_for_dealer_location_id_on_dealer_sales_history.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Inventory\Inventory;
+
+class AddIndexForDealerLocationIdOnDealerSalesHistory extends Migration
+{
+    
+    /**
+     * Run the migrations.
+     *
+     * @return void 
+     */
+    public function up()
+    {
+        Schema::table('dealer_sales_history', function (Blueprint $table) { 
+            $table->index(['dealer_location_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dealer_sales_history', function (Blueprint $table) {
+            $table->dropIndex(['dealer_location_id']);
+        });
+    }
+}
