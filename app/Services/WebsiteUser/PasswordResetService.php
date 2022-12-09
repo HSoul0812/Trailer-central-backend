7c1680cec23cf246b123340423795612ad6339d4
Mert Öksüz <xrefresh@gmail.com>
2022-12-09 14:59:49 +0300

    Exclude tester emails

--- app/Services/WebsiteUser/PasswordResetService.php
+++ app/Services/WebsiteUser/PasswordResetService.php
@@ -15,6 +15,11 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class PasswordResetService implements PasswordResetServiceInterface
 {
     private DatabaseTokenRepository $tokenRepository;
+
+    const EXCLUDE_THROTTLE_MAILS = [
+        'qa-team@trailercentral.com',
+    ];
+
     public function __construct(
         private WebsiteUserRepositoryInterface $userRepository
     )
@@ -41,8 +46,11 @@ class PasswordResetService implements PasswordResetServiceInterface
             throw new NotFoundHttpException("User doesn't exist");
         }
         $user = $users->first();
-        if ($this->tokenRepository->recentlyCreatedToken($user)) {
-            throw new ThrottleRequestsException("Too many requests");
+
+        if (!in_array($email, self::EXCLUDE_THROTTLE_MAILS)) {
+            if ($this->tokenRepository->recentlyCreatedToken($user)) {
+                throw new ThrottleRequestsException("Too many requests");
+            }
         }
 
         $token = $this->tokenRepository->create($user);
