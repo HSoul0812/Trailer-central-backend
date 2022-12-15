5cd049f3337f0a7be4a5ff078c7f2f4a96def2c5
Li Jiang <master.dev0511@gmail.com>
2022-12-15 15:24:32 +0800

    DMSS-1018 Revert the formatting changes

--- app/Http/Controllers/v1/Parts/PartsController.php
+++ app/Http/Controllers/v1/Parts/PartsController.php
@@ -766,17 +766,20 @@ class PartsController extends RestfulController
                     'per_page' => $request->get('per_page', 10),
                 ], $paginator
             );
-
             $data = new Collection($result, $this->partsTransformer, 'data');
+
             // if a paginator is requested
             if ($request->get('page')) {
                 $data->setPaginator(new IlluminatePaginatorAdapter($paginator));
             }
+
             // parses the include params
             $this->fractal->parseIncludes($request->get('include', []));
+
             // build the api response
             $result = (array) $this->fractal->createData($data)->toArray();
             return $this->response->array($result);
+
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             Log::error($e->getTraceAsString());
