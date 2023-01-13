89bab02907a0aefcc3fba84071ddcf0c43d0bb87
Pond <pond@trailercentral.com>
2023-01-13 14:48:16 +0700

    DMSS-1249: Add an artisan command to update empty geolocation data to default as POINT(0,0)

---
+++ app/Console/Commands/Database/UpdateEmptyInventoryGeolocationCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands\Database;
+
+use App\Models\User\User;
+use DB;
+use Illuminate\Console\Command;
+
+/**
+ * Use this command to update all the empty geolocation value to POINT(0,0)
+ *
+ * It's IMPORTANT to note that we can't do this in the MySQL version 8 (empty point causes a problem when update)
+ */
+class UpdateEmptyInventoryGeolocationCommand extends Command
+{
+    protected $signature = 'database:inventory:update-empty-geolocation';
+
+    protected $description = 'Update the empty geolocation to default as POINT(0,0)';
+
+    public function handle(): int
+    {
+        $dealerIds = User::whereHas('inventories')->pluck('dealer_id');
+
+        $totalUpdatedRow = 0;
+        foreach ($dealerIds as $dealerId) {
+            $totalUpdatedRow += $this->updateForDealerId($dealerId);
+        }
+
+        $this->info("All the empty geolocations are now being updated to POINT(0,0)!");
+        $this->info("Total updated row: $totalUpdatedRow");
+
+        return 0;
+    }
+
+    private function updateForDealerId($dealerId): int
+    {
+        $updatedRow = DB::update("update inventory set geolocation = POINT(0, 0) where dealer_id = ? and trim(geolocation) = ''", [$dealerId]);
+
+        if ($updatedRow !== 0) {
+            $this->line("Dealer ID: $dealerId, Updated $updatedRow rows.");
+        }
+
+        return $updatedRow;
+    }
+}
