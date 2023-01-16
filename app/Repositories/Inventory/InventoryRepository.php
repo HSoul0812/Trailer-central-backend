848ac05a642e86e440d252d728c31d25cd62dc7e
HuascarOna <huascar@operatebeyond.com>
2023-01-16 12:48:23 -0500

    #CDW-1119 improve readability

--- app/Repositories/Inventory/InventoryRepository.php
+++ app/Repositories/Inventory/InventoryRepository.php
@@ -859,7 +859,7 @@ class InventoryRepository implements InventoryRepositoryInterface
         if (isset($params['is_publishable_classified'])) {
             $query = $query->join('dealer', 'dealer.dealer_id', '=', 'inventory.dealer_id');
             if ($params['is_publishable_classified']) {
-                $query = $query->where('clsf_active', (int)$params['is_publishable_classified'])->where('show_on_website', (int)$params['is_publishable_classified']);
+                $query = $query->where('clsf_active', User::CLASSIFIED_ACTIVE)->where('show_on_website', Inventory::SHOW_IN_WEBSITE);
             } else {
                 $query = $query->where('clsf_active', (int)$params['is_publishable_classified'])->orWhere('show_on_website', (int)$params['is_publishable_classified']);
             }
