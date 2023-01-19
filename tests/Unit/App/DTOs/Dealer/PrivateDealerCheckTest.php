f525817e00aff3d790c25d60867b515a77628423
zhao songxun <songxunzhao1991@gmail.com>
2023-01-19 03:53:54 -0500

    #TR-774: add unit test for PrivateDealerCheck class

---
+++ tests/Unit/App/DTOs/Dealer/PrivateDealerCheckTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\App\DTOs\Dealer;
+
+use App\DTOs\Dealer\PrivateDealerCheck;
+use Tests\Common\TestCase;
+
+class PrivateDealerCheckTest extends TestCase
+{
+    /**
+     * @dataProvider privateDealerProvider
+     */
+    public function testPrivateDealer(array $dealer) {
+        $checker = new PrivateDealerCheck;
+        $this->assertTrue($checker->checkArray($dealer));
+    }
+
+    /**
+     * @dataProvider regularDealerProvider
+     */
+    public function testRegularDealer(array $dealer) {
+        $checker = new PrivateDealerCheck;
+        $this->assertFalse($checker->checkArray($dealer));
+    }
+
+    public function privateDealerProvider() {
+        return [
+            [[ 'id' => 8410 ]],
+            [[ 'id' => 1004 ]],
+            [[ 'id' => 12213 ]],
+            [[ 'id' => 10005 ]],
+            [[ 'id' => 1234, 'from' => 'trailertrader' ]]
+        ];
+    }
+
+    public function regularDealerProvider() {
+        return [
+            [[ 'id' => 353524 ]],
+            [[ 'id' => 1234, 'from' => 'trailercentral' ]]
+        ];
+    }
+}
