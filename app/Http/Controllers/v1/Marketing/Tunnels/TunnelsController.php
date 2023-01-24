bf75a88b1687a3ee571e3c26d9d8e91ff4b053bb
Ioan Bercea <ioan@ioans-mbp.bbb>
2023-01-24 18:15:03 +0100

    using the repository

--- app/Http/Controllers/v1/Marketing/Tunnels/TunnelsController.php
+++ app/Http/Controllers/v1/Marketing/Tunnels/TunnelsController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers\v1\Marketing\Tunnels;
 
 
 use App\Http\Controllers\RestfulControllerV2;
-use App\Models\User\User;
+use App\Repositories\User\DealerUserRepositoryInterface;
 use Illuminate\Http\Request;
 
 class TunnelsController extends RestfulControllerV2
 {
-    public function check(Request $request)
+    public function check(Request $request, DealerUserRepositoryInterface $dealerUserRepo)
     {
-        $dealer = User::where('email', '=', $request->get('email', ''))->firstOrFail();
-        return $this->response->array([
-            'dealerId' => $dealer->dealer_id,
-            'dealerName' => $dealer->name
-        ]);
+        $dealerEmail = $request->get('email', '');
+        if ($dealerEmail === '') {
+            abort(404);
+        }
+        $dealer = $dealerUserRepo->getByDealerEmail($dealerEmail);
+        if ($dealer) {
+            return $this->response->array([
+                'dealerId' => $dealer->dealer_id,
+                'dealerName' => $dealer->name
+            ]);
+        } else {
+            abort(404);
+        }
     }
 }
