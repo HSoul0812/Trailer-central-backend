7e7f14d80f1db51aaad324d9e5423b7448f6f2ba
Vishnu D <vishnu@trailercentral.com>
2023-01-27 19:13:14 +0530

    DMSS-1263: Removed SSN key value pair from form submissions

--- app/Console/Commands/Database/PruneSSNCommand.php
+++ app/Console/Commands/Database/PruneSSNCommand.php
@@ -4,6 +4,9 @@ namespace App\Console\Commands\Database;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
+use App\Console\Traits\PrependsOutput;
+use App\Console\Traits\PrependsTimestamp;
 
 /**
  * Class PruneSSNCommand
@@ -12,13 +15,19 @@ use Illuminate\Support\Facades\DB;
  */
 class PruneSSNCommand extends Command
 {
+    use PrependsOutput, PrependsTimestamp;
+
     /**
      * @var string
      */
     protected $signature = '
         database:prune-ssn
-        {--olderThanDays=30 : To prune SSN older than specifed value, default is 30 days.}
+        {--olderThanDays=30 : To prune SSN older than specified value, default is 30 days.}
+        {--chunkSize=1000 : The size for each chunk.}
+        {--delay=5 : Delay time in seconds after each delayChunkCount parts is being dispatched.}
     ';
+
+    const WEBSITE_FORM_SUBMISSIONS_TABLE_SSN_KEY = 'ssn';
 
     /**
      * @var string
@@ -33,7 +42,7 @@ class PruneSSNCommand extends Command
         $days = $this->option('olderThanDays');
 
         if (!is_numeric($days)) {
-            $this->info('Specified values for days is not valid! Terminating the script now, have a good day! 😃');
+            $this->line('Specified values for days is not valid! Terminating the script now, have a good day! 😃');
 
             return 1;
         }
@@ -44,18 +53,67 @@ class PruneSSNCommand extends Command
 
         foreach ($tableData as $key => $value) {
             $infoMsg = 'SSN data created before "' . $selectedDate . '" in "' . $value['tableName'] . '" table.';
-            $this->info('Removing ' . $infoMsg);
+            $this->line('Removing ' . $infoMsg);
 
             DB::table($value['tableName'])
                 ->where($value['whereCondition'])
                 ->update($value['updateData']);
 
-            $this->info('Removed ' . $infoMsg);
+            $this->line('Removed ' . $infoMsg);
         }
 
-        $this->info('The command has finished!');
+        $chunkSize = $this->option('chunkSize');
+        $delay = $this->option('delay');
+
+        $infoMsg = 'SSN data created before "' . $selectedDate . '" in "website_form_submissions" table.';
+        $this->line('Removing ' . $infoMsg);
+
+        DB::table('website_form_submissions')
+            ->select('id', 'answers')
+            ->where([
+                ['created_at', '<', $selectedDate],
+            ])
+            ->whereNotNull('answers')
+            ->whereRaw("answers LIKE '%ssn%'")
+            ->orderBy('id')
+            ->chunkById($chunkSize, function (Collection $submissions) use ($delay) {
+                foreach ($submissions as $submission) {
+                    try {
+                        if (self::isValidJson($submission->answers)) {
+                            $alteredAnswer = null;
+                            $answersCollection = collect(json_decode($submission->answers, true));
+                            if($answersCollection->isNotEmpty() & $answersCollection->contains('name', 'ssn')) {
+                                $alteredAnswer = $answersCollection->reject(function ($value, $key) {
+                                    return $value['name'] === 'ssn';
+                                });
+
+                                DB::table('website_form_submissions')
+                                    ->where('id', '=', $submission->id)
+                                    ->update(['answers' => $alteredAnswer->toJson()]);
+                            }
+                        } else {
+                            $this->line('Invalid JSON for id: ' . $submission->id);
+                        }
+                    } catch (\Throwable $th) {
+                        $this->line($th->getMessage());
+                    }
+                }
+
+                // We will sleep for a certain seconds
+                sleep($delay);
+            });
+
+        $this->line('Removed ' . $infoMsg);
+
+        $this->line('The command has finished!');
 
         return 0;
+    }
+
+    private static function isValidJson(string $string): bool
+    {
+        json_decode($string);
+        return json_last_error() === JSON_ERROR_NONE;
     }
 
     /**
@@ -75,15 +133,6 @@ class PruneSSNCommand extends Command
                     ['date_imported', '<', $selectedDate],
                 ],
             ],
-            /* [
-                'tableName' => 'dealer_employee',
-                'updateData' => [
-                    'ssn' => null,
-                ],
-                'whereCondition' => [
-                    ['date_imported', '<', $selectedDate],
-                ],
-            ], */
         ];
     }
 }
