a2885ee3ea359347b4fa1871c00e92c041dac5c0
zhao songxun <songxunzhao1991@gmail.com>
2023-02-03 06:49:21 -0500

    #TR-571: Added location api

---
+++ app/Http/Requests/WebsiteUser/CreateLocationRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Requests\WebsiteUser;
+
+use App\Http\Requests\CreateRequestInterface;
+use App\Http\Requests\Request;
+
+class CreateLocationRequest extends Request implements CreateRequestInterface
+{}
