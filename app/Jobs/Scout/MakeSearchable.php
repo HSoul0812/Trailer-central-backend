4f814b54f893320b75ee1c4e47f52737d1f7f5ef
Rober <rad8329@gmail.com>
2023-02-09 06:02:58 -0500

    #CDW-1187 `WithTags` trait was implemented and used in `MakeSearchable`

--- app/Jobs/Scout/MakeSearchable.php
+++ app/Jobs/Scout/MakeSearchable.php
@@ -2,58 +2,9 @@
 
 namespace App\Jobs\Scout;
 
-use App\Jobs\Job;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\SerializesModels;
-use Laravel\Horizon\Tags;
+use App\Traits\Horizon\WithTags;
 
-class MakeSearchable implements ShouldQueue
+class MakeSearchable extends \Laravel\Scout\Jobs\MakeSearchable
 {
-    use Queueable, SerializesModels;
-
-    /**
-     * The models to be made searchable.
-     *
-     * @var \Illuminate\Database\Eloquent\Collection
-     */
-    public $models;
-
-    public function tags(): array
-    {
-        $tags = Tags::modelsFor(Tags::targetsFor($this))->map(function ($model): string {
-            return get_class($model).':'.$model->getKey();
-        })->all();
-
-        if (Job::batchId()) {
-            $tags[] = Job::batchId();
-        }
-
-        return $tags;
-    }
-
-    /**
-     * Create a new job instance.
-     *
-     * @param  \Illuminate\Database\Eloquent\Collection  $models
-     * @return void
-     */
-    public function __construct($models)
-    {
-        $this->models = $models;
-    }
-
-    /**
-     * Handle the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-        if (count($this->models) === 0) {
-            return;
-        }
-
-        $this->models->first()->searchableUsing()->update($this->models);
-    }
+    use WithTags;
 }
