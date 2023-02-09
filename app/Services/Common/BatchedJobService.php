c5aee8e38f249d0141d8a32fbb624e864ba6e23e
Rober <rad8329@gmail.com>
2023-02-08 19:03:33 -0500

    #CDW-1187 `BatchedJobService` was implemented

---
+++ app/Services/Common/BatchedJobService.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Services\Common;
+
+use App\Models\BatchedJob;
+use Illuminate\Contracts\Redis\Factory as RedisFactory;
+use Laravel\Horizon\Contracts\JobRepository;
+use Laravel\Horizon\Contracts\TagRepository;
+use Str;
+
+class BatchedJobService implements BatchedJobServiceInterface
+{
+    private const COMPLETED = 'completed';
+
+    private const WAIT_TIME = 2;
+
+    private const NO_GROUP = 'no-group';
+
+    /** @var \Laravel\Horizon\Contracts\TagRepository */
+    private $tagRepository;
+
+    /** @var \Laravel\Horizon\Contracts\JobRepository */
+    private $jobRepository;
+
+    /** @var \Illuminate\Contracts\Redis\Factory */
+    private $redis;
+
+    public function __construct(TagRepository $tagRepository, JobRepository $jobRepository, RedisFactory $redis)
+    {
+        $this->tagRepository = $tagRepository;
+        $this->jobRepository = $jobRepository;
+        $this->redis = $redis;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(?string $group = null): BatchedJob
+    {
+        $batch = BatchedJob::create([
+            'batch_id' => Str::uuid()->toString(),
+            'group' => $group ?: self::NO_GROUP
+        ]);
+
+        $this->tagRepository->monitor($batch->batch_id);
+
+        return $batch;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function stop(BatchedJob $batch): void
+    {
+        $processed_jobs = $batch->total_jobs - $this->count($batch);
+
+        $this->update($batch, [
+            'processed_jobs' => $processed_jobs,
+            'failed_jobs' => $batch->total_jobs - $processed_jobs
+        ]);
+
+        $this->tagRepository->stopMonitoring($batch->batch_id);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function monitor(BatchedJob $batch): void
+    {
+        do {
+            $this->wait();
+        } while ($this->isRunning($batch));
+
+        $this->forget($batch);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function count(BatchedJob $batch): int
+    {
+        return $this->tagRepository->count($batch->batch_id);
+    }
+
+    /**
+     * Determines if the batch is still running based on jobs numbers, when it is zero, that means it was fully processed
+     *
+     * @param  BatchedJob  $batch
+     * @return bool
+     */
+    protected function isRunning(BatchedJob $batch): bool
+    {
+        $jobIds = $this->tagRepository->jobs($batch->batch_id);
+
+        $this->jobRepository->getJobs($jobIds)->each(function (\stdClass $job) use ($batch, &$jobIds) {
+            if ($job->status === self::COMPLETED) {
+                $this->connection()->zrem($batch->batch_id, $job->id);
+                unset($jobIds[$job->id]);
+            }
+        });
+
+        return count($jobIds);
+    }
+
+    /**
+     * Updates a given batch by using given properties
+     *
+     * @param  BatchedJob  $job
+     * @param  array  $properties
+     * @return BatchedJob
+     */
+    protected function update(BatchedJob $job, array $properties): BatchedJob
+    {
+        $job->fill($properties)->save();
+
+        return $job;
+    }
+
+    /**
+     * Forget a batch given a batch
+     *
+     * @param  BatchedJob  $batch
+     * @return void
+     */
+    protected function forget(BatchedJob $batch): void
+    {
+        $this->tagRepository->forget($batch->batch_id);
+    }
+
+    /**
+     * Get the Redis connection instance.
+     *
+     * @return \Illuminate\Redis\Connections\Connection
+     */
+    protected function connection()
+    {
+        return $this->redis->connection('horizon');
+    }
+
+    protected function wait(): void
+    {
+        sleep(self::WAIT_TIME);
+    }
+}
---
+++ app/Services/Common/BatchedJobServiceInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Common;
+
+use App\Models\BatchedJob;
+
+interface BatchedJobServiceInterface
+{
+    /**
+     * Creates a batch, then start to monitoring it
+     *
+     * @param  string|null  $group
+     * @return BatchedJob
+     */
+    public function create(?string $group = null): BatchedJob;
+
+    /**
+     * Stop monitoring a given batch
+     *
+     * @param  BatchedJob  $batch
+     * @return void
+     */
+    public function stop(BatchedJob $batch): void;
+
+    /**
+     * Count number of jobs for a given batch
+     *
+     * @param  BatchedJob  $batch
+     * @return int
+     */
+    public function count(BatchedJob $batch): int;
+
+    /**
+     * Monitors a given batch, it will lock the current process until it is finished, or some another
+     * rule provokes an batch interruption
+     *
+     * @param  BatchedJob  $batch
+     * @return void
+     */
+    public function monitor(BatchedJob $batch): void;
+}
