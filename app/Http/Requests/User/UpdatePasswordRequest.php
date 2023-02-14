ddd7383b563f1972fa8a22c18b0de4278693f51b
Li Jiang <li@trailercentral.com>
2023-02-14 16:28:27 +0800

    DMSS-1316 Account: Change password page should require the current password

--- app/Http/Requests/User/UpdatePasswordRequest.php
+++ app/Http/Requests/User/UpdatePasswordRequest.php
@@ -14,7 +14,8 @@ class UpdatePasswordRequest extends Request
     protected $rules = [
         'dealer_id' => 'integer|min:1|required|exists:dealer,dealer_id',
         'dealer_user_id' => 'integer|min:1|exists:dealer_users,dealer_user_id',
-        'password' => ['required', 'min:6', 'max:8']
+        'password' => ['required', 'min:6', 'max:8'],
+        'current_password' => ['required'],
     ];
 
     public function messages(): array
