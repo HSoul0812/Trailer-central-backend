05d3f7344d7faaf12253fb61d5097384f1b6484c
Calo <Carlos@operatebeyond.com>
2023-02-16 15:00:10 -0500

    #NTGRTNS-715 Prevent Delete functionality

--- app/Nova/Policies/CollectorChangeReportPolicy.php
+++ app/Nova/Policies/CollectorChangeReportPolicy.php
@@ -2,9 +2,9 @@
 
 namespace App\Nova\Policies;
 
+use App\Models\Integration\Collector\CollectorChangeReport;
 use App\Models\User\NovaUser;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use App\Models\Integration\Collector\CollectorChangeReport;
 
 /**
  * Class CollectorChangeReportPolicy
@@ -14,25 +14,25 @@ class CollectorChangeReportPolicy
 {
     use HandlesAuthorization;
 
+    private const VALID_ROLES = ['Admin', 'Support'];
+
     /**
      * Create a new policy instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         //
     }
 
     /**
      * Determine whether the user can view any change reports.
      *
-     * @param  NovaUser $user
+     * @param NovaUser $user
      * @return bool
      */
-    public function viewAny(NovaUser $user): bool
-    {
-        return $user->hasAnyRole('Admin', 'Support');
+    public function viewAny(NovaUser $user): bool {
+        return $user->hasAnyRole(self::VALID_ROLES);
     }
 
     /**
@@ -42,19 +42,17 @@ class CollectorChangeReportPolicy
      * @param CollectorChangeReport $report
      * @return bool
      */
-    public function view(?NovaUser $user, CollectorChangeReport $report): bool
-    {
-        return $user->hasAnyRole('Admin', 'Support');
+    public function view(?NovaUser $user, CollectorChangeReport $report): bool {
+        return $user->hasAnyRole(self::VALID_ROLES);
     }
 
     /**
      * Determine whether the user can create change reports.
      *
-     * @param  NovaUser  $user
+     * @param NovaUser $user
      * @return bool
      */
-    public function create(NovaUser $user): bool
-    {
+    public function create(NovaUser $user): bool {
         return false;
     }
 
@@ -65,8 +63,7 @@ class CollectorChangeReportPolicy
      * @param CollectorChangeReport $report
      * @return bool
      */
-    public function update(NovaUser $user, CollectorChangeReport $report): bool
-    {
+    public function update(NovaUser $user, CollectorChangeReport $report): bool {
         return false;
     }
 
@@ -77,8 +74,7 @@ class CollectorChangeReportPolicy
      * @param CollectorChangeReport $report
      * @return bool
      */
-    public function delete(NovaUser $user, CollectorChangeReport $report): bool
-    {
+    public function delete(NovaUser $user, CollectorChangeReport $report): bool {
         return false;
     }
 
@@ -89,8 +85,7 @@ class CollectorChangeReportPolicy
      * @param CollectorChangeReport $report
      * @return void
      */
-    public function restore(NovaUser $user, CollectorChangeReport $report): void
-    {
+    public function restore(NovaUser $user, CollectorChangeReport $report): void {
         //
     }
 
@@ -101,8 +96,7 @@ class CollectorChangeReportPolicy
      * @param CollectorChangeReport $report
      * @return void
      */
-    public function forceDelete(NovaUser $user, CollectorChangeReport $report): void
-    {
+    public function forceDelete(NovaUser $user, CollectorChangeReport $report): void {
         //
     }
 }
--- app/Nova/Policies/UserPolicy.php
+++ app/Nova/Policies/UserPolicy.php
@@ -14,25 +14,25 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    private const VALID_ROLES = ['Admin'];
+
     /**
      * Create a new policy instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         //
     }
 
     /**
      * Determine whether the user can view any user.
      *
-     * @param  NovaUser  $user
+     * @param NovaUser $user
      * @return bool
      */
-    public function viewAny(NovaUser $user): bool
-    {
-        return $user->hasAnyRole('Admin');
+    public function viewAny(NovaUser $user): bool {
+        return $user->hasAnyRole(self::VALID_ROLES);
     }
 
     /**
@@ -42,20 +42,18 @@ class UserPolicy
      * @param NovaUser $novaUser
      * @return bool
      */
-    public function view(?NovaUser $user, NovaUser $novaUser): bool
-    {
-        return $user->hasAnyRole('Admin');
+    public function view(?NovaUser $user, NovaUser $novaUser): bool {
+        return $user->hasAnyRole(self::VALID_ROLES);
     }
 
     /**
      * Determine whether the user can create users.
      *
-     * @param  NovaUser  $user
+     * @param NovaUser $user
      * @return bool
      */
-    public function create(NovaUser $user): bool
-    {
-        return $user->hasAnyRole('Admin');
+    public function create(NovaUser $user): bool {
+        return $user->hasAnyRole(self::VALID_ROLES);
     }
 
     /**
@@ -65,9 +63,8 @@ class UserPolicy
      * @param NovaUser $novaUser
      * @return bool
      */
-    public function update(NovaUser $user, NovaUser $novaUser): bool
-    {
-        return $user->hasAnyRole('Admin');
+    public function update(NovaUser $user, NovaUser $novaUser): bool {
+        return $user->hasAnyRole(self::VALID_ROLES);
     }
 
     /**
@@ -77,9 +74,8 @@ class UserPolicy
      * @param NovaUser $novaUser
      * @return bool
      */
-    public function delete(NovaUser $user, NovaUser $novaUser): bool
-    {
-        return $user->hasAnyRole('Admin');
+    public function delete(NovaUser $user, NovaUser $novaUser): bool {
+        return false;
     }
 
     /**
@@ -89,8 +85,7 @@ class UserPolicy
      * @param NovaUser $novaUser
      * @return void
      */
-    public function restore(NovaUser $user, NovaUser $novaUser)
-    {
+    public function restore(NovaUser $user, NovaUser $novaUser) {
         //
     }
 
@@ -101,8 +96,7 @@ class UserPolicy
      * @param NovaUser $novaUser
      * @return void
      */
-    public function forceDelete(NovaUser $user, NovaUser $novaUser)
-    {
+    public function forceDelete(NovaUser $user, NovaUser $novaUser) {
         //
     }
 }
