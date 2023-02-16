c857bbee4e1220c94cfb2240a1a3cfa3cd4313d5
Pond <pond@trailercentral.com>
2023-02-16 21:09:24 +0700

    TR-811: Block bots from accessing google map routes

---
+++ app/Http/Middleware/HumanOnly.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Jaybizzle\LaravelCrawlerDetect\Facades\LaravelCrawlerDetect;
+use Symfony\Component\HttpFoundation\Response;
+
+class HumanOnly
+{
+    /**
+     * List of user agent that we want to allow the request to go through
+     *
+     * @var string[]
+     */
+    private array $allowUserAgents = [
+        // Postman use this one, and it's being seen as a bot from the Crawler class,
+        // so we need to add it here
+        'PostmanRuntime',
+    ];
+
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$this->shouldAllowRequestToGoThrough($request)) {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+
+    private function shouldAllowRequestToGoThrough(Request $request): bool
+    {
+        $userAgent = $request->userAgent();
+
+        // Do not allow empty user agent to go through
+        if (empty($userAgent)) {
+            return false;
+        }
+
+        // Allow request to go through if it's in the allow list
+        if ($this->allowUserAgent($userAgent)) {
+            return true;
+        }
+
+        // Do not allow request to go through if it's from a bot
+        // Ref: https://github.com/JayBizzle/Crawler-Detect/blob/master/raw/Crawlers.json
+        return !LaravelCrawlerDetect::isCrawler();
+    }
+
+    private function allowUserAgent(string $userAgent): bool
+    {
+        foreach ($this->allowUserAgents as $allowUserAgent) {
+            if (str_contains($userAgent, $allowUserAgent)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- routes/api/v1/map_search.php
+++ routes/api/v1/map_search.php
@@ -7,9 +7,12 @@ use App\Http\Controllers\v1\MapSearch\MapSearchController;
 $api = app(Dingo\Api\Routing\Router::class);
 
 $api->version('v1', function ($api) {
-    $api->group(['prefix' => '/map_search'], function ($api) {
+    $api->group(['prefix' => '/map_search', 'middleware' => ['human-only']], function ($api) {
         $api->get('/geocode', [MapSearchController::class, 'geocode']);
         $api->get('/autocomplete', [MapSearchController::class, 'autocomplete']);
         $api->get('/reverse', [MapSearchController::class, 'reverse']);
+        $api->get('/test', function() {
+            return [];
+        });
     });
 });
