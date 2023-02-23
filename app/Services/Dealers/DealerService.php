299d4d77d4c31d02eaf75a0f3890e43969b6d80a
Pond <pond@trailercentral.com>
2023-02-23 11:26:57 +0700

    TR-807: Code refactoring

--- app/Services/Dealers/DealerService.php
+++ app/Services/Dealers/DealerService.php
@@ -2,16 +2,34 @@
 
 namespace App\Services\Dealers;
 
+use Closure;
 use Http;
+use Illuminate\Http\Client\Response;
+use Log;
+use Throwable;
 
 class DealerService implements DealerServiceInterface
 {
     const ENDPOINT_USERS_BY_NAME = '/users-by-name';
-    
+
     public function listByName(string $name): ?array
     {
-        return Http::tcApi()->get(self::ENDPOINT_USERS_BY_NAME, [
-            'name' => $name,
-        ])->json('data');
+        return $this->handleHttpRequest('GET', self::ENDPOINT_USERS_BY_NAME, [
+            'query' => [
+                'name' => $name,
+            ],
+        ])?->json('data');
+    }
+
+    private function handleHttpRequest(string $method, string $url, array $options = []): ?Response
+    {
+        try {
+            return Http::tcApi()->send($method, $url, $options);
+        } catch (Throwable $e) {
+            Log::info('Exception was thrown while calling TrailerCentral API.');
+            Log::info($e->getCode() . ': ' . $e->getMessage());
+        }
+
+        return null;
     }
 }
