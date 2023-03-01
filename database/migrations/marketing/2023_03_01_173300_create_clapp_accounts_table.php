50f7412e7176e2d9dfde9bf91c01638af23b0c4b
David A Conway Jr <david.a.conway.jr@gmail.com>
2023-03-01 16:59:37 -0600

    * MARK-306 Added Support for Virtual Cards and Clapp Accounts

--- database/migrations/marketing/2023_03_01_173300_create_clapp_accounts_table.php
+++ database/migrations/marketing/2023_03_01_173300_create_clapp_accounts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Integration\Auth\AccessToken;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,9 +15,26 @@ class CreateClappAccountsTable extends Migration
     public function up()
     {
         Schema::create('clapp_accounts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('dealer_id')->index();
+            $table->integer('profile_id')->nullable()->index();
+            $table->integer('virtual_card_id')->nullable()->index();
+            $table->string('username')->index();
+            $table->string('password');
+            $table->string('smtp_password');
+            $table->string('smtp_server');
+            $table->integer('smtp_port', 10);
+            $table->string('smtp_security', 10);
+            $table->string('smtp_auth');
+            $table->string('imap_password');
+            $table->string('imap_server');
+            $table->integer('imap_port', 10);
+            $table->string('imap_security', 10);
             $table->timestamps();
         });
+
+        // Update Integration Token Relation Type
+        DB::statement("ALTER TABLE integration_token MODIFY COLUMN token_type ENUM('" . implode("', '", array_keys(AccessToken::TOKEN_TYPES)) . "')");
     }
 
     /**
