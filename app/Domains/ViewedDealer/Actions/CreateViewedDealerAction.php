f7bb514bbe1a23bd4d840df91a81606e875092ef
Pond <pond@trailercentral.com>
2023-03-01 23:15:11 +0700

    HOTFIX: Cache the viewed dealers in Redis for faster read speed

--- app/Domains/ViewedDealer/Actions/CreateViewedDealerAction.php
+++ app/Domains/ViewedDealer/Actions/CreateViewedDealerAction.php
@@ -9,15 +9,13 @@ use Arr;
 use DB;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Str;
 use Throwable;
 
 class CreateViewedDealerAction
 {
-    /**
-     * A regular expression to capture the dealer id from the error message
-     */
-    const REGEX_CAPTURE_DEALER_ID = '/(Key \(dealer_id\)=\((?<dealerId>\d*)\))/m';
+    const VIEWED_DEALER_CACHE_SECONDS = 86_400;
 
     /**
      * Upsert the given dealer_id & name pair using name as unique identifier
@@ -25,37 +23,49 @@ class CreateViewedDealerAction
      * a new record
      *
      * @param array<int, array{dealer_id: int, name: string}> $viewedDealers
-     * @return Collection
-     * @throws DealerIdExistsException
+     * @return array
      * @throws Throwable
      */
-    public function execute(array $viewedDealers): Collection
+    public function execute(array $viewedDealers): array
     {
         $viewedDealers = $this->transformAndValidate($viewedDealers);
 
-        DB::transaction(function () use ($viewedDealers) {
+        $returnValues = collect([]);
+
+        DB::transaction(function () use ($viewedDealers, $returnValues) {
             foreach ($viewedDealers as $viewedDealer) {
-                try {
-                    /** @var ViewedDealer $viewedDealer */
-                    $model = ViewedDealer::firstOrNew([
-                        'name' => $viewedDealer['name'],
-                    ]);
+                $nameSlug = Str::of($viewedDealer['name'])->slug();
 
-                    $model->fill([
-                        'dealer_id' => $viewedDealer['dealer_id'],
-                        'inventory_id' => $viewedDealer['inventory_id'],
-                    ]);
+                $cachedModel = Cache::remember(
+                    key: "viewed-dealer.$nameSlug",
+                    ttl: self::VIEWED_DEALER_CACHE_SECONDS,
+                    callback: function () use ($viewedDealer) {
+                        /** @var ViewedDealer $viewedDealer */
+                        $model = ViewedDealer::firstOrNew([
+                            'name' => $viewedDealer['name'],
+                        ]);
 
-                    $model->save();
-                } catch (QueryException $exception) {
-                    throw $this->captureQueryException($exception);
+                        $model->fill([
+                            'dealer_id' => $viewedDealer['dealer_id'],
+                            'inventory_id' => $viewedDealer['inventory_id'],
+                        ]);
+
+                        try {
+                            $model->save();
+                        } catch (Throwable) {
+                            return null;
+                        }
+
+                        return $model->toArray();
+                    });
+
+                if ($cachedModel !== null) {
+                    $returnValues->push($cachedModel);
                 }
             }
         });
 
-        return ViewedDealer::query()
-            ->whereIn('name', Arr::pluck($viewedDealers, 'name'))
-            ->get();
+        return $returnValues->toArray();
     }
 
     /**
@@ -117,35 +127,5 @@ class CreateViewedDealerAction
         }
 
         return array_values($dealerIds);
-    }
-
-    /**
-     * Transform the query exception if it's the duplicate dealer id error
-     * we want to do this, so we only have unique dealer id at one time in the viewed_dealers table
-     *
-     * @param QueryException $exception
-     * @return QueryException|DealerIdExistsException
-     */
-    private function captureQueryException(QueryException $exception): QueryException|DealerIdExistsException
-    {
-        $message = Str::of($exception->getMessage());
-
-        $isDuplicateDealerIdError = $message->contains('duplicate key value violates unique constraint "viewed_dealers_dealer_id_unique"');
-
-        // We don't transform it if it's not the duplicate dealer id error
-        if (!$isDuplicateDealerIdError) {
-            return $exception;
-        }
-
-        preg_match(self::REGEX_CAPTURE_DEALER_ID, $message, $matches, PREG_OFFSET_CAPTURE);
-
-        $dealerId = Arr::get($matches, 'dealerId.0');
-
-        // We won't return custom error message if we can't find the dealer id from the error message
-        if ($dealerId === null) {
-            return $exception;
-        }
-
-        return DealerIdExistsException::make($dealerId);
     }
 }
