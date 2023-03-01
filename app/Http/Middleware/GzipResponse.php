9dfbf74311053dcd3cb737f26fbbf74aa1498dfe
Pond <pond@trailercentral.com>
2023-03-01 15:05:56 +0700

    TR-656: Do not compress the response if the client doesn't accept it

--- app/Http/Middleware/GzipResponse.php
+++ app/Http/Middleware/GzipResponse.php
@@ -4,27 +4,37 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Dingo\Api\Http\Response as DingoResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Http\Response as IlluminateResponse;
+use Str;
 
 class GzipResponse
 {
+    const HEADER_ACCEPT_ENCODING = 'Accept-Encoding';
+
+    const HEADER_CONTENT_ENCODING = 'Content-Encoding';
+
     const COMPRESSION_LEVEL = 9;
 
-    public function handle(Request $request, Closure $next): IlluminateResponse
+    public function handle(Request $request, Closure $next): mixed
     {
         $response = $next($request);
 
-        if ($response instanceof DingoResponse) {
-            return $this->compressDingoResponse($response);
+        // No need to compress if the request can't accept it
+        if (!$this->shouldCompress($request)) {
+            return $response;
         }
 
-        if ($response instanceof IlluminateResponse) {
-            return $this->compressIlluminateResponse($response);
+        if (gettype($response) === 'string') {
+            return $response;
         }
 
-        return $next($response);
+        return match (get_class($response)) {
+            DingoResponse::class => $this->compressDingoResponse($response),
+            JsonResponse::class => $this->compressIlluminateResponse($response),
+            default => $response,
+        };
     }
 
     private function compressDingoResponse(DingoResponse $response): Response
@@ -32,7 +42,7 @@ class GzipResponse
         return $this->encodedResponse($response->morph()->content());
     }
 
-    private function compressIlluminateResponse(IlluminateResponse $response): Response
+    private function compressIlluminateResponse(JsonResponse $response): Response
     {
         return $this->encodedResponse($response->content());
     }
@@ -44,7 +54,14 @@ class GzipResponse
         return response($data)->withHeaders([
             'Content-type' => 'application/json; charset=utf-8',
             'Content-Length' => strlen($data),
-            'Content-Encoding' => 'gzip'
+            self::HEADER_CONTENT_ENCODING => 'gzip'
         ]);
+    }
+
+    private function shouldCompress(Request $request): bool
+    {
+        $acceptEncoding = $request->header(self::HEADER_ACCEPT_ENCODING, '');
+
+        return Str::of($acceptEncoding)->contains('gzip');
     }
 }
