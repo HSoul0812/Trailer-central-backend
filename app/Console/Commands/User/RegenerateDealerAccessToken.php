bafec72e2abb6f0d3cf28ea21e56aa767a42619b
Ethan Rivas <ethan@operatebeyond.com>
2023-03-02 10:32:18 -0600

    #NTGRTNS-753 Use SQL instead to improve memory usage and execution time

--- app/Console/Commands/User/RegenerateDealerAccessToken.php
+++ app/Console/Commands/User/RegenerateDealerAccessToken.php
@@ -2,8 +2,7 @@
 
 namespace App\Console\Commands\User;
 
-use App\Models\User\User;
-use App\Models\User\AuthToken;
+use DB;
 use Illuminate\Console\Command;
 
 /**
@@ -25,43 +24,11 @@ class RegenerateDealerAccessToken extends Command
      */
     public function handle()
     {
-        // Get All Users
-        $addCount = 0;
-        $users = User::with('dealerUsers')->get();
-        foreach ($users as $user) {
-            // Get Auth Token for User!
-            $authToken = AuthToken::where([
-                ['user_id', $user->dealer_id],
-                ['user_type', 'dealer']
-            ])->first();
-            if (!empty($authToken)) {
-                $addCount++;
-                echo "Regenerating token for dealer id: {$user->dealer_id} ".PHP_EOL;
-                $authToken->update([
-                    'access_token' => uniqid()
-                ]);
-            }
+        DB::statement("UPDATE auth_token
+        SET access_token = UUID()
+        WHERE `user_type` IN ( 'dealer', 'dealer_user' );");
 
-            // Loop Dealer Users
-            if(!empty($user->dealerUsers)) {
-                foreach($user->dealerUsers as $dealerUser) {
-                    // Get Auth Token for User!
-                    $authToken = AuthToken::where([
-                        ['user_id', $dealerUser->dealer_user_id],
-                        ['user_type', 'dealer_user']
-                    ])->first();
-                    if (!empty($authToken)) {
-                        $addCount++;
-                        echo "Regenerating token for dealer user id: {$dealerUser->dealer_user_id} ".PHP_EOL;
-                        $authToken->update([
-                            'access_token' => uniqid()
-                        ]);
-                    }
-                }
-            }
-        }
-
-        echo "Regenerated {$addCount} access tokens".PHP_EOL;
+        echo "Regenerated all dealer access tokens" . PHP_EOL;
     }
 }
 
