152cd9c2ed0520ab17171d81b8216a395d0a0e33
David A Conway Jr <david.a.conway.jr@gmail.com>
2023-03-03 03:05:53 -0600

    * MARK-311 Fixed Returning Prices on Profile

--- app/Models/Marketing/Craigslist/Profile.php
+++ app/Models/Marketing/Craigslist/Profile.php
@@ -11,7 +11,7 @@ use App\Models\Traits\TableAware;
 use Awobaz\Compoships\Compoships;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class Profile
