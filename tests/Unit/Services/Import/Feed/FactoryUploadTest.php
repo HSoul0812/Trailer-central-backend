d444e15b2d370f82cf26e5e360ad6085a2bfd7f7
Ethan Rivas <ethan@operatebeyond.com>
2023-03-08 16:49:28 -0600

    #NTGRTNS-698 Update FactoryUpload Test

--- tests/Unit/Services/Import/Feed/FactoryUploadTest.php
+++ tests/Unit/Services/Import/Feed/FactoryUploadTest.php
@@ -41,7 +41,7 @@ class FactoryUploadTest extends TestCase
 
     /**
      *
-     * @dataProvider validDataProvider
+     * @dataProvider validInventoryDataProvider
      *
      * @throws PropertyDoesNotExists
      */
@@ -69,64 +69,113 @@ class FactoryUploadTest extends TestCase
 
     /**
      *
+     * @dataProvider validDealerDataProvider
+     *
+     * @throws PropertyDoesNotExists
+     */
+    public function testCanAddDealer($code, $type, $dealerId, $data) {
+        $decode = json_decode($data, true);
+        $transaction = $decode['transactions'][0];
+
+        $this->uploadRepository
+            ->shouldReceive('createOrUpdate')
+            ->with([
+                'code' => $code,
+                'key' => $dealerId,
+                'type' => $type,
+                'data' => json_encode($transaction['parameters']),
+            ], $code, $dealerId)
+            ->once();
+
+        Log::shouldReceive('info')->with("{$code} Import: adding dealer", [
+            'dealer' => $transaction['parameters']
+        ]);
+
+        $upload = app(FactoryUpload::class);
+        $result = $upload->run(json_decode($data, true));
+
+        $this->assertNull($result);
+    }
+
+    /**
+     *
      * @dataProvider invalidDataProvider
      *
      * @throws PropertyDoesNotExists
      */
-    public function testCantAddInventory($data) {
-        $this->expectExceptionMessage('transactions invalid or not found in rawData');
+    public function testCantAddInventory($code, $type, $vin, $data) {
+        $decode = json_decode($data, true);
+
+        if (empty($decode['transactions'])) {
+            $this->expectExceptionMessage('transactions invalid or not found in rawData');
+        }
 
         $upload = app(FactoryUpload::class);
-        $upload->run(json_decode($data, true));
+        $upload->run($decode);
     }
 
-    public function validDataProvider(): array
+    /**
+     *
+     * @dataProvider invalidEmptyVinOrVinNoDataProvider
+     *
+     * @throws PropertyDoesNotExists
+     */
+    public function testInventoryWithoutVinOrVinNo($code, $type, $vin, $data) {
+        $decode = json_decode($data, true);
+        $inventory = json_encode($decode['transactions'][0]['parameters']);
+
+        $this->assertTrue(empty($inventory['vin']) && empty($inventory['vin_no']));
+
+        $this->uploadRepository
+            ->shouldNotReceive('createOrUpdate');
+
+        $upload = app(FactoryUpload::class);
+        $upload->run($decode);
+    }
+
+    /**
+     *
+     * @dataProvider invalidActionDataProvider
+     *
+     * @throws PropertyDoesNotExists
+     */
+    public function testTransactionWithInvalidAction($code, $type, $vin, $data) {
+        $decode = json_decode($data, true);
+        $transaction = $decode['transactions'][0];
+
+        $this->assertTrue($transaction['action'] !== 'addInventory' && $transaction['action'] !== 'addDealer');
+
+        Log::shouldReceive('warning')->with("{$code} Import: invalid action {$transaction['action']}");
+
+        $this->uploadRepository
+            ->shouldNotReceive('createOrUpdate');
+
+        $upload = app(FactoryUpload::class);
+        $upload->run($decode);
+    }
+
+    /**
+     *
+     * @dataProvider invalidEmptyActionDataProvider
+     *
+     * @throws PropertyDoesNotExists
+     */
+    public function testInventoryWithoutTransactionAction($code, $type, $vin, $data) {
+        $decode = json_decode($data, true);
+        $inventory = json_encode($decode['transactions'][0]['parameters']);
+
+        $this->assertTrue(empty($inventory['action']));
+
+        $this->uploadRepository
+            ->shouldNotReceive('createOrUpdate');
+
+        $upload = app(FactoryUpload::class);
+        $upload->run($decode);
+    }
+
+    public function validInventoryDataProvider(): array
     {
         return [
-            'Norstar' => [
-                'code' => 'norstar',
-                'type' => 'inventory',
-                'vin' => '50HDB1422P1087058',
-                'data' => '{
-                    "code":"norstar",
-                    "transactions": [
-                        {
-                            "action":"addInventory",
-                            "parameters": {
-                                "stock":"DTB8314072ES2R50S62BLK",
-                                "vin":"50HDB1422P1087058",
-                                "model":"Dump",
-                                "year":"2022",
-                                "manufacturer":"NORSTAR",
-                                "brand":"IRON BULL",
-                                "condition":"new",
-                                "msrp":"14467.15",
-                                "price":"9910",
-                                "category":"Dump",
-                                "dealer_id":null,
-                                "dealer_name":"271 TRAILERS",
-                                "dealer_email":null,
-                                "dealer_location_street":"2601 N Main St",
-                                "dealer_location_city":"Paris",
-                                "dealer_location_state":"TX",
-                                "dealer_location_zip":"75460-9354",
-                                "dealer_location_phone":"90378312",
-                                "color_interior":"Black",
-                                "color_exterior":"Black",
-                                "attributes_length":"14",
-                                "attributes_width":"83",
-                                "attributes_gvwr":"14999",
-                                "attributes_axle_capacity":"7000",
-                                "attributes_axle_count":"2",
-                                "attributes_hitch_type":"Bumper",
-                                "ship_date":"2/22/2023",
-                                "photos":"https://statics.mynorstar.com/Quotes/Cap_7_cznrc{uzitymj{cu.png",
-                                "source":"NORSTAR"
-                            }
-                        }
-                    ]
-                }'
-            ],
             'Lt' => [
                 'code' => 'lt',
                 'type' => 'inventory',
@@ -181,6 +230,35 @@ class FactoryUploadTest extends TestCase
         ];
     }
 
+    public function validDealerDataProvider(): array
+    {
+        return [
+            'Norstar' => [
+                'code' => 'norstar',
+                'type' => 'dealer',
+                'dealerId' => '12312',
+                'data' => '{
+                    "code":"norstar",
+                    "transactions": [
+                        {
+                            "action":"addDealer",
+                            "parameters": {
+                                "locationId":41173,
+                                "dealerId":12312,
+                                "address":"100 Main Street",
+                                "city":"Dodge",
+                                "state":"Nevada",
+                                "zip":"54321",
+                                "name":"ACME corp",
+                                "locationName":"Some Location"
+                            }
+                        }
+                    ]
+                }'
+            ]
+        ];
+    }
+
     public function invalidDataProvider(): array
     {
         return [
@@ -191,6 +269,92 @@ class FactoryUploadTest extends TestCase
                 'data' => '{
                     "code":"norstar",
                     "transactions": []
+                }'
+            ]
+        ];
+    }
+
+    public function invalidEmptyVinOrVinNoDataProvider(): array
+    {
+        return [
+            'Lt without vin or vin_no' => [
+                'code' => 'lt',
+                'type' => 'inventory',
+                'vin' => '',
+                'data' => '{
+                    "code":"lt",
+                    "transactions": [
+                        {
+                            "action":"addInventory",
+                            "parameters": {
+                                "descrip2": "102\" x 20\' Deck Over Pintle Hook",
+                                "model_yr": "2022",
+                                "arcusto_id": "25581",
+                                "arcusto_company": "JacksSon\'s Trailers, Inc.",
+                                "ship_date": "2022-12-02",
+                                "data1": "6\" Channel Frame\r2 - 7,000 Lb Dexter Spring Axles (2 Elec FSA Brakes)\rST235\/80 R16 LRE 10 Ply. (BLACK WHEELS)\rCoupler 2-5\/16\" Adjustable (6 HOLE)\rTreated Wood Floor\rREAR Slide-IN Ramps 8\' x 16\"\r16\" Cross-Members\rJack Spring Loaded Drop Leg 1-10K\rLights LED (w\/Cold Weather Harness)\r4 - D-Rings 3\" Weld On\rSpare Tire Mount\rBlack (w\/Primer)\rRoad Service Program 903-783-3933 for Info.\r\r",
+                                "series": "DK14"
+                            }
+                        }
+                    ]
+                }'
+            ]
+        ];
+    }
+
+    public function invalidEmptyActionDataProvider(): array
+    {
+        return [
+            'Lt with empty action' => [
+                'code' => 'lt',
+                'type' => 'inventory',
+                'vin' => '4ZEDK2021N1276855',
+                'data' => '{
+                    "code":"lt",
+                    "transactions": [
+                        {
+                            "action":"",
+                            "parameters": {
+                                "vin_no": "4ZEDK2021N1276855",
+                                "descrip2": "102\" x 20\' Deck Over Pintle Hook",
+                                "model_yr": "2022",
+                                "arcusto_id": "25581",
+                                "arcusto_company": "JacksSon\'s Trailers, Inc.",
+                                "ship_date": "2022-12-02",
+                                "data1": "6\" Channel Frame\r2 - 7,000 Lb Dexter Spring Axles (2 Elec FSA Brakes)\rST235\/80 R16 LRE 10 Ply. (BLACK WHEELS)\rCoupler 2-5\/16\" Adjustable (6 HOLE)\rTreated Wood Floor\rREAR Slide-IN Ramps 8\' x 16\"\r16\" Cross-Members\rJack Spring Loaded Drop Leg 1-10K\rLights LED (w\/Cold Weather Harness)\r4 - D-Rings 3\" Weld On\rSpare Tire Mount\rBlack (w\/Primer)\rRoad Service Program 903-783-3933 for Info.\r\r",
+                                "series": "DK14"
+                            }
+                        }
+                    ]
+                }'
+            ]
+        ];
+    }
+
+    public function invalidActionDataProvider(): array
+    {
+        return [
+            'Lt with invalid action' => [
+                'code' => 'lt',
+                'type' => 'inventory',
+                'vin' => '4ZEDK2021N1276855',
+                'data' => '{
+                    "code":"lt",
+                    "transactions": [
+                        {
+                            "action":"addUnit",
+                            "parameters": {
+                                "vin_no": "4ZEDK2021N1276855",
+                                "descrip2": "102\" x 20\' Deck Over Pintle Hook",
+                                "model_yr": "2022",
+                                "arcusto_id": "25581",
+                                "arcusto_company": "JacksSon\'s Trailers, Inc.",
+                                "ship_date": "2022-12-02",
+                                "data1": "6\" Channel Frame\r2 - 7,000 Lb Dexter Spring Axles (2 Elec FSA Brakes)\rST235\/80 R16 LRE 10 Ply. (BLACK WHEELS)\rCoupler 2-5\/16\" Adjustable (6 HOLE)\rTreated Wood Floor\rREAR Slide-IN Ramps 8\' x 16\"\r16\" Cross-Members\rJack Spring Loaded Drop Leg 1-10K\rLights LED (w\/Cold Weather Harness)\r4 - D-Rings 3\" Weld On\rSpare Tire Mount\rBlack (w\/Primer)\rRoad Service Program 903-783-3933 for Info.\r\r",
+                                "series": "DK14"
+                            }
+                        }
+                    ]
                 }'
             ]
         ];
