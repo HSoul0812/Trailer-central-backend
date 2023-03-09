4fcc460ee4a47e00d70019190d6fe4d2d7dedaec
Muhammad Nauman <muhammadnauman@trailercentral.com>
2023-03-09 17:07:18 +0500

    fix zip archive

--- app/Domains/DealerExports/CreateZipArchiveAction.php
+++ app/Domains/DealerExports/CreateZipArchiveAction.php
@@ -7,8 +7,6 @@ use App\Models\User\User;
 use ZipArchive;
 use Illuminate\Support\Facades\Storage;
 use Exception;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Facades\File;
 
 /**
  * Class CreateZipArchiveAction
@@ -39,34 +37,33 @@ class CreateZipArchiveAction
             ->where('entity_type', 'zip')
             ->first();
 
-        $zipFilePath = 'exports/'. $this->dealer->dealer_id . '/dealer-archive.zip';
+        $s3ZipFilePath = 'exports/'. $this->dealer->dealer_id . '/dealer-archive.zip';
+        $tmpZipFilePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $s3ZipFilePath;
 
         $zip = new ZipArchive();
-        Storage::disk('s3')->put($zipFilePath, '');
-        dump($zip->open(Storage::disk('s3')->url($zipFilePath), ZipArchive::CREATE | ZipArchive::OVERWRITE));
+        $zip->open($tmpZipFilePath, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
         $files = Storage::disk('s3')->files('exports/' . $this->dealer->dealer_id);
 
         foreach($files as $file) {
-            if ($file !== $zipFilePath) {
+            if ($file !== $s3ZipFilePath) {
                 try {
                     $fileToUpload = Storage::disk('s3')->get($file);
                     $zip->addFromString(basename($file), $fileToUpload);
-                    dump($zip->numFiles);
                 } catch (Exception $e) {
-                    dump($e);
                 }
 
             }
         }
         $zip->setPassword(decrypt($dealerExport->zip_password, false));
-        // dd($zip->count());
         if ($zip->close()) {
-            // Storage::disk('s3')->put($zipFilePath, File::get(public_path($zipFilePath)));
-
+            Storage::disk('s3')->put($s3ZipFilePath, $tmpZipFilePath);
             $dealerExport->update([
-                'file_path' => Storage::disk('s3')->url($zipFilePath),
+                'file_path' => Storage::disk('s3')->url($s3ZipFilePath),
+                'status' => DealerExport::STATUS_PROCESSED,
             ]);
+
+            unlink($tmpZipFilePath);
         }
     }
 }
