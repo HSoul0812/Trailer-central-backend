10e1688baeb69ea2ce37bf7de1dbaba0e08ab4b9
Mert Öksüz <xrefresh@gmail.com>
2023-03-14 14:48:59 +0300

    #CDW-1389 include manufacturer, series and brand into search_term

--- app/Repositories/Showroom/ShowroomRepository.php
+++ app/Repositories/Showroom/ShowroomRepository.php
@@ -85,7 +85,13 @@ class ShowroomRepository implements ShowroomRepositoryInterface {
         }
 
         if (isset($params['search_term'])) {
-            $query = $query->where('model', 'LIKE', '%' . $params['search_term'] . '%');
+
+            $query = $query->where(function ($q) use ($params) {
+                $q->where('model', 'LIKE', '%' . $params['search_term'] . '%')
+                    ->orWhere('manufacturer', 'LIKE', '%' . $params['search_term'] . '%')
+                    ->orWhere('series', 'LIKE', '%' . $params['search_term'] . '%')
+                ;
+            });
         }
 
         if (isset($params['manufacturer'])) {
