d154d6794a286d16226f3a699cd90b7fd0a5fb48
Pond <pond@trailercentral.com>
2023-03-17 21:40:15 +0700

    TR-834: Add feature test for upload local image

--- tests/Common/FeatureTestCase.php
+++ tests/Common/FeatureTestCase.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\Common;
 
+use App\Models\WebsiteUser\WebsiteUser;
+
 abstract class FeatureTestCase extends TestCase
 {
+    public function jwtAuthToken(): string
+    {
+        $websiteUser = WebsiteUser::factory()->create();
+
+        return auth('api')->login($websiteUser);
+    }
 }
