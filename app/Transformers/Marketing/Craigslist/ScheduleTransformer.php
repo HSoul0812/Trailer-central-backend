f74bd63fcb89d1cf13da64d71671a7c68d7106de
David A Conway Jr <david.a.conway.jr@gmail.com>
2023-03-17 15:04:34 -0500

    * MARK-323 Deleting Status Should be Prioritized Over Edit Status

--- app/Transformers/Marketing/Craigslist/ScheduleTransformer.php
+++ app/Transformers/Marketing/Craigslist/ScheduleTransformer.php
@@ -89,12 +89,12 @@ class ScheduleTransformer extends TransformerAbstract
         if ($queue->queueDeleted->count() > 0) {
             $className = 'deleted';
             $color = 'black';
-        } elseif ($queue->queueEdits->count() > 0) {
-            $className = 'edit';
-            $color = 'darkolivegreen';
         } elseif ($queue->queueDeleting->count() > 0) {
             $className = 'deleting';
             $color = 'darkred';
+        } elseif ($queue->queueEdits->count() > 0) {
+            $className = 'edit';
+            $color = 'darkolivegreen';
         } elseif ($queue->status === 'error' || $queue->status === 'canceled' ||
                   $queue->session->status === 'error') {
             if ($queue->state === 'missing-data' ||
