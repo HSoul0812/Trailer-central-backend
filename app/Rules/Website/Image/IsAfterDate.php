e4524a2956fed58895df866cc842b6df2db8d474
Norris Oduro <norrisjibril@gmail.com>
2023-03-21 11:07:54 +0000

    CDW-176: Update rule logic

--- app/Rules/Website/Image/IsAfterDate.php
+++ app/Rules/Website/Image/IsAfterDate.php
@@ -35,7 +35,9 @@ class IsAfterDate implements Rule
     {
         if ($this->startDate) {
             try {
-                return Carbon::parse($value)->gte(Carbon::parse($this->startDate));
+                $current = Carbon::parse($value);
+                $startDate = Carbon::parse($this->startDate);
+                return $current->isSameDay($startDate) || $current->isAfter($startDate);
             } catch (Exception $exception) {
                 return false;
             }
