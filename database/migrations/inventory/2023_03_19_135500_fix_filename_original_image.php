4b1c4929fa7642322d35a8d023074f275b843223
Rober <rad8329@gmail.com>
2023-03-28 16:06:05 -0500

    #CDW-1399 Filename columns are updated in a faster way
    - Since there are no indices, we need to use a where criteria by dealer_id
      to be able to perform this query with decent speed

--- database/migrations/inventory/2023_03_19_135500_fix_filename_original_image.php
+++ database/migrations/inventory/2023_03_19_135500_fix_filename_original_image.php
@@ -1,20 +1,28 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use stdClass as Dealer;
 
 class FixFilenameOriginalImage extends Migration
 {
     public function up(): void
     {
-        // to be able to swap the image with/without overlay to filename we need to have them stored as concrete columns
-        $updateFilenameCompanionsSQL = <<<SQL
-            UPDATE inventory_image ii
-            JOIN image i on ii.image_id = i.image_id
-            SET filename_without_overlay = IF(filename_noverlay IS NOT NULL AND filename_noverlay != '', filename_noverlay, filename),
-            filename_with_overlay = IF(filename_noverlay IS NOT NULL AND filename_noverlay != '', filename, NULL)
+        $dealers = DB::table('dealer')->select('dealer_id')->get();
+
+        $dealers->each(static function (Dealer $dealer): void {
+            // to be able to swap the image with/without overlay to filename we need to have them stored as concrete columns
+            $updateFilenameCompanionsSQL = <<<SQL
+                UPDATE image
+                    JOIN inventory_image on inventory_image.image_id = image.image_id
+                    JOIN inventory on inventory.inventory_id = inventory_image.inventory_id
+                SET
+                    filename_without_overlay = IF(filename_noverlay IS NOT NULL AND filename_noverlay != '', filename_noverlay, filename),
+                    filename_with_overlay = IF(filename_noverlay IS NOT NULL AND filename_noverlay != '', filename, NULL)
+                WHERE inventory.dealer_id = :dealer_id
 SQL;
 
-        DB::statement($updateFilenameCompanionsSQL);
+            DB::statement($updateFilenameCompanionsSQL, ['dealer_id' => $dealer->dealer_id]);
+        });
     }
 
     public function down(): void
