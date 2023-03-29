ff4e406e136d7eedf2fa41cf39f69c29b60c11e1
Rober <rad8329@gmail.com>
2023-03-29 16:37:47 -0500

    #CDW-1399 `ImageService::tryToRestoreOriginalImage` proper working was ensured

--- tests/Unit/Services/Inventory/ImageServiceTest.php
+++ tests/Unit/Services/Inventory/ImageServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Services\Inventory;
 
+use App\Exceptions\File\MissingS3FileException;
 use App\Jobs\Inventory\GenerateOverlayImageJobByDealer;
 use App\Jobs\Inventory\ReIndexInventoriesByDealersJob;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -38,6 +39,7 @@ class ImageServiceTest extends TestCase
     use WithFaker;
 
     const DEALER_ID = 1;
+
     /**
      * @var LegacyMockInterface|ImageRepositoryInterface
      */
@@ -102,69 +104,54 @@ class ImageServiceTest extends TestCase
     }
 
     /**
+     * Test that SUT will throw an specific exception when the S3 object doesn't exists
+     *
      * @group Marketing
      * @group Marketing_Overlays
+     * @covers ::saveOverlay
      */
-    public function testSaveOverlayWithEmptyNoverlay()
+    public function testSaveOverlayWillThrowExceptionWhenThereIsNotOverlayYet()
     {
-        Storage::disk('s3')->put('image_1', '');
-        Storage::disk('s3')->put('image_with_overlay_1', '');
+        $newImageOverlay = 'image_with_overlay_1';
 
         /** @var Image|LegacyMockInterface $image1 */
         $image1 = $this->getEloquentMock(Image::class);
         $image1->image_id = 1;
         $image1->filename = 'image_1';
-        $image1->filename_noverlay = '';
 
-        $this->imageRepositoryMock->shouldReceive('update')
-            ->once()
-            ->with([
-                'filename' => 'image_with_overlay_1',
-                'filename_with_overlay' => 'image_with_overlay_1',
-                'hash' => 'test_hash',
-                'id' => 1
-            ]);
+        $this->expectException(MissingS3FileException::class);
+        $this->expectExceptionMessage(sprintf("S3 object '%s' is missing", $newImageOverlay));
 
-        $this->imageService->expects($this->once())
-            ->method('getFileHash')
-            ->willReturn('test_hash');
-
-        $this->inventoryRepositoryMock->shouldReceive('markImageAsOverlayGenerated')
-            ->once()
-            ->with($image1->image_id);
-
-        $this->imageRepositoryMock->shouldReceive('scheduleObjectToBeDroppedByURL')
-            ->once()
-            ->with($image1->filename);
-
-        $this->imageService->saveOverlay($image1, 'image_with_overlay_1');
-
-        Storage::disk('s3')->assertExists([
-            'image_with_overlay_1',
-            'image_1'
-        ]);
+        $this->imageService->saveOverlay($image1, $newImageOverlay);
     }
 
     /**
+     * Test that SUT will save the image when the S3 object exists, then it will mark the image as overlay generated,
+     * finally it will mark previous S3 object to be dropped from storage
+     *
      * @group Marketing
      * @group Marketing_Overlays
+     * @covers ::saveOverlay
      */
-    public function testSaveOverlayWithExistingFilename()
+    public function testSaveOverlayWithEmptyNoverlay()
     {
-        Storage::disk('s3')->put('image_1', '');
-        Storage::disk('s3')->put('image_with_overlay_1', '');
-        Storage::disk('s3')->put('new_image_with_overlay_1', '');
+        Storage::fake('s3');
 
+        $oldImageOverlay = 'image_with_overlay_1_'.$this->faker->slug('2');
+        $newImageOverlay = 'image_with_overlay_2_'.$this->faker->slug('2');
+
+        Storage::disk('s3')->put($oldImageOverlay, '');
+        Storage::disk('s3')->put($newImageOverlay, '');
+
+        /** @var Image|LegacyMockInterface $image1 */
         $image1 = $this->getEloquentMock(Image::class);
         $image1->image_id = 1;
-        $image1->filename = 'image_with_overlay_1';
-        $image1->filename_noverlay = 'image_1';
+        $image1->filename = $oldImageOverlay;
 
-        $this->imageRepositoryMock->shouldReceive('update')
-            ->once()
+        $this->imageRepositoryMock->expects('update')
             ->with([
-                'filename' => 'new_image_with_overlay_1',
-                'filename_noverlay' => 'image_1',
+                'filename' => $newImageOverlay,
+                'filename_with_overlay' => $newImageOverlay,
                 'hash' => 'test_hash',
                 'id' => 1
             ]);
@@ -173,85 +160,80 @@ class ImageServiceTest extends TestCase
             ->method('getFileHash')
             ->willReturn('test_hash');
 
-        $this->imageService->saveOverlay($image1, 'new_image_with_overlay_1');
+        $this->inventoryRepositoryMock->expects('markImageAsOverlayGenerated')
+            ->with($image1->image_id);
+
+        $this->imageRepositoryMock->expects('scheduleObjectToBeDroppedByURL')
+            ->with($image1->filename);
+
+        $this->imageService->saveOverlay($image1, $newImageOverlay);
 
         Storage::disk('s3')->assertExists([
-            'new_image_with_overlay_1',
-            'image_1'
+            $oldImageOverlay,
+            $newImageOverlay
         ]);
-
-        /*
-         * // due we have avoided to remove from S3 bucket because it is causing ES6 have broken images
-         Storage::disk('s3')->assertMissing([
-            'image_with_overlay_1'
-        ]);
-        */
     }
 
     /**
+     * Test that SUT will do nothing when the image doesn't have an overlay yet
+     *
      * @group Marketing
      * @group Marketing_Overlays
+     * @covers ::tryToRestoreOriginalImage
      */
-    public function testResetOverlayWithEmptyNoverlay()
+    public function testResetOverlayWillDoNothingWhenNoOverlay()
     {
-        Storage::disk('s3')->put('image_1', '');
-
+        /** @var Image|LegacyMockInterface $image1 */
         $image1 = $this->getEloquentMock(Image::class);
         $image1->image_id = 1;
         $image1->filename = 'image_1';
-        $image1->filename_noverlay = '';
+        $image1->filename_with_overlay = null;
 
-        $this->imageService->expects($this->exactly(0))
-            ->method('getFileHash');
-
-        $this->imageRepositoryMock->shouldNotReceive('update');
+        $this->imageRepositoryMock->allows('update')->never();
 
         $this->imageService->tryToRestoreOriginalImage($image1);
-
-        Storage::disk('s3')->assertExists([
-            'image_1'
-        ]);
     }
 
     /**
+     * Test that SUT will reset the overlay image by restoring the original image, also both S3 objects should remains
+     *
      * @group Marketing
      * @group Marketing_Overlays
+     * @covers ::tryToRestoreOriginalImage
      */
-    public function testResetOverlayWithExistingFilename()
+    public function testResetOverlayWithEmptyNoverlay()
     {
-        Storage::disk('s3')->put('image_1', '');
-        Storage::disk('s3')->put('image_with_overlay_1', '');
+        Storage::fake('s3');
 
+        $imageOverlay = 'image_with_overlay_1_'.$this->faker->slug('2');
+        $originalImage = 'image_without_overlay_'.$this->faker->slug('2');
+
+        Storage::disk('s3')->put($imageOverlay, '');
+        Storage::disk('s3')->put($originalImage, '');
+
+        /** @var Image|LegacyMockInterface $image1 */
         $image1 = $this->getEloquentMock(Image::class);
         $image1->image_id = 1;
-        $image1->filename = 'image_with_overlay_1';
-        $image1->filename_noverlay = 'image_1';
-
-        $this->imageRepositoryMock->shouldReceive('update')
-            ->once()
-            ->with([
-                'filename' => 'image_1',
-                'filename_noverlay' => '',
-                'hash' => 'test_hash',
-                'id' => 1
-            ]);
+        $image1->filename = $imageOverlay;
+        $image1->filename_with_overlay = $imageOverlay;
+        $image1->filename_without_overlay = $originalImage;
 
         $this->imageService->expects($this->once())
             ->method('getFileHash')
             ->willReturn('test_hash');
 
+        $this->imageRepositoryMock->allows('update')->with([
+            'id' => $image1->image_id,
+            'hash' => 'test_hash',
+            'filename' => $image1->filename_without_overlay
+        ]);
+
         $this->imageService->tryToRestoreOriginalImage($image1);
 
         Storage::disk('s3')->assertExists([
-            'image_1'
+            $imageOverlay,
+            $originalImage
         ]);
-
-        // due we have avoided to remove from S3 bucket because it is causing ES6 have broken images
-        /*
-         Storage::disk('s3')->assertMissing([
-            'image_with_overlay_1'
-        ]);
-        */
     }
 
     /**
