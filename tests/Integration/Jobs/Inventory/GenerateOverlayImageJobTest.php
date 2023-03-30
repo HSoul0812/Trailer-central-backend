2b44d8bbccb854b45ff4b17135dcbf00759d55e0
Rober <rad8329@gmail.com>
2023-03-29 22:33:45 -0500

    #CDW-1399 `GenerateOverlayImageJob` proper working was ensured

--- tests/Integration/Jobs/Inventory/GenerateOverlayImageJobTest.php
+++ tests/Integration/Jobs/Inventory/GenerateOverlayImageJobTest.php
@@ -2,11 +2,9 @@
 
 namespace Tests\Integration\Jobs\Inventory;
 
+use Psr\Log\LoggerInterface;
 use Tests\TestCase;
-use App\Services\Inventory\InventoryServiceInterface;
-use App\Jobs\Inventory\GenerateOverlayImageJob;
 use Mockery\LegacyMockInterface;
-use Illuminate\Support\Facades\Log;
 
 class GenerateOverlayImageJobTest extends TestCase {
 
@@ -22,29 +20,5 @@ class GenerateOverlayImageJobTest extends TestCase {
         parent::setUp();
 
         $this->instanceMock('logMock', LoggerInterface::class);
-    }
-
-    /**
-     * @group Marketing
-     * @group Marketing_Overlays
-     */
-    public function testMissingInventoryExceptionMessage()
-    {
-        Log::shouldReceive('channel')
-            ->once()
-            ->andReturn($this->logMock);
-
-        $this->logMock->shouldReceive('error')
-            ->with(GenerateOverlayImageJob::MISSING_INVENTORY_ERROR_MESSAGE)
-            ->once();
-
-        $this->logMock->shouldReceive('error')
-            ->once();
-
-        $service = app()->make(InventoryServiceInterface::class);
-
-        $job = new GenerateOverlayImageJob(self::NON_EXISTING_INVENTORY_ID);
-
-        $job->handle($service);
     }
 }
