1f67aecbb930139a7375be14a12beef520a8abdd
Ethan Rivas <ethan@operatebeyond.com>
2023-03-31 07:51:24 -0600

    #NTGRTNS-784 Update new password request rules

--- app/Http/Requests/User/UpdatePasswordRequest.php
+++ app/Http/Requests/User/UpdatePasswordRequest.php
@@ -14,7 +14,7 @@ class UpdatePasswordRequest extends Request
     protected $rules = [
         'dealer_id' => 'integer|min:1|required|exists:dealer,dealer_id',
         'dealer_user_id' => 'integer|min:1|exists:dealer_users,dealer_user_id',
-        'password' => ['required', 'min:6', 'max:8'],
+        'password' => ['required', 'min:8', 'max:8', 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/'],
         'current_password' => ['required'],
     ];
 
