1d20f2c0c383d3e2d0286121420559be666ce5ef
Rober <rad8329@gmail.com>
2023-04-04 15:03:03 -0500

    Migration script fixes for image overlay

--- app/Console/Commands/Inventory/FixInventoryImageOverlayUrls.php
+++ app/Console/Commands/Inventory/FixInventoryImageOverlayUrls.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands\Inventory;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use App\Models\User as Dealer;
+use stdClass as Dealer;
 
 class FixInventoryImageOverlayUrls extends Command
 {
