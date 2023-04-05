43a97b620228e7bb0d19e727ec219cf359b5cf13
HuascarOna <huascar@operatebeyond.com>
2023-04-04 20:22:49 -0500

    #CDW-1452 added attributes to model for duplicate inventory

--- app/Models/Inventory/Inventory.php
+++ app/Models/Inventory/Inventory.php
@@ -510,6 +510,11 @@ class Inventory extends Model
         return $this->hasMany(AttributeValue::class, 'inventory_id', 'inventory_id');
     }
 
+    public function attributes(): HasMany
+    {
+        return $this->hasMany(AttributeValue::class, 'inventory_id', 'inventory_id');
+    }
+
     public function lotVantageInventory(): HasOne
     {
         return $this->hasOne(DealerInventory::class, 'inventory_id', 'inventory_id');
