e3b400a0e99aa99bf496cebaee898106b69857bc
Ethan Rivas <ethan@operatebeyond.com>
2023-04-06 09:24:48 -0600

    #NTGRTNS-784 Update reset password workflow to avoid conflicts with dashboard password update

--- app/Http/Requests/User/FinishPasswordResetRequest.php
+++ app/Http/Requests/User/FinishPasswordResetRequest.php
@@ -8,15 +8,12 @@ class FinishPasswordResetRequest extends Request
 {
     protected $rules = [
         'code' => 'required',
-        'current_password' => ['required'],
         'password' => ['required', 'min:6', 'max:8']
     ];
 
     public function messages(): array
     {
         return [
-            'current_password.min' => 'The :attribute should be at least :min characters.',
-            'current_password.max' => 'The :attribute should not be greater than :max characters.',
             'password.min' => 'The :attribute should be at least :min characters.',
             'password.max' => 'The :attribute should not be greater than :max characters.',
         ];
