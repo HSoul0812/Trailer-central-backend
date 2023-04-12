bc817e078fae023aa416f034998b5138a6eddee6
Rober <rad8329@gmail.com>
2023-04-11 21:42:34 -0500

    #CDW-1438 Code clean up

--- app/Services/Inventory/InventoryService.php
+++ app/Services/Inventory/InventoryService.php
@@ -691,7 +691,6 @@ class InventoryService implements InventoryServiceInterface
             $image['filename_with_overlay'] = null;
             $image['filename_without_overlay'] = $fileDto->getPath();
             $image['hash'] = $fileDto->getHash();
-
         }
 
         return $images;
