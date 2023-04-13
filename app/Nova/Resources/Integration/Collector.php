ec1dff42881aefbfbf079c8c1e11242a75efbc3a
Calo <carlos@operatebeyond.com>
2023-04-13 09:02:41 -0500

    NTGRTNS-771 Add Nullable option

--- app/Nova/Resources/Integration/Collector.php
+++ app/Nova/Resources/Integration/Collector.php
@@ -260,16 +260,17 @@ class Collector extends Resource
                             "If a unit is not found in the factory vantage, do not save it"
                         ),
                 ])->dependsOn('use_factory_mapping', true),
-
-                Number::make('Filter since a certain year', 'factory_mapping_filter_year_from')->hideFromIndex()->help(
-                    'If the unit have Year present, this option will filter incoming units <strong>from specific year</strong><br>' .
-                    'If FV is enabled and next option is null or not enabled all the old units be processed in regular way'
-                )->min(date('Y', strtotime('-20 year')))->max(date('Y', strtotime('+2 year')))->step(1)->rules('numeric')->withMeta(['extraAttributes' => [
-                    'placeholder' => date('Y', strtotime('-2 year'))]
-                ]),
-                Boolean::make('Skip Filtered Year Units?', 'factory_mapping_filter_skip_units')->hideFromIndex()->help(
-                    'If you enable this option, older units from the desired year will be skipped'
-                )
+                NovaDependencyContainer::make([
+                    Number::make('Filter since a certain year', 'factory_mapping_filter_year_from')->hideFromIndex()->help(
+                        'If the unit have Year present, this option will filter incoming units <strong>from specific year</strong><br>' .
+                        'If FV is enabled and next option is null or not enabled all the old units be processed in regular way'
+                    )->nullable()->min(date('Y', strtotime('-20 year')))->max(date('Y', strtotime('+2 year')))->step(1)->withMeta(['extraAttributes' => [
+                        'placeholder' => date('Y', strtotime('-2 year'))]
+                    ]),
+                    Boolean::make('Skip Filtered Year Units?', 'factory_mapping_filter_skip_units')->nullable()->hideFromIndex()->help(
+                        'If you enable this option, older units from the desired year will be skipped'
+                    ),
+                ])->dependsOn('use_factory_mapping', true),
             ]),
 
             new Panel('Actions With Items', [
