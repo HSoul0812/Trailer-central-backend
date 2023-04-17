4f0e54741afe1a518b828cc5fa9dc588bbf9397a
Rober <rad8329@gmail.com>
2023-04-16 23:06:12 -0500

    #CDW-1469 `GenerateOverlayAndReIndexInventoriesByDealersJob` is providing queues to `Job::batch`

--- app/Jobs/Inventory/GenerateOverlayAndReIndexInventoriesByDealersJob.php
+++ app/Jobs/Inventory/GenerateOverlayAndReIndexInventoriesByDealersJob.php
@@ -14,7 +14,10 @@ use App\Services\Inventory\InventoryServiceInterface;
 class GenerateOverlayAndReIndexInventoriesByDealersJob extends Job
 {
     /** @var int time in seconds */
-    private const WAIT_TIME = 15;
+    private const WAIT_TIME_FOR_INDEXATION_IN_SECONDS = 15;
+
+    /** @var string[] list of queues which are monitored */
+    private const MONITORED_QUEUES = ['scout'];
 
     /**  @var array<integer> */
     private $dealerIds;
@@ -67,9 +70,15 @@ class GenerateOverlayAndReIndexInventoriesByDealersJob extends Job
 
             $logger->info('Enqueueing the job to reindex inventory by dealer id', ['dealer_id' => $dealerId]);
 
-            Job::batch(function (BatchedJob $batch) use ($dealerId): void {
-                Inventory::makeAllSearchableByDealers([$dealerId]);
-            }, __CLASS__, self::WAIT_TIME, array_merge($this->context, ['process' => 'indexation']));
+            Job::batch(
+                function (BatchedJob $batch) use ($dealerId): void {
+                    Inventory::makeAllSearchableByDealers([$dealerId]);
+                },
+                self::MONITORED_QUEUES,
+                __CLASS__,
+                self::WAIT_TIME_FOR_INDEXATION_IN_SECONDS,
+                array_merge($this->context, ['process' => 'indexation'])
+            );
 
             $logger->info('Enqueueing the job to invalidate cache by dealer id', ['dealer_id' => $dealerId]);
 
