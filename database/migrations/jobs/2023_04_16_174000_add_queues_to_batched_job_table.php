53c94fe5063b86715f5280478700902fc6de58b1
Rober <rad8329@gmail.com>
2023-04-16 22:59:33 -0500

    #CDW-1469 Migration script to add `queues` column to `batched_job` table

---
+++ database/migrations/jobs/2023_04_16_174000_add_queues_to_batched_job_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Database\traits\WithMysqlServerVersion;
+
+class AddQueuesToBatchedJobTable extends Migration
+{
+    use WithMysqlServerVersion;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('batched_job', function (Blueprint $table): void {
+            $type = $this->version() < '5.7.0' ? 'text' : 'json';
+
+            $table->{$type}('queues')
+                ->nullable()
+                ->after('group')
+                ->comment('a valid array of monitored queues');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('batched_job', static function (Blueprint $table): void {
+            $table->dropColumn('queues');
+        });
+    }
+}
