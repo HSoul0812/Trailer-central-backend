7506abaa07dd34fd344924f7f7b3d59c062657e6
Sergey Pekar <sergeypekar77@gmail.com>
2023-04-18 14:50:28 +0300

    #NTGRTNS-810 added field images_delimiter_used_in_image_url

---
+++ database/migrations/collectors/2023_04_18_073346_add_images_delimiter_used_in_image_urls_field_to_collector_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddImagesDelimiterUsedInImageUrlsFieldToCollectorTable extends Migration
+{
+    private const TABLE = 'collector';
+    private const FIELD = 'images_delimiter_used_in_image_url';
+    private const DEALER_ID = 6223;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(self::TABLE, function (Blueprint $table) {
+            $table->boolean(self::FIELD)->default(false);
+        });
+
+        DB::table(self::TABLE)->where('dealer_id', '=', self::DEALER_ID)->update([self::FIELD => true]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(self::TABLE, function (Blueprint $table) {
+            $table->dropColumn(self::FIELD);
+        });
+    }
+}
