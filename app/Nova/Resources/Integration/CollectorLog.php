754f72d4e48ab5f7b80ece8675c3355db55aaa5c
Ethan Rivas <ethan@operatebeyond.com>
2023-04-20 10:20:04 -0600

    #NTGRTNS-815 Fix issue on validation errors boolean return

--- app/Nova/Resources/Integration/CollectorLog.php
+++ app/Nova/Resources/Integration/CollectorLog.php
@@ -93,7 +93,7 @@ class CollectorLog extends Resource
                 ->hideFromIndex(),
 
             Boolean::make('Validation Errors', function () {
-                return !$this->validation_errors;
+                return empty(json_decode($this->validation_errors, true));
             })->onlyOnIndex(),
 
             Code::make('Validation Errors')->language('javascript')->json()
