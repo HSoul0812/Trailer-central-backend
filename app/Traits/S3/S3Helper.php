2162ee15655ef272b755e65cda5c9d9fd7009e8d
Rober <rad8329@gmail.com>
2023-04-25 16:52:58 -0500

    #CDW-1501 `S3Helper::exist` was added

--- app/Traits/S3/S3Helper.php
+++ app/Traits/S3/S3Helper.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits\S3;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Trait UrlHelper
  * @package App\Traits\S3
@@ -29,5 +31,27 @@ trait S3Helper
     protected function getS3Url(string $path): string
     {
         return $this->getS3BaseUrl() . DIRECTORY_SEPARATOR . ltrim($path, '/');
+    }
+
+    /**
+     * Will only check the status code, when it is 200 or 300 it will return true
+     */
+    protected function exist(string $url): bool
+    {
+        $resource = curl_init();
+
+        curl_setopt($resource, CURLOPT_URL, $url);
+        curl_setopt($resource, CURLOPT_NOBODY, true);
+        curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($resource, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($resource, CURLOPT_TIMEOUT, 2);
+
+        curl_exec($resource);
+
+        $httpCode = curl_getinfo($resource, CURLINFO_HTTP_CODE);
+
+        curl_close($resource);
+
+        return $httpCode >= Response::HTTP_OK && $httpCode < Response::HTTP_MULTIPLE_CHOICES;
     }
 }
