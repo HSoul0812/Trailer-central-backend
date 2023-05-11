b5324c9a23ca34df123f5917ab016374a02fa14a
songxunzhao <zhao@operatebeyond.com>
2023-05-11 09:33:06 -0500

    #TR-889: rename $data to $ids parameter

--- app/Services/ElasticSearch/Inventory/Builders/CustomQueryBuilder.php
+++ app/Services/ElasticSearch/Inventory/Builders/CustomQueryBuilder.php
@@ -336,14 +336,14 @@ doc['status'].value != 2 && doc['dealer.name'].value != 'Operate Beyond'";
         return $this->query;
     }
 
-    public function buildExcludeIdsQuery(array $id): array {
+    public function buildExcludeIdsQuery(array $ids): array {
         return [
             'query' => [
                 'bool' => [
                     'must_not' => [
                         [
                             'ids' => [
-                                'values' => $id
+                                'values' => $ids
                             ]
                         ]
                     ]
