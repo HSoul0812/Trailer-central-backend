7a7bb6d4dc0409b469df282dab07568b7747b0ce
zhao songxun <songxunzhao1991@gmail.com>
2023-05-12 06:40:42 -0500

    #TR-889: Add unit test

---
+++ tests/Integration/App/Api/Inventory/SearchInventoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Integration\App\Api\Inventory;
+
+use App\DTOs\Inventory\TcEsResponseInventoryList;
+use App\Services\Inventory\InventorySDKServiceInterface;
+use Config;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Mockery\MockInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Common\IntegrationTestCase;
+
+class SearchInventoryTest extends IntegrationTestCase
+{
+    public const SEARCH_INVENTORY_ENDPOINT = '/api/inventory';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Config::set('inventory-sdk.url', 'https://abc.com');
+
+        $this->mock(InventorySDKServiceInterface::class, function (MockInterface $mock) {
+            $list = new TcEsResponseInventoryList();
+            $list->aggregations = [];
+            $list->limits = [];
+            $list->inventories = new LengthAwarePaginator([], 1, 1);
+            $mock->shouldReceive('list')->andReturn($list);
+        });
+    }
+
+    public function testItReturnValidationErrorWithInvalidParams(): void
+    {
+        $this
+            ->json('GET', self::SEARCH_INVENTORY_ENDPOINT, ['sort' => 'createdAt|aaa'])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSeeText('The sort format is invalid.');
+
+        $this
+            ->json('GET', self::SEARCH_INVENTORY_ENDPOINT, ['exclude_ids' => '1,2,3,3'])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertSeeText('The exclude ids format is invalid.');
+    }
+
+    public function testItReturnValidResponseWithValidParams(): void
+    {
+        $this
+            ->json('GET', self::SEARCH_INVENTORY_ENDPOINT, ['exclude_ids' => '1;2;3;3'])
+            ->assertStatus(Response::HTTP_OK);
+    }
+}
