4179fc5e4be70d47d6a170b265556910dd809e0c
Pond <pond@trailercentral.com>
2023-05-15 22:31:27 +0700

    TR-835: Add supervisor config file

---
+++ supervisor-conf.d
@@ -0,0 +1,9 @@
+[program:horizon]
+process_name=%(program_name)s
+command=php /var/www/backend horizon
+autostart=true
+autorestart=true
+user=ubuntu
+redirect_stderr=true
+;stdout_logfile=/home/forge/example.com/horizon.log
+stopwaitsecs=3600
