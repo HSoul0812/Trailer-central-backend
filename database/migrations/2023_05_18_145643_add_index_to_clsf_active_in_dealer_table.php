7c8fc00c5fe36921db710a5a2b9f645a9f267324
Li <li@trailercentral.com>
2023-05-18 15:30:01 +0800

    Add drop code

--- database/migrations/2023_05_18_145643_add_index_to_clsf_active_in_dealer_table.php
+++ database/migrations/2023_05_18_145643_add_index_to_clsf_active_in_dealer_table.php
@@ -26,7 +26,7 @@ class AddIndexToClsfActiveInDealerTable extends Migration
     public function down()
     {
         Schema::table('dealer', function (Blueprint $table) {
-            //
+            $table->dropIndex(['clsf_active']);
         });
     }
 }
