6d5b824fe1164b1c5aa99838b0c5277957d523bb
Pond <pond@trailercentral.com>
2023-06-02 15:22:32 +0700

    TR-954: Do not throw exception if we can't send the email to the user, just allow registration to complete

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Domains\Auth\Listeners\SendConfirmationEmail;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,7 +17,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            SendConfirmationEmail::class,
         ],
     ];
 
