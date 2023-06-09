e8b441914b8d3232e261a4b96e39a6f201858034
Pond <pond@trailercentral.com>
2023-06-09 22:43:15 +0700

    TR-988: Update the test case

--- tests/Unit/App/Services/WebsiteUser/PasswordResetServiceTest.php
+++ tests/Unit/App/Services/WebsiteUser/PasswordResetServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\App\Services\WebsiteUser;
 
 use App\Models\WebsiteUser\WebsiteUser;
 use App\Repositories\WebsiteUser\WebsiteUserRepositoryInterface;
+use App\Services\Captcha\CaptchaServiceInterface;
 use App\Services\WebsiteUser\PasswordResetService;
 use Illuminate\Database\Eloquent\Collection;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -13,6 +14,8 @@ use Tests\Common\TestCase;
 class PasswordResetServiceTest extends TestCase
 {
     private MockObject $websiteUserRepository;
+
+    private MockObject $captchaService;
 
     public function testForgetPassword()
     {
@@ -27,22 +30,38 @@ class PasswordResetServiceTest extends TestCase
     public function testForgetPasswordWithNoUser()
     {
         $service = $this->getConcreteService();
+
         $this->websiteUserRepository->expects($this->once())
             ->method('get')
             ->willReturn(new Collection());
+
+        $this->captchaService->expects($this->once())
+            ->method('validate')
+            ->willReturn(true);
+
         $this->expectException(NotFoundHttpException::class);
-        $service->forgetPassword('test@test.com', null);
+
+        $service->forgetPassword('test@test.com', null, '');
     }
 
     private function getConcreteService(): PasswordResetService
     {
         $this->websiteUserRepository = $this->mockWebsiteUserRepository();
+        $this->captchaService = $this->mockCaptchaService();
 
-        return new PasswordResetService($this->websiteUserRepository);
+        return new PasswordResetService(
+            $this->websiteUserRepository,
+            $this->captchaService
+        );
     }
 
     private function mockWebsiteUserRepository(): MockObject
     {
         return $this->createMock(WebsiteUserRepositoryInterface::class);
+    }
+
+    private function mockCaptchaService(): MockObject
+    {
+        return $this->createMock(CaptchaServiceInterface::class);
     }
 }
