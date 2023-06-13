d4d0bc0f7e71ed6bd151704621c57bc2f3748b1a
Rober <rad8329@gmail.com>
2023-06-13 15:36:35 -0500

    #CDW-1607 It ensures the images relation always exists

--- app/Models/Inventory/Inventory.php
+++ app/Models/Inventory/Inventory.php
@@ -468,7 +468,8 @@ class Inventory extends Model
 
     public function inventoryImages(): HasMany
     {
-        return $this->hasMany(InventoryImage::class, 'inventory_id', 'inventory_id');
+        return $this->hasMany(InventoryImage::class, 'inventory_id', 'inventory_id')
+            ->join('image', 'image.image_id', '=', 'inventory_image.image_id');
     }
 
     public function orderedImages(): HasMany
